c657861f0df85e941125d59f922910986aac628a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:30 +0000

    Add package oc-dms-mirror/2.4.5/oc_dms_mirror-2.4.5-py3-none-any.whl

---
+++ packages/oc-dms-mirror/oc_dms_mirror-2.4.5-py3-none-any.whl/oc_dms_mirror/rest_api/app/routes.py
@@ -0,0 +1,68 @@
+import json
+
+import structlog
+from flask import Response, request, current_app
+from oc_logging import setup_json_logging
+
+from . import dms_mirror_bp
+from oc_dms_mirror.dms_mirror import DmsMirror
+
+setup_json_logging()
+logger = structlog.get_logger()
+
+def get_dms_mirror():
+    _dmsMirror = DmsMirror()
+    _dmsMirror.setup_from_args(current_app.args)
+    _dmsMirror.load_config()
+    return _dmsMirror
+
+
+def response_json(code, data):
+    """
+    Return JSON data response
+    :param int code: HTTP response code
+    :param data: dict or list to send as response content
+    """
+    if not isinstance(data, str):
+        data = json.dumps(data)
+
+    return Response(
+        status=code,
+        mimetype='application/json',
+        content_type='application/json',
+        response=data)
+
+
+@dms_mirror_bp.route('/register-component-version-artifact', methods=['POST'])
+def register_component_version_artifact():
+    """
+    Endpoint performing component/version sync with DMS on demand.
+    """
+    logger.info(f"POST {request.url_rule.rule} from [{request.remote_addr}] with payload: {request.get_json()}")
+    try:
+        get_dms_mirror().process_component_webhook(request.get_json())
+    except Exception as _e:
+        logger.exception(_e)
+        return response_json(400, {"result": str(_e)})
+
+    return response_json(200, {"result": "Success"})
+
+@dms_mirror_bp.route('/get-gav', methods=['POST'])
+def generate_gav():
+    """
+    Endpoint for getting or generating GAV Template.
+    """
+    logger.info(f"POST {request.url_rule.rule} from [{request.remote_addr}] with payload: {request.get_json()}")
+    try:
+        component = request.json.get('componentId')
+        if not component:
+            return response_json(400, {"result": "componentId cannot be blank"})
+
+        gav_template = get_dms_mirror().get_component_config(component)
+        if not gav_template:
+            return response_json(404, {"result": f"gav template for component {component} not found"})
+    except Exception as _e:
+        logger.exception(_e)
+        return response_json(400, {"result": str(_e)})
+
+    return response_json(200, gav_template)
