6667cecd073b3ed67dfd5cbe74e2104947e6a2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:56 +0000

    Add package splink/4.0.10/splink-4.0.10.tar.gz

---
+++ packages/splink/splink-4.0.10.tar.gz/splink-4.0.10/splink/__init__.py
@@ -0,0 +1,94 @@
+from sys import version_info
+from typing import TYPE_CHECKING
+from warnings import warn
+
+from splink.internals.blocking_rule_library import block_on
+from splink.internals.column_expression import ColumnExpression
+from splink.internals.datasets import splink_datasets
+from splink.internals.linker import Linker
+from splink.internals.settings_creator import SettingsCreator
+
+# The following is a workaround for the fact that dependencies of particular backends
+# may not be installed, but we don't want this to prevent import
+# of the other backends.
+
+# This enables auto-complete to be used to import the various DBAPIs
+# and ensures that typing information is retained so e.g. the arguments autocomplete
+# without importing them at runtime
+if TYPE_CHECKING:
+    from splink.internals.duckdb.database_api import DuckDBAPI
+    from splink.internals.spark.database_api import SparkAPI
+
+_LOWEST_SUPPORTED_MINOR_VERSION = 10
+
+if (installed_minor_version := version_info.minor) < _LOWEST_SUPPORTED_MINOR_VERSION:
+    warn(
+        (
+            f"Python 3.{installed_minor_version} has reached end-of-life.  "
+            "Future releases of Splink may no longer be compatible with "
+            "this python version.\n"
+            "Please consider upgrading your python version if you wish "
+            "to continue to be able to install the latest version of Splink."
+        ),
+        category=DeprecationWarning,
+        stacklevel=2,
+    )
+
+
+# custom warning class, so that users can filter if they need to
+class SparkCompatibilityWarning(UserWarning):
+    pass
+
+
+# warning for Spark 4
+try:
+    import pyspark
+
+    major, _, _ = pyspark.__version__.split(".")
+    if int(major) >= 4:
+        warn(
+            (
+                "You are using Spark 4. Some small amount of functionality in Splink "
+                "may be unavailable - in-built UDFs, and automatic date-parsing for "
+                "malformed dates.  Core functionality should remain unaffected."
+            ),
+            category=SparkCompatibilityWarning,
+            stacklevel=2,
+        )
+except ModuleNotFoundError:
+    pass
+
+
+# Use getarr to make the error appear at the point of use
+def __getattr__(name):
+    try:
+        if name == "SparkAPI":
+            from splink.internals.spark.database_api import SparkAPI
+
+            return SparkAPI
+        elif name == "DuckDBAPI":
+            from splink.internals.duckdb.database_api import DuckDBAPI
+
+            return DuckDBAPI
+    except ImportError as err:
+        if name in ["SparkAPI", "DuckDBAPI"]:
+            raise ImportError(
+                f"{name} cannot be imported because its dependencies are not "
+                "installed. Please `pip install` the required package(s) as "
+                "specified in the optional dependencies in pyproject.toml"
+            ) from err
+    raise AttributeError(f"module 'splink' has no attribute '{name}'") from None
+
+
+__version__ = "4.0.10"
+
+
+__all__ = [
+    "block_on",
+    "ColumnExpression",
+    "DuckDBAPI",
+    "Linker",
+    "SettingsCreator",
+    "SparkAPI",
+    "splink_datasets",
+]
