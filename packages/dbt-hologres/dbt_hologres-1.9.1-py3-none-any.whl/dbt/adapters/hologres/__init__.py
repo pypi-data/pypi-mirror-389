48c4aea05bf3c63259ceaa360cbf99a3d8bbc2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:36 +0000

    Add package dbt-hologres/1.9.1/dbt_hologres-1.9.1-py3-none-any.whl

---
+++ packages/dbt-hologres/dbt_hologres-1.9.1-py3-none-any.whl/dbt/adapters/hologres/__init__.py
@@ -0,0 +1,28 @@
+# these are mostly just exports, #noqa them so flake8 will be happy
+import dbt.context.base
+
+from dbt.adapters.base import AdapterPlugin
+
+from dbt.include import hologres
+from dbt.context.base import get_context_modules
+from .impl import HologresAdapter, HologresCredentials
+from . import date
+
+Plugin = AdapterPlugin(
+    adapter=HologresAdapter,
+    credentials=HologresCredentials,
+    include_path=hologres.PACKAGE_PATH
+)
+
+
+def new_context_modules():
+    return get_context_modules() | {
+        "date": date
+    }
+
+
+dbt.context.base.get_context_modules = new_context_modules
+ 
+
+
+
