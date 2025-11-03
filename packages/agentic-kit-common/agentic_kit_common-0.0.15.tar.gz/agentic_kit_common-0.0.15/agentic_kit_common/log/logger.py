e403ad8ed8a11a4a66efaffd47af356a3cc6c4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:04:39 +0000

    Add package agentic-kit-common/0.0.15/agentic_kit_common-0.0.15.tar.gz

---
+++ packages/agentic-kit-common/agentic_kit_common-0.0.15.tar.gz/agentic_kit_common-0.0.15/agentic_kit_common/log/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+
+def setup_logger(tag, level):
+    logger = logging.getLogger(tag)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
