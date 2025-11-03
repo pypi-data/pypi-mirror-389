24985afcd597e7d8f738dca87ddba964022b2662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:05:46 +0000

    Add package cesnet-service-path-plugin/5.2.1b2/cesnet_service_path_plugin-5.2.1b2.tar.gz

---
+++ packages/cesnet-service-path-plugin/cesnet_service_path_plugin-5.2.1b2.tar.gz/cesnet_service_path_plugin-5.2.1b2/cesnet_service_path_plugin/utils/__init__.py
@@ -0,0 +1,29 @@
+from .utils_gis import (
+    check_gis_environment,
+    determine_file_format_from_extension,
+    ensure_2d_geometry,
+    export_segment_paths_as_geojson,
+    extract_all_kml_from_kmz,
+    extract_kml_from_kmz,
+    gdf_to_multilinestring,
+    process_path_data,
+    process_path_file,
+    read_all_kml_layers,
+    read_kmz_file,
+    validate_path_geometry,
+)
+
+__all__ = [
+    "check_gis_environment",
+    "determine_file_format_from_extension",
+    "ensure_2d_geometry",
+    "export_segment_paths_as_geojson",
+    "extract_all_kml_from_kmz",
+    "extract_kml_from_kmz",
+    "gdf_to_multilinestring",
+    "process_path_data",
+    "process_path_file",
+    "read_all_kml_layers",
+    "read_kmz_file",
+    "validate_path_geometry",
+]
