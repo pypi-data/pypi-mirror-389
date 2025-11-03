a2c6356d9ddbbf78e0e5d4334baf746448b615df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:05:07 +0000

    Add package data-inclusion-schema/1.0.2/data_inclusion_schema-1.0.2.tar.gz

---
+++ packages/data-inclusion-schema/data_inclusion_schema-1.0.2.tar.gz/data_inclusion_schema-1.0.2/src/data_inclusion/schema/v1/__init__.py
@@ -0,0 +1,43 @@
+from data_inclusion.schema.common import (
+    CodeCommune,
+    CodeDepartement,
+    CodeEPCI,
+    CodePostal,
+    CodeRegion,
+    CodeSiren,
+    CodeSiret,
+)
+from data_inclusion.schema.v1.frais import Frais
+from data_inclusion.schema.v1.modes_accueil import ModeAccueil
+from data_inclusion.schema.v1.modes_mobilisation import (
+    ModeMobilisation,
+    PersonneMobilisatrice,
+)
+from data_inclusion.schema.v1.publics import Public
+from data_inclusion.schema.v1.reseaux_porteurs import ReseauPorteur
+from data_inclusion.schema.v1.thematiques import Thematique
+from data_inclusion.schema.v1.types_de_services import TypeService
+
+# Keep those import last to avoid circular imports
+from data_inclusion.schema.v1.services import Service  # isort: skip
+from data_inclusion.schema.v1.structures import Structure  # isort: skip
+
+__all__ = [
+    "CodeCommune",
+    "CodeDepartement",
+    "CodeEPCI",
+    "CodePostal",
+    "CodeRegion",
+    "CodeSiren",
+    "CodeSiret",
+    "Frais",
+    "ModeAccueil",
+    "ModeMobilisation",
+    "PersonneMobilisatrice",
+    "Public",
+    "ReseauPorteur",
+    "Service",
+    "Structure",
+    "Thematique",
+    "TypeService",
+]
