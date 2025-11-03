a48b683b5875e7bff0db8e50c3709caef856e0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:05:06 +0000

    Add package knwl/1.6.1/knwl-1.6.1-py3-none-any.whl

---
+++ packages/knwl/knwl-1.6.1-py3-none-any.whl/knwl/__init__.py
@@ -0,0 +1,14 @@
+from knwl.models import *
+from knwl.services import services
+from knwl.semantic.graph_rag.graph_rag import GraphRAG 
+from knwl.format import print_knwl
+from knwl.chunking.chunking_base import ChunkingBase
+from knwl.chunking.tiktoken_chunking import TiktokenChunking
+from knwl.di import (
+    singleton_service,
+    inject_config,
+    inject_services,
+    auto_inject,
+    defaults,
+)
+from knwl.knwl import Knwl
