eb93748e103ffcbedd3c280246f683deed8c70cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:05:01 +0000

    Add package tfmx/0.2/tfmx-0.2.tar.gz

---
+++ packages/tfmx/tfmx-0.2.tar.gz/tfmx-0.2/README.md
@@ -0,0 +1,22 @@
+# tfmx
+
+![](https://img.shields.io/pypi/v/tfmx?label=tfmx&color=blue&cacheSeconds=60)
+
+## Install
+
+```sh
+pip install tfmx --upgrade
+```
+
+## Usage
+
+Run example:
+
+```sh
+python example.py
+```
+
+See: [example.py](https://github.com/Hansimov/tfmx/blob/main/example.py)
+
+```python
+```
---
+++ packages/tfmx/tfmx-0.2.tar.gz/tfmx-0.2/src/tfmx/__init__.py
@@ -0,0 +1,6 @@
+from .llm import LLMConfigsType, LLMClient, LLMClientByConfig
+from .embed_client import EmbedClientConfigsType
+from .embed_client import EmbedClient, EmbedClientByConfig
+from .embed_server import TEIEmbedServerConfigsType
+from .embed_server import TEIEmbedServer, TEIEmbedServerByConfig
+from .embed_server import TEIEmbedServerArgParser, EmbedServerArgParser
