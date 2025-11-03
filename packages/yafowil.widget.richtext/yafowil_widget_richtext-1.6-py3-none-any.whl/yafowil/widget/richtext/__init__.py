4926a04d8d92050a692139f0cfb6041e10e4d120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:05:11 +0000

    Add package yafowil.widget.richtext/1.6/yafowil_widget_richtext-1.6-py3-none-any.whl

---
+++ packages/yafowil.widget.richtext/yafowil_widget_richtext-1.6-py3-none-any.whl/yafowil/widget/richtext/__init__.py
@@ -0,0 +1,40 @@
+from yafowil.base import factory
+from yafowil.utils import entry_point
+import os
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+js = [{
+    'group': 'yafowil.widget.richtext.dependencies',
+    'resource': 'tinymce/jscripts/tiny_mce/tiny_mce.js',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.richtext.jquery',
+    'resource': 'tinymce/jscripts/tiny_mce/jquery.tinymce.js',
+    'order': 21,
+}, {
+    'group': 'yafowil.widget.richtext.common',
+    'resource': 'widget.js',
+    'order': 22,
+}]
+js_plone4 = [{
+    'group': 'yafowil.widget.richtext.common',
+    'resource': 'widget_p4.js',
+    'order': 20,
+}]
+js_plone5 = [{
+    'group': 'yafowil.widget.richtext.common',
+    'resource': 'widget_p5.js',
+    'order': 20,
+}]
+
+
+@entry_point(order=10)
+def register():
+    from yafowil.widget.richtext import widget  # noqa
+    factory.register_theme('default', 'yafowil.widget.richtext',
+                           resourcedir, js=js)
+    factory.register_theme('plone4', 'yafowil.widget.richtext',
+                           resourcedir, js=js_plone4)
+    factory.register_theme('plone5', 'yafowil.widget.richtext',
+                           resourcedir, js=js_plone5)
---
+++ packages/yafowil.widget.richtext/yafowil_widget_richtext-1.6-py3-none-any.whl/yafowil/widget/richtext/tests.py
@@ -0,0 +1,96 @@
+from yafowil.base import ExtractionError
+from yafowil.base import factory
+from yafowil.compat import IS_PY2
+from yafowil.tests import YafowilTestCase
+import unittest
+import yafowil.loader  # noqa
+
+
+if not IS_PY2:
+    from importlib import reload
+
+
+class TestRichtextWidget(YafowilTestCase):
+
+    def setUp(self):
+        super(TestRichtextWidget, self).setUp()
+        from yafowil.widget.richtext import widget
+        reload(widget)
+
+    def test_edit_renderer_no_preset_value(self):
+        # Render widget
+        widget = factory(
+            'richtext',
+            name='rt',
+            props={
+                'required': True
+            })
+        self.assertEqual(widget(), (
+            '<textarea class="richtext" cols="80" id="input-rt" name="rt" '
+            'required="required" rows="25"></textarea>'
+        ))
+
+    def test_edit_renderer_preset_value(self):
+        widget = factory(
+            'richtext',
+            name='rt',
+            value='<p>1</p>',
+            props={
+                'required': True
+            })
+        self.assertEqual(widget(), (
+            '<textarea class="richtext" cols="80" id="input-rt" name="rt" '
+            'required="required" rows="25"><p>1</p></textarea>'
+        ))
+
+    def test_display_renderer(self):
+        # Display renderer
+        widget = factory(
+            'richtext',
+            name='rt',
+            value='<p>foo</p>',
+            mode='display')
+        self.assertEqual(
+            widget(),
+            '<div class="display-richtext"><p>foo</p></div>'
+        )
+
+        widget = factory(
+            'richtext',
+            name='rt',
+            mode='display')
+        self.assertEqual(widget(), '<div class="display-richtext"></div>')
+
+    def test_extraction(self):
+        # Widget extraction
+        widget = factory(
+            'richtext',
+            name='rt',
+            props={
+                'required': True
+            })
+
+        # Required extraction
+        request = {'rt': ''}
+        data = widget.extract(request)
+        # No input was given
+        self.assertEqual(
+            data.errors,
+            [ExtractionError('Mandatory field was empty')]
+        )
+        # Empty string in extracted
+        self.assertEqual(data.extracted, '')
+
+        # Widget extraction. Returns markup from tinymce
+        request = {'rt': '<p>1</p>'}
+        data = widget.extract(request)
+        self.assertEqual(data.errors, [])
+        self.assertEqual(data.extracted, '<p>1</p>')
+        self.assertEqual(widget(data), (
+            '<textarea class="richtext" cols="80" id="input-rt" name="rt" '
+            'required="required" rows="25"><p>1</p></textarea>'
+        ))
+
+
+if __name__ == '__main__':
+    unittest.main()
