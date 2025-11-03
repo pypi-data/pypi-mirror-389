765e3786be3fb9ee88e85c730f3cfee68ca82617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:55 +0000

    Add package vanrickshaw/0.1.3/vanrickshaw-0.1.3.tar.gz

---
+++ packages/vanrickshaw/vanrickshaw-0.1.3.tar.gz/vanrickshaw-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vanrickshaw',
+    version='0.1.3',
+    packages=['vanrickshaw'],
+    include_package_data=True,
+    install_requires=['Pillow'],
+    entry_points={
+        'console_scripts': [
+            'van=vanrickshaw.banik:run',
+        ],
+    },
+    author='Souparno',
+    description='Rickshaw ASCII art animation in terminal',
+)
