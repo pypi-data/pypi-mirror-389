ec116da2f5065e2f10a9be47e2c9dc705fed9093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:05:08 +0000

    Add package mazapackagehaha/1.0.0/mazapackagehaha-1.0.0.tar.gz

---
+++ packages/mazapackagehaha/mazapackagehaha-1.0.0.tar.gz/mazapackagehaha-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mazapackagehaha',
+    version='1.0.0',
+    author='Amine Mazari',
+    author_email='lacosteamine128@gmail.com',
+    packages=find_packages(),
+    url='https://pypi.org/project/mazapackagehaha/',
+    description='An awesome package that does something',
+    long_description="No README file provided.",
+    long_description_content_type="text/plain",
+)
