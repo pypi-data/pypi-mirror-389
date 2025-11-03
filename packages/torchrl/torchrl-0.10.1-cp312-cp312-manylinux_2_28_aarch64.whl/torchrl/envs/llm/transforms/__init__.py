eccab92c3832bace5fce824721b1df6ebc36035e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:26 +0000

    Add package torchrl/0.10.1/torchrl-0.10.1-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/torchrl/torchrl-0.10.1-cp312-cp312-manylinux_2_28_aarch64.whl/torchrl/envs/llm/transforms/__init__.py
@@ -0,0 +1,36 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .browser import BrowserTransform
+from .dataloading import (
+    as_nested_tensor,
+    as_padded_tensor,
+    DataLoadingPrimer,
+    RayDataLoadingPrimer,
+)
+from .format import TemplateTransform
+from .kl import KLComputation, KLRewardTransform, RetrieveKL, RetrieveLogProb
+from .policy_version import PolicyVersion
+from .reason import AddThinkingPrompt
+from .tokenizer import Tokenizer
+from .tools import MCPToolTransform, PythonInterpreter
+
+__all__ = [
+    "BrowserTransform",
+    "DataLoadingPrimer",
+    "RayDataLoadingPrimer",
+    "KLRewardTransform",
+    "RetrieveLogProb",
+    "RetrieveKL",
+    "MCPToolTransform",
+    "PolicyVersion",
+    "PythonInterpreter",
+    "AddThinkingPrompt",
+    "KLComputation",
+    "TemplateTransform",
+    "Tokenizer",
+    "as_nested_tensor",
+    "as_padded_tensor",
+]
