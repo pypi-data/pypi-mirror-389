2c12f8fcc00b53d720d05edf079e17358a89359d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:04:45 +0000

    Add package sourcepp/2025.11.2/sourcepp-2025.11.2-cp311-cp311-win_amd64.whl

---
+++ packages/sourcepp/sourcepp-2025.11.2-cp311-cp311-win_amd64.whl/sourcepp/__init__.py
@@ -0,0 +1,5 @@
+from ._sourcepp_impl import __doc__, bsppp, gamepp, sourcepp, steampp, toolpp, vcryptpp, vpkpp, vtfpp
+
+__author__: str = "craftablescience"
+__version__: str = "2025.11.02"
+__all__: list[str] = ['__author__', '__doc__', '__version__', 'bsppp', 'gamepp', 'sourcepp', 'steampp', 'toolpp', 'vcryptpp', 'vpkpp', 'vtfpp']
