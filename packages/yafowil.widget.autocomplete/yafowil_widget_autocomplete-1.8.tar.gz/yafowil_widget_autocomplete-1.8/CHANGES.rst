bd3a323f40af0316d49a3b04f66efbdb66630112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:04:57 +0000

    Add package yafowil.widget.autocomplete/1.8/yafowil_widget_autocomplete-1.8.tar.gz

---
+++ packages/yafowil.widget.autocomplete/yafowil_widget_autocomplete-1.8.tar.gz/yafowil_widget_autocomplete-1.8/CHANGES.rst
@@ -0,0 +1,100 @@
+Changes
+=======
+
+1.8 (2025-11-03)
+----------------
+
+- Pin upper versions of dependencies.
+  [lenadax]
+
+
+1.7 (2018-07-16)
+----------------
+
+- Python 3 compatibility.
+  [rnix]
+
+- Convert doctests to unittests.
+  [rnix]
+
+
+1.6 (2017-03-01)
+----------------
+
+- Add dedicated CSS for ``plone5`` theme provided by ``yafowil.plone``.
+  [rnix, 2016-06-28]
+
+- Use ``yafowil.utils.entry_point`` decorator.
+  [rnix, 2016-06-28]
+
+
+1.5.1 (2015-06-25)
+------------------
+
+- Resolve JSHint errors and warnings.
+  [thet]
+
+
+1.5 (2015-01-23)
+----------------
+
+- Update jquery UI autocomplete to 1.10.3 and use latest jquery ui boostrap
+  styles.
+  [rnix, 2014-07-05]
+
+1.4.1
+-----
+
+- use ``yafowil.utils.attr_value`` wherever possible.
+  [rnix, 2012-10-25]
+
+1.4
+---
+
+- Add ``generic_required_extractor`` to extraction chain
+  [rnix, 2012-07-25]
+
+- Adopt resource providing
+  [rnix, 2012-06-12]
+
+- Remove example app
+  [rnix, 2012-06-12]
+
+1.3
+---
+
+- Support JavaScript callback functions in source attribute
+  [rnix, 2011-10-03]
+
+- register autocomplete binder in ``yafowil.widget.array`` hooks if installed
+  [rnix, 2011-10-03]
+
+- register plan and depend on yafowil 1.3
+  [jensens, 2011-09-30]
+
+1.2
+---
+
+- adopt to yafowil 1.2
+  [jensens, 2011-09-20]
+
+- resource available in plain Zope2
+  [jensens, 2011-09-20]
+
+1.1
+---
+
+- adopt to yafowil 1.1
+  [rnix, 2011-07-08]
+
+1.0.1
+-----
+
+- test coverage
+  [rnix, 2011-05-07]
+
+1.0
+---
+
+- made it work
+  [jensens, 2010-11-24]
---
+++ packages/yafowil.widget.autocomplete/yafowil_widget_autocomplete-1.8.tar.gz/yafowil_widget_autocomplete-1.8/src/yafowil/widget/autocomplete/__init__.py
@@ -0,0 +1,69 @@
+from yafowil.base import factory
+from yafowil.utils import entry_point
+import os
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+js = [{
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.menu.min.js',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.autocomplete.min.js',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.common',
+    'resource': 'widget.js',
+    'order': 21,
+}]
+default_css = [{
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.menu.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.autocomplete.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.common',
+    'resource': 'widget.css',
+    'order': 21,
+}]
+bootstrap_css = [{
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.menu.bootstrap.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.autocomplete.bootstrap.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.common',
+    'resource': 'widget.css',
+    'order': 21,
+}]
+plone5_css = [{
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.menu.plone5.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.dependencies',
+    'resource': 'jquery.ui.autocomplete.plone5.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.autocomplete.common',
+    'resource': 'widget.css',
+    'order': 21,
+}]
+
+
+@entry_point(order=10)
+def register():
+    from yafowil.widget.autocomplete import widget  # noqa
+    factory.register_theme('default', 'yafowil.widget.autocomplete',
+                           resourcedir, js=js, css=default_css)
+    factory.register_theme('bootstrap', 'yafowil.widget.autocomplete',
+                           resourcedir, js=js, css=bootstrap_css)
+    factory.register_theme('plone5', 'yafowil.widget.autocomplete',
+                           resourcedir, js=js, css=plone5_css)
