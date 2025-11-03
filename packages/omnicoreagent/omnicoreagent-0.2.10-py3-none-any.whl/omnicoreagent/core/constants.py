993f56d6b284f63bf1315fb9c9a6dc074eb5e57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:04:22 +0000

    Add package omnicoreagent/0.2.10/omnicoreagent-0.2.10-py3-none-any.whl

---
+++ packages/omnicoreagent/omnicoreagent-0.2.10-py3-none-any.whl/omnicoreagent/core/constants.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+
+TOOL_ACCEPTING_PROVIDERS = {
+    "groq",
+    "openai",
+    "openrouter",
+    "gemini",
+    "deepseek",
+    "azureopenai",
+    "anthropic",
+}
+
+AGENTS_REGISTRY = {}
+MCP_TOOLS_REGISTRY = {}
+date_time_func = {
+    "format_date": lambda data=None: datetime.now(timezone.utc).strftime(
+        "%Y-%m-%dT%H:%M:%SZ"
+    )
+}
