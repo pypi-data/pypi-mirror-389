e699e6a9a93883f9e634d47381f3903edbedea80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:05:07 +0000

    Add package yafowil.widget.multiselect/1.4/yafowil_widget_multiselect-1.4.tar.gz

---
+++ packages/yafowil.widget.multiselect/yafowil_widget_multiselect-1.4.tar.gz/yafowil_widget_multiselect-1.4/CHANGES.rst
@@ -0,0 +1,39 @@
+Changes
+=======
+
+1.4 (2025-11-03)
+----------------
+
+- Pin upper versions of dependencies.
+  [lenadax]
+
+
+1.3 (2018-07-16)
+----------------
+
+- Python 3 compatibility.
+  [rnix]
+
+- Convert doctests to unittests.
+  [rnix]
+
+
+1.2 (2017-03-01)
+----------------
+
+- Use ``yafowil.utils.entry_point`` decorator.
+  [rnix, 2016-06-28]
+
+
+1.1 (2015-01-23)
+----------------
+
+- Update to jquery.multi-select 0.9.12.
+  [rnix, 2014-08-06]
+
+
+1.0
+---
+
+- make it work
+  [thet, 2012-06-14]
---
+++ packages/yafowil.widget.multiselect/yafowil_widget_multiselect-1.4.tar.gz/yafowil_widget_multiselect-1.4/src/yafowil/widget/multiselect/__init__.py
@@ -0,0 +1,31 @@
+from yafowil.base import factory
+from yafowil.utils import entry_point
+import os
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+js = [{
+    'group': 'yafowil.widget.multiselect.dependencies',
+    'resource': 'multi-select/js/jquery.multi-select.js',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.multiselect.common',
+    'resource': 'widget.js',
+    'order': 21,
+}]
+css = [{
+    'group': 'yafowil.widget.multiselect.dependencies',
+    'resource': 'multi-select/css/multi-select.css',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.multiselect.common',
+    'resource': 'widget.css',
+    'order': 21,
+}]
+
+
+@entry_point(order=10)
+def register():
+    from yafowil.widget.multiselect import widget  # noqa
+    factory.register_theme('default', 'yafowil.widget.multiselect',
+                           resourcedir, js=js, css=css)
