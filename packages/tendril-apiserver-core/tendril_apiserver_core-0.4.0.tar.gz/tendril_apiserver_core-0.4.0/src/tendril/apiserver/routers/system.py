b0ce83310774b73d336d7a845d9e5c614a19f26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:48 +0000

    Add package tendril-apiserver-core/0.4.0/tendril_apiserver_core-0.4.0.tar.gz

---
+++ packages/tendril-apiserver-core/tendril_apiserver_core-0.4.0.tar.gz/tendril_apiserver_core-0.4.0/src/tendril/apiserver/routers/system.py
@@ -0,0 +1,39 @@
+
+
+from fastapi import APIRouter
+from fastapi import Depends
+
+from tendril.authn.users import authn_dependency
+from tendril.authn.users import AuthUserModel
+from tendril.authn.users import auth_spec
+
+
+system_monitoring = APIRouter(prefix='/system',
+                              tags=["System Monitoring"],
+                              dependencies=[Depends(authn_dependency),
+                                            auth_spec(scopes=['system:monitoring'])])
+
+system_administration = APIRouter(prefix='/system',
+                                  tags=["System Administration"],
+                                  dependencies=[Depends(authn_dependency),
+                                                auth_spec(scopes=['system:administration'])])
+
+
+from tendril import config
+from tendril.utils.versions import versions
+
+
+@system_monitoring.get("/versions")
+async def package_versions():
+    return versions()
+
+
+@system_monitoring.get("/config")
+async def tendril_config():
+    return config.json_render()
+
+
+routers = [
+    system_monitoring,
+    system_administration
+]
