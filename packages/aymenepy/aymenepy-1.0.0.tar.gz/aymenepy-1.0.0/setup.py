05f5f73391e810c607eb7e10d74fc594119de0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:04:32 +0000

    Add package aymenepy/1.0.0/aymenepy-1.0.0.tar.gz

---
+++ packages/aymenepy/aymenepy-1.0.0.tar.gz/aymenepy-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aymenepy',
+    version='1.0.0',
+    author='Aymene Laidani',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/aymenepy/',
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[]  
+)
