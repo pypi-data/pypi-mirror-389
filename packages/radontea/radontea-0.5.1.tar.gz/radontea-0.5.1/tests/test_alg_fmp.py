2e677e5f0e58529224937f39d33a9cd84e0c8290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 15:05:12 +0000

    Add package radontea/0.5.1/radontea-0.5.1.tar.gz

---
+++ packages/radontea/radontea-0.5.1.tar.gz/radontea-0.5.1/tests/test_alg_fmp.py
@@ -0,0 +1,34 @@
+import os
+import pathlib
+
+import numpy as np
+
+import pytest
+
+import radontea
+
+import sinogram
+
+# See https://github.com/RI-imaging/radontea/issues/6
+CI_FAILS = (os.environ.get("RUNNER_OS", "None") == "Linux"
+            or os.environ.get("RUNNER_OS", "None") == "macOS"
+            or os.environ.get("RUNNER_OS", "None") == "Windows")
+
+
+@pytest.mark.xfail(CI_FAILS, reason="Unexplained issue #6")
+def test_2d_fmp():
+    sino, angles = sinogram.create_test_sino(A=100, N=101)
+    r = radontea.fourier_map(sino, angles)
+
+    # np.savetxt('outfile.txt', np.array(r).flatten().view(float), fmt="%.8f")
+    reffile = pathlib.Path(__file__).parent / "data" / "2d_fmp.txt"
+    ref = np.loadtxt(str(reffile))
+    assert np.allclose(np.array(r).flatten().view(float), ref)
+
+
+if __name__ == "__main__":
+    # Run all tests
+    loc = locals()
+    for key in list(loc.keys()):
+        if key.startswith("test_") and hasattr(loc[key], "__call__"):
+            loc[key]()
