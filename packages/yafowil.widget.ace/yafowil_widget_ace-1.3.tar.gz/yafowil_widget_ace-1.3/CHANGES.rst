19d91ac4e36dd2e299bcf308154f2e07a675a3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-03 14:04:50 +0000

    Add package yafowil.widget.ace/1.3/yafowil_widget_ace-1.3.tar.gz

---
+++ packages/yafowil.widget.ace/yafowil_widget_ace-1.3.tar.gz/yafowil_widget_ace-1.3/CHANGES.rst
@@ -0,0 +1,38 @@
+Changes
+=======
+
+1.3 (2025-11-03)
+----------------
+
+- Pin upper versions of dependencies.
+  [lenadax]
+
+
+1.2 (2018-07-16)
+----------------
+
+- Python 3 compatibility.
+  [rnix]
+
+- Convert doctests to unittests.
+  [rnix]
+
+
+1.1 (2017-03-01)
+----------------
+
+- Use ``yafowil.utils.entry_point`` decorator.
+  [rnix]
+
+
+1.0.1 (2015-01-23)
+------------------
+
+- Cleanup examples.
+  [rnix]
+
+1.0
+---
+
+- Make it work.
+  [rnix]
---
+++ packages/yafowil.widget.ace/yafowil_widget_ace-1.3.tar.gz/yafowil_widget_ace-1.3/src/yafowil/widget/ace/__init__.py
@@ -0,0 +1,27 @@
+from yafowil.base import factory
+from yafowil.utils import entry_point
+import os
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+js = [{
+    'group': 'yafowil.widget.ace.dependencies',
+    'resource': 'ace/ace.js',
+    'order': 20,
+}, {
+    'group': 'yafowil.widget.ace.common',
+    'resource': 'widget.js',
+    'order': 21,
+}]
+css = [{
+    'group': 'yafowil.widget.ace.common',
+    'resource': 'widget.css',
+    'order': 20,
+}]
+
+
+@entry_point(order=10)
+def register():
+    from yafowil.widget.ace import widget  # noqa
+    factory.register_theme('default', 'yafowil.widget.ace',
+                           resourcedir, js=js, css=css)
