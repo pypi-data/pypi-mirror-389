d1c4910f90bea0d34568d21147890ec8cbb3fedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:47 +0000

    Add package dbcp/0.1.1/dbcp-0.1.1-py3-none-any.whl

---
+++ packages/dbcp/dbcp-0.1.1-py3-none-any.whl/dbcp/__init__.py
@@ -0,0 +1,9 @@
+from .problem import BiconvexProblem, BiconvexRelaxProblem
+
+__all__ = ["BiconvexProblem", "BiconvexRelaxProblem", "__version__"]
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("dbcp")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
