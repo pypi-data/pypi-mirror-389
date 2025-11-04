895d626ae5ed9d24ed3c298eeaa1c87f871b29fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:56 +0000

    Add package yayawallet-python-sdk/1.9.2/yayawallet_python_sdk-1.9.2.tar.gz

---
+++ packages/yayawallet-python-sdk/yayawallet_python_sdk-1.9.2.tar.gz/yayawallet_python_sdk-1.9.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yayawallet-python-sdk',
+    version='1.9.2',
+    author="YaYa Wallet",
+    author_email="contact@yayawallet.com",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages()
+)
