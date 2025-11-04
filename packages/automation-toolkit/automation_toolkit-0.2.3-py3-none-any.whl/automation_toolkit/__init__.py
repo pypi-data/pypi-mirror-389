4dad4986cc93fcb74bee3168bc5d60de8dcc7ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:35 +0000

    Add package automation-toolkit/0.2.3/automation_toolkit-0.2.3-py3-none-any.whl

---
+++ packages/automation-toolkit/automation_toolkit-0.2.3-py3-none-any.whl/automation_toolkit/__init__.py
@@ -0,0 +1,15 @@
+"""
+Automation Toolkit - 基于uiautomator2的自动化测试工具包
+
+提供设备控制、元素定位、图像识别、颜色检测等功能。
+"""
+
+from .core import AutomationToolkit
+
+__version__ = "0.1.2"
+__author__ = "白_jiujiu"
+__email__ = "1138545214@qq.com"
+
+__all__ = [
+    "AutomationToolkit",
+]
