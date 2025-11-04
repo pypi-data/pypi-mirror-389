55311a76351e2f58783d7d2ce3a9947963ceb078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:04:57 +0000

    Add package deepteam/0.2.8/deepteam-0.2.8.tar.gz

---
+++ packages/deepteam/deepteam-0.2.8.tar.gz/deepteam-0.2.8/deepteam/frameworks/__init__.py
@@ -0,0 +1,5 @@
+from .frameworks import OWASPTop10
+
+__all__ = [
+    "OWASPTop10",
+]
---
+++ packages/deepteam/deepteam-0.2.8.tar.gz/deepteam-0.2.8/deepteam/frameworks/frameworks.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List
+
+from deepteam.frameworks.utils import (
+    _get_owasp_attacks,
+    _get_owasp_vulnerabilities,
+)
+from deepteam.vulnerabilities import (
+    BaseVulnerability,
+)
+
+
+from deepteam.attacks import BaseAttack
+
+
+class AISafetyFramework(BaseModel):
+    name: str
+    description: str
+    vulnerabilities: List[BaseVulnerability]
+    attacks: List[BaseAttack]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class OWASPTop10(AISafetyFramework):
+    def __init__(self):
+        super().__init__(
+            name="OWASP",
+            description="The OWASP Top 10 for LLMs 2025",
+            vulnerabilities=_get_owasp_vulnerabilities(),
+            attacks=_get_owasp_attacks(),
+        )
