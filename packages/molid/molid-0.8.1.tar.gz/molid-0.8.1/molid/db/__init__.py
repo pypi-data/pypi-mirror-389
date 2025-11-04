405a287da39428a83bb65980e456d24dd1be87e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:24 +0000

    Add package molid/0.8.1/molid-0.8.1.tar.gz

---
+++ packages/molid/molid-0.8.1.tar.gz/molid-0.8.1/molid/db/__init__.py
@@ -0,0 +1,15 @@
+# from .db_utils import (
+#     create_offline_db,
+#     save_to_database,
+#     get_archive_state,
+#     upsert_archive_state,
+#     initialize_database,  # optional: low-level schema init
+# )
+
+# __all__ = [
+#     "create_offline_db",
+#     "save_to_database",
+#     "get_archive_state",
+#     "upsert_archive_state",
+#     "initialize_database",
+# ]
---
+++ packages/molid/molid-0.8.1.tar.gz/molid-0.8.1/molid/pubchemproc/__init__.py
@@ -0,0 +1,11 @@
+# from .pubchem import process_file, unpack_and_process_file
+# from .file_handler import validate_gz_file, unpack_gz_file, cleanup_files, move_file
+
+# __all__ = [
+#     "process_file",
+#     "unpack_and_process_file",
+#     "validate_gz_file",
+#     "unpack_gz_file",
+#     "cleanup_files",
+#     "move_file",
+# ]
