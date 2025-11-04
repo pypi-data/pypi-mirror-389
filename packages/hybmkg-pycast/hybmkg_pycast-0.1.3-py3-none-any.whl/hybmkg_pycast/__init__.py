f912a1830272669afbd068d672852f4de94ec953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:46 +0000

    Add package hybmkg-pycast/0.1.3/hybmkg_pycast-0.1.3-py3-none-any.whl

---
+++ packages/hybmkg-pycast/hybmkg_pycast-0.1.3-py3-none-any.whl/hybmkg_pycast/__init__.py
@@ -0,0 +1,3 @@
+from . import model  # membuat hybmkg_pycast.model bisa langsung diakses
+from . import data  # membuat hybmkg_pycast.data_preparation bisa langsung diakses
+from . import config  # membuat hybmkg_pycast.evaluation bisa langsung diakses
