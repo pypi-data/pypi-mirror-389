107b07d2b6ac0f72f4824c5e903a6e378bcfb365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:04:29 +0000

    Add package pygid/0.2.0/pygid-0.2.0-py3-none-any.whl

---
+++ packages/pygid/pygid-0.2.0-py3-none-any.whl/pygid/__init__.py
@@ -0,0 +1,6 @@
+from .expparams import ExpParams
+from .dataloader import DataLoader
+from .datasaver import DataSaver, ExpMetadata, SampleMetadata
+from .coordmaps import CoordMaps
+from .conversion import Conversion
+__version__ = "0.2.0"
