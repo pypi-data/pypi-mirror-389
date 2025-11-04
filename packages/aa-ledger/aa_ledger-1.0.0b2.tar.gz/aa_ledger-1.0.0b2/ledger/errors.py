f957dfb38ba0f77312998325c178b98e1467314b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:27 +0000

    Add package aa-ledger/1.0.0b2/aa_ledger-1.0.0b2.tar.gz

---
+++ packages/aa-ledger/aa_ledger-1.0.0b2.tar.gz/aa_ledger-1.0.0b2/ledger/errors.py
@@ -0,0 +1,32 @@
+"""Custom exceptions."""
+
+# Alliance Auth
+from esi.errors import TokenError
+
+
+class HTTPGatewayTimeoutError(Exception):
+    pass
+
+
+class TokenDoesNotExist(TokenError):
+    """A token with a specific scope does not exist for a user."""
+
+
+class MemberNotActive(Exception):
+    pass
+
+
+class DatabaseError(Exception):
+    pass
+
+
+class ESSError(Exception):
+    pass
+
+
+class CustomError(Exception):
+    pass
+
+
+class LedgerImportError(Exception):
+    pass
