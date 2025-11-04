7e32f8ccbae94d3adc5bed2b292e9c0ce0ffca38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:04:29 +0000

    Add package ckanext-tables/1.3.2/ckanext_tables-1.3.2-py3-none-any.whl

---
+++ packages/ckanext-tables/ckanext_tables-1.3.2-py3-none-any.whl/ckanext/tables/plugin.py
@@ -0,0 +1,32 @@
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+from ckan.common import CKANConfig
+
+from ckanext.tables.table import table_registry
+from ckanext.tables.types import collect_tables_signal
+
+
+@tk.blanket.helpers
+@tk.blanket.blueprints
+class TablesPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurer)
+    p.implements(p.IConfigurable)
+
+    # IConfigurer
+
+    def update_config(self, config_: CKANConfig) -> None:
+        tk.add_template_directory(config_, "templates")
+        tk.add_resource("assets", "tables")
+
+    # IConfigurable
+
+    def configure(self, config_: CKANConfig):
+        self.register_tables()
+
+    @staticmethod
+    def register_tables():
+        table_registry.reset()
+
+        for _, tables in collect_tables_signal.send():
+            for table_name, table in tables.items():
+                table_registry.register(table_name, table)
