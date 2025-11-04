9acbf33808b162fb1e996b6325453d94b1d82b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:52 +0000

    Add package flowrep/0.1.0/flowrep-0.1.0-py3-none-any.whl

---
+++ packages/flowrep/flowrep-0.1.0-py3-none-any.whl/flowrep/tools.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+def recursive_defaultdict() -> defaultdict:
+    """
+    Create a recursively nested defaultdict.
+
+    Example:
+    >>> d = recursive_defaultdict()
+    >>> d['a']['b']['c'] = 1
+    >>> print(d)
+
+    Output: 1
+    """
+    return defaultdict(recursive_defaultdict)
+
+
+def dict_to_recursive_dd(d: dict | defaultdict) -> defaultdict:
+    """Convert a regular dict to a recursively nested defaultdict."""
+    if isinstance(d, dict) and not isinstance(d, defaultdict):
+        return defaultdict(
+            recursive_defaultdict, {k: dict_to_recursive_dd(v) for k, v in d.items()}
+        )
+    return d
+
+
+def recursive_dd_to_dict(d: dict | defaultdict) -> dict:
+    """Convert a recursively nested defaultdict to a regular dict."""
+    if isinstance(d, defaultdict):
+        return {k: recursive_dd_to_dict(v) for k, v in d.items()}
+    return d
