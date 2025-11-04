3eeb0be9e623a49918ba8cd35b9115412508e0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 15:05:28 +0000

    Add package fusion-bench/0.2.28/fusion_bench-0.2.28.tar.gz

---
+++ packages/fusion-bench/fusion_bench-0.2.28.tar.gz/fusion_bench-0.2.28/fusion_bench/utils/fabric.py
@@ -0,0 +1,24 @@
+import time
+from typing import Optional
+
+import lightning as L
+
+from fusion_bench.utils.pylogger import get_rankzero_logger
+
+log = get_rankzero_logger(__name__)
+
+
+def seed_everything_by_time(fabric: Optional[L.Fabric] = None):
+    """
+    Set seed for all processes by time.
+    """
+    # set seed for all processes
+    if fabric is None or fabric.is_global_zero:
+        seed = int(time.time())
+    else:
+        seed = None
+    if fabric is not None:
+        log.debug(f"Broadcasting seed `{seed}` to all processes")
+        fabric.barrier()
+        seed = fabric.broadcast(seed, src=0)
+    L.seed_everything(seed)
