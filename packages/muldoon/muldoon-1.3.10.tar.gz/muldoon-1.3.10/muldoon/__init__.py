13d30ea02b36a75f81631ad5e532d9f00098daf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:05:24 +0000

    Add package muldoon/1.3.10/muldoon-1.3.10.tar.gz

---
+++ packages/muldoon/muldoon-1.3.10.tar.gz/muldoon-1.3.10/muldoon/__init__.py
@@ -0,0 +1,11 @@
+"""A package for sifting meteorological data to look for and analyze vortex
+encounters"""
+
+__version__ = '1.3.10'
+__author__ = 'Brian Jackson <bjackson@boisestate.edu>'
+__all__ = ['met_timeseries', 'utils', 'read_data']
+
+from muldoon import met_timeseries
+from muldoon import utils
+from muldoon import read_data
+
