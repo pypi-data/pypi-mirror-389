3b8b02b4dbb7418ab6a03310e64def42525e610b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:46 +0000

    Add package vilocify-sdk/0.7.0/vilocify_sdk-0.7.0.tar.gz

---
+++ packages/vilocify-sdk/vilocify_sdk-0.7.0.tar.gz/vilocify_sdk-0.7.0/README.md
@@ -0,0 +1,166 @@
+# Vilocify SDK and CLI tool
+
+The Python SDK for [Vilocify](https://docs.vilocify.com), built using Vilocify's APIv2 JSON:API.
+This project also includes a CLI tool to manage Vilocify Monitoring Lists e.g. to update or create lists from CycloneDX SBOM files.
+
+## Prerequisites
+The Vilocify SDK needs Python 3.12 or newer.
+
+## CLI usage
+1. Get [pipx](https://pipx.pypa.io/stable/)
+2. Install the CLI with `pipx install vilocify-sdk`
+3. Run `vilocify --help` for documentation of the CLI tool
+
+### Example
+Following example imports a monitoring list from a CycloneDX SBOM.
+Note that the monitoring list is identified by its name.
+If a monitoring list with the same name already exists, it gets overwritten with the contents from the SBOM file.
+```bash
+vilocify monitoringlist import --name "My Project v1.2.3" --from-cyclonedx my_project_v1.2.3.sbom.json
+```
+
+## SDK usage
+The SDK is built on Vilocify's API.
+We recommend you also take a quick look at the raw API docs at https://portal.vilocify.com/documentation.
+In particular, study the "API resources" section and `filter[...][...]` parameters of main resources.
+Those will come in handy for understanding the models of the SDK and arguments that can be used in `.where()` methods.
+
+### Installation
+Get `vilocify-sdk` from [PyPI](https://pypi.org/project/vilocify-sdk/).
+If you use [Poetry](https://python-poetry.org/docs/) get it with `poetry add vilocify-sdk`.
+
+### Authentication
+You can generate an API Token at https://portal.vilocify.com/api-tokens.
+
+By default, authentication for the SDK is configured through the `VILOCIFY_API_TOKEN` environment variable.
+Alternatively, the token can be set programmatically:
+```python
+from vilocify import api_config
+api_config.token = "<your token here>"
+```
+
+### Examples
+
+#### Inviting a new member into your org
+
+Note that this code needs a token with "admin" role.
+```python
+from vilocify.models import Membership
+m = Membership(username="John Doe", email="john.doe@example.com", role="user", expires_at="2025-10-30T00:00:00Z")
+m.create()
+
+print(m.invitation_state)
+print(m.created_at)
+```
+
+#### Creating a monitoring list
+```python
+from vilocify.models import MonitoringList, Subscription, Component, Membership
+
+# Creating a new monitoring list automatically adds the authenticated user as 'owner'
+ml = MonitoringList(name="Example list", comment="created by the Vilocify SDK")
+ml.components = [
+    Component(id="40866"),
+    Component(id="148860")
+]
+ml.create()
+
+# Add a second subscriber to the list.
+sub = Subscription(role="reader")
+sub.monitoring_list = ml
+sub.membership = Membership.where("email", "eq", "john.doe@example.com").first()
+sub.create()
+
+for subscription in ml.subscriptions:
+    print(subscription.membership.email, "-", subscription.role)
+```
+
+#### Listing notifications for a monitoring list
+
+```python
+from vilocify.models import MonitoringList, Notification
+
+ml = MonitoringList.where("name", "eq", "Example list").first()
+
+# Find Vilocify notifications for the monitoring list since 2023
+notifications = Notification.where("monitoringLists.id", "any", ml.id).where("createdAt", "after", "2023-01-01T00:00:00Z")
+for n in notifications:
+    print(n.title)
+```
+
+### Filtering
+To filter models by supported attributes, use the `.where()` method, which takes three arguments.
+The three arguments map 1-to-1 to `filter` parameters described in the [API Docs](https://portal.vilocify.com/documentation).
+
+Take for example following request with a filter parameter from the docs:
+```
+GET /api/v2/notifications?filter[monitoringLists.id][any]=8c63252a-893e-4563-876b-a45ac9bdfff2
+```
+The corresponding Python code is
+```python
+Notification.where("monitoringLists.id", "any", "8c63252a-893e-4563-876b-a45ac9bdfff2")
+```
+The third parameter of `.where()` can either be a string or a list of strings.
+Note that the SDK does not perform further validations which operators accept lists and which don't; lists simply get concatenated to comma-separated strings.
+Check the API docs which filters support multiple values.
+
+### Sorting
+Sorting is handled by `.asc()` and `.desc()`.
+Check the documentation of the `sort` parameter in top-level `GET /api/v2/{resources}` in the [API Docs](https://portal.vilocify.com/documentation) for available sorters.
+The Vilocify API can only sort by a single attribute and does not support sorting by multiple attributes.
+The SDK will raise an exception when attempting to sort an already sorted request.
+
+### Relationships
+The [models.py](vilocify/models.py) module defines the SDK models and their relationships, which closely reflects the "API Resources" drawing of the [API Docs](https://portal.vilocify.com/documentation).
+Relationships cannot be set through the model constructor, but instead must be set using assignment. For example:
+
+```python
+sub = Subscription(role="user") # role is an attribute
+sub.membership = Membership(id="<a_membership_id_here>")  # `.membership` is a to-one relation
+sub.monitoring_list = MonitoringList(id="<a_ml_id_here>")  # `.monitoring_list` is a to-one relation
+sub.create()  # Performs the API request to create the subscription
+
+
+ml = MonitoringList(name="Example list") # name is an attribute
+ml.components = [  # components is a to-many relationship
+    Component(id="40866"),
+    Component(id="148860")
+]
+ml.create()  # Creates a monitoring list with the two given components
+```
+
+#### Updating to-many relationships
+The SDK provides two mechanisms to update to-many relationships.
+One replaces all existing relationships and is not immediate, but needs a call to `.update()`.
+The other extends the existing relationship and is immediate.
+For example
+
+```python
+ml = MonitoringList.first()
+ml.components = [  # Replaces all components on the monitoring list, no request is sent, yet.
+    Component(id="1"),
+    Component(id="2")
+]
+ml.update()  # Commit the change to the Vilocify API
+
+
+ml = MonitoringList.first()
+ml.components.extend(Component(id="3"), Component(id="5"))  # Adds component with IDs 1 and 2, and immediately sends the change to the API backend
+```
+
+### Proxy setup
+The SDK uses a `requests.Session()` to handle its HTTP requests, which picks up the proxy settings from the `https_proxy` environment variable as documented [here](https://requests.readthedocs.io/en/latest/user/advanced/#proxies).
+To override that behavior do the following:
+```python
+from vilocify import api_config
+api_config.client.trust_env = False
+api_config.client.proxies = { "https": "https://your.proxy:8080" }
+```
+
+## Contributing
+See [Contributing.md](docs/CONTRIBUTING.md).
+
+## License
+[MIT License](LICENSE)
+
+Copyright (c) 2025 Siemens AG
---
+++ packages/vilocify-sdk/vilocify_sdk-0.7.0.tar.gz/vilocify_sdk-0.7.0/vilocify/cli.py
@@ -0,0 +1,287 @@
+"""Cli to interact with Vilocify API."""
+
+#  SPDX-FileCopyrightText: 2025 Siemens AG
+#  SPDX-License-Identifier: MIT
+
+import csv
+import io
+import json
+import logging
+import sys
+import textwrap
+from datetime import UTC, datetime, timedelta
+
+import click
+from click import BadParameter, UsageError
+from click.exceptions import Exit
+from cyclonedx.model.bom import Bom
+from cyclonedx.model.bom import Component as BomComponent
+
+import vilocify
+from vilocify.http import JSONAPIRequestError, RequestError
+from vilocify.match import MissingPurlError, match_bom_component
+from vilocify.models import (
+    Component,
+    ComponentRequest,
+    MonitoringList,
+    Notification,
+    Vulnerability,
+)
+
+ComponentCache = dict[tuple[str, str], Component]
+
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+version_text = """Vilocify Python SDK, version %(version)s
+
+Copyright (C) 2025 Siemens AG
+MIT License
+"""
+
+
+class BadCycloneDXFileError(Exception):
+    """Raised on unsupported file extensions of a CycloneDX file"""
+
+
+@click.group()
+@click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "ERROR"]), default="INFO", help="Set the log level.")
+@click.version_option(version=vilocify.__version__, message=version_text)
+def cli(log_level: str):
+    """To see help pages, you can run
+
+    \b
+    vilocify --help
+    vilocify <command> --help
+    vilocify <command> <subcommand> --help
+    """
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level, force=True)
+
+
+@cli.command()
+@click.option("--for", "monitoring_list", required=True, help="The monitoring list to print notifications for.")
+@click.option(
+    "--since",
+    default=datetime.now(tz=UTC) - timedelta(days=1),
+    type=click.DateTime(),
+    help="Print only notifications published after the given date.",
+)
+def notifications(monitoring_list: str, since: datetime):
+    """Print all notifications for the given monitoring list since a certain date."""
+    ns = Notification.where("monitoringLists.id", "any", monitoring_list).where("createdAt", "after", since.isoformat())
+    has_notifications = False
+    for notification in ns:
+        has_notifications = True
+        print()
+        print("---")
+        print("Title:", notification.title)
+        print("Description:")
+        print(textwrap.indent(notification.description, "  "))
+        print("Vulnerabilities:")
+        for vuln in Vulnerability.where("id", "in", notification.vulnerabilities.ids()):
+            print("  - CVE: ", vuln.cve)
+            print("    CVSS: ", vuln.cvss)
+            print("    Description: ", vuln.description)
+
+    if not has_notifications:
+        print(f"No new notifications for monitoringlist #{monitoring_list} since {since.isoformat()}.")
+
+
+def _find_vilocify_component(cache: ComponentCache, bom_component: BomComponent) -> Component | None:
+    vilocify_name, vilocify_version, url = match_bom_component(bom_component)
+    if vilocify_version is None:
+        return None
+
+    if vilocify_name is not None:
+        if (k := (vilocify_name, vilocify_version)) in cache:
+            return cache[k]
+
+        return (
+            Component.where("name", "eq", vilocify_name)
+            .where("version", "eq", vilocify_version)
+            .where("active", "eq", "true")
+            .first()
+        )
+
+    if url is not None:
+        return (
+            Component.where("url", "like", url)
+            .where("version", "eq", vilocify_version)
+            .where("active", "eq", "true")
+            .first()
+        )
+
+    return None
+
+
+def _load_bom(file: io.FileIO) -> Bom:
+    if file.name.endswith(".json"):
+        bom = Bom.from_json(data=json.load(file))  # type: ignore[attr-defined]
+    elif file.name.endswith(".xml"):
+        bom = Bom.from_xml(data=file)  # type: ignore[attr-defined]
+    else:
+        raise BadCycloneDXFileError("The CyclondeDX file must end with .json or .xml.")
+
+    if len(bom.components) > MonitoringList.MAX_COMPONENTS:
+        raise BadCycloneDXFileError(
+            f"The CycloneDX file contains more than {MonitoringList.MAX_COMPONENTS} components, but Vilocify monitoring"
+            f" lists cannot have more than {MonitoringList.MAX_COMPONENTS} components."
+            "Consider creating separate lists for dev, prod, or build dependencies."
+        )
+
+    return bom
+
+
+def _load_ml(name: str, comment: str) -> MonitoringList:
+    ml = MonitoringList.where("name", "eq", name).where("comment", "eq", comment).first()
+    if ml is None:
+        logger.info("No monitoring list with given name and comment found. Creating new list.")
+        ml = MonitoringList(name=name, comment=comment)
+        ml.create()
+
+    logger.info("Using monitoring list %s", ml.id)
+    return ml
+
+
+def _match_bom(cache: ComponentCache, bom: Bom) -> tuple[list[Component], list[BomComponent]]:
+    components = []
+    unidentified_components = []
+    for bom_component in bom.components:
+        try:
+            c = _find_vilocify_component(cache, bom_component)
+        except MissingPurlError:
+            logger.warning("Ignoring BOM component %s due to missing PURL", bom_component.name)
+        else:
+            if c is not None:
+                logger.info("Found component %s for %s", c.id, bom_component.purl)
+                components.append(c)
+            else:
+                unidentified_components.append(bom_component)
+
+    return components, unidentified_components
+
+
+@cli.group()
+def monitoringlist():
+    """Manage monitoring lists."""
+
+
+@monitoringlist.command("show")
+@click.option(
+    "--format",
+    "export_format",
+    type=click.Choice(["extendedcsv"]),
+    default="extendedcsv",
+    help="The format used to print details.",
+)
+@click.option("--id", "monitoring_list_id", required=True, help="The monitoring list ID.")
+def monitoringlist_show(monitoring_list_id: str, export_format: str):
+    """Show monitoring list details."""
+    if export_format != "extendedcsv":
+        raise Exit(1)
+
+    ml = MonitoringList.get(monitoring_list_id)
+    csvwriter = csv.writer(sys.stdout)
+    csvwriter.writerow(["device", "vendor", "name", "version"])
+    for component in ml.components:
+        csvwriter.writerow([ml.name, component.vendor, component.name, component.version])
+
+
+@monitoringlist.command("import")
+@click.option("--name", required=True, help="The monitoring list name.")
+@click.option("--comment", default="", help="The comment set for the monitoring list.")
+@click.option("--yes", is_flag=True, help="Skip interactive questions. Assumes 'yes' for all answers.")
+@click.option("--from-cyclonedx", type=click.File("rt"), required=True, help="The CycloneDX file to import.")
+def monitoringlist_import(name: str, comment: str, yes: bool, from_cyclonedx: io.FileIO):
+    """Creates or updates a monitoring list from a CycloneDX JSON or XML file.
+
+    The monitoring list is identified by the given name and comment. Changing the name or comment between runs will
+    create a new monitoring list. The JSON or XML filetype is identified by the filename ending.
+
+    Some components might not be found on Vilocify. A ComponentRequest is created for components that cannot be
+    identified. ComponentRequests might need several days to get processed and integrated into Vilocify. Running the
+    same command repeatedly will update the monitoring list once the component requests are processed.
+    """
+
+    component_requests = []
+    bom = _load_bom(from_cyclonedx)
+    ml = _load_ml(name, comment)
+    components_cache = {(c.name, c.version): c for c in ml.components}
+    components, unidentified_components = _match_bom(components_cache, bom)
+
+    for bom_component in unidentified_components:
+        cr = ComponentRequest.where("componentUrl", "eq", str(bom_component.purl)).first()
+        if cr is None:
+            component_name, version, _ = match_bom_component(bom_component)
+            cr = ComponentRequest(
+                name=component_name or bom_component.name,
+                version=version or bom_component.version,
+                component_url=str(bom_component.purl),
+                comment="Auto-created by vilocify-sdk-python",
+            )
+            component_requests.append(cr)
+            logger.info("Could not find component for %s", bom_component.purl)
+        elif (c := cr.component) is not None:
+            logger.info("Found component %s for %s through component request %s", c.id, bom_component.purl, cr.id)
+            components.append(c)
+        elif cr.state in ("unprocessed", "rejected"):
+            logger.info("The component request %s for %s is %s", cr.id, bom_component.purl, cr.state)
+
+    if component_requests:
+        logger.info(
+            "%d components could not be identified directly nor through existing component requests.",
+            len(component_requests),
+        )
+        if yes or click.prompt(f"\nCreate {len(component_requests)} component requests? (y/n)", type=bool):
+            for cr in component_requests:
+                cr.create()
+
+    ml.components = components
+    ml.update()
+    logger.info("Finished updating monitoring list %s", ml.id)
+
+
+@cli.command()
+@click.option(
+    "--state",
+    type=click.Choice(["unprocessed", "rejected", "mapped"]),
+    multiple=True,
+    default=["unprocessed", "rejected", "mapped"],
+    help="Filter component requests by state.",
+)
+def component_request(state: tuple[str]):
+    """List component requests by processing state."""
+    for cr in ComponentRequest.where("state", "in", list(state)):
+        print("title:", cr.vendor, "-", cr.name, "-", cr.version)
+        print("URL:", cr.component_url)
+        print("state:", cr.state)
+        print()
+
+
+def main():
+    try:
+        exit_code = cli.main(standalone_mode=False)
+        sys.exit(exit_code)
+    except JSONAPIRequestError as e:
+        logger.error("%s - %s", e.error_code, e.message)
+        for error in e.errors:
+            logger.error("%s. Detail: %s", error.title, error.detail)
+        sys.exit(1)
+    except RequestError as e:
+        logger.error("%s - %s", e.error_code, e.message)
+        sys.exit(1)
+    except (BadCycloneDXFileError, FileNotFoundError, BadParameter) as e:
+        logger.error("%s", e)
+        sys.exit(1)
+    except UsageError as e:
+        logger.error("Bad command invocation")
+        print()
+        print(e.message)
+        sys.exit(1)
+    except Exception as e:
+        logger.error("Unknown error occurred", exc_info=e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
