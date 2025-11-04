53011fe066f6b9a4de81e065fafde680e8a7654d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:39 +0000

    Add package winipedia-utils/0.6.6/winipedia_utils-0.6.6-py3-none-any.whl

---
+++ packages/winipedia-utils/winipedia_utils-0.6.6-py3-none-any.whl/winipedia_utils/projects/project.py
@@ -0,0 +1,17 @@
+"""Utilities for working with Python projects."""
+
+from winipedia_utils.modules.module import create_module
+from winipedia_utils.projects.poetry.config import (
+    PyprojectConfigFile,  # avoid circular import
+)
+from winipedia_utils.text.config import (
+    ConfigFile,  # avoid circular import
+)
+
+
+def create_project_root() -> None:
+    """Create the project root."""
+    ConfigFile.init_config_files()
+
+    src_package_name = PyprojectConfigFile.get_package_name()
+    create_module(src_package_name, is_package=True)
