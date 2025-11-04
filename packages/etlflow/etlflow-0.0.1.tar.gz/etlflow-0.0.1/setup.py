aa914a1c52470b3a33d862bb600c7e558468a9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 06:05:23 +0000

    Add package etlflow/0.0.1/etlflow-0.0.1.tar.gz

---
+++ packages/etlflow/etlflow-0.0.1.tar.gz/etlflow-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="etlflow",
+    version="0.0.1",
+    author="Farshid Ashouri",
+    author_email="farsheed.ashouri@gmail.com",
+    description="A basic 'Hello World' package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rodmena-limited/etlflow",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
