e66fed73ddc2bb3518251d52b7260afe194f3d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:06:01 +0000

    Add package snacktime/0.2.3/snacktime-0.2.3.tar.gz

---
+++ packages/snacktime/snacktime-0.2.3.tar.gz/snacktime-0.2.3/snacktime/__init__.py
@@ -0,0 +1,11 @@
+from .random_snack import get_random_snack
+from .sweet_treat import get_sweet_treat
+from .healthy_treat import get_healthy_treat
+from .recipe import get_recipe
+
+__all__ = [
+    "get_random_snack",
+    "get_sweet_treat",
+    "get_healthy_treat",
+    "get_recipe"
+]
