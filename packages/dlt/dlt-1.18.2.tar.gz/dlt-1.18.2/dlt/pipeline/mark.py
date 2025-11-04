558f4dbd5348c37b03e406976e52e8f7cb02f616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:05:30 +0000

    Add package dlt/1.18.2/dlt-1.18.2.tar.gz

---
+++ packages/dlt/dlt-1.18.2.tar.gz/dlt-1.18.2/dlt/pipeline/mark.py
@@ -0,0 +1,9 @@
+"""Module with mark functions that make data to be specially processed"""
+from dlt.extract import (
+    with_table_name,
+    with_hints,
+    with_file_import,
+    make_hints,
+    make_nested_hints,
+    materialize_schema_item as materialize_table_schema,
+)
