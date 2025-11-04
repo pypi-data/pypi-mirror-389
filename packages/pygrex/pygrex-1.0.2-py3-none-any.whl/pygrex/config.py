27881ea24cd50c7c6f99fc3dbaedd537025f594b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:08 +0000

    Add package pygrex/1.0.2/pygrex-1.0.2-py3-none-any.whl

---
+++ packages/pygrex/pygrex-1.0.2-py3-none-any.whl/pygrex/config.py
@@ -0,0 +1,7 @@
+import yaml
+from box import Box
+
+with open("configs/config.yml", "r") as yml_file:
+    full_cfg = yaml.safe_load(yml_file)
+
+cfg = Box({**full_cfg["base"]}, default_box=True, default_box_attr=None)
