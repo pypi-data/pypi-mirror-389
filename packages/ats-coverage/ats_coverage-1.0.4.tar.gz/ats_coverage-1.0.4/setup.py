a224b4393cd984abb75e9cca6f57a763a173fee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:04:44 +0000

    Add package ats-coverage/1.0.4/ats_coverage-1.0.4.tar.gz

---
+++ packages/ats-coverage/ats_coverage-1.0.4.tar.gz/ats_coverage-1.0.4/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Module
+    setup.py
+Copyright
+    Copyright (C) 2024 - 2025 Vladimir Roncevic <elektron.ronca@gmail.com>
+    ats_coverage is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    ats_coverage is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program. If not, see <http://www.gnu.org/licenses/>.
+Info
+    Defines setup for package ats_coverage.
+'''
+
+from typing import List, Optional
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+__author__: str = 'Vladimir Roncevic'
+__copyright__: str = '(C) 2025, https://vroncevic.github.io/ats_coverage'
+__credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
+__license__: str = 'https://github.com/vroncevic/ats_coverage/blob/dev/LICENSE'
+__version__: str = '1.0.4'
+__maintainer__: str = 'Vladimir Roncevic'
+__email__: str = 'elektron.ronca@gmail.com'
+__status__: str = 'Updated'
+
+TOOL_DIR: str = 'ats_coverage/'
+CONF: str = 'conf'
+LOG: str = 'log'
+THIS_DIR: str = abspath(dirname(__file__))
+long_description: Optional[str] = None
+with open(join(THIS_DIR, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+PROGRAMMING_LANG: str = 'Programming Language :: Python ::'
+VERSIONS: List[str] = ['3.10', '3.11', '3.12']
+SUPPORTED_PY_VERSIONS: List[str] = [
+    f'{PROGRAMMING_LANG} {VERSION}' for VERSION in VERSIONS
+]
+LICENSE_PREFIX: str = 'License :: OSI Approved ::'
+LICENSES: List[str] = [
+    'GNU Lesser General Public License v3 or later (LGPLv3+)'
+]
+PYP_CLASSIFIERS: List[str] = SUPPORTED_PY_VERSIONS
+setup(
+    name='ats_coverage',
+    version='1.0.4',
+    description='Python code coverage automation',
+    author='Vladimir Roncevic',
+    author_email='elektron.ronca@gmail.com',
+    url='https://vroncevic.github.io/ats_coverage',
+    license='GPL-3.0-or-later',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='code, coverage, automation',
+    platforms='any',
+    classifiers=PYP_CLASSIFIERS,
+    packages=['ats_coverage', 'ats_coverage.pro'],
+    install_requires=['ats_utilities', 'coverage', 'pathlib'],
+    package_data={
+        'ats_coverage': [
+            'py.typed',
+            f'{CONF}/ats_coverage.logo',
+            f'{CONF}/ats_coverage.cfg',
+            f'{CONF}/ats_coverage_util.cfg',
+            f'{LOG}/ats_coverage.log'
+        ]
+    },
+    data_files=[(
+        '/usr/local/bin/', [
+            f'{TOOL_DIR}run/ats_coverage_run.py'
+        ]
+    )]
+)
