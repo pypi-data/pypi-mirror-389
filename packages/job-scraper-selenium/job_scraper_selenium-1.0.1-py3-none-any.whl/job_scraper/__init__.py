f939fb2896d2375247eee7946e566d8c7d0ced40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:23:15 +0000

    Add package job-scraper-selenium/1.0.1/job_scraper_selenium-1.0.1-py3-none-any.whl

---
+++ packages/job-scraper-selenium/job_scraper_selenium-1.0.1-py3-none-any.whl/job_scraper/__init__.py
@@ -0,0 +1,18 @@
+"""
+Job Scraper - A Python package for scraping job postings from Indeed and LinkedIn
+
+This package provides easy-to-use functions for extracting job details from popular
+job posting websites using Selenium for browser automation.
+
+Author: Adil C
+License: MIT
+"""
+
+__version__ = "1.0.1"
+__author__ = "Adil C"
+__email__ = "adilc0070@gmail.com"
+
+from .scraper import scrape_job, scrape_indeed_job, scrape_linkedin_job
+
+__all__ = ['scrape_job', 'scrape_indeed_job', 'scrape_linkedin_job']
+
