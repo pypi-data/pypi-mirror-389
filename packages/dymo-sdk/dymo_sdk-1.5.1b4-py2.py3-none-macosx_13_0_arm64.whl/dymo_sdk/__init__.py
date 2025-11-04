dea96b6127921283084ba61cc3e307011a7e22c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:05:31 +0000

    Add package dymo-sdk/1.5.1b4/dymo_sdk-1.5.1b4-py2.py3-none-macosx_13_0_arm64.whl

---
+++ packages/dymo-sdk/dymo_sdk-1.5.1b4-py2.py3-none-macosx_13_0_arm64.whl/dymo_sdk/__init__.py
@@ -0,0 +1,13 @@
+from .core import *
+
+__all__ = [
+    "LabelObject",
+    "DymoLabel",
+    "DymoPrinter",
+    "RollStatus",
+    "get_printers",
+    "print_label",
+    "is_roll_status_supported",
+    "refresh_printer",
+    "get_roll_status"
+]
