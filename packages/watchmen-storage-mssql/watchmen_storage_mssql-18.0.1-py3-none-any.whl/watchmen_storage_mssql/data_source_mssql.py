614c10fe22e482f372f824c64b40b29b3a0056d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 17:05:21 +0000

    Add package watchmen-storage-mssql/18.0.1/watchmen_storage_mssql-18.0.1-py3-none-any.whl

---
+++ packages/watchmen-storage-mssql/watchmen_storage_mssql-18.0.1-py3-none-any.whl/watchmen_storage_mssql/data_source_mssql.py
@@ -0,0 +1,76 @@
+import urllib.parse
+from typing import List, Optional
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+from watchmen_model.common import DataModel
+from watchmen_model.system import DataSource, DataSourceParam
+from watchmen_storage import DataSourceHelper
+from watchmen_utilities import is_not_blank, serialize_to_json
+from .storage_mssql import StorageMSSQL, TopicDataStorageMSSQL
+
+
+def redress_url_by_mssql(url: str) -> str:
+	if url.startswith('mssql://'):
+		return url.replace('mssql://', 'mssql+pyodbc://')
+	else:
+		return url
+
+
+class MSSQLDataSourceParams(DataModel):
+	echo: bool = False
+	poolRecycle: int = 3600
+
+
+# noinspection DuplicatedCode
+class MSSQLDataSourceHelper(DataSourceHelper):
+	def __init__(self, data_source: DataSource, params: MSSQLDataSourceParams = MSSQLDataSourceParams()):
+		super().__init__(data_source)
+		self.engine = self.acquire_engine(params)
+
+	@staticmethod
+	def acquire_engine_by_url(url: str, params: MSSQLDataSourceParams) -> Engine:
+		return create_engine(
+			redress_url_by_mssql(url),
+			echo=params.echo,
+			future=True,
+			pool_recycle=params.poolRecycle,
+			json_serializer=serialize_to_json
+		)
+
+	@staticmethod
+	def find_param(params: Optional[List[DataSourceParam]], key: str) -> Optional[str]:
+		if params is None:
+			return None
+		
+		for param in params:
+			if is_not_blank(param.name) and param.name.strip().lower() == key:
+				value = param.value
+				if is_not_blank(value):
+					return value.strip()
+		return None
+
+	@staticmethod
+	def acquire_engine_by_params(
+			username: str, password: str, host: str, port: str, name: str,
+			data_source_params: Optional[List[DataSourceParam]],
+			params: MSSQLDataSourceParams
+	) -> Engine:
+		dsn = MSSQLDataSourceHelper.find_param(data_source_params, 'dsn')
+		encoded_password = urllib.parse.quote_plus(password)
+		if dsn is not None:
+			url = f'mssql+pyodbc://{username}:{encoded_password}@{dsn}'
+			return MSSQLDataSourceHelper.acquire_engine_by_url(url, params)
+		else:
+			# todo for the tls connection
+			url = f'mssql+pyodbc://{username}:{encoded_password}@{host}:{port}/{name}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes'
+			return MSSQLDataSourceHelper.acquire_engine_by_url(url, params)
+
+	def acquire_storage(self) -> StorageMSSQL:
+		return StorageMSSQL(self.engine)
+
+	def acquire_topic_data_storage(self) -> TopicDataStorageMSSQL:
+		return TopicDataStorageMSSQL(self.engine)
+#
+#
---
+++ packages/watchmen-storage-mssql/watchmen_storage_mssql-18.0.1-py3-none-any.whl/watchmen_storage_mssql/storage_mssql_configuration.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from watchmen_model.system import DataSource, DataSourceType
+
+from .data_source_mssql import MSSQLDataSourceHelper, MSSQLDataSourceParams
+from .storage_mssql import StorageMSSQL, TopicDataStorageMSSQL
+
+
+# noinspection PyRedeclaration
+class Configuration:
+	dataSource: DataSource = DataSource(dataSourceType=DataSourceType.MSSQL)
+	params: MSSQLDataSourceParams = MSSQLDataSourceParams()
+	storageHolder: StorageMSSQLConfiguration = None
+
+	def host(self, host: str, port: int = 3306) -> Configuration:
+		self.dataSource.host = host
+		self.dataSource.port = str(port)
+		return self
+
+	def account(self, username: str, password: str) -> Configuration:
+		self.dataSource.username = username
+		self.dataSource.password = password
+		return self
+
+	def url(self, url: str) -> Configuration:
+		"""
+		url should include account information, like:
+		1. mssql://username:password@dsn
+		2. mssql+pyodbc://username:password@dsn
+		1. mssql://username:password@host:port/name?driver=ODBC+Driver+17+for+SQL+Server
+		2. mssql+pyodbc://username:password@host:port/name?driver=ODBC+Driver+17+for+SQL+Server
+		"""
+		self.dataSource.url = url
+		return self
+
+	def schema(self, schema: str) -> Configuration:
+		self.dataSource.name = schema
+		return self
+
+	def echo(self, enabled: bool = False) -> Configuration:
+		self.params.echo = enabled
+		return self
+
+	def get_or_create_storage_holder(self) -> StorageMSSQLConfiguration:
+		if self.storageHolder is None:
+			self.storageHolder = StorageMSSQLConfiguration(self.dataSource, self.params)
+		return self.storageHolder
+
+	def build(self) -> StorageMSSQL:
+		return self.get_or_create_storage_holder().create_storage()
+
+	def build_topic_data(self) -> TopicDataStorageMSSQL:
+		return self.get_or_create_storage_holder().create_topic_data_storage()
+
+
+class StorageMSSQLConfiguration:
+	"""
+	configuration of MSSQL storage
+	"""
+
+	def __init__(self, data_source: DataSource, params: MSSQLDataSourceParams):
+		super().__init__()
+		self.helper = MSSQLDataSourceHelper(data_source, params)
+
+	def create_storage(self) -> StorageMSSQL:
+		return self.helper.acquire_storage()
+
+	def create_topic_data_storage(self) -> TopicDataStorageMSSQL:
+		return self.helper.acquire_topic_data_storage()
+
+	@staticmethod
+	def config() -> Configuration:
+		return Configuration()
