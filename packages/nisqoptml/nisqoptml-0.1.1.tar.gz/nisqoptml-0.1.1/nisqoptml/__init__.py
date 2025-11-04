ea4514bba28f15a3eaee54c6b5686e61c85ec285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:05:01 +0000

    Add package nisqoptml/0.1.1/nisqoptml-0.1.1.tar.gz

---
+++ packages/nisqoptml/nisqoptml-0.1.1.tar.gz/nisqoptml-0.1.1/nisqoptml/__init__.py
@@ -0,0 +1,18 @@
+"""
+NISQOptML: NISQ-Optimized Machine Learning Library
+
+A noise-resilient Quantum Machine Learning library with support for:
+- Quantum Neural Networks (QNN)
+- Error mitigation techniques
+- Federated learning
+- Differential privacy
+- Distributed execution
+
+Author: Venkata Vikhyat Choppa
+Email: venkata_choppa@outlook.com
+"""
+
+from .qnn import QNN
+
+__version__ = "0.1.0"
+__all__ = ['QNN']
