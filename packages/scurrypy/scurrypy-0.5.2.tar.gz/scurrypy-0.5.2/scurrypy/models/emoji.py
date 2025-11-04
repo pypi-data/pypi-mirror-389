d68c4832508ab2a5ab022c38ea27ecf453317fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 14:05:04 +0000

    Add package scurrypy/0.5.2/scurrypy-0.5.2.tar.gz

---
+++ packages/scurrypy/scurrypy-0.5.2.tar.gz/scurrypy-0.5.2/scurrypy/models/emoji.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from ..model import DataModel
+
+from urllib.parse import quote
+
+@dataclass
+class EmojiModel(DataModel):
+    """Represents a Discord emoji."""
+    name: str
+    """Name of emoji."""
+
+    id: int = 0
+    """ID of the emoji (if custom)."""
+
+    animated: bool = False
+    """If the emoji is animated. Defaults to False."""
+
+    @property
+    def mention(self) -> str:
+        """For use in message content."""
+        return f"<a:{self.name}:{self.id}>" if self.animated else f"<:{self.name}:{self.id}>"
+
+    @property
+    def api_code(self) -> str:
+        """Return the correct API code for this emoji (URL-safe)."""
+        if not self.id:
+            # unicode emoji
+            return quote(self.name)
+
+        # custom emoji
+        if self.animated:
+            return quote(f"a:{self.name}:{self.id}")
+        
+        return quote(f"{self.name}:{self.id}")
