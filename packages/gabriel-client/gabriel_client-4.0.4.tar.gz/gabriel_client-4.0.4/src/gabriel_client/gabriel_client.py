1704963b339fe467bbe99e5938ec3124c65133bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:04:58 +0000

    Add package gabriel-client/4.0.4/gabriel_client-4.0.4.tar.gz

---
+++ packages/gabriel-client/gabriel_client-4.0.4.tar.gz/gabriel_client-4.0.4/src/gabriel_client/gabriel_client.py
@@ -0,0 +1,196 @@
+"""Abstract base class for a Gabriel client and related classes."""
+
+import asyncio
+import logging
+import threading
+import uuid
+from abc import ABC, abstractmethod
+from collections.abc import Callable, Coroutine
+from typing import Any, Union
+
+from gabriel_protocol.gabriel_pb2 import InputFrame
+
+logger = logging.getLogger(__name__)
+
+
+class InputProducer:
+    """An input producer that produces inputs to send to the server.
+
+    Stores a callback for producing input frames as well as the identifiers
+    of the cognitive engines to target.
+
+    The input producer can be stopped and resumed using :meth:`stop` and
+    :meth:`resume`. The current status (stopped/running) can be obtained using
+    :meth:`is_running`. By default, the input producer is running.
+
+    The methods of this class are thread-safe.
+    """
+
+    def __init__(
+        self,
+        producer: Callable[[], Coroutine[Any, Any, InputFrame | None]],
+        target_engine_ids: list[str],
+        source_name: Union[str, None] = None,
+    ):
+        """Initialize the input producer.
+
+        Args:
+            producer (coroutine function):
+                A coroutine function that produces input data
+            target_engine_ids (list[str]):
+                A list of target engine IDs for the input
+            source_name (str, optional):
+                The name of the source producing the input
+        """
+        self._running = threading.Event()
+        self._running.set()
+        self._producer = producer
+        self._target_engine_ids = target_engine_ids
+        self._target_engine_lock = threading.Lock()
+        self.source_id = (
+            source_name + "-" + str(uuid.uuid4())
+            if source_name
+            else str(uuid.uuid4())
+        )
+        self._loop = None
+
+    async def produce(self) -> InputFrame | None:
+        """Invoke the producer to generate input.
+
+        Raises:
+            Exception: if the producer is not running
+        """
+        if not self._running.is_set():
+            raise Exception("Producer called when not running")
+        res = await self._producer()
+        return res
+
+    def resume(self) -> None:
+        """Resume the producer."""
+        if self._running.is_set():
+            raise Exception("Producer already started")
+        self._running.set()
+        with self._target_engine_lock:
+            logger.info(
+                f"Starting producer and targeting engines"
+                f"{self._target_engine_ids}"
+            )
+
+    def stop(self) -> None:
+        """Stop the producer."""
+        logger.info("Stopping producer")
+        self._running.clear()
+
+    def change_target_engines(self, target_engine_ids: list[str]) -> None:
+        """Change the target engines for the producer.
+
+        Args:
+            target_engine_ids (list[str]):
+                A list of target engine IDs for the input
+
+        """
+        with self._target_engine_lock:
+            self._target_engine_ids = target_engine_ids
+        logger.info(f"Changing target engines to {target_engine_ids}")
+
+    def is_running(self) -> bool:
+        """Check if the producer is running."""
+        return self._running.is_set()
+
+    def _wait_for_running_internal(self, shutdown_event):
+        while not shutdown_event.is_set():
+            self._running.wait(0.1)
+
+    async def wait_for_running(self) -> None:
+        """Block until the producer is running."""
+        shutdown_event = threading.Event()
+        try:
+            await asyncio.get_running_loop().run_in_executor(
+                None, self._wait_for_running_internal, shutdown_event
+            )
+        except asyncio.CancelledError:
+            shutdown_event.set()
+            raise
+
+    def get_target_engines(self) -> list[str]:
+        """Return the target engines for the producer."""
+        with self._target_engine_lock:
+            return self._target_engine_ids
+
+
+class TokenPool:
+    """A pool of tokens.
+
+    Used to limit the number of in-flight requests for a particular
+    input source.
+    """
+
+    def __init__(self, num_tokens):
+        """Initialize the token pool.
+
+        Args:
+            num_tokens (int): The number of tokens in the pool
+
+        """
+        self._max_tokens = num_tokens
+        self._num_tokens = num_tokens
+        self._sem = asyncio.BoundedSemaphore(num_tokens)
+
+    def return_token(self) -> None:
+        """Return a token to the pool."""
+        self._sem.release()
+
+    async def get_token(self) -> None:
+        """Acquire a token from the pool.
+
+        Wait if necessary until a token is available.
+        """
+        logger.debug("Waiting for token")
+        await self._sem.acquire()
+        logger.debug("Token acquired")
+
+    def is_locked(self) -> bool:
+        """Check if the semaphore is locked."""
+        return self._sem.locked()
+
+    def reset_tokens(self) -> None:
+        """Reset the number of tokens in the pool to the max number."""
+        self._sem = asyncio.Semaphore(self._max_tokens)
+        self._num_tokens = self._max_tokens
+
+    def get_remaining_tokens(self) -> int:
+        """Return the number of remaining tokens in the pool."""
+        return self._sem._value
+
+
+class GabrielClient(ABC):
+    """Abstract base class for a Gabriel client."""
+
+    def __init__(self):
+        """Initialize the Gabriel client."""
+        self._running = True
+        # Whether a welcome message has been received from the server
+        self._welcome_event = asyncio.Event()
+        self.input_producers = set()
+        # The number of tokens per input source, as specified by the \
+        # server
+        self._num_tokens_per_source = None
+        # Mapping from source id to tokens
+        self._tokens = {}
+
+    @abstractmethod
+    def launch(self) -> None:
+        """Launch the client synchronously.
+
+        This method will block execution until the client is stopped.
+        """
+        pass
+
+    @abstractmethod
+    def launch_async(self) -> None:
+        """Launch the client asynchronously."""
+        pass
+
+    def stop(self) -> None:
+        """Stop the client."""
+        self._running = False
---
+++ packages/gabriel-client/gabriel_client-4.0.4.tar.gz/gabriel_client-4.0.4/src/gabriel_client/websocket_client.py
@@ -0,0 +1,177 @@
+"""WebSocket Gabriel client used to communicate with a Gabriel server."""
+
+import asyncio
+import logging
+from typing import Callable
+from urllib.parse import urlparse
+
+import websockets
+import websockets.client
+from gabriel_protocol import gabriel_pb2
+
+from gabriel_client.gabriel_client import (
+    GabrielClient,
+    InputProducer,
+    TokenPool,
+)
+
+URI_FORMAT = "ws://{host}:{port}"
+
+
+logger = logging.getLogger(__name__)
+websockets_logger = logging.getLogger(websockets.__name__)
+
+# The entire payload will be printed if this is allowed to be DEBUG
+websockets_logger.setLevel(logging.INFO)
+
+
+class WebsocketClient(GabrielClient):
+    """A Gabriel client that talks to the server over WebSockets.
+
+    The Gabriel server must be configured to use Websockets for client
+    communication.
+    """
+
+    def __init__(
+        self,
+        server_endpoint: str,
+        input_producers: list[InputProducer],
+        consumer: Callable[[gabriel_pb2.ResultWrapper], None],
+    ):
+        """Initialize the client.
+
+        Args:
+        server_endpoint (str):
+            The server endpoint to connect to. Must have the form
+            'ws://interface:port'.
+        input_producers (List[InputProducer]):
+            A list of instances of InputProducer for the inputs
+            produced by this client
+        consumer (Callable[[gabriel_pb2.ResultWrapper], None]):
+            Callback for results from server
+
+        """
+        super().__init__()
+        self.consumer = consumer
+        self.input_producers = set(input_producers)
+
+        self.server_endpoint = server_endpoint
+        parsed_server_endpoint = urlparse(server_endpoint.lower())
+        if parsed_server_endpoint.scheme != "ws":
+            raise ValueError(
+                f"Unsupported protocol {parsed_server_endpoint.scheme}"
+            )
+
+    def launch(self, message_max_size=None):
+        """Launch the client synchronously."""
+        asyncio.run(self.launch_async())
+
+    async def launch_async(self):
+        """Launch the client asynchronously."""
+        async with websockets.connect(
+            self.server_endpoint,
+            max_size=None,
+        ) as websocket:
+            self._websocket = websocket
+            consumer_task = asyncio.create_task(self._consumer_handler())
+            tasks = [
+                asyncio.create_task(self._producer_handler(input_producer))
+                for input_producer in self.input_producers
+            ]
+            tasks.append(consumer_task)
+
+            try:
+                done, pending = await asyncio.wait(
+                    tasks, return_when=asyncio.FIRST_COMPLETED
+                )
+            except asyncio.CancelledError:
+                for task in tasks:
+                    task.cancel()
+                await asyncio.gather(*tasks, return_exceptions=True)
+                raise
+            for task in pending:
+                task.cancel()
+            logger.info("Disconnected From Server")
+
+    async def _consumer_handler(self):
+        while self._running:
+            try:
+                raw_input = await self._websocket.recv()
+            except websockets.exceptions.ConnectionClosed:
+                return  # stop the handler
+            logger.debug("Received input from server")
+
+            to_client = gabriel_pb2.ToClient()
+            to_client.ParseFromString(raw_input)
+
+            if to_client.HasField("welcome"):
+                self._process_welcome(to_client.welcome)
+            elif to_client.HasField("response"):
+                self._process_response(to_client.response)
+            else:
+                raise Exception("Empty to_client message")
+
+    def _process_welcome(self, welcome):
+        self._num_tokens_per_source = welcome.num_tokens_per_source
+        self._welcome_event.set()
+
+    def _process_response(self, response):
+        result_wrapper = response.result_wrapper
+        if result_wrapper.status == gabriel_pb2.ResultWrapper.SUCCESS:
+            self.consumer(result_wrapper)
+        elif (
+            result_wrapper.status
+            == gabriel_pb2.ResultWrapper.NO_ENGINE_FOR_SOURCE
+        ):
+            raise Exception("No engine for source")
+        else:
+            status = result_wrapper.Status.Name(result_wrapper.status)
+            logger.error("Output status was: %s", status)
+
+        if response.return_token:
+            self._tokens[response.source_id].return_token()
+
+    async def _producer_handler(self, producer: InputProducer):
+        """Generate inputs and sends them to the server.
+
+        Loop waiting until there is a token available. Then call
+        producer to get the gabriel_pb2.InputFrame to send.
+
+        Args:
+            producer (InputProducer):
+                The InputProducer instance that produces inputs for
+                this client
+
+        """
+        await self._welcome_event.wait()
+        token_pool = TokenPool(self._num_tokens_per_source)
+        self._tokens[producer.source_id] = token_pool
+        frame_id = 1
+
+        while self._running:
+            await token_pool.get_token()
+
+            input_frame = await producer.produce()
+            if input_frame is None:
+                token_pool.return_token()
+                logger.info("Received None from producer")
+                continue
+
+            input = gabriel_pb2.ClientInput()
+            input.frame_id = frame_id
+            frame_id += 1
+            input.source_id = producer.source_id
+            input.target_engine_ids.extend(producer.get_target_engines())
+            input.input_frame.CopyFrom(input_frame)
+
+            from_client = gabriel_pb2.FromClient()
+            from_client.input.CopyFrom(input)
+
+            try:
+                await self._send_from_client(from_client)
+            except websockets.exceptions.ConnectionClosed:
+                return  # stop the handler
+
+    async def _send_from_client(self, from_client):
+        # Removing this method will break measurement_client
+        await self._websocket.send(from_client.SerializeToString())
