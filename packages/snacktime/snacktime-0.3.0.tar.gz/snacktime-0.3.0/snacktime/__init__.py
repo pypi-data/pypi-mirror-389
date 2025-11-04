7fc10df8ab010e7b671f94d0e81fb2d1765334f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:57 +0000

    Add package snacktime/0.3.0/snacktime-0.3.0.tar.gz

---
+++ packages/snacktime/snacktime-0.3.0.tar.gz/snacktime-0.3.0/snacktime/__init__.py
@@ -0,0 +1,8 @@
+from .core import random_snack, random_vegetable, random_treat, recipe_salad
+
+__all__ = [
+    "random_snack",
+    "random_vegetable",
+    "random_treat",
+    "recipe_salad",
+]
