2b2d9f478408ad36fdd7ef455c13cd07b39087c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:23:02 +0000

    Add package chap-core/1.1.0/chap_core-1.1.0-py3-none-any.whl

---
+++ packages/chap-core/chap_core-1.1.0-py3-none-any.whl/chap_core/exceptions.py
@@ -0,0 +1,21 @@
+class ModelFailedException(Exception): ...
+
+
+class InvalidModelException(Exception): ...
+
+
+class CommandLineException(Exception): ...
+
+
+class NoPredictionsError(Exception):
+    pass
+
+
+class GEEError(Exception):
+    pass
+
+
+class ModelConfigurationException(Exception): ...
+
+
+class InvalidDateError(Exception): ...
