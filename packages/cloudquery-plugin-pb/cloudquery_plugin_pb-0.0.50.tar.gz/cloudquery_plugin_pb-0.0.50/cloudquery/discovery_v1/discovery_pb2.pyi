2b3a5d48937d0c05566d2657df55b0d7e919a072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:41 +0000

    Add package cloudquery-plugin-pb/0.0.50/cloudquery_plugin_pb-0.0.50.tar.gz

---
+++ packages/cloudquery-plugin-pb/cloudquery_plugin_pb-0.0.50.tar.gz/cloudquery_plugin_pb-0.0.50/cloudquery/discovery_v1/discovery_pb2.pyi
@@ -0,0 +1,19 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetVersions(_message.Message):
+    __slots__ = ()
+    class Request(_message.Message):
+        __slots__ = ()
+        def __init__(self) -> None: ...
+    class Response(_message.Message):
+        __slots__ = ("versions",)
+        VERSIONS_FIELD_NUMBER: _ClassVar[int]
+        versions: _containers.RepeatedScalarFieldContainer[int]
+        def __init__(self, versions: _Optional[_Iterable[int]] = ...) -> None: ...
+    def __init__(self) -> None: ...
