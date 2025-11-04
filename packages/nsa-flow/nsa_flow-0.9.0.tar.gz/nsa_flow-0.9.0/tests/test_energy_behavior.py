062e4d75f5840d84360042a4534845c4321d9018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:06:48 +0000

    Add package nsa-flow/0.9.0/nsa_flow-0.9.0.tar.gz

---
+++ packages/nsa-flow/nsa_flow-0.9.0.tar.gz/nsa_flow-0.9.0/tests/test_energy_behavior.py
@@ -0,0 +1,26 @@
+import torch
+import matplotlib.pyplot as plt
+import nsa_flow  # make sure your module is importable
+from nsa_flow import nsa_flow,  nsa_flow_retract_auto, invariant_orthogonality_defect, defect_fast, nsa_flow_autograd, plot_nsa_trace
+torch.manual_seed(42)
+Y = torch.randn(50, 10)
+X0 = torch.randn(Y.shape[0], Y.shape[1])
+retraction='soft_polar'
+o='lars'
+###################
+result = nsa_flow_autograd(
+        Y * 1e4,
+        w=0.5, 
+        retraction=retraction,
+        optimizer=o,
+        max_iter=500,
+        record_every=1,
+        tol=1e-8,
+        initial_learning_rate=None,
+        lr_strategy="auto",
+        apply_nonneg=True,
+        verbose=True,
+    )
+invariant_orthogonality_defect(Y)
+invariant_orthogonality_defect(result['Y'])
+plot_nsa_trace( result['traces']   )
