fd2e4f2e379b1ddc8da1f3cb6cb26a62f56db3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 14:04:48 +0000

    Add package chalkpy/2.89.37/chalkpy-2.89.37-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-2.89.37-py3-none-any.whl/chalk/utils/__init__.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from typing_extensions import TypeAlias
+
+MachineType: TypeAlias = str
+"""The type of machine to use.
+
+You can optionally specify that resolvers need to run
+on a machine other than the default. Must be configured
+in your deployment.
+"""
+
+AnyDataclass: TypeAlias = Any
+"""Any class decorated by `@dataclass`.
+
+There isn't a base class for `dataclass`, so we use this
+`TypeAlias` to refer to indicate any class decorated with
+`@dataclass`.
+"""
