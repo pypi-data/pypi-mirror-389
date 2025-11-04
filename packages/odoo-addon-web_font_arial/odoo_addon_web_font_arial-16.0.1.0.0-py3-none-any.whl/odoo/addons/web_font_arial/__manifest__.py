3f9095108d048154fa912c480dbf5a5d2b4cca44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:51 +0000

    Add package odoo-addon-web_font_arial/16.0.1.0.0/odoo_addon_web_font_arial-16.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-web_font_arial/odoo_addon_web_font_arial-16.0.1.0.0-py3-none-any.whl/odoo/addons/web_font_arial/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Web Font Arial",
+    "summary": """
+        Add Arial font to font selection.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Administration",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
