92509f40c735108f4ff0f870800ab0db5c66217e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 06:05:21 +0000

    Add package allotropy/0.1.110/allotropy-0.1.110.tar.gz

---
+++ packages/allotropy/allotropy-0.1.110.tar.gz/allotropy-0.1.110/src/allotropy/parsers/qiacuity_dpcr/constants.py
@@ -0,0 +1,11 @@
+BRAND_NAME = "Qiacuity Digital PCR System"
+PRODUCT_MANUFACTURER = "Qiagen"
+SOFTWARE_NAME = "Qiacuity Software Suite"
+DEVICE_TYPE = "dPCR"
+DEVICE_IDENTIFIER = "Qiacuity dPCR"
+
+SAMPLE_ROLE_TYPE_MAPPING = {
+    "Sample": "Sample Role",
+    "Control": "Control Sample Role",
+    "Non Template Control": "Blank Role",
+}
