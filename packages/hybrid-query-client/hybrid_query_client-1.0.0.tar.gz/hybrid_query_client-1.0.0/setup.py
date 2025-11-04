c642be074f9db22b1d794ac05f9ea0d06f320f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:19 +0000

    Add package hybrid-query-client/1.0.0/hybrid_query_client-1.0.0.tar.gz

---
+++ packages/hybrid-query-client/hybrid_query_client-1.0.0.tar.gz/hybrid_query_client-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hybrid_query_client",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Socket client for NL hybrid query engine",
+)
