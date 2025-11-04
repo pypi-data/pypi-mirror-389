caf47a698db9466c785c55da883d70be6bdb9230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:28 +0000

    Add package singtown_ai/1.8.0/singtown_ai-1.8.0-py3-none-any.whl

---
+++ packages/singtown_ai/singtown_ai-1.8.0-py3-none-any.whl/singtown_ai/type.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+
+class BoundingBox(BaseModel):
+    label: str
+    xmin: float
+    ymin: float
+    xmax: float
+    ymax: float
+
+
+class Annotation(BaseModel):
+    url: str
+    subset: Literal["TRAIN", "VALID", "TEST"]
+    classification: str = ""
+    object_detection: List[BoundingBox] = []
+
+
+class Project(BaseModel):
+    labels: List[str]
+    type: Literal["CLASSIFICATION", "OBJECT_DETECTION"]
+
+
+class LogEntry(BaseModel):
+    timestamp: float
+    content: str
+
+
+class TaskResponse(BaseModel):
+    project: Project
+    device: str
+    model_name: str
+    freeze_backbone: bool
+    batch_size: int
+    epochs: int
+    learning_rate: float
+    early_stopping: int
+    export_width: int
+    export_height: int
+    metrics: List[dict] = []
+    logs: List[LogEntry] = []
