df5ee632a76d56acc0193f0d2f2672ee4bd524c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:46 +0000

    Add package dr-widget/0.1.0/dr_widget-0.1.0-py3-none-any.whl

---
+++ packages/dr-widget/dr_widget-0.1.0-py3-none-any.whl/dr_widget/widgets/file_drop/src/index.js
@@ -0,0 +1,5 @@
+import { defineWidget } from "@anywidget/svelte";
+import FileDropWidget from "./FileDropWidget.svelte";
+import "./app.css";  // Import styles
+
+export default defineWidget(FileDropWidget);
