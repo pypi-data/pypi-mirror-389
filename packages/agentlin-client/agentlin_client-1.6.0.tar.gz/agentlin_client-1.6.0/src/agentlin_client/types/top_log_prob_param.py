dea4de562cacbe720ffcd71a5c3936c22402f8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:27 +0000

    Add package agentlin-client/1.6.0/agentlin_client-1.6.0.tar.gz

---
+++ packages/agentlin-client/agentlin_client-1.6.0.tar.gz/agentlin_client-1.6.0/src/agentlin_client/types/top_log_prob_param.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Required, TypedDict
+
+__all__ = ["TopLogProbParam"]
+
+
+class TopLogProbParam(TypedDict, total=False):
+    token: Required[str]
+
+    bytes: Required[Iterable[int]]
+
+    logprob: Required[float]
