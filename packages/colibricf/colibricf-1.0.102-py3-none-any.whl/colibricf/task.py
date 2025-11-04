4b9274d44afdfce8719f224c4b91002dc6fcae7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 06:05:13 +0000

    Add package colibricf/1.0.102/colibricf-1.0.102-py3-none-any.whl

---
+++ packages/colibricf/colibricf-1.0.102-py3-none-any.whl/colibricf/task.py
@@ -0,0 +1,56 @@
+# Information: https://clover.coex.tech/programming
+
+import rospy
+from abc import ABC, abstractmethod
+from .drone import Drone
+from .camera import Camera
+from .servo import Servo
+
+class Task(ABC):
+    '''
+    An abstract class to write mission.
+    '''
+
+    drone = Drone()
+    camera = Camera()
+
+    def __init__(self, gpio:int = -1) -> None:
+        if gpio != -1:
+            self.servo = Servo(gpio)
+    
+    @abstractmethod
+    def mission(self):
+        raise Exception("Need implementation.")
+
+    def run(self):
+        '''
+        A secure method to run a mission. Useful in most cases.
+        '''
+
+        try:
+            self.mission()
+
+        except KeyboardInterrupt:
+            print("Aborting")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            print('Landing')
+            self.drone.land_wait()
+
+    def return_to_launch_confim(self):
+        '''
+        Use if you need to confirm the return.
+        '''
+
+        pass
+
+    def change_servo_pin(self, gpio:int):
+        '''
+        Change the servo gpio.
+        '''
+
+        self.servo.gpio = gpio
+
