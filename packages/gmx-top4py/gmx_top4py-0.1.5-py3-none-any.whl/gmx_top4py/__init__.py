53f7215e8dafdf0dddbb4a1d6d2387a5933a3ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:04:40 +0000

    Add package gmx-top4py/0.1.5/gmx_top4py-0.1.5-py3-none-any.whl

---
+++ packages/gmx-top4py/gmx_top4py-0.1.5-py3-none-any.whl/gmx_top4py/__init__.py
@@ -0,0 +1,12 @@
+"""
+Welcome to gmx-top4py!
+"""
+
+from __future__ import annotations
+import warnings
+
+warnings.simplefilter("once", DeprecationWarning)
+warnings.warn(
+    "gmx-top4py has been renamed to gmxtop. Use 'pip install gmxtop'.",
+    DeprecationWarning,
+)
