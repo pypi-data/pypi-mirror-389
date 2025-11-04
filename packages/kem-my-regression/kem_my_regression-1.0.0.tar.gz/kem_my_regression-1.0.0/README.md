f31cf1ece4e7428ef7d9eb8116d93902cdc595c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:23:00 +0000

    Add package kem-my-regression/1.0.0/kem_my_regression-1.0.0.tar.gz

---
+++ packages/kem-my-regression/kem_my_regression-1.0.0.tar.gz/kem_my_regression-1.0.0/README.md
@@ -0,0 +1,7 @@
+# My Regression
+
+A simple linear regression package implemented from scratch using NumPy.
+
+## Installation
+```bash
+pip install my_regression
---
+++ packages/kem-my-regression/kem_my_regression-1.0.0.tar.gz/kem_my_regression-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kem_my_regression',
+    version='1.0.0',
+    author='Kambouz Nouha',
+    author_email='kambouz907@gmail.com',
+    packages=find_packages(),
+    url='https://pypi.org/project/my_regression/',
+    license='MIT',
+    description='A simple linear regression package implemented from scratch.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['numpy'],
+)
