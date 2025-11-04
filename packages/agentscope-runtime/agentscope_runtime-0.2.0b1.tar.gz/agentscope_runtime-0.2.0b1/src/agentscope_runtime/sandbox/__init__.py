84634bd9946a68a23412354ab0bd6b51ff3c7621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:57 +0000

    Add package agentscope-runtime/0.2.0b1/agentscope_runtime-0.2.0b1.tar.gz

---
+++ packages/agentscope-runtime/agentscope_runtime-0.2.0b1.tar.gz/agentscope_runtime-0.2.0b1/src/agentscope_runtime/sandbox/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from .custom import *
+from .box.base.base_sandbox import BaseSandbox
+from .box.browser.browser_sandbox import BrowserSandbox
+from .box.filesystem.filesystem_sandbox import FilesystemSandbox
+from .box.gui.gui_sandbox import GuiSandbox
+from .box.training_box.training_box import TrainingSandbox
+
+
+__all__ = [
+    "BaseSandbox",
+    "BrowserSandbox",
+    "FilesystemSandbox",
+    "GuiSandbox",
+    "TrainingSandbox",
+]
