44b1d774efc154ffea40bf8123677451a570c38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:04:51 +0000

    Add package hf-libero/0.1.2/hf_libero-0.1.2.tar.gz

---
+++ packages/hf-libero/hf_libero-0.1.2.tar.gz/hf_libero-0.1.2/setup.py
@@ -0,0 +1,41 @@
+# read the contents of your README file
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "./README.md"), encoding="utf-8") as f:
+    lines = f.readlines()
+
+# remove images from README
+lines = [x for x in lines if ".png" not in x]
+long_description = "".join(lines)
+
+setup(
+    name="hf_libero",
+    packages=[package for package in find_packages() if package.startswith("libero")],
+    install_requires=[],
+    include_package_data=True,
+    package_data={
+        "libero": [
+            "libero/bddl_files/**/*",
+            "libero/assets/**/*",
+            "libero/init_files/**/*",
+        ]
+    },
+    python_requires=">=3",
+    description="LIBERO: Benchmarking Knowledge Transfer for Lifelong Robot Learning",
+    author="Bo Liu, Yifeng Zhu, Chongkai Gao, Yihao Feng, Qiang Liu, Yuke Zhu, Peter Stone",
+    author_email="bliu@cs.utexas.edu, yifengz@cs.utexas.edu",
+    version="0.1.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "lifelong.main=libero.lifelong.main:main",
+            "lifelong.eval=libero.lifelong.evaluate:main",
+            "libero.config_copy=scripts.config_copy:main",
+            "libero.create_template=scripts.create_template:main",
+        ]
+    },
+)
