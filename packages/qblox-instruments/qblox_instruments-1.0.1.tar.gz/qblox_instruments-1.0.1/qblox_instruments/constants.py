af96bab6e92768c3efe6da7e3baa7bca471a33ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:23 +0000

    Add package qblox-instruments/1.0.1/qblox_instruments-1.0.1.tar.gz

---
+++ packages/qblox-instruments/qblox_instruments-1.0.1.tar.gz/qblox_instruments-1.0.1/qblox_instruments/constants.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Description    : IOChannelQSM QCoDeS interface
+# Description    : Project wide constants
+# Git repository : https://gitlab.com/qblox/packages/software/qblox_instruments.git
+# Copyright (C) Qblox BV (2025)
+# ----------------------------------------------------------------------------
+
+from typing import Final
+
+MIN_SAFE_VOLTAGE: Final[float] = -10.0
+MAX_SAFE_VOLTAGE: Final[float] = +10.0
