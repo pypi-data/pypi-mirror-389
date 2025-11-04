eee15299fe78a322061a3f76755e76d2cac484db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:04:45 +0000

    Add package fhir-sheets/2.1.6/fhir_sheets-2.1.6.tar.gz

---
+++ packages/fhir-sheets/fhir_sheets-2.1.6.tar.gz/fhir_sheets-2.1.6/src/fhir_sheets/core/model/resource_link_entity.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict
+
+from .common import get_value_from_keys
+
+
+class ResourceLink:
+    """
+    A class to represent a Fhir Reference between two resources.
+    """
+    originResource_keys = ['OriginResource', 'Origin Resource', 'origin_resource']
+    referencePath_keys = ['ReferencePath', 'Reference Path', 'reference_path']
+    destinationResource_keys = ['DestinationResource', 'Destination Resource', 'destination_resource']
+    def __init__(self, originResource: str, referencePath: str, destinationResource: str):
+        """
+        Initializes the ResourceLink object from a dictionary.
+
+        Args:
+            data: A dictionary containing 'OriginResource', 'ReferencePath', and 'DestinationResource'.
+        """
+        self.originResource = originResource
+        self.referencePath = referencePath
+        self.destinationResource = destinationResource
+        
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]):
+        return cls(get_value_from_keys(data, cls.originResource_keys, ''), get_value_from_keys(data, cls.referencePath_keys, ''), 
+            get_value_from_keys(data, cls.destinationResource_keys, ''))
+        
+    def __repr__(self) -> str:
+        return (f"ResourceLink(originResource='{self.originResource}', "
+                f"referencePath='{self.referencePath}', "
+                f"destinationResource='{self.destinationResource}')")
