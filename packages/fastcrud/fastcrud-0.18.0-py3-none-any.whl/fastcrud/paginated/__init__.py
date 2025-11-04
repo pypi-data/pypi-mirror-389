f7ebb403ab5fee5c0736e8200797d6b9656373ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 15:05:33 +0000

    Add package fastcrud/0.18.0/fastcrud-0.18.0-py3-none-any.whl

---
+++ packages/fastcrud/fastcrud-0.18.0-py3-none-any.whl/fastcrud/paginated/__init__.py
@@ -0,0 +1,17 @@
+from .response import paginated_response
+from .helper import compute_offset
+from .schemas import (
+    PaginatedListResponse,
+    ListResponse,
+    PaginatedRequestQuery,
+    CursorPaginatedRequestQuery,
+)
+
+__all__ = [
+    "paginated_response",
+    "compute_offset",
+    "PaginatedListResponse",
+    "ListResponse",
+    "PaginatedRequestQuery",
+    "CursorPaginatedRequestQuery",
+]
