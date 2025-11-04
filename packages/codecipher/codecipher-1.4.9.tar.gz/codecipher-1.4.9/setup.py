3bc030ab5b65d6d911a9c6f4cb54bf37c66f458f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:04:52 +0000

    Add package codecipher/1.4.9/codecipher-1.4.9.tar.gz

---
+++ packages/codecipher/codecipher-1.4.9.tar.gz/codecipher-1.4.9/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Module
+    setup.py
+Copyright
+    Copyright (C) 2021 - 2025 Vladimir Roncevic <elektron.ronca@gmail.com>
+    codecipher is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    codecipher is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program. If not, see <http://www.gnu.org/licenses/>.
+Info
+    Defines setup for package codecipher.
+'''
+
+from __future__ import print_function
+from typing import List, Optional
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+__author__: str = 'Vladimir Roncevic'
+__copyright__: str = '(C) 2025, https://electux.github.io/codecipher'
+__credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
+__license__: str = 'https://github.com/electux/codecipher/blob/main/LICENSE'
+__version__: str = '1.4.9'
+__maintainer__: str = 'Vladimir Roncevic'
+__email__: str = 'elektron.ronca@gmail.com'
+__status__: str = 'Updated'
+
+THIS_DIR: str = abspath(dirname(__file__))
+long_description: Optional[str] = None
+with open(join(THIS_DIR, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+PROGRAMMING_LANG: str = 'Programming Language :: Python ::'
+VERSIONS: List[str] = ['3.10', '3.11', '3.12']
+SUPPORTED_PY_VERSIONS: List[str] = [
+    f'{PROGRAMMING_LANG} {VERSION}' for VERSION in VERSIONS
+]
+LICENSE_PREFIX: str = 'License :: OSI Approved ::'
+LICENSES: List[str] = [
+    'GNU Lesser General Public License v3 or later (LGPLv3+)',
+]
+PYP_CLASSIFIERS: List[str] = SUPPORTED_PY_VERSIONS
+setup(
+    name='codecipher',
+    version='1.4.9',
+    description='Python Cipher Utilities',
+    author='Vladimir Roncevic',
+    author_email='elektron.ronca@gmail.com',
+    url='https://electux.github.io/codecipher',
+    license='GPL-3.0-or-later',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='cipher, encryption, decryption, cryptology, cryptography',
+    platforms='any',
+    classifiers=PYP_CLASSIFIERS,
+    packages=[
+        'codecipher',
+        'codecipher.a1z52n62',
+        'codecipher.atbs',
+        'codecipher.b64',
+        'codecipher.caesar',
+        'codecipher.vernam',
+        'codecipher.vigenere'
+    ],
+    package_data={
+        'codecipher': [
+            'py.typed'
+        ]
+    }
+)
