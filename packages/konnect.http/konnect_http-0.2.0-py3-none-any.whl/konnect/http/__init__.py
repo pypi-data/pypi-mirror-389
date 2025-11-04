f18286aca434aca11a8221715e60bd99126bb26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 06:05:16 +0000

    Add package konnect.http/0.2.0/konnect_http-0.2.0-py3-none-any.whl

---
+++ packages/konnect.http/konnect_http-0.2.0-py3-none-any.whl/konnect/http/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023-2024  Dom Sekotill <dom.sekotill@kodo.org.uk>
+
+from .request import Method as Method
+from .request import Request as Request
+from .response import Response as Response
+from .session import Session as Session
