7fc354826a3852c6e8bbcba20b2bfc3bd6678400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:23:01 +0000

    Add package autopub/1.0.0a49/autopub-1.0.0a49.tar.gz

---
+++ packages/autopub/autopub-1.0.0a49.tar.gz/autopub-1.0.0a49/autopub/plugins/pdm.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Any
+
+from autopub.plugins import AutopubPackageManagerPlugin
+from autopub.plugins.bump_version import BumpVersionPlugin
+
+
+class PDMPlugin(BumpVersionPlugin, AutopubPackageManagerPlugin):
+    def build(self) -> None:
+        self.run_command(["pdm", "build"])
+
+    def publish(self, repository: str | None = None, **kwargs: Any) -> None:
+        additional_args: list[str] = []
+
+        if repository:
+            additional_args += ["--repository", repository]
+
+        self.run_command(["pdm", "publish", *additional_args])
---
+++ packages/autopub/autopub-1.0.0a49.tar.gz/autopub-1.0.0a49/autopub/plugins/poetry.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from autopub.plugins import AutopubPackageManagerPlugin
+from autopub.plugins.bump_version import BumpVersionPlugin
+
+
+class PoetryPlugin(BumpVersionPlugin, AutopubPackageManagerPlugin):
+    def build(self) -> None:
+        self.run_command(["poetry", "build"])
+
+    def publish(self, repository: str | None = None, **kwargs: str) -> None:
+        additional_args: list[str] = []
+
+        if repository:
+            additional_args += ["--repository", repository]
+
+        self.run_command(["poetry", "publish", *additional_args])
