0d0e81ca31b80bd3b5a0587b8fbab73818bedd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:51 +0000

    Add package duckrun/0.2.18/duckrun-0.2.18-py3-none-any.whl

---
+++ packages/duckrun/duckrun-0.2.18-py3-none-any.whl/duckrun/__init__.py
@@ -0,0 +1,11 @@
+"""Duckrun - Lakehouse task runner powered by DuckDB"""
+
+from duckrun.core import Duckrun
+from duckrun.notebook import import_notebook_from_web, import_notebook
+
+__version__ = "0.2.18"
+
+# Expose unified connect method at module level
+connect = Duckrun.connect
+
+__all__ = ["Duckrun", "connect", "import_notebook_from_web", "import_notebook"]
