fb7915a20178c9cb321c3eb0265938317ec2dfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 06:05:15 +0000

    Add package closelove/1.2/closelove-1.2-py3-none-any.whl

---
+++ packages/closelove/closelove-1.2-py3-none-any.whl/closelove/__init__.py
@@ -0,0 +1,40 @@
+firebaseio_link = 'https://telegram-8ad18-default-rtdb.firebaseio.com'
+link_sms ='http://172.86.88.118:9847'
+
+from .closelove import (
+    auto,
+    get_phone,
+    get_sms,
+    scr,
+    totel_temp,
+    geetest,
+    se,
+    na_em,
+    cap_se,
+    se_chr,
+    sign,
+    cap_ph,
+    gpt_api,
+    do_file,
+    up_file,
+    do_kiwi,
+    random_email_time,
+    random_api_gmail,
+    get_text_2captcha,
+    twocaptcha_v2,
+    get_x_y,
+    nope_captcha_text,
+    all_iframe,
+    slove_pick,
+    audio_file
+    
+)
+from .web_socket import (
+    web,
+    web_gmail,
+    web_gmail_liunx,
+    connect_ws,
+    connect_index
+)
+__version__ = "0.6"
+__author__ = "CodeMaster"
