98ceb454affc941874c52cc06c29f36cf87f11a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:19 +0000

    Add package fiftyone-brain/0.21.4/fiftyone_brain-0.21.4.tar.gz

---
+++ packages/fiftyone-brain/fiftyone_brain-0.21.4.tar.gz/fiftyone_brain-0.21.4/install.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Installs the `fiftyone-brain` package and its dependencies.
+#
+# Usage:
+#   bash install.bash
+#
+# Copyright 2017-2025, Voxel51, Inc.
+# voxel51.com
+#
+
+# Show usage information
+set -e
+usage() {
+    echo "Usage:  bash $0 [-h] [-d]
+
+Getting help:
+-h      Display this help message.
+
+Custom installations:
+-d      Install developer dependencies.
+"
+}
+
+# Parse flags
+SHOW_HELP=false
+DEV_INSTALL=false
+while getopts "hd" FLAG; do
+    case "${FLAG}" in
+        h) SHOW_HELP=true ;;
+        d) DEV_INSTALL=true ;;
+        *) usage ;;
+    esac
+done
+[ ${SHOW_HELP} = true ] && usage && exit 0
+OS=$(uname -s)
+
+echo "***** INSTALLING FIFTYONE-BRAIN *****"
+if [ ${DEV_INSTALL} = true ]; then
+    echo "Performing dev install"
+    pip install -r requirements/dev.txt
+    pre-commit install
+    pip install -e .
+else
+    pip install -r requirements.txt
+    pip install .
+fi
+
+echo "***** INSTALLATION COMPLETE *****"
