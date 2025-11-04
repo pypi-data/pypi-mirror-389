e08a40468a859ba9270bc0f65658dd2f0029f18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 17:05:28 +0000

    Add package kfcv50/0.1.0/kfcv50-0.1.0.tar.gz

---
+++ packages/kfcv50/kfcv50-0.1.0.tar.gz/kfcv50-0.1.0/kfcv50/__init__.py
@@ -0,0 +1,13 @@
+"""
+package_mikezhou_talk - 一个示例Python包
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+# 在这里添加您的包的主要功能
+def hello():
+    """示例函数"""
+    return "Hello from package_mikezhou_talk!"
+
