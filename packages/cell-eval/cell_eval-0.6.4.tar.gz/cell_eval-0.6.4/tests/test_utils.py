b68ca5e3cca6c5a08171c0cf7a0f55109ef0d7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:04:50 +0000

    Add package cell-eval/0.6.4/cell_eval-0.6.4.tar.gz

---
+++ packages/cell-eval/cell_eval-0.6.4.tar.gz/cell_eval-0.6.4/tests/test_utils.py
@@ -0,0 +1,18 @@
+from cell_eval.data import build_random_anndata
+from cell_eval.utils import guess_is_lognorm
+
+
+def test_is_lognorm_true():
+    data = build_random_anndata(normlog=True)
+    assert guess_is_lognorm(data)
+
+
+def test_is_lognorm_view():
+    data = build_random_anndata(normlog=True)
+    sub = data[:100]
+    assert guess_is_lognorm(sub)
+
+
+def test_is_lognorm_false():
+    data = build_random_anndata(normlog=False)
+    assert not guess_is_lognorm(data)
