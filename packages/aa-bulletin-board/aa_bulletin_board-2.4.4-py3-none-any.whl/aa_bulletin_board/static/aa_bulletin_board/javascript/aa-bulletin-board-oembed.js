36b6045ac0cd6cd4133ee2133d61abb6247d3d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 14:05:33 +0000

    Add package aa-bulletin-board/2.4.4/aa_bulletin_board-2.4.4-py3-none-any.whl

---
+++ packages/aa-bulletin-board/aa_bulletin_board-2.4.4-py3-none-any.whl/aa_bulletin_board/static/aa_bulletin_board/javascript/aa-bulletin-board-oembed.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+    'use strict';
+
+    /**
+     * Replace oembed YouTube video with an iframe
+     *
+     * CKEditor5 oembed plugin is used to embed YouTube videos, and browsers cannot render the oembed element.
+     * This function replaces the oembed element with an iframe and is using the YouTube-nocookie domain.
+     *
+     * @param {string} url The YouTube video URL
+     * @returns {`<div class="oembed-video youtube-oembed-video"><iframe src="${string}" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`}
+     */
+    const youtubeOembedToIframe = (url) => {
+        const videoId = new URLSearchParams(new URL(url).search).get('v'); // jshint ignore:line
+        const videoUrl = `https://www.youtube-nocookie.com/embed/${videoId}`;
+        const divClasses = 'oembed-video youtube-oembed-video';
+        const iframeAllow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+
+        return `<div class="${divClasses}"><iframe src="${videoUrl}" allow="${iframeAllow}" allowfullscreen></iframe></div>`;
+    };
+
+    /**
+     * Look for oembed elements and replace them with iframes
+     */
+    const checkForOembed = () => {
+        // Find all oembed elements and loop through them
+        $('.ck-content figure.media oembed').filter((_, element) => {
+            // Check if the source is a YouTube video
+            const source = $(element).attr('url');
+
+            if (source.includes('youtube.com/watch')) {
+                $(element).replaceWith(youtubeOembedToIframe(source));
+            }
+        });
+    };
+
+    // Run the checkForOembed function when the document is ready
+    checkForOembed();
+});
