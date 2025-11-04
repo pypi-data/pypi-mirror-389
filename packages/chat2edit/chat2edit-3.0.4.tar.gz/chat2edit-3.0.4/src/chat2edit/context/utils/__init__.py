3f1bdba68a8512144de883483a7e14a56ec34475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:00 +0000

    Add package chat2edit/3.0.4/chat2edit-3.0.4.tar.gz

---
+++ packages/chat2edit/chat2edit-3.0.4.tar.gz/chat2edit-3.0.4/src/chat2edit/context/utils/__init__.py
@@ -0,0 +1,5 @@
+from chat2edit.context.utils.path_to_value import path_to_value
+from chat2edit.context.utils.safe_deepcopy import safe_deepcopy
+from chat2edit.context.utils.value_to_path import value_to_path
+
+__all__ = ["path_to_value", "safe_deepcopy", "value_to_path"]
