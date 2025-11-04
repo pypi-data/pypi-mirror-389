62f81096f7d7a2c197f37315c039b709f660a663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:06:01 +0000

    Add package reflspeckit/0.1.4/reflspeckit-0.1.4.tar.gz

---
+++ packages/reflspeckit/reflspeckit-0.1.4.tar.gz/reflspeckit-0.1.4/src/reflspeckit/__init__.py
@@ -0,0 +1,16 @@
+"""
+# spectralcubekit
+
+A toolkit for working with any and all flavors of spectral data cubes.
+"""
+
+from .misc_utils import last_nonzero_val_3D
+from .linear_fitting import fit_linear_cube, fit_linear_cube_with_error
+from .band_parameters import three_band_depth
+
+__all__ = [
+    "last_nonzero_val_3D",
+    "three_band_depth",
+    "fit_linear_cube",
+    "fit_linear_cube_with_error",
+]
