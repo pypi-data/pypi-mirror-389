bdddf981bacbbb32f30d6879396951009ecd11c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:04:46 +0000

    Add package smartwater/0.0.10/smartwater-0.0.10-py3-none-any.whl

---
+++ packages/smartwater/smartwater-0.0.10-py3-none-any.whl/smartwater/__init__.py
@@ -0,0 +1,21 @@
+from .api_async import (
+    AsyncSmartWaterApi, 
+)
+from .api_sync import (
+    SmartWaterApi, 
+)
+from .data import (
+    SmartWaterConnectError, 
+    SmartWaterAuthError, 
+    SmartWaterDataError, 
+    SmartWaterError, 
+)
+
+# for unit tests
+from .data import (
+    LoginMethod,
+)
+
+
+# https://github.com/python-trio/unasync
+# https://spwoodcock.dev/blog/2025-02-python-dry-async/
