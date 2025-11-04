6dbdba972fd74c6784b865532b0aa95fc2cb8476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 07:04:57 +0000

    Add package dandy/1.3.0/dandy-1.3.0.tar.gz

---
+++ packages/dandy/dandy-1.3.0.tar.gz/dandy-1.3.0/dandy/llm/request/request.py
@@ -0,0 +1,48 @@
+from abc import abstractmethod, ABC
+
+from pydantic import BaseModel, Field
+
+from dandy.llm.request.message import RequestMessage, RoleLiteralStr
+
+
+class BaseRequestBody(BaseModel, ABC):
+    model: str
+    messages: list[RequestMessage] = Field(default_factory=list)
+
+    @abstractmethod
+    def add_message(
+        self, role: RoleLiteralStr, content: str, images: list[str] | None = None
+    ): ...
+
+    @abstractmethod
+    def get_context_length(self) -> int: ...
+
+    @abstractmethod
+    def get_max_completion_tokens(self) -> int: ...
+
+    @abstractmethod
+    def get_seed(self) -> int: ...
+
+    @abstractmethod
+    def get_temperature(self) -> float: ...
+
+    def reset_messages(self):
+        self.messages = []
+
+    @property
+    @abstractmethod
+    def token_usage(self) -> int:
+        pass
+
+    def get_total_context_length(self) -> int:
+        return self.get_context_length() + self.get_max_completion_tokens()
+
+    @abstractmethod
+    def set_format_to_json_schema(self, json_schema: dict): ...
+
+    @abstractmethod
+    def set_format_to_text(self): ...
+
+    @abstractmethod
+    def to_dict(self): ...
+
