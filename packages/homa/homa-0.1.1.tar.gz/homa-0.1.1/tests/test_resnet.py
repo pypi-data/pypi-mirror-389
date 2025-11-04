fc1b8cacd9b7d7c12a63410049168e5ec8cbb5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:04:58 +0000

    Add package homa/0.1.1/homa-0.1.1.tar.gz

---
+++ packages/homa/homa-0.1.1.tar.gz/homa-0.1.1/tests/test_resnet.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+from homa.vision import Resnet, Model
+from homa.vision.modules import ResnetModule
+
+
+@pytest.fixture
+def resnet_model():
+    model = Resnet(num_classes=5, lr=0.001)
+    return model
+
+
+def test_resnet_initialization(resnet_model):
+    assert isinstance(resnet_model, Resnet)
+    assert isinstance(resnet_model, Model)
+    assert hasattr(resnet_model, "network")
+    assert hasattr(resnet_model, "optimizer")
+    assert hasattr(resnet_model, "criterion")
+    assert isinstance(resnet_model.network, ResnetModule)
+    assert isinstance(resnet_model.optimizer, torch.optim.SGD)
+    assert isinstance(resnet_model.criterion, torch.nn.CrossEntropyLoss)
