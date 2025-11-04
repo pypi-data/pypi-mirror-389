100fa90de7537aaf3746fe976251320347b3939e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 17:05:24 +0000

    Add package kickcom.py/0.1.0/kickcom_py-0.1.0-py3-none-any.whl

---
+++ packages/kickcom.py/kickcom_py-0.1.0-py3-none-any.whl/kickpy/models/stream.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass(slots=True)
+class Stream:
+    """Represents a Kick.com stream."""
+
+    url: str
+    key: str
+    is_live: bool
+    is_mature: bool
+    language: str
+    start_time: datetime
+    thumbnail: str
+    viewer_count: int
+
+    def __post_init__(self) -> None:
+        self.start_time = datetime.fromisoformat(self.start_time)
