315c731525bae977e05592be0647da5c59516a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 15:06:26 +0000

    Add package chat2edit/3.0.10/chat2edit-3.0.10-py3-none-any.whl

---
+++ packages/chat2edit/chat2edit-3.0.10-py3-none-any.whl/chat2edit/execution/feedbacks/empty_list_parameters_feedback.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from pydantic import Field
+
+from chat2edit.models import ContextualizedFeedback
+
+
+class EmptyListParametersFeedback(ContextualizedFeedback):
+    severity: str = Field(default="error")
+    parameters: List[str]
