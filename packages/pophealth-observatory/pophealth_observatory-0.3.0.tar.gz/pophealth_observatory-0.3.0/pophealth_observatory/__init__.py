354cd43fd95156bb38c06faa51d1ef87cda17c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 03:22:27 +0000

    Add package pophealth-observatory/0.3.0/pophealth_observatory-0.3.0.tar.gz

---
+++ packages/pophealth-observatory/pophealth_observatory-0.3.0.tar.gz/pophealth_observatory-0.3.0/pophealth_observatory/__init__.py
@@ -0,0 +1,11 @@
+"""Package exports for PopHealth Observatory.
+
+SPDX-License-Identifier: MIT
+Copyright (c) 2025 Paul Boys and PopHealth Observatory contributors
+"""
+
+from .observatory import NHANESExplorer, PopHealthObservatory
+
+__version__ = "0.1.4"
+
+__all__ = ["PopHealthObservatory", "NHANESExplorer", "__version__"]
