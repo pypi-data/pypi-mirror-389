b12277945d9debd6efee7c302c322fdb7cf55fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:06 +0000

    Add package homa/0.1.5/homa-0.1.5-py3-none-any.whl

---
+++ packages/homa/homa-0.1.5-py3-none-any.whl/homa/ensemble/concerns/RecordsStateDictionaries.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+from .HasStateDicts import HasStateDicts
+from ...vision import Model
+
+
+class RecordsStateDictionaries(HasStateDicts):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def record(self, model: Model):
+        if self.network is None:
+            self.network = deepcopy(model.network)
+
+        self.state_dicts.append(model.network.state_dict())
+
+    def push(self, *args, **kwargs):
+        self.record(*args, **kwargs)
+
+    def append(self, *args, **kwargs):
+        self.record(*args, **kwargs)
+
+    def add(self, *args, **kwargs):
+        self.record(*args, **kwargs)
