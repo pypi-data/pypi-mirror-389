84a02ad841a2fd69fcc80ddc2125a73cf03948c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 17:05:09 +0000

    Add package ckanext-dcor-theme/1.1.4/ckanext_dcor_theme-1.1.4.tar.gz

---
+++ packages/ckanext-dcor-theme/ckanext_dcor_theme-1.1.4.tar.gz/ckanext_dcor_theme-1.1.4/README.rst
@@ -0,0 +1,70 @@
+ckanext-dcor_theme
+==================
+
+|PyPI Version| |Build Status| |Coverage Status|
+
+The CKAN theme of DCOR. What this plugin does:
+
+- remove the language selection from footer.html
+- remove links from footer.html
+- set the title of the page to "DCOR"
+- insert a custom about text
+- remove social media buttons
+- change icons / logo
+- use i18n to change the terms "Organization" -> "Circle",
+  "Group" -> "Collection", etc.::
+
+     ckan dcor-theme-i18n-branding
+
+- create a custom theme based on the default main.css file
+  (hide action buttons (Fullscreen, Embed) in resource view via css,
+  change colors) using::
+
+    ckan dcor-theme-main-css-branding
+
+- patch CKAN templates with minor changes that would be too complicated to
+  implement with jinja templating::
+
+    ckan dcor-patch-ckan-templates
+
+
+Installation
+------------
+
+::
+
+    pip install ckanext-dcor_theme
+
+Edit ckan.ini::
+
+    ckan.plugins = [...] dcor_theme
+
+and run the dcor-i18n-hack command::
+
+    ckan -c /etc/ckan/default/ckan.ini dcor-i18n-hack
+
+Then, edit ckan.ini again::
+
+    ckan.locale_default = en_US
+
+
+Testing
+-------
+If CKAN/DCOR is installed and setup for testing, this extension can
+be tested with pytest:
+
+::
+
+    pytest ckanext
+
+Testing is implemented via GitHub Actions. You may also set up a local
+docker container with CKAN and MinIO. Take a look at the GitHub Actions
+workflow for more information.
+
+
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/ckanext.dcor_theme.svg
+   :target: https://pypi.python.org/pypi/ckanext.dcor_theme
+.. |Build Status| image:: https://img.shields.io/github/actions/workflow/status/DCOR-dev/ckanext-dcor_theme/check.yml
+   :target: https://github.com/DCOR-dev/ckanext-dcor_theme/actions?query=workflow%3AChecks
+.. |Coverage Status| image:: https://img.shields.io/codecov/c/github/DCOR-dev/ckanext-dcor_theme
+   :target: https://codecov.io/gh/DCOR-dev/ckanext-dcor_theme
---
+++ packages/ckanext-dcor-theme/ckanext_dcor_theme-1.1.4.tar.gz/ckanext_dcor_theme-1.1.4/ckanext/dcor_theme/cli/patch_ckan_templates.py
@@ -0,0 +1,62 @@
+import logging
+import pathlib
+
+import ckan
+import click
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+def dcor_patch_ckan_templates():
+    """
+    patch CKAN templates with minor changes that would be too complicated to
+    implement with jinja templating
+    """
+    click.secho("Patching templates to improve resource listing")
+    patch_wider_resource_listing()
+
+
+def patch_wider_resource_listing():
+    """Make sure the full filename is visible in the resource view"""
+    ckan_path = pathlib.Path(ckan.__file__).parent
+
+    # patch resource listing package template
+    path_template_res_item = \
+        ckan_path / "templates/package/snippets/resource_item.html"
+    data_res_item = path_template_res_item.read_text()
+    # This makes the browser force a line-break even inside a word,
+    # which means that it breaks long filenames.
+    st = 'style="overflow-wrap: break-word"'
+    for old, new in [
+        # remove the truncate filter for file names
+        ("| truncate(50)", ""),
+        # force word wrap of the file names
+        ('<a class="heading"',
+         f'<a {st} class="heading"'),
+    ]:
+        data_res_item = data_res_item.replace(old, new)
+    path_template_res_item.write_text(data_res_item)
+
+    # patch resource listing details template
+    path_template_res = ckan_path / "templates/package/snippets/resources.html"
+    data_res = path_template_res.read_text()
+    # This makes the browser force a line-break even inside a word,
+    # which means that it breaks long filenames.
+    st = 'style="overflow-wrap: break-word"'
+    for old, new in [
+        # remove the truncate filter for file names
+        ("|truncate(25)", ""),
+        # force word wrap of the file names
+        ('<a href="',
+         f'<a {st} href="'),
+        ('<a class="flex-fill" href="',
+         f'<a class="flex-fill" {st} href="'),
+        # remove d-flex which breaks the overflow-wrap hack
+        ('<li class="nav-item d-flex justify-content-between position-relative">',  # noqa: E501
+         '<li class="nav-item justify-content-between position-relative">'
+         ),
+    ]:
+        data_res = data_res.replace(old, new)
+    path_template_res.write_text(data_res)
