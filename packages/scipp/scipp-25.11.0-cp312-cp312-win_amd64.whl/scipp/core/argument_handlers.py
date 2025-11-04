41ca55ab39ed5de561a1d09a418cef15ebadfccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:05 +0000

    Add package scipp/25.11.0/scipp-25.11.0-cp312-cp312-win_amd64.whl

---
+++ packages/scipp/scipp-25.11.0-cp312-cp312-win_amd64.whl/scipp/core/argument_handlers.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+from typing import TypeVar
+
+from .cpp_classes import Variable
+
+_ValueType = TypeVar('_ValueType', str, Variable)
+
+
+def combine_dict_args(
+    arg: dict[str, _ValueType] | None, kwargs: dict[str, _ValueType]
+) -> dict[str, _ValueType]:
+    pos_dict = {} if arg is None else arg
+
+    overlapped = set(pos_dict).intersection(kwargs)
+    if overlapped:
+        raise ValueError(
+            'The names passed in the dict and as keyword arguments must be distinct. '
+            f'Following names are used in both arguments: {overlapped}'
+        )
+
+    return {**pos_dict, **kwargs}
