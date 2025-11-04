a8aea489677ffdaf5d9f3751be1c97a73e1db778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:27 +0000

    Add package Rocket-rocket-rocket-rocket/1.0.0/rocket_rocket_rocket_rocket-1.0.0.tar.gz

---
+++ packages/Rocket-rocket-rocket-rocket/rocket_rocket_rocket_rocket-1.0.0.tar.gz/rocket_rocket_rocket_rocket-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='Rocket_rocket_rocket_rocket',
+   version='1.0.0',
+   author='mohamed23333444',
+   author_email='mohmooh33@gmail.com',
+   packages=['Rocket'],
+   url='http://pypi.python.org/pypi/Rocket_rocket_rocket_rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[]
+)
