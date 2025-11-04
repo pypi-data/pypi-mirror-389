a9e569120976da89231ef33babba13e403b21a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 04:05:22 +0000

    Add package mi-maica/1.2.0rc7/mi_maica-1.2.0rc7.tar.gz

---
+++ packages/mi-maica/mi_maica-1.2.0rc7.tar.gz/mi_maica-1.2.0rc7/maica/mtools/__init__.py
@@ -0,0 +1,38 @@
+
+from .api_keys import TpAPIKeys
+from .mpostal import make_postmail
+from .mspire import make_inspire
+from .wiki_get import get_page
+from .weather_get import weather_api_get
+from .serp_get import internet_search
+from .event_get import RegEvent, EventsCollection
+from .post_proc import emo_proc, emo_proc_llm, emo_proc_auto, post_proc, zlist, elist
+from .nv_watcher import NvWatcher
+from .mvista import ProcessingImg, query_vlm
+
+__all__ = [
+    'TpAPIKeys',
+    'make_postmail',
+    'make_inspire',
+    'weather_api_get',
+    'internet_search',
+    'RegEvent',
+    'EventsCollection',
+    'emo_proc',
+    'emo_proc_llm',
+    'emo_proc_auto',
+    'post_proc',
+    'zlist',
+    'elist',
+    'NvWatcher',
+    'ProcessingImg',
+    'query_vlm',
+    ]
+
+from .mcp import pkg_init_mcp
+from .api_keys import pkg_init_api_keys
+from .providers import pkg_init_serp_provider
+def pkg_init_mtools():
+    pkg_init_mcp()
+    pkg_init_api_keys()
+    pkg_init_serp_provider()
