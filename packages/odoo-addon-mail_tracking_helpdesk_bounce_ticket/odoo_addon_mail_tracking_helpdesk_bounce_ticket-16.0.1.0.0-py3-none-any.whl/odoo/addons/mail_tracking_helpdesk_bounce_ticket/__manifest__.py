e9c2f1d20443a6d92f332d74a349319054aef285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 16:05:51 +0000

    Add package odoo-addon-mail_tracking_helpdesk_bounce_ticket/16.0.1.0.0/odoo_addon_mail_tracking_helpdesk_bounce_ticket-16.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-mail_tracking_helpdesk_bounce_ticket/odoo_addon_mail_tracking_helpdesk_bounce_ticket-16.0.1.0.0-py3-none-any.whl/odoo/addons/mail_tracking_helpdesk_bounce_ticket/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Mail Tracking Helpdesk Bounce Ticket",
+    "summary": """
+        When email is bounced create a helpdesk ticket.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Helpdesk",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mail_tracking", "helpdesk"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
---
+++ packages/odoo-addon-mail_tracking_helpdesk_bounce_ticket/odoo_addon_mail_tracking_helpdesk_bounce_ticket-16.0.1.0.0-py3-none-any.whl/odoo/addons/mail_tracking_helpdesk_bounce_ticket/models/mail_message.py
@@ -0,0 +1,46 @@
+import logging
+
+from odoo import _, api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class MailMessage(models.Model):
+    _inherit = "mail.message"
+
+    is_bounced_message = fields.Boolean(compute="_compute_is_bounced_message", store=True)
+
+    @api.model
+    def get_bounced_states(self):
+        """The 'bounced' states of the message"""
+        return {"bounced", "soft-bounced"}
+
+    @api.depends(
+        "mail_tracking_ids.state",
+    )
+    def _compute_is_bounced_message(self):
+        """Compute 'is_bounced_message' field for the active user"""
+        bounced_states = self.get_bounced_states()
+        subtype_id = self.env.ref("mail.mt_comment")
+
+        for message in self:
+            has_bounced_trackings = bounced_states.intersection(message.mapped("mail_tracking_ids.state"))
+            message.is_bounced_message = has_bounced_trackings
+
+            if message.is_bounced_message and message.message_type == "comment" and message.subtype_id == subtype_id:
+                message._create_helpdesk_ticket()
+
+    def _create_helpdesk_ticket(self):
+        self.ensure_one()
+        ticket_id = self.env["helpdesk.ticket"].create(
+            {
+                "name": _("Mail Delivery Issue"),
+                "partner_id": self.partner_ids[0].id if self.partner_ids else False,
+            }
+        )
+        message_body = _(
+            "Email with ID %s has been bounced. This ticket has been created from: %s",
+            self.id,
+            self._get_html_link(),
+        )
+        ticket_id.message_post(body=message_body, message_type="notification")
