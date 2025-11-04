c2b138cf878f15a516703975b5a5e87a62d4033e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 05:05:01 +0000

    Add package datamorph-airflow/0.0.88/datamorph_airflow-0.0.88.tar.gz

---
+++ packages/datamorph-airflow/datamorph_airflow-0.0.88.tar.gz/datamorph_airflow-0.0.88/datamorphairflow/__version__.py
@@ -0,0 +1,9 @@
+"""
+Module contains the version of datamorph-airflow,
+ChangeLog.md with the release details for the version change needs to be updated
+"""
+__version__ = "0.0.88"
+
+"""
+0.0.88 - datamorph and glue action to include load parameters from a s3 location
+"""
