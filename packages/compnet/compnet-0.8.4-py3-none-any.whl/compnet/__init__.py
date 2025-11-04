7b8d4c24d59872ada8fda8fc9b18e9cc03d25992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-04 14:05:01 +0000

    Add package compnet/0.8.4/compnet-0.8.4-py3-none-any.whl

---
+++ packages/compnet/compnet-0.8.4-py3-none-any.whl/compnet/__init__.py
@@ -0,0 +1,10 @@
+"""  Created on 10/10/2022::
+------------- __init__.py -------------
+
+**Authors**: L. Mingarelli
+"""
+
+from compnet.__about__ import (__about__, __author__, __email__, __version__, __url__)
+from compnet.algo import Graph, compression_factor
+
+SUPPRESS_WARNINGS = False
