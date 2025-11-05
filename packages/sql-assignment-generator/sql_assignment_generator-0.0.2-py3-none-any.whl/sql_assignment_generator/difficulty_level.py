38f52b4344fe72125671a80a125d9f68433e8226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 05:05:30 +0000

    Add package sql-assignment-generator/0.0.2/sql_assignment_generator-0.0.2-py3-none-any.whl

---
+++ packages/sql-assignment-generator/sql_assignment_generator-0.0.2-py3-none-any.whl/sql_assignment_generator/difficulty_level.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class DifficultyLevel(Enum):
+    EASY = 1
+    '''Minimal cognitive load, the assignments contains only elements related to triggering the error'''
+
+    MEDIUM = 2
+    '''Moderate cognitive load, the assignments contains some elements not related to triggering the error'''
+
+    HARD = 3
+    '''High cognitive load, the assignments contains elements not related to triggering the error and may require complex reasoning'''
