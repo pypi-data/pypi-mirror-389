e7632c0c9d1ebc2b3ee138a89c45eb476d877deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:07:06 +0000

    Add package panelbeater/0.0.5/panelbeater-0.0.5.tar.gz

---
+++ packages/panelbeater/panelbeater-0.0.5.tar.gz/panelbeater-0.0.5/panelbeater/__main__.py
@@ -0,0 +1,81 @@
+"""The CLI for finding mispriced options."""
+
+import datetime
+
+import requests_cache
+import tqdm
+import wavetrainer as wt
+
+from .download import download
+from .features import features
+from .normalizer import denormalize, normalize
+
+_TICKERS = [
+    # Equities
+    "SPY",
+    "QQQ",
+    "EEM",
+    # Commodities
+    "GC=F",
+    "CL=F",
+    "SI=F",
+    # FX
+    "EURUSD=X",
+    "USDJPY=X",
+    # Crypto
+    "BTC-USD",
+    "ETH-USD",
+]
+_MACROS = [
+    "GDP",
+    "UNRATE",
+    "CPIAUCSL",
+    "FEDFUNDS",
+    "DGS10",
+    "T10Y2Y",
+    "M2SL",
+    "VIXCLS",
+    "DTWEXBGS",
+    "INDPRO",
+]
+_WINDOWS = [
+    5,
+    10,
+    20,
+    60,
+    120,
+    200,
+]
+_LAGS = [1, 3, 5, 10, 20, 30]
+_DAYS_OUT = 30
+
+
+def main() -> None:
+    """The main CLI function."""
+    # Setup main objects
+    session = requests_cache.CachedSession("panelbeater-cache")
+    wavetrainer = wt.create(
+        "panelbeater-train",
+        walkforward_timedelta=datetime.timedelta(days=30),
+        validation_size=datetime.timedelta(days=365),
+        test_size=datetime.timedelta(days=365),
+        allowed_models={"catboost"},
+        max_false_positive_reduction_steps=0,
+    )
+
+    # Fit the models
+    df_y = download(tickers=_TICKERS, macros=_MACROS, session=session)
+    df_x = features(df=df_y.copy(), windows=_WINDOWS, lags=_LAGS)
+    df_y_norm = normalize(df=df_y.copy())
+    wavetrainer.fit(df_x, y=df_y_norm)
+    for _ in tqdm.tqdm(range(_DAYS_OUT), desc="Running t+X simulation"):
+        df_next = wavetrainer.transform(df_y_norm)
+        df_y = denormalize(df_next)
+        df_x = features(df=df_y.copy(), windows=_WINDOWS, lags=_LAGS)
+        df_y_norm = normalize(df=df_y.copy())
+
+    # Find the current options prices
+
+
+if __name__ == "__main__":
+    main()
