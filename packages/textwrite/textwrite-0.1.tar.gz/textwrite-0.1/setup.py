abfb90038ed629c27a59e145b988432104dead56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:06:22 +0000

    Add package textwrite/0.1/textwrite-0.1.tar.gz

---
+++ packages/textwrite/textwrite-0.1.tar.gz/textwrite-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textwrite",
+    version="0.1",
+    description="A simple typewriter-style text output function",
+    author="Charles Q",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
