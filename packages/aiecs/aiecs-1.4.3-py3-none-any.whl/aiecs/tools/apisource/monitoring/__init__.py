e50877e91ff1b0ea49bd91580776b3913977031b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:13 +0000

    Add package aiecs/1.4.3/aiecs-1.4.3-py3-none-any.whl

---
+++ packages/aiecs/aiecs-1.4.3-py3-none-any.whl/aiecs/tools/apisource/monitoring/__init__.py
@@ -0,0 +1,12 @@
+"""
+Monitoring Module
+
+Contains metrics and health monitoring components.
+"""
+
+from aiecs.tools.apisource.monitoring.metrics import DetailedMetrics
+
+__all__ = [
+    'DetailedMetrics'
+]
+
