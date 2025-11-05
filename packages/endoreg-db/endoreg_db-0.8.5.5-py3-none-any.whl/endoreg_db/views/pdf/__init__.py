44a063b2876181c9ab3871fae9e5cd7a1c900ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:20 +0000

    Add package endoreg-db/0.8.5.5/endoreg_db-0.8.5.5-py3-none-any.whl

---
+++ packages/endoreg-db/endoreg_db-0.8.5.5-py3-none-any.whl/endoreg_db/views/pdf/__init__.py
@@ -0,0 +1,11 @@
+from .pdf_media import (
+    PDFMediaView,
+    UpdateSensitiveMetaView,
+    ClosingFileWrapper
+)
+
+__all__ = [
+    "PDFMediaView",
+    "UpdateSensitiveMetaView",
+    "ClosingFileWrapper",
+]
