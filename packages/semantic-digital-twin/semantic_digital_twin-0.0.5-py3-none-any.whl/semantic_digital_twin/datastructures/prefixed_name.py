3003c9abc9c2997e4e65e95f27338fffeed4ddeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 05:07:38 +0000

    Add package semantic-digital-twin/0.0.5/semantic_digital_twin-0.0.5-py3-none-any.whl

---
+++ packages/semantic-digital-twin/semantic_digital_twin-0.0.5-py3-none-any.whl/semantic_digital_twin/datastructures/prefixed_name.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+from krrood.entity_query_language.predicate import Symbol
+from typing_extensions import Optional, Dict, Any, Self
+
+from random_events.utils import SubclassJSONSerializer
+
+
+@dataclass
+class PrefixedName(Symbol, SubclassJSONSerializer):
+    name: str
+    prefix: Optional[str] = None
+
+    def __hash__(self):
+        return hash((self.prefix, self.name))
+
+    def __str__(self):
+        return f"{self.prefix}/{self.name}"
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.prefix == other.prefix and self.name == other.name
+
+    def to_json(self) -> Dict[str, Any]:
+        return {**super().to_json(), "name": self.name, "prefix": self.prefix}
+
+    @classmethod
+    def _from_json(cls, data: Dict[str, Any]) -> Self:
+        return cls(name=data["name"], prefix=data["prefix"])
+
+    def __lt__(self, other):
+        return str(self) < str(other)
+
+    def __le__(self, other):
+        return str(self) <= str(other)
+
+    def __gt__(self, other):
+        return str(self) > str(other)
+
+    def __ge__(self, other):
+        return str(self) >= str(other)
