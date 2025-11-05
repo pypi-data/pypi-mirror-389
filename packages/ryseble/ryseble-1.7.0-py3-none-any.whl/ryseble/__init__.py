050256c3011a6ff8cd431c78196bb19a9197ed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:36 +0000

    Add package ryseble/1.7.0/ryseble-1.7.0-py3-none-any.whl

---
+++ packages/ryseble/ryseble-1.7.0-py3-none-any.whl/ryseble/__init__.py
@@ -0,0 +1,26 @@
+"""
+Ryse BLE Python Library
+~~~~~~~~~~~~~~~~~~~~~~~
+This library provides tools to communicate with Ryse gears
+over Bluetooth Low Energy (BLE).
+
+Modules:
+- device: RyseBLEDevice class for managing connections
+- packets: Helpers to build BLE packets
+- constants: Protocol constants and UUIDs
+- bluetoothctl: Bluetoothctl wrapper functions
+"""
+
+from .device import RyseBLEDevice
+from .packets import build_position_packet, build_get_position_packet
+from .constants import HARDCODED_UUIDS, PAIRING_MODE_FLAG
+from . import bluetoothctl
+
+__all__ = [
+    "RyseBLEDevice",
+    "build_position_packet",
+    "build_get_position_packet",
+    "HARDCODED_UUIDS",
+    "PAIRING_MODE_FLAG",
+    "bluetoothctl",
+]
