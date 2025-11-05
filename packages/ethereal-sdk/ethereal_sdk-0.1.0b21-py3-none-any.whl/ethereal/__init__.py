336cc82f4776779cd6fed50122b20cb72a588d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:07:10 +0000

    Add package ethereal-sdk/0.1.0b21/ethereal_sdk-0.1.0b21-py3-none-any.whl

---
+++ packages/ethereal-sdk/ethereal_sdk-0.1.0b21-py3-none-any.whl/ethereal/__init__.py
@@ -0,0 +1,5 @@
+from ethereal.rest_client import RESTClient
+from ethereal.async_rest_client import AsyncRESTClient
+from ethereal.ws_client import WSClient
+
+__all__ = ["RESTClient", "AsyncRESTClient", "WSClient"]
