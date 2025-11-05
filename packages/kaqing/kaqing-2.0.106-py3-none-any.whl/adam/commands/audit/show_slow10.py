5ce38641e75c1a8b699e051078b07fa79aab7b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:07:07 +0000

    Add package kaqing/2.0.106/kaqing-2.0.106-py3-none-any.whl

---
+++ packages/kaqing/kaqing-2.0.106-py3-none-any.whl/adam/commands/audit/show_slow10.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+
+from adam.commands.command import Command
+from adam.repl_state import ReplState
+from adam.utils import log2
+from adam.utils_audits import Audits
+
+class ShowSlow10(Command):
+    COMMAND = 'show slow'
+
+    # the singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'): cls.instance = super(ShowSlow10, cls).__new__(cls)
+
+        return cls.instance
+
+    def __init__(self, successor: Command=None):
+        super().__init__(successor)
+
+    def command(self):
+        return ShowSlow10.COMMAND
+
+    def run(self, cmd: str, state: ReplState):
+        if not(args := self.args(cmd)):
+            return super().run(cmd, state)
+
+        state, args = self.apply_state(args, state)
+        limit = 10
+        if args:
+            try:
+                limit = int(args[0])
+            except:
+                pass
+
+        query = '\n    '.join([
+            "SELECT * FROM audit",
+            f"WHERE drive <> 'z' and ({Audits.date_from(datetime.now() - timedelta(days=30))})",
+            f"ORDER BY CAST(duration AS REAL) DESC LIMIT {limit};"])
+        log2(query)
+        log2()
+        Audits.run_audit_query(query)
+
+        return state
+
+    def completion(self, state: ReplState):
+        if state.device == ReplState.L:
+            return super().completion(state, {'10': None})
+
+        return {}
+
+    def help(self, _: ReplState):
+        return f'{ShowSlow10.COMMAND} [limit]\t show slow <limit> audit lines'
