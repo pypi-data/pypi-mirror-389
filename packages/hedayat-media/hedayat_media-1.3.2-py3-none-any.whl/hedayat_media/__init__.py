aa0c93912480c6a10c584a1e05e36730498f3ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:16 +0000

    Add package hedayat-media/1.3.2/hedayat_media-1.3.2-py3-none-any.whl

---
+++ packages/hedayat-media/hedayat_media-1.3.2-py3-none-any.whl/hedayat_media/__init__.py
@@ -0,0 +1,21 @@
+"""
+HedayatMedia Package
+
+این ماژول شامل کلاس HedayatMedia 
+در پروژه‌های مختلف طراحی شده است.
+
+سازنده: امیرحسین خزاعی
+ایمیل:amirhossinpython03@gmail.com  
+تاریخ ایجاد: 2025-09-22
+نسخه: 1.0.0
+"""
+
+# وارد کردن کلاس اصلی از ماژول core
+from .core import HedayatMedia
+
+
+__all__ = ["HedayatMedia"]
+__author__ = "amirhossinekhazai"
+__email__ = "amirhossinpython03@gmail.com"
+__version__ = "1.3.2"
+
