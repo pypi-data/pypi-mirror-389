137e565f5682f5388b44d2cfb723be90fa13fb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:06:51 +0000

    Add package maestral/1.9.6/maestral-1.9.6.tar.gz

---
+++ packages/maestral/maestral-1.9.6.tar.gz/maestral-1.9.6/src/maestral/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+__version__ = "1.9.6"
+__author__ = "Sam Schott"
+__url__ = "https://maestral.app"
+
+
+# suppress Python 3.9 warning from rubicon-objc
+warnings.filterwarnings("ignore", module="rubicon", category=UserWarning)
