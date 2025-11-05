90f3ab385f4faa111da9566c5dfd83f2803a9734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:25:06 +0000

    Add package prompt-to-query/1.0.9/prompt_to_query-1.0.9-py3-none-any.whl

---
+++ packages/prompt-to-query/prompt_to_query-1.0.9-py3-none-any.whl/prompt_to_query/__init__.py
@@ -0,0 +1,9 @@
+"""
+Prompt to Query SDK for Python
+Converts natural language prompts to MongoDB queries using LLMs
+"""
+
+from .client import PromptToQuery
+
+__version__ = "1.0.1"
+__all__ = ["PromptToQuery"]
