3cf296394ca65abfff9a9a3ae02112bf08c5103a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:05:36 +0000

    Add package xfem/2.1.2507.dev26/xfem-2.1.2507.dev26-cp310-cp310-manylinux_2_28_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xfem/xfem-2.1.2507.dev26-cp310-cp310-manylinux_2_28_x86_64.manylinux2014_x86_64.whl/xfem/_version.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from builtins import object as VERSION_TUPLE
+from builtins import object as COMMIT_ID
+__all__: list = ['__version__', '__version_tuple__', 'version', 'version_tuple', '__commit_id__', 'commit_id']
+__commit_id__: str = 'gefd8b752f'
+__version__: str = '2.1.2507.dev26'
+__version_tuple__: tuple = (2, 1, 2507, 'dev26')
+commit_id: str = 'gefd8b752f'
+version: str = '2.1.2507.dev26'
+version_tuple: tuple = (2, 1, 2507, 'dev26')
