e00065088b41a35da0f7e7ef341471c5bd635a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:05:48 +0000

    Add package moat/0.11.4/moat-0.11.4-py3-none-any.whl

---
+++ packages/moat/moat-0.11.4-py3-none-any.whl/moat/__init__.py
@@ -0,0 +1,14 @@
+# noqa: D104
+from __future__ import annotations
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+# Monorepo. No longer required.
+# try:
+#    from moat._dev_fix import _fix
+# except ImportError:
+#    pass
+# else:
+#    _fix()
+
+# from ._config import default_cfg
