2e17f783a7b639b2bc0af0a86d822f70afeb8804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:07:06 +0000

    Add package codehackspackage/0.2.0/codehackspackage-0.2.0-py3-none-any.whl

---
+++ packages/codehackspackage/codehackspackage-0.2.0-py3-none-any.whl/codehackscalculator/__init__.py
@@ -0,0 +1,12 @@
+def add_numbers(a,b):
+    return a + b
+
+def subtract_numbers(a,b):
+    return a - b
+def multiply_numbers(a,b):
+    return a * b
+def divide_numbers(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
