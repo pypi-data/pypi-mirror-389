c32914344a4d90264c47b840c11866c1a72f89ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:22 +0000

    Add package llnl-hatchet/2025.1.1/llnl_hatchet-2025.1.1-cp38-cp38-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/llnl-hatchet/llnl_hatchet-2025.1.1-cp38-cp38-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/hatchet/util/deprecated.py
@@ -0,0 +1,59 @@
+# Copyright 2017-2023 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+import functools
+
+try:
+    from warnings import deprecated  # type: ignore
+except ImportError:
+    from warnings import warn
+
+    def deprecated(msg):
+        def deprecated_deco(f):
+            @functools.wraps(f)
+            def wrapper(*args, **kwargs):
+                warn(msg, DeprecationWarning)
+                return f(*args, **kwargs)
+
+            return wrapper
+
+        return deprecated_deco
+
+
+def deprecated_params(**old_to_new):
+    def deco(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            rename_kwargs(f.__name__, old_to_new, kwargs)
+            return f(*args, **kwargs)
+
+        return wrapper
+
+    return deco
+
+
+def rename_kwargs(fname, old_to_new, kwargs):
+    for old, new in old_to_new.items():
+        if old in kwargs:
+            if new in kwargs:
+                raise TypeError(
+                    '{}() received both "{}=" and "{}=".'.format(fname, old, new)
+                )
+
+            # if parameter has been removed
+            if not new:
+                raise ValueError(
+                    '{}() parameter "{}=" will be removed in a future release. Please remove it from this script.'.format(
+                        fname, old
+                    )
+                )
+            # if parameter has been deprecated and renamed
+            else:
+                raise ValueError(
+                    '{}() parameter "{}=" has been deprecated, please use "{}=".'.format(
+                        fname, old, new
+                    )
+                )
+                kwargs[new] = kwargs.pop(old)
