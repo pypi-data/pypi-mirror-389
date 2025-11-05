5f1fb3a958b778ce5cb467b1588f1f5cef93ed2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:06:14 +0000

    Add package dsnd-probability-tom-1608/1.0/dsnd_probability_tom_1608-1.0.tar.gz

---
+++ packages/dsnd-probability-tom-1608/dsnd_probability_tom_1608-1.0.tar.gz/dsnd_probability_tom_1608-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dsnd_probability_tom_1608",
+    version="1.0",
+    description="Gaussian and Binomial distributions",
+    packages=["distributions"],
+    author="Tamoghna Purkait",
+    author_email="tamoghna10@gmail.com",
+    zip_safe=False,
+)
