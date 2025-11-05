18571846971d3a853f47980826b8e21df3f8fadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:06:17 +0000

    Add package orthax/0.2.6/orthax-0.2.6.tar.gz

---
+++ packages/orthax/orthax-0.2.6.tar.gz/orthax-0.2.6/README.rst
@@ -0,0 +1,54 @@
+
+########
+orthax
+########
+|License| |DOI| |Issues| |Pypi|
+
+|Docs| |UnitTests| |Codecov|
+
+``orthax`` is a Python package for working with orthogonal (and other) polynomials in JAX.
+It largely seeks to replicate the functionality of the ``numpy.polynomial`` package,
+through there are some API differences due to limitations of JAX, primarily that
+trailing zeros are not automatically trimmed from series, so you should do that
+manually if it becomes a concern.
+
+For full details of various options see the `Documentation <https://orthax.readthedocs.io/en/latest/>`__
+
+Installation
+============
+
+orthax is installable with ``pip``:
+
+.. code-block:: console
+
+    pip install orthax
+
+
+
+.. |License| image:: https://img.shields.io/github/license/f0uriest/orthax?color=blue&logo=open-source-initiative&logoColor=white
+    :target: https://github.com/f0uriest/orthax/blob/master/LICENSE
+    :alt: License
+
+.. |DOI| image:: https://zenodo.org/badge/763821973.svg
+    :target: https://zenodo.org/doi/10.5281/zenodo.10774399
+    :alt: DOI
+
+.. |Docs| image:: https://img.shields.io/readthedocs/orthax?logo=Read-the-Docs
+    :target: https://orthax.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation
+
+.. |UnitTests| image:: https://github.com/f0uriest/orthax/actions/workflows/unittest.yml/badge.svg
+    :target: https://github.com/f0uriest/orthax/actions/workflows/unittest.yml
+    :alt: UnitTests
+
+.. |Codecov| image:: https://codecov.io/github/f0uriest/orthax/graph/badge.svg?token=MB11I7WE3I
+    :target: https://codecov.io/github/f0uriest/orthax
+    :alt: Coverage
+
+.. |Issues| image:: https://img.shields.io/github/issues/f0uriest/orthax
+    :target: https://github.com/f0uriest/orthax/issues
+    :alt: GitHub issues
+
+.. |Pypi| image:: https://img.shields.io/pypi/v/orthax
+    :target: https://pypi.org/project/orthax/
+    :alt: Pypi
---
+++ packages/orthax/orthax-0.2.6.tar.gz/orthax-0.2.6/tests/test_recurrence.py
@@ -0,0 +1,64 @@
+"""Tests for general orthogonal series functions."""
+
+import numpy as np
+import pytest
+from jax import config
+from numpy.testing import assert_allclose
+
+import orthax
+
+config.update("jax_enable_x64", True)
+
+names = [
+    "leg",
+    "sleg",
+    "chebT",
+    "chebU",
+    "chebV",
+    "chebW",
+    "geg",
+    "jac",
+    "lag",
+    "glag",
+    "herm",
+    "herme",
+]
+
+recs = {
+    "leg": (orthax.recurrence.Legendre, (), 1e-12),
+    "sleg": (orthax.recurrence.ShiftedLegendre, (), 1e-12),
+    "chebT": (orthax.recurrence.ChebyshevT, (), 1e-8),
+    "chebU": (orthax.recurrence.ChebyshevU, (), 1e-8),
+    "chebV": (orthax.recurrence.ChebyshevV, (), 1e-8),
+    "chebW": (orthax.recurrence.ChebyshevW, (), 1e-8),
+    "jac_pp": (orthax.recurrence.Jacobi, (0.27, 0.27), 1e-8),
+    "jac_pn": (orthax.recurrence.Jacobi, (0.27, -0.27), 1e-8),
+    "jac_np": (orthax.recurrence.Jacobi, (-0.27, 0.27), 1e-8),
+    "jac_nn": (orthax.recurrence.Jacobi, (-0.27, -0.27), 1e-8),
+    "geg_pos": (orthax.recurrence.Gegenbauer, (0.1,), 1e-8),
+    "geg_neg": (orthax.recurrence.Gegenbauer, (-0.1,), 1e-6),
+    "lag": (orthax.recurrence.Laguerre, (), 1e-12),
+    "glag_pos": (orthax.recurrence.GeneralizedLaguerre, (2.5,), 1e-8),
+    "glag_neg": (orthax.recurrence.GeneralizedLaguerre, (-0.34,), 1e-8),
+    "herm": (orthax.recurrence.Hermite, (), 1e-12),
+    "herme": (orthax.recurrence.HermiteE, (), 1e-12),
+}
+
+
+@pytest.mark.parametrize("scale", ["monic", "normalized"])
+@pytest.mark.parametrize("name", recs.keys())
+def test_generate_recurrence(name, scale):
+    rec, args, tol = recs[name]
+    rec1 = rec(*args, scale=scale)
+
+    n = 10
+    rec2 = orthax.recurrence.generate_recurrence(
+        rec1.weight, rec1.domain, n, scale=scale
+    )
+
+    nn = np.arange(n)
+
+    assert_allclose(rec1.a(nn), rec2.a(nn), atol=tol, rtol=tol)
+    assert_allclose(rec1.b(nn), rec2.b(nn), atol=tol, rtol=tol)
+    assert_allclose(rec1.g(nn), rec2.g(nn), atol=tol, rtol=tol)
+    assert_allclose(abs(rec1.m(nn)), abs(rec2.m(nn)), atol=tol, rtol=tol)
