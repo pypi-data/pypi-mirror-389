9701d50196a1e2c3258619fa68878670a0079232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:06:48 +0000

    Add package boulder-opal-scale-up-sdk/1.0.10/boulder_opal_scale_up_sdk-1.0.10.tar.gz

---
+++ packages/boulder-opal-scale-up-sdk/boulder_opal_scale_up_sdk-1.0.10.tar.gz/boulder_opal_scale_up_sdk-1.0.10/boulderopalscaleupsdk/solutions/placeholder_solution.py
@@ -0,0 +1,28 @@
+# Copyright 2025 Q-CTRL. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#    https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+
+from pydantic import PrivateAttr
+from typing_extensions import deprecated
+
+from .common import Solution
+
+
+@deprecated(
+    "This is a placeholder for a specific solution in the future.",
+)
+class PlaceholderSolution(Solution):
+    """
+    Parameters for running a placeholder solution.
+    """
+
+    _solution_name: str = PrivateAttr("placeholder_solution")
