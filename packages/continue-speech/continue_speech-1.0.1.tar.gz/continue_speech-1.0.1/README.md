0131519c97b737649557404d507e5dc051861997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:07:19 +0000

    Add package continue-speech/1.0.1/continue_speech-1.0.1.tar.gz

---
+++ packages/continue-speech/continue_speech-1.0.1.tar.gz/continue_speech-1.0.1/README.md
@@ -0,0 +1,5 @@
+# Continue-1-OSS Speech Models
+
+See GitHub repository: https://github.com/SVECTOR-CORPORATION/Continue-TTS
+
+For full documentation and examples, visit the main repository.
---
+++ packages/continue-speech/continue_speech-1.0.1.tar.gz/continue_speech-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="continue-speech",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=["snac", "vllm"],
+    author="SVECTOR",
+    author_email="team@svector.co.in",
+    description="Continue-TTS Text-to-Speech System",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SVECTOR-CORPORATION/Continue-TTS",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
