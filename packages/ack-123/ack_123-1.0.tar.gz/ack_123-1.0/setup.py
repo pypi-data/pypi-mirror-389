4aed8f6aea5869f57814580b6c0f4e45947ae48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:24:36 +0000

    Add package ack-123/1.0/ack_123-1.0.tar.gz

---
+++ packages/ack-123/ack_123-1.0.tar.gz/ack_123-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ack-123',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+    ],
+    author='John Doe',
+    author_email='youremail@example.com',
+    description='register check',
+    license='MIT',
+    keywords='sample setuptools development',
+    url='https://github.com/yourusername/mypackage'
+)
