b0b5da8731e799fb419e5e9f5f568b1ff42eaafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:06:27 +0000

    Add package TopDownHockey-Scraper/6.0.2/topdownhockey_scraper-6.0.2.tar.gz

---
+++ packages/TopDownHockey-Scraper/topdownhockey_scraper-6.0.2.tar.gz/topdownhockey_scraper-6.0.2/setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="TopDownHockey_Scraper", # Replace with your own username
+    version="6.0.2",
+    author="Patrick Bacon",
+    author_email="patrick.s.bacon@gmail.com",
+    description="The TopDownHockey Scraper",
+    long_description=long_description,
+    license = 'MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/TopDownHockey/TopDownHockey_Scraper",
+    project_urls={
+        "Bug Tracker": "https://github.com/TopDownHockey/TopDownHockey_Scraper/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=['TopDownHockey_Scraper'],
+    python_requires=">=3.6",
+    install_requires = [
+    'numpy',
+    'pandas',
+    'bs4',
+    'datetime',
+    'xmltodict',
+    'lxml',
+    'natsort'
+]
+)
+
+
+
+#if __name__ == '__main__':
+ #   setup(**setup_args, install_requires=install_requires)
