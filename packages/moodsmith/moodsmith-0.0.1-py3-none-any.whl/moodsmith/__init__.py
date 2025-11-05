18ef5bec762c5c6ec043cb13b5f97634082039d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:06:38 +0000

    Add package moodsmith/0.0.1/moodsmith-0.0.1-py3-none-any.whl

---
+++ packages/moodsmith/moodsmith-0.0.1-py3-none-any.whl/moodsmith/__init__.py
@@ -0,0 +1,7 @@
+"""Exports functions we want users to import"""
+
+from .core import motivational, negative, positive, funny
+
+# imported functions
+__all__ = ["positive", "motivational", "negative","funny"]
+__version__ = "0.0.1"  # packages current version
