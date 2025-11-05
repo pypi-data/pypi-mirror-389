2e74b2758f0ec3bd22c480577ae67eb56af4d246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:25:01 +0000

    Add package maps4fs/2.9.39/maps4fs-2.9.39.tar.gz

---
+++ packages/maps4fs/maps4fs-2.9.39.tar.gz/maps4fs-2.9.39/maps4fs/logger.py
@@ -0,0 +1,26 @@
+"""This module contains the Logger class for logging to the file and stdout."""
+
+import logging
+import sys
+from typing import Literal
+
+LOGGER_NAME = "maps4fs"
+
+
+class Logger(logging.Logger):
+    """Handles logging to stdout with timestamps."""
+
+    def __init__(
+        self,
+        level: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO",
+        to_stdout: bool = True,
+        **kwargs,
+    ):
+        super().__init__(kwargs.pop("name", LOGGER_NAME))
+        self.setLevel(level)
+        self.stdout_handler = logging.StreamHandler(sys.stdout)
+        formatter = "%(name)s | %(levelname)s | %(asctime)s | %(message)s"
+        self.fmt = formatter
+        self.stdout_handler.setFormatter(logging.Formatter(formatter))
+        if to_stdout:
+            self.addHandler(self.stdout_handler)
