e4770ce3be6300c8b49b0b0ca5c70a3221f8821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 17:07:50 +0000

    Add package pyavrocd/0.16.5/pyavrocd-0.16.5.tar.gz

---
+++ packages/pyavrocd/pyavrocd-0.16.5.tar.gz/pyavrocd-0.16.5/pyavrocd/deviceinfo/devices/attiny15.py
@@ -0,0 +1,62 @@
+
+from pymcuprog.deviceinfo.eraseflags import ChiperaseEffect
+
+DEVICE_INFO = {
+    'name': 'attiny15',
+    'architecture': 'avr8',
+
+    # eeprom
+    'eeprom_address_byte': 0x0000,
+    'eeprom_size_bytes': 0x0040,
+    'eeprom_page_size_bytes': 0x02,
+    'eeprom_read_size_bytes': 0x01,
+    'eeprom_write_size_bytes': 0x01,
+    'eeprom_chiperase_effect': ChiperaseEffect.CONDITIONALLY_ERASED_AVR,
+    'eeprom_isolated_erase': False,
+
+    # flash
+    'flash_address_byte': 0x0000,
+    'flash_size_bytes': 0x0400,
+    'flash_page_size_bytes': 0x80,
+    'flash_read_size_bytes': 0x02,
+    'flash_write_size_bytes': 0x80,
+    'flash_chiperase_effect': ChiperaseEffect.ALWAYS_ERASED,
+    'flash_isolated_erase': False,
+
+    # fuses
+    'fuses_address_byte': 0,
+    'fuses_size_bytes': 0x0001,
+    'fuses_page_size_bytes': 0x01,
+    'fuses_read_size_bytes': 0x01,
+    'fuses_write_size_bytes': 0x01,
+    'fuses_chiperase_effect': ChiperaseEffect.NOT_ERASED,
+    'fuses_isolated_erase': False,
+
+    # lockbits
+    'lockbits_address_byte': 0,
+    'lockbits_size_bytes': 0x0001,
+    'lockbits_page_size_bytes': 0x01,
+    'lockbits_read_size_bytes': 0x01,
+    'lockbits_write_size_bytes': 0x01,
+    'lockbits_chiperase_effect': ChiperaseEffect.ALWAYS_ERASED,
+    'lockbits_isolated_erase': False,
+
+    # signatures
+    'signatures_address_byte': 0,
+    'signatures_size_bytes': 3,
+    'signatures_page_size_bytes': 0x01,
+    'signatures_read_size_bytes': 0x01,
+    'signatures_write_size_bytes': 0x00,
+    'signatures_chiperase_effect': ChiperaseEffect.NOT_ERASED,
+    'signatures_isolated_erase': False,
+
+    # Some extra AVR specific fields
+    'address_size': '16-bit',
+    'eear_base' : 0x-2,
+    'eear_size' : 1,
+    'eecr_base' : 0x-4,
+    'eedr_base' : 0x-3,
+    'osccal_base' : 0x11,
+    'device_id': 0x1E9006,
+    'interface': 'ISP+HVSP'
+}
