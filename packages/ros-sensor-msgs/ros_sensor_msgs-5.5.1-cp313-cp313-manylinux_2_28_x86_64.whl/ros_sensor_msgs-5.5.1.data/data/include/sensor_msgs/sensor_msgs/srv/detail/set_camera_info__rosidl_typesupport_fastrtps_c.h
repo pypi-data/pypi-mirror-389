c37d72e89e28ae2f0a05ebb17ecf716493e606d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:22 +0000

    Add package ros-sensor-msgs/5.5.1/ros_sensor_msgs-5.5.1-cp313-cp313-manylinux_2_28_x86_64.whl

---
+++ packages/ros-sensor-msgs/ros_sensor_msgs-5.5.1-cp313-cp313-manylinux_2_28_x86_64.whl/ros_sensor_msgs-5.5.1.data/data/include/sensor_msgs/sensor_msgs/srv/detail/set_camera_info__rosidl_typesupport_fastrtps_c.h
@@ -0,0 +1,210 @@
+// generated from rosidl_typesupport_fastrtps_c/resource/idl__rosidl_typesupport_fastrtps_c.h.em
+// with input from sensor_msgs:srv/SetCameraInfo.idl
+// generated code does not contain a copyright notice
+#ifndef SENSOR_MSGS__SRV__DETAIL__SET_CAMERA_INFO__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+#define SENSOR_MSGS__SRV__DETAIL__SET_CAMERA_INFO__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+
+
+#include <stddef.h>
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "sensor_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+#include "sensor_msgs/srv/detail/set_camera_info__struct.h"
+#include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_sensor_msgs__srv__SetCameraInfo_Request(
+  const sensor_msgs__srv__SetCameraInfo_Request * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_deserialize_sensor_msgs__srv__SetCameraInfo_Request(
+  eprosima::fastcdr::Cdr &,
+  sensor_msgs__srv__SetCameraInfo_Request * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_sensor_msgs__srv__SetCameraInfo_Request(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_sensor_msgs__srv__SetCameraInfo_Request(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_key_sensor_msgs__srv__SetCameraInfo_Request(
+  const sensor_msgs__srv__SetCameraInfo_Request * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Request(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Request(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, sensor_msgs, srv, SetCameraInfo_Request)();
+
+#ifdef __cplusplus
+}
+#endif
+
+// already included above
+// #include <stddef.h>
+// already included above
+// #include "rosidl_runtime_c/message_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "sensor_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+// already included above
+// #include "sensor_msgs/srv/detail/set_camera_info__struct.h"
+// already included above
+// #include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_sensor_msgs__srv__SetCameraInfo_Response(
+  const sensor_msgs__srv__SetCameraInfo_Response * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_deserialize_sensor_msgs__srv__SetCameraInfo_Response(
+  eprosima::fastcdr::Cdr &,
+  sensor_msgs__srv__SetCameraInfo_Response * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_sensor_msgs__srv__SetCameraInfo_Response(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_sensor_msgs__srv__SetCameraInfo_Response(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_key_sensor_msgs__srv__SetCameraInfo_Response(
+  const sensor_msgs__srv__SetCameraInfo_Response * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Response(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Response(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, sensor_msgs, srv, SetCameraInfo_Response)();
+
+#ifdef __cplusplus
+}
+#endif
+
+// already included above
+// #include <stddef.h>
+// already included above
+// #include "rosidl_runtime_c/message_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "sensor_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+// already included above
+// #include "sensor_msgs/srv/detail/set_camera_info__struct.h"
+// already included above
+// #include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_sensor_msgs__srv__SetCameraInfo_Event(
+  const sensor_msgs__srv__SetCameraInfo_Event * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_deserialize_sensor_msgs__srv__SetCameraInfo_Event(
+  eprosima::fastcdr::Cdr &,
+  sensor_msgs__srv__SetCameraInfo_Event * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_sensor_msgs__srv__SetCameraInfo_Event(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_sensor_msgs__srv__SetCameraInfo_Event(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+bool cdr_serialize_key_sensor_msgs__srv__SetCameraInfo_Event(
+  const sensor_msgs__srv__SetCameraInfo_Event * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t get_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Event(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+size_t max_serialized_size_key_sensor_msgs__srv__SetCameraInfo_Event(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, sensor_msgs, srv, SetCameraInfo_Event)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "rosidl_runtime_c/service_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "sensor_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_sensor_msgs
+const rosidl_service_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, sensor_msgs, srv, SetCameraInfo)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // SENSOR_MSGS__SRV__DETAIL__SET_CAMERA_INFO__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
