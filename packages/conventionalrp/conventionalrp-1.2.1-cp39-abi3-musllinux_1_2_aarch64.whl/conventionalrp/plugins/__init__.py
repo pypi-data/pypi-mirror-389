df7109d32156be97a6814122a5d3a49320d5fd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:24:11 +0000

    Add package conventionalrp/1.2.1/conventionalrp-1.2.1-cp39-abi3-musllinux_1_2_aarch64.whl

---
+++ packages/conventionalrp/conventionalrp-1.2.1-cp39-abi3-musllinux_1_2_aarch64.whl/conventionalrp/plugins/__init__.py
@@ -0,0 +1,17 @@
+from .base import (
+    Plugin,
+    ParserPlugin,
+    ProcessorPlugin,
+    RendererPlugin,
+    AnalyzerPlugin,
+)
+from .plugin_manager import PluginManager
+
+__all__ = [
+    "Plugin",
+    "ParserPlugin",
+    "ProcessorPlugin",
+    "RendererPlugin",
+    "AnalyzerPlugin",
+    "PluginManager",
+]
