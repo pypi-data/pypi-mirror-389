a61c7f976764d022b3e232a9c1c50848807b90f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:23:34 +0000

    Add package arbi/0.4.1/arbi-0.4.1-py3-none-any.whl

---
+++ packages/arbi/arbi-0.4.1-py3-none-any.whl/arbi/resources/api/document/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .document import (
+    DocumentResource,
+    AsyncDocumentResource,
+    DocumentResourceWithRawResponse,
+    AsyncDocumentResourceWithRawResponse,
+    DocumentResourceWithStreamingResponse,
+    AsyncDocumentResourceWithStreamingResponse,
+)
+from .annotation import (
+    AnnotationResource,
+    AsyncAnnotationResource,
+    AnnotationResourceWithRawResponse,
+    AsyncAnnotationResourceWithRawResponse,
+    AnnotationResourceWithStreamingResponse,
+    AsyncAnnotationResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "AnnotationResource",
+    "AsyncAnnotationResource",
+    "AnnotationResourceWithRawResponse",
+    "AsyncAnnotationResourceWithRawResponse",
+    "AnnotationResourceWithStreamingResponse",
+    "AsyncAnnotationResourceWithStreamingResponse",
+    "DocumentResource",
+    "AsyncDocumentResource",
+    "DocumentResourceWithRawResponse",
+    "AsyncDocumentResourceWithRawResponse",
+    "DocumentResourceWithStreamingResponse",
+    "AsyncDocumentResourceWithStreamingResponse",
+]
