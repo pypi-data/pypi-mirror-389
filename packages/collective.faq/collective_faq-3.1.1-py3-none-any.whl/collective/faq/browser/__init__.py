85a4ffb6cbcc3e8ef8032facdcc7283390e235ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 04:06:40 +0000

    Add package collective.faq/3.1.1/collective_faq-3.1.1-py3-none-any.whl

---
+++ packages/collective.faq/collective_faq-3.1.1-py3-none-any.whl/collective/faq/browser/__init__.py
@@ -0,0 +1,122 @@
+from AccessControl.SecurityManagement import getSecurityManager
+from collective.faq.interfaces import IFAQ
+from collective.faq.interfaces import IFAQItem
+from plone import api
+from plone.dexterity.browser.view import DefaultView
+from zope.deprecation import deprecate
+
+import json
+
+
+class FAQView(DefaultView):
+    """Default view for FAQ content type."""
+
+    def _faqs(self):
+        return api.content.find(
+            context=self.context,
+            depth=1,
+            object_provides=IFAQ,
+            sort_on="getObjPositionInParent",
+        )
+
+    def _questions(self, faq):
+        questions = api.content.find(
+            context=faq,
+            depth=1,
+            object_provides=[IFAQItem],
+            sort_on="getObjPositionInParent",
+        )
+        results = []
+        if not questions:
+            return results
+        checkPermission = getSecurityManager().checkPermission
+        for brain in questions:
+            # We have brains, but we need the real objects for the answer,
+            # detailed question, and related items.
+            obj = brain.getObject()
+            related = []
+            results.append({"question": obj, "related": related})
+            for relation in obj.relatedItems:
+                if relation.isBroken():
+                    continue
+                target_obj = relation.to_object
+                if not checkPermission("View", target_obj):
+                    continue
+                related.append(target_obj)
+        return results
+
+    def faqs(self):
+        """Return all our FAQ items plus nested FAQs and their items.
+
+        We return a list of dictionaries with 'faq' and 'questions' keys.
+        In the first, the faq will be empty: it is the current context
+        and we don't want to repeat its title and description.
+        """
+        # First, our own questions
+        faqs = [
+            {
+                "faq": None,
+                "questions": self._questions(self.context),
+            }
+        ]
+        # Then, nested FAQs and their questions
+        for faq_brain in self._faqs():
+            faq = faq_brain.getObject()
+            faqs.append(
+                {
+                    "faq": faq,
+                    "questions": self._questions(faq),
+                }
+            )
+        return faqs
+
+    def jsonld(self):
+        """Return JSON-LD representation of the FAQ.
+
+        We only include our own questions, not nested FAQs.
+        They will have their own JSON-LD representation on their pages.
+        Each question and answer should be in a JSON-LD only once
+        per site.
+        """
+        faq_items = []
+        for result in self._questions(self.context):
+            question = result["question"]
+            faq_items.append(
+                {
+                    "@type": "Question",
+                    "name": question.title,
+                    "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": question.answer.output,
+                    },
+                }
+            )
+        faq_jsonld = {
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            "mainEntity": faq_items,
+        }
+        # We don't really need indentation, it can just be on one line.
+        # But for readability during debugging, this is much friendlier.
+        return json.dumps(faq_jsonld, indent=2)
+
+    @deprecate("This method is only used by an old template. Did you customize it?")
+    def questions(self, faq=None):
+        provides = [IFAQItem]
+        if not faq:
+            faq = self.context
+            provides += [IFAQ]
+        return api.content.find(
+            context=faq,
+            depth=1,
+            object_provides=provides,
+            sort_on="getObjPositionInParent",
+        )
+
+    @deprecate("This method is only used by an old template. Did you customize it?")
+    def nested_item(self, obj):
+        return IFAQ.providedBy(obj)
+
+
+class FAQItemView(DefaultView):
+    """Default view for FAQ Item content type."""
---
+++ packages/collective.faq/collective_faq-3.1.1-py3-none-any.whl/collective/faq/setuphandlers.py
@@ -0,0 +1,36 @@
+from plone import api
+from plone.base.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles:
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation and quickinstaller"""
+        return [
+            "collective.faq:uninstall",
+            "collective.faq:upgrade_plone60",
+            "collective.faq:upgrade_3001",
+        ]
+
+
+def post_install(context):
+    """Post install script"""
+
+
+def uninstall(context):
+    """Uninstall script"""
+    # Do something at the end of the uninstallation of this package.
+
+
+def add_catalog_indexes(context, wanted=None):
+    """Method to add our wanted indexes to the portal_catalog."""
+    catalog = api.portal.get_tool("portal_catalog")
+    indexes = catalog.indexes()
+    indexables = []
+    for name, meta_type in wanted:
+        if name not in indexes:
+            catalog.addIndex(name, meta_type)
+            indexables.append(name)
+    if len(indexables) > 0:
+        catalog.manage_reindexIndex(ids=indexables)
