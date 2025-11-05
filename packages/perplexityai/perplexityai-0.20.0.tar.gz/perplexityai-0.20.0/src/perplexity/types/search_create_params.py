dca04e13449e059af6924feabe49776554efdbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:06:40 +0000

    Add package perplexityai/0.20.0/perplexityai-0.20.0.tar.gz

---
+++ packages/perplexityai/perplexityai-0.20.0.tar.gz/perplexityai-0.20.0/src/perplexity/types/search_create_params.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+from .._types import SequenceNotStr
+
+__all__ = ["SearchCreateParams"]
+
+
+class SearchCreateParams(TypedDict, total=False):
+    query: Required[Union[str, SequenceNotStr[str]]]
+
+    country: Optional[str]
+
+    display_server_time: bool
+
+    max_results: int
+
+    max_tokens: int
+
+    max_tokens_per_page: int
+
+    search_after_date_filter: Optional[str]
+
+    search_before_date_filter: Optional[str]
+
+    search_domain_filter: Optional[SequenceNotStr[str]]
+
+    search_language_filter: Optional[SequenceNotStr[str]]
+
+    search_mode: Optional[Literal["web", "academic", "sec"]]
+
+    search_recency_filter: Optional[Literal["hour", "day", "week", "month", "year"]]
