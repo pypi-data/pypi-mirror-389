8cd55eff1c596b4e10a5a7cfb3efb3d869c29aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:05:21 +0000

    Add package python-woc/0.4.0/python_woc-0.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/python-woc/python_woc-0.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/woc/local.pyi
@@ -0,0 +1,58 @@
+from typing import Iterable, List, Literal, Optional, Tuple, Union
+
+from .base import WocMapsBase
+
+class WocMapsLocal(WocMapsBase):
+    def __init__(
+        self,
+        profile_path: Union[str, Iterable[str], None] = None,
+        version: Union[str, Iterable[str], None] = None,
+        on_large: Literal["ignore", "head", "all"] = "all",
+        on_bad: Literal["allow", "error"] = "allow",
+    ) -> None:
+        """
+        Initialize local WoC maps with a profile.
+
+        :param profile_path: path to the woc profile.
+                             if not provided, use `./wocprofile.json`, `~/.wocprofile.json`, `/home/wocprofile.json`, `/etc/wocprofile.json`.
+        :param version: version of the profile, default to the latest version.
+                        can be a single version like 'R' or a list of versions like ['R', 'U'].
+        :param on_large: how to handle large files, default to 'all' (read all content). 'ignore' to ignore large files, 'head' to read only the first chunk.
+        :param on_bad: how to handle bad entries, default to 'allow' (read all entries). 'error' to raise KeyError when a bad entry is accessed.
+        """
+        ...
+
+    def _get_tch_bytes(
+        self, map_name: str, key: Union[bytes, str], cursor=0
+    ) -> Tuple[bytes, str, Optional[int]]: ...
+    def _get_pos(
+        self,
+        obj_name: str,
+        key: Union[bytes, str],
+    ) -> Tuple[int, int]:
+        """
+        Get offset and length of a stacked binary object, currently only support blob.
+
+        Extract this part because it's much cheaper than decode the content.
+        >>> self._get_pos('blob', bytes.fromhex('7a374e58c5b9dec5f7508391246c48b73c40d200'))
+        (0, 123)
+        """
+        ...
+
+# The following functions are internal and should not be used by the user
+# Exposing them here for testing purposes
+def fnvhash(data: bytes) -> int: ...
+def unber(buf: bytes) -> bytes: ...
+def lzf_length(raw_data: bytes) -> Tuple[int, int]: ...
+def decomp(data: bytes) -> bytes: ...
+def decomp_or_raw(data: bytes) -> bytes: ...
+def get_tch(path: str): ...
+def get_shard(key: bytes, sharding_bits: int, use_fnv_keys: bool) -> int: ...
+
+# def get_from_tch(key: bytes, shards: List[bytes], sharding_bits: int, use_fnv_keys: bool) -> bytes: ...
+def decode_value(value: bytes, out_dtype: str): ...
+def decode_tree(value: bytes) -> List[Tuple[str, str, str]]: ...
+def decode_commit(
+    commit_bin: bytes,
+) -> Tuple[str, Tuple[str, str, str], Tuple[str, str, str], str]: ...
+def decode_str(raw_data: str, encoding="utf-8"): ...
---
+++ packages/python-woc/python_woc-0.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/woc/tch.pyi
@@ -0,0 +1,70 @@
+from typing import Iterator
+
+class TCHashDB:
+    """Object representing a TokyoCabinet Hash table"""
+
+    def __init__(self, path: str, ro: bool = False) -> None:
+        """
+        Create a new TokyoCabinet hash table object.
+
+        :param path: path to the database file
+        :param ro: if True, open in lock-free read-only mode; if False, lock and open in write mode (create if not exists)
+        :raises OSError: if the database cannot be opened
+        """
+        ...
+
+    def __iter__(self) -> "Iterator[bytes]": ...
+    def get(self, key: bytes) -> bytes:
+        """
+        Get a record.
+
+        :raises KeyError: if the key is not found
+        :raises OSError: if the operation fails
+        """
+        ...
+
+    def put(self, key: bytes, value: bytes) -> None:
+        """
+        Upsert a record.
+
+        :raises OSError: if the operation fails
+        """
+        ...
+
+    def delete(self, key: bytes) -> None:
+        """
+        Delete a record from the database.
+
+        :raises OSError: if the operation fails
+        """
+        ...
+
+    def drop(self) -> None:
+        """
+        Delete all records in the database.
+
+        :raises OSError: if the operation fails
+        """
+        ...
+
+    def close(self) -> None:
+        """
+        Close the database.
+
+        :raises OSError: if the operation fails
+        """
+        ...
+        
+    def optimize(self) -> None:
+        """
+        Optimize the database.
+
+        :raises OSError: if the operation fails
+        """
+        ...
+
+    def __getitem__(self, key: bytes) -> bytes: ...
+    def __setitem__(self, key: bytes, value: bytes) -> None: ...
+    def __delitem__(self, key: bytes) -> None: ...
+    def __len__(self) -> int: ...
+    def __del__(self) -> None: ...
