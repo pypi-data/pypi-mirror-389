e212d7cea655ad16b435d376511cb22e79376828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:06:26 +0000

    Add package fairseq2n/0.7.0/fairseq2n-0.7.0-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/fairseq2n/fairseq2n-0.7.0-cp310-cp310-macosx_14_0_arm64.whl/fairseq2n/lib/cmake/fairseq2n-0.7.0/fairseq2n-targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "fairseq2n::fairseq2n" for configuration "Release"
+set_property(TARGET fairseq2n::fairseq2n APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fairseq2n::fairseq2n PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "SndFile::sndfile"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfairseq2n.0.7.0.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libfairseq2n.0.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets fairseq2n::fairseq2n )
+list(APPEND _cmake_import_check_files_for_fairseq2n::fairseq2n "${_IMPORT_PREFIX}/lib/libfairseq2n.0.7.0.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
