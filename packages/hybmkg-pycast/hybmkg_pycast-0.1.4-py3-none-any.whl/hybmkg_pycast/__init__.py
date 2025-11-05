ad7c025b2c390107aae8655349d5a3fe0a81e55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:05:17 +0000

    Add package hybmkg-pycast/0.1.4/hybmkg_pycast-0.1.4-py3-none-any.whl

---
+++ packages/hybmkg-pycast/hybmkg_pycast-0.1.4-py3-none-any.whl/hybmkg_pycast/__init__.py
@@ -0,0 +1,10 @@
+"""
+hybmkg_pycast package
+Hybrid forecasting tools for BMKG time series and climate data.
+"""
+
+__version__ = "0.1.4"
+
+# Tidak melakukan import langsung agar menghindari circular import
+# Import hanya dilakukan oleh user saat butuh, misalnya:
+#   from hybmkg_pycast.model import stats
