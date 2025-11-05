35be003b2beecbd0595a5c2506b1995769c41f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:07:09 +0000

    Add package async-firebase/4.1.0/async_firebase-4.1.0.tar.gz

---
+++ packages/async-firebase/async_firebase-4.1.0.tar.gz/async_firebase-4.1.0/README.md
@@ -0,0 +1,330 @@
+# async-firebase is a lightweight asynchronous client to interact with Firebase Cloud Messaging for sending push notification to Android and iOS devices
+
+[![PyPI download month](https://img.shields.io/pypi/dm/async-firebase.svg)](https://pypi.python.org/pypi/async-firebase/)
+[![PyPI version fury.io](https://badge.fury.io/py/async-firebase.svg)](https://pypi.python.org/pypi/async-firebase/)
+[![PyPI license](https://img.shields.io/pypi/l/async-firebase.svg)](https://pypi.python.org/pypi/async-firebase/)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/async-firebase.svg)](https://pypi.python.org/pypi/async-firebase/)
+[![CI](https://github.com/healthjoy/async-firebase/actions/workflows/ci.yml/badge.svg)](https://github.com/healthjoy/async-firebase/actions/workflows/ci.yml)
+[![Codacy coverage](https://img.shields.io/codacy/coverage/b6a59cdf5ca64eab9104928d4f9bbb97?logo=codacy)](https://app.codacy.com/gh/healthjoy/async-firebase/dashboard)
+
+
+  * Free software: MIT license
+  * Requires: Python 3.9+
+
+## Features
+
+  * Extremely lightweight and does not rely on ``firebase-admin`` which is hefty
+  * Send push notifications to Android and iOS devices
+  * Send Multicast push notification to Android and iOS devices
+  * Send Web push notifications
+  * Set TTL (time to live) for notifications
+  * Set priority for notifications
+  * Set collapse-key for notifications
+  * Dry-run mode for testing purpose
+  * Topic Management
+
+## Installation
+To install `async-firebase`, simply execute the following command in a terminal:
+```shell script
+$ pip install async-firebase
+```
+
+## Getting started
+### async-firebase < 3.0.0
+To send push notification to Android:
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_file("secret-store/mobile-app-79225efac4bb.json")
+
+    # or using dictionary object
+    # client.creds_from_service_account_info({...}})
+
+    device_token = "..."
+
+    android_config = client.build_android_config(
+        priority="high",
+        ttl=2419200,
+        collapse_key="push",
+        data={"discount": "15%", "key_1": "value_1", "timestamp": "2021-02-24T12:00:15"},
+        title="Store Changes",
+        body="Recent store changes",
+    )
+    response = await client.push(device_token=device_token, android=android_config)
+
+    print(response.success, response.message_id)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+To send push notification to iOS:
+
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_file("secret-store/mobile-app-79225efac4bb.json")
+
+    # or using dictionary object
+    # client.creds_from_service_account_info({...}})
+
+    device_token = "..."
+
+    apns_config = client.build_apns_config(
+        priority="normal",
+        ttl=2419200,
+        apns_topic="store-updated",
+        collapse_key="push",
+        title="Store Changes",
+        alert="Recent store changes",
+        badge=1,
+        category="test-category",
+        custom_data={"discount": "15%", "key_1": "value_1", "timestamp": "2021-02-24T12:00:15"}
+    )
+    response = await client.push(device_token=device_token, apns=apns_config)
+
+    print(response.success)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+This prints:
+
+```shell script
+"projects/mobile-app/messages/0:2367799010922733%7606eb557606ebff"
+```
+
+To manual construct message:
+```python3
+import asyncio
+from datetime import datetime
+
+from async_firebase.messages import APNSConfig, APNSPayload, ApsAlert, Aps
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    apns_config = APNSConfig(**{
+        "headers": {
+            "apns-expiration": str(int(datetime.utcnow().timestamp()) + 7200),
+            "apns-priority": "10",
+            "apns-topic": "test-topic",
+            "apns-collapse-id": "something",
+        },
+        "payload": APNSPayload(**{
+            "aps": Aps(**{
+                "alert": ApsAlert(title="some-title", body="alert-message"),
+                "badge": 0,
+                "sound": "default",
+                "content_available": True,
+                "category": "some-category",
+                "mutable_content": False,
+                "custom_data": {
+                    "link": "https://link-to-somewhere.com",
+                    "ticket_id": "YXZ-655512",
+                },
+            })
+        })
+    })
+
+    device_token = "..."
+
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_info({...})
+    response = await client.push(device_token=device_token, apns=apns_config)
+    print(response.success)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+### async-firebase >= 3.0.0
+To send push notification to Android:
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+from async_firebase.messages import Message
+
+
+async def main():
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_file("secret-store/mobile-app-79225efac4bb.json")
+
+    # or using dictionary object
+    # client.creds_from_service_account_info({...}})
+
+    device_token: str = "..."
+
+    android_config = client.build_android_config(
+        priority="high",
+        ttl=2419200,
+        collapse_key="push",
+        data={"discount": "15%", "key_1": "value_1", "timestamp": "2021-02-24T12:00:15"},
+        title="Store Changes",
+        body="Recent store changes",
+    )
+    message = Message(android=android_config, token=device_token)
+    response = await client.send(message)
+
+    print(response.success, response.message_id)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+To send push notification to iOS:
+
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+from async_firebase.messages import Message
+
+
+async def main():
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_file("secret-store/mobile-app-79225efac4bb.json")
+
+    # or using dictionary object
+    # client.creds_from_service_account_info({...}})
+
+    device_token: str = "..."
+
+    apns_config = client.build_apns_config(
+        priority="normal",
+        ttl=2419200,
+        apns_topic="store-updated",
+        collapse_key="push",
+        title="Store Changes",
+        alert="Recent store changes",
+        badge=1,
+        category="test-category",
+        custom_data={"discount": "15%", "key_1": "value_1", "timestamp": "2021-02-24T12:00:15"}
+    )
+    message = Message(apns=apns_config, token=device_token)
+    response = await client.send(message)
+
+    print(response.success)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+This prints:
+
+```shell script
+"projects/mobile-app/messages/0:2367799010922733%7606eb557606ebff"
+```
+
+To manual construct message:
+```python3
+import asyncio
+from datetime import datetime
+
+from async_firebase.messages import APNSConfig, APNSPayload, ApsAlert, Aps, Message
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    apns_config = APNSConfig(**{
+        "headers": {
+            "apns-expiration": str(int(datetime.utcnow().timestamp()) + 7200),
+            "apns-priority": "10",
+            "apns-topic": "test-topic",
+            "apns-collapse-id": "something",
+        },
+        "payload": APNSPayload(**{
+            "aps": Aps(**{
+                "alert": ApsAlert(title="some-title", body="alert-message"),
+                "badge": 0,
+                "sound": "default",
+                "content_available": True,
+                "category": "some-category",
+                "mutable_content": False,
+                "custom_data": {
+                    "link": "https://link-to-somewhere.com",
+                    "ticket_id": "YXZ-655512",
+                },
+            })
+        })
+    })
+
+    device_token: str = "..."
+
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_info({...})
+    message = Message(apns=apns_config, token=device_token)
+    response = await client.send(message)
+    print(response.success)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+### Topic Management
+You can subscribe and unsubscribe client app instances in bulk approach by passing a list of registration tokens to the subscription method to subscribe the corresponding devices to a topic:
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    device_tokens: list[str] = ["...", "..."]
+
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_info({...})
+    response = await client.subscribe_devices_to_topic(
+        device_tokens=device_tokens, topic_name="some-topic"
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+To unsubscribe devices from a topic by passing registration tokens to the appropriate method:
+```python3
+import asyncio
+
+from async_firebase import AsyncFirebaseClient
+
+
+async def main():
+    device_tokens: list[str] = ["...", "..."]
+
+    client = AsyncFirebaseClient()
+    client.creds_from_service_account_info({...})
+    response = await client.unsubscribe_devices_from_topic(
+        device_tokens=device_tokens, topic_name="some-topic"
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+```
+
+## License
+
+``async-firebase`` is offered under the MIT license.
+
+## Source code
+
+The latest developer version is available in a GitHub repository:
+[https://github.com/healthjoy/async-firebase](https://github.com/healthjoy/async-firebase)
---
+++ packages/async-firebase/async_firebase-4.1.0.tar.gz/async_firebase-4.1.0/async_firebase/utils.py
@@ -0,0 +1,377 @@
+import io
+import json
+import typing as t
+from abc import ABC, abstractmethod
+from copy import deepcopy
+from dataclasses import fields, is_dataclass
+from email.generator import Generator
+from email.mime.multipart import MIMEMultipart
+from email.mime.nonmultipart import MIMENonMultipart
+from email.parser import FeedParser
+from enum import Enum
+from urllib.parse import quote, urlencode, urljoin
+
+import httpx
+
+from async_firebase import errors
+from async_firebase.errors import (
+    AsyncFirebaseError,
+    DeadlineExceededError,
+    FcmErrorCode,
+    QuotaExceededError,
+    SenderIdMismatchError,
+    ThirdPartyAuthError,
+    UnavailableError,
+    UnknownError,
+    UnregisteredError,
+)
+from async_firebase.messages import FCMBatchResponse, FCMResponse, TopicManagementResponse
+
+
+def join_url(
+    base: str,
+    *parts: t.Union[str, int],
+    params: t.Optional[dict] = None,
+    leading_slash: bool = False,
+    trailing_slash: bool = False,
+) -> str:
+    """Construct a full ("absolute") URL by combining a "base URL" (base) with another URL (url) parts.
+
+    :param base: base URL part
+    :param parts: another url parts that should be joined
+    :param params: dict with query params
+    :param leading_slash: flag to force leading slash
+    :param trailing_slash: flag to force trailing slash
+
+    :return: full URL
+    """
+    url = base
+    if parts:
+        quoted_and_stripped_parts = [quote(str(part).strip("/"), safe=": /") for part in parts]
+        url = "/".join([base.strip("/"), *quoted_and_stripped_parts])
+
+    # trailing slash can be important
+    if trailing_slash:
+        url = f"{url}/"
+    # as well as a leading slash
+    if leading_slash:
+        url = f"/{url}"
+
+    if params:
+        url = urljoin(url, "?{}".format(urlencode(params)))
+
+    return url
+
+
+def remove_null_values(dict_value: t.Dict[str, t.Any]) -> t.Dict[str, t.Any]:
+    """Remove Falsy values from the dictionary."""
+    return {k: v for k, v in dict_value.items() if v not in [None, [], {}]}
+
+
+def cleanup_firebase_message(dataclass_obj, dict_factory: t.Callable = dict) -> dict:
+    """
+    The instrumentation to cleanup firebase message from null values.
+
+    Example::
+
+        considering following dataclass
+
+        msg = Message(
+            token='qwe',
+            data={},
+            notification=Notification(title='push-title', body='push-body'),
+            android=None,
+            webpush={},
+            apns=APNSConfig(
+                headers={'hdr': 'qwe'},
+                payload=APNSPayload(
+                    aps=Aps(
+                        alert=None,
+                        badge=None,
+                        sound='generic',
+                        content_available=None,
+                        category=None,
+                        thread_id=None,
+                        mutable_content=None,
+                        custom_data={}
+                    ),
+                    custom_data={}
+                )
+            ),
+            topic=None,
+            condition=None
+        )
+
+        >>> dataclass_to_dict_remove_null_values(msg)
+        {
+            'token': 'qwe',
+            'notification': {'title': 'push-title', 'body': 'push-body'},
+            'apns': {
+                'headers': {'hdr': 'qwe'},
+                'payload': {
+                    'aps': {'sound': 'generic'}
+                }
+            }
+        }
+
+    :param dataclass_obj: instance of dataclass. This suppose to be instance of ``messages.PushNotification`` or
+        ``messages.Message``.
+    :param dict_factory: if given, ``dict_factory`` will be used instead of built-in dict.
+        The function applies recursively to field values that are dataclass instances.
+    :return: the fields of a dataclass instance as a new dictionary mapping field names to field values.
+    """
+    if is_dataclass(dataclass_obj):
+        result = []
+        for f in fields(dataclass_obj):
+            value = cleanup_firebase_message(getattr(dataclass_obj, f.name), dict_factory)
+            if isinstance(value, Enum):
+                value = value.value
+            result.append((f.name, value))
+        return remove_null_values(dict_factory(result))
+    elif isinstance(dataclass_obj, (list, tuple)):
+        return type(dataclass_obj)(cleanup_firebase_message(v, dict_factory) for v in dataclass_obj)  # type: ignore
+    elif isinstance(dataclass_obj, dict):
+        return remove_null_values({k: cleanup_firebase_message(v, dict_factory) for k, v in dataclass_obj.items()})
+    return deepcopy(dataclass_obj)
+
+
+def serialize_mime_message(
+    message: t.Union[MIMEMultipart, MIMENonMultipart],
+    mangle_from: t.Optional[bool] = None,
+    max_header_len: t.Optional[int] = None,
+) -> str:
+    """
+    Serialize the MIME type message.
+
+    :param message: MIME type message
+    :param mangle_from: is a flag that, when True (the default if policy
+        is not set), escapes From_ lines in the body of the message by putting
+        a `>' in front of them.
+    :param max_header_len: specifies the longest length for a non-continued
+        header.  When a header line is longer (in characters, with tabs
+        expanded to 8 spaces) than max_header_len, the header will split as
+        defined in the Header class.  Set max_header_len to zero to disable
+        header wrapping. The default is 78, as recommended (but not required)
+        by RFC 2822.
+    :return: the entire contents of the object.
+    """
+    fp = io.StringIO()
+    gen = Generator(fp, mangle_from_=mangle_from, maxheaderlen=max_header_len)
+    gen.flatten(message, unixfrom=False)
+    return fp.getvalue()
+
+
+FCMResponseType = t.TypeVar("FCMResponseType", FCMResponse, FCMBatchResponse, TopicManagementResponse)
+
+
+class FCMResponseHandlerBase(ABC, t.Generic[FCMResponseType]):
+    ERROR_CODE_TO_EXCEPTION_TYPE: t.Dict[str, t.Type[AsyncFirebaseError]] = {
+        FcmErrorCode.INVALID_ARGUMENT.value: errors.InvalidArgumentError,
+        FcmErrorCode.FAILED_PRECONDITION.value: errors.FailedPreconditionError,
+        FcmErrorCode.OUT_OF_RANGE.value: errors.OutOfRangeError,
+        FcmErrorCode.UNAUTHENTICATED.value: errors.UnauthenticatedError,
+        FcmErrorCode.PERMISSION_DENIED.value: errors.PermissionDeniedError,
+        FcmErrorCode.NOT_FOUND.value: errors.NotFoundError,
+        FcmErrorCode.ABORTED.value: errors.AbortedError,
+        FcmErrorCode.ALREADY_EXISTS.value: errors.AlreadyExistsError,
+        FcmErrorCode.CONFLICT.value: errors.ConflictError,
+        FcmErrorCode.RESOURCE_EXHAUSTED.value: errors.ResourceExhaustedError,
+        FcmErrorCode.CANCELLED.value: errors.CancelledError,
+        FcmErrorCode.DATA_LOSS.value: errors.DataLossError,
+        FcmErrorCode.UNKNOWN.value: errors.UnknownError,
+        FcmErrorCode.INTERNAL.value: errors.InternalError,
+        FcmErrorCode.UNAVAILABLE.value: errors.UnavailableError,
+        FcmErrorCode.DEADLINE_EXCEEDED.value: errors.DeadlineExceededError,
+    }
+
+    HTTP_STATUS_TO_ERROR_CODE = {
+        400: FcmErrorCode.INVALID_ARGUMENT.value,
+        401: FcmErrorCode.UNAUTHENTICATED.value,
+        403: FcmErrorCode.PERMISSION_DENIED.value,
+        404: FcmErrorCode.NOT_FOUND.value,
+        409: FcmErrorCode.CONFLICT.value,
+        412: FcmErrorCode.FAILED_PRECONDITION.value,
+        429: FcmErrorCode.RESOURCE_EXHAUSTED.value,
+        500: FcmErrorCode.INTERNAL.value,
+        503: FcmErrorCode.UNAVAILABLE.value,
+    }
+
+    FCM_ERROR_TYPES = {
+        "APNS_AUTH_ERROR": ThirdPartyAuthError,
+        "QUOTA_EXCEEDED": QuotaExceededError,
+        "SENDER_ID_MISMATCH": SenderIdMismatchError,
+        "THIRD_PARTY_AUTH_ERROR": ThirdPartyAuthError,
+        "UNREGISTERED": UnregisteredError,
+    }
+
+    @abstractmethod
+    def handle_response(self, response: httpx.Response) -> FCMResponseType:
+        pass
+
+    @abstractmethod
+    def handle_error(self, error: httpx.HTTPError) -> FCMResponseType:
+        pass
+
+    @staticmethod
+    def _handle_response(response: httpx.Response) -> FCMResponse:
+        return FCMResponse(fcm_response=response.json())
+
+    def _handle_error(self, error: httpx.HTTPError) -> FCMResponse:
+        exc = (
+            (isinstance(error, httpx.HTTPStatusError) and self._handle_fcm_error(error))
+            or (isinstance(error, httpx.HTTPError) and self._handle_request_error(error))
+            or AsyncFirebaseError(
+                code=FcmErrorCode.UNKNOWN.value,
+                message="Unexpected error has happened when hitting the FCM API",
+                cause=error,
+            )
+        )
+        return FCMResponse(exception=exc)
+
+    def _handle_request_error(self, error: httpx.HTTPError):
+        if isinstance(error, httpx.TimeoutException):
+            return DeadlineExceededError(message=f"Timed out while making an API call: {error}", cause=error)
+        elif isinstance(error, httpx.ConnectError):
+            return UnavailableError(message=f"Failed to establish a connection: {error}", cause=error)
+        elif not hasattr(error, "response"):
+            return UnknownError(message="Unknown error while making a remote service call: {error}", cause=error)
+
+        return self._get_error_by_status_code(t.cast(httpx.HTTPStatusError, error))
+
+    def _get_error_by_status_code(self, error: httpx.HTTPStatusError):
+        error_data = self._parse_platform_error(error.response)
+        code = error_data.get("status", self._http_status_to_error_code(error.response.status_code))
+        err_type = self._error_code_to_exception_type(code)
+        return err_type(message=error_data["message"], cause=error, http_response=error.response)  # type: ignore
+
+    def _handle_fcm_error(self, error: httpx.HTTPStatusError):
+        error_data = self._parse_platform_error(error.response)
+        err_type = self._get_fcm_error_type(error_data)
+        return err_type(error_data["message"], cause=error, http_response=error.response) if err_type else None
+
+    @classmethod
+    def _http_status_to_error_code(cls, http_status_code: int) -> str:
+        return cls.HTTP_STATUS_TO_ERROR_CODE.get(http_status_code, FcmErrorCode.UNKNOWN.value)
+
+    @classmethod
+    def _error_code_to_exception_type(cls, error_code: str) -> t.Type[AsyncFirebaseError]:
+        return cls.ERROR_CODE_TO_EXCEPTION_TYPE.get(error_code, errors.UnknownError)
+
+    @classmethod
+    def _get_fcm_error_type(cls, error_data: dict):
+        if not error_data:
+            return None
+
+        fcm_code = None
+        for detail in error_data.get("details", []):
+            if detail.get("@type") == "type.googleapis.com/google.firebase.fcm.v1.FcmError":
+                fcm_code = detail.get("errorCode")
+                break
+
+        if not fcm_code:
+            return None
+
+        return cls.FCM_ERROR_TYPES.get(fcm_code)
+
+    @staticmethod
+    def _parse_platform_error(response: httpx.Response):
+        """Extract the code and mesage from GCP API Error HTTP response."""
+        data: dict = {}
+        try:
+            data = response.json()
+        except ValueError:
+            pass
+
+        error_data = data.get("error", {})
+        if not error_data.get("message"):
+            error_data["message"] = (
+                f"Unexpected HTTP response with status: {response.status_code}; body: {response.content!r}"
+            )
+        return error_data
+
+
+class FCMResponseHandler(FCMResponseHandlerBase[FCMResponse]):
+    def handle_error(self, error: httpx.HTTPError) -> FCMResponse:
+        return self._handle_error(error)
+
+    def handle_response(self, response: httpx.Response) -> FCMResponse:
+        return self._handle_response(response)
+
+
+class FCMBatchResponseHandler(FCMResponseHandlerBase[FCMBatchResponse]):
+    def handle_error(self, error: httpx.HTTPError):
+        fcm_response = self._handle_error(error)
+        return FCMBatchResponse(responses=[fcm_response])
+
+    def handle_response(self, response: httpx.Response):
+        fcm_push_responses = []
+        responses = self._deserialize_batch_response(response)
+        for single_resp in responses:
+            if single_resp.status_code >= 300:
+                exc = httpx.HTTPStatusError("FCM Error", response=single_resp, request=response.request)
+                fcm_push_responses.append(self._handle_error(exc))
+            else:
+                fcm_push_responses.append(self._handle_response(single_resp))
+
+        return FCMBatchResponse(responses=fcm_push_responses)
+
+    @staticmethod
+    def _deserialize_batch_response(response: httpx.Response) -> t.List[httpx.Response]:
+        """Convert batch response into list of `httpx.Response` responses for each multipart.
+
+        :param response: string, headers and body as a string.
+        :return: list of `httpx.Response` responses.
+        """
+        # Prepend with a content-type header so FeedParser can handle it.
+        header = f"content-type: {response.headers['content-type']}\r\n\r\n"
+        # PY3's FeedParser only accepts unicode. So we should decode content here, and encode each payload again.
+        content = response.content.decode()
+        for_parser = f"{header}{content}"
+
+        parser = FeedParser()
+        parser.feed(for_parser)
+        mime_response = parser.close()
+
+        if not mime_response.is_multipart():
+            raise ValueError("Response not in multipart/mixed format.")
+
+        responses = []
+        for part in mime_response.get_payload():
+            request_id = part["Content-ID"].split("-", 1)[-1]  # type: ignore
+            status_line, payload = part.get_payload().split("\n", 1)  # type: ignore
+            _, status_code, _ = status_line.split(" ", 2)
+            status_code = int(status_code)
+
+            # Parse the rest of the response
+            parser = FeedParser()
+            parser.feed(payload)
+            msg = parser.close()
+            msg["status_code"] = t.cast(str, status_code)
+
+            # Create httpx.Response from the parsed headers.
+            resp = httpx.Response(
+                status_code=status_code,
+                headers=httpx.Headers({"Content-Type": msg.get_content_type(), "X-Request-ID": request_id}),
+                content=msg.get_payload(),
+                json=json.loads(msg.get_payload()),  # type: ignore
+            )
+            responses.append(resp)
+
+        return responses
+
+
+class TopicManagementResponseHandler(FCMResponseHandlerBase[TopicManagementResponse]):
+    def handle_error(self, error: httpx.HTTPError) -> TopicManagementResponse:
+        exc = (
+            (isinstance(error, httpx.HTTPStatusError) and self._handle_fcm_error(error))
+            or (isinstance(error, httpx.HTTPError) and self._handle_request_error(error))
+            or AsyncFirebaseError(
+                code=FcmErrorCode.UNKNOWN.value,
+                message="Unexpected error has happened when hitting the FCM API",
+                cause=error,
+            )
+        )
+        return TopicManagementResponse(exception=exc)
+
+    def handle_response(self, response: httpx.Response) -> TopicManagementResponse:
+        return TopicManagementResponse(response)
