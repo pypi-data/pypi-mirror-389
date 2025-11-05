98e63ffde9b22a881471a11687bdf0f41192bf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:05:48 +0000

    Add package moat/0.11.4/moat-0.11.4.tar.gz

---
+++ packages/moat/moat-0.11.4.tar.gz/moat-0.11.4/src/moat/main.py
@@ -0,0 +1,68 @@
+"""
+This module contains the entry point to the MOAT command line interface
+"""
+
+from __future__ import annotations
+
+import anyio
+import os
+import sys
+
+import asyncclick as click
+from asyncscope import main_scope
+
+from moat.util import attrdict, exc_iter, main_, ungroup
+from moat.util.exc import ExpectedError
+
+
+def cmd(backend="trio"):
+    """
+    The main command entry point, as declared in ``pyproject.toml``.
+    """
+
+    # @click.* decorators change the semantics
+    # pylint: disable=no-value-for-parameter
+    main_.help = """\
+This is the main command handler for MoaT, the Master of all Things.
+"""
+
+    async def runner():
+        async with main_scope() as m_s:
+            obj = attrdict(moat=attrdict(main_scope=m_s, sub_pre="moat", sub_post="_main.cli"))
+            await main_.main(obj=obj)
+
+    ec = 0
+    try:
+        with ungroup:
+            anyio.run(runner, backend=backend)
+    except ExpectedError as exc:
+        if "MOAT_TB" in os.environ:
+            raise
+        print(repr(exc), file=sys.stderr)
+        ec = 1
+    except BaseException as exc:
+        for e in exc_iter(exc):
+            if isinstance(e, KeyboardInterrupt):
+                if "MOAT_TB" in os.environ:
+                    raise
+                print("\rInterrupted.   ", file=sys.stderr)
+                break
+            elif isinstance(e, SystemExit):
+                ec |= e.code
+            elif isinstance(e, click.exceptions.Exit):
+                ec |= e.exit_code
+            else:
+                raise
+    return ec
+
+
+@main_.command(short_help="Import the debugger")
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+async def pdb(args):  # pylint: disable=unused-argument  # safe
+    """
+    This command imports PDB and continues to process arguments.
+    """
+    breakpoint()  # noqa:T100
+    if not args:
+        return
+    return await main_.main(args)
