1ac4b853d91b2f949fa7eaf17ee9345507daa1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:31 +0000

    Add package carconnectivity-plugin-webui/0.5.3a1/carconnectivity_plugin_webui-0.5.3a1.tar.gz

---
+++ packages/carconnectivity-plugin-webui/carconnectivity_plugin_webui-0.5.3a1.tar.gz/carconnectivity_plugin_webui-0.5.3a1/src/carconnectivity_plugins/webui/plugin.py
@@ -0,0 +1,113 @@
+"""Module implements the plugin to provide a web based ui."""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import logging
+import threading
+import locale
+
+from carconnectivity.errors import ConfigurationError
+from carconnectivity.util import config_remove_credentials
+from carconnectivity_plugins.base.plugin import BasePlugin
+from carconnectivity_plugins.webui.ui.webui import WebUI
+from carconnectivity_plugins.webui._version import __version__
+
+if TYPE_CHECKING:
+    from typing import Dict, Optional
+    from carconnectivity.carconnectivity import CarConnectivity
+
+LOG: logging.Logger = logging.getLogger("carconnectivity.plugins.webui")
+
+
+class Plugin(BasePlugin):  # pylint: disable=too-many-instance-attributes
+    """
+    Plugin class for Web User Interface.
+    Args:
+        car_connectivity (CarConnectivity): An instance of CarConnectivity.
+        config (Dict): Configuration dictionary containing connection details.
+    """
+    def __init__(self, plugin_id: str, car_connectivity: CarConnectivity, config: Dict) -> None:  # pylint: disable=too-many-branches, too-many-statements
+        BasePlugin.__init__(self, plugin_id=plugin_id, car_connectivity=car_connectivity, config=config, log=LOG)
+
+        self.webthread: Optional[threading.Thread] = None
+
+        werkzeug_logger: logging.Logger = logging.getLogger('werkzeug')
+        if 'log_level' in self.active_config and self.active_config['log_level'] is not None:
+            werkzeug_logger.setLevel(self.active_config['log_level'])
+        werkzeug_logger.addHandler(self.log_storage)
+
+        if 'host' not in config or not config['host']:
+            self.active_config['host'] = '0.0.0.0'  # nosec
+        else:
+            self.active_config['host'] = config['host']
+
+        if 'port' in config and config['port'] is not None:
+            self.active_config['port'] = config['port']
+            if not self.active_config['port'] or self.active_config['port'] < 1 or self.active_config['port'] > 65535:
+                raise ConfigurationError('Invalid port specified in config ("port" out of range, must be 1-65535)')
+        else:
+            self.active_config['port'] = 4000
+
+        users: Dict[str, str] = {}
+        if 'username' in config and config['username'] is not None \
+                and 'password' in config and config['password'] is not None:
+            users[config['username']] = config['password']
+
+        if 'users' in config and config['users'] is not None:
+            for user in config['users']:
+                if 'username' in user and 'password' in user:
+                    users[user['username']] = user['password']
+        self.active_config['passwords'] = users
+
+        if 'time_format' in config and config['time_format'] is not None:
+            self.active_config['time_format'] = config['time_format']
+        else:
+            self.active_config['time_format'] = None
+
+        if 'locale' in config and config['locale'] is not None:
+            self.active_config['locale'] = config['locale']
+            try:
+                locale.setlocale(locale.LC_ALL, self.active_config['locale'])
+                if self.active_config['time_format'] is None or self.active_config['time_format'] == '':
+                    self.active_config['time_format'] = locale.nl_langinfo(locale.D_T_FMT)
+            except locale.Error as err:
+                raise ConfigurationError(f'Invalid locale specified in config ("locale" must be a valid locale): {str(err)}', ) from err
+        else:
+            self.active_config['locale'] = locale.getlocale()[0]
+
+        if 'app_config' in config:
+            self.active_config['app_config'] = config['app_config']
+        else:
+            self.active_config['app_config'] = {}
+
+        self.webui = WebUI(car_connectivity=car_connectivity, host=self.active_config['host'], port=self.active_config['port'],
+                           app_config=self.active_config['app_config'], users=users, locale=self.active_config['locale'])
+
+        LOG.info("Loading webui plugin with config %s", config_remove_credentials(config))
+
+    def startup(self) -> None:
+        LOG.info("Starting WebUI plugin")
+        self.webui.load_blueprints()
+        self.webthread = threading.Thread(target=self.webui.server.serve_forever)
+        self.webthread.name = 'carconnectivity.plugins.webui-webthread'
+        self.webthread.start()
+        self.healthy._set_value(value=True)  # pylint: disable=protected-access
+        LOG.debug("Starting WebUI plugin done")
+
+    def shutdown(self) -> None:
+        """
+        Shuts down the connector by persisting current state, closing the session,
+        and cleaning up resources.
+        """
+        if self.webthread is not None and self.webthread.is_alive():
+            self.webui.server.shutdown()
+        return super().shutdown()
+
+    def get_version(self) -> str:
+        return __version__
+
+    def get_type(self) -> str:
+        return "carconnectivity-plugin-webui"
+
+    def get_name(self) -> str:
+        return "WebUI Plugin"
---
+++ packages/carconnectivity-plugin-webui/carconnectivity_plugin_webui-0.5.3a1.tar.gz/carconnectivity_plugin_webui-0.5.3a1/src/carconnectivity_plugins/webui/ui/garage.py
@@ -0,0 +1,131 @@
+""" Garage UI module for the web UI"""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import io
+import json
+from base64 import b64encode
+
+import flask
+from flask_login import login_required
+
+# pylint: disable=duplicate-code
+SUPPORT_IMAGES = False  # pylint: disable=invalid-name
+try:
+    from PIL import Image  # pylint: disable=unused-import # noqa: F401
+    SUPPORT_IMAGES = True  # pylint: disable=invalid-name
+except ImportError:
+    pass
+# pylint: enable=duplicate-code
+
+if TYPE_CHECKING:
+    from typing import Optional, Dict
+
+    from werkzeug import Response
+
+    from carconnectivity.carconnectivity import CarConnectivity
+    from carconnectivity.vehicle import GenericVehicle
+
+blueprint = flask.Blueprint(name='garage', import_name='garage', url_prefix='/garage')
+
+
+@blueprint.route('/', methods=['GET'])
+@login_required
+def garage() -> str:
+    """
+    Renders the garage page if the car_connectivity instance is connected.
+
+    This function checks if the 'car_connectivity' extension is present and connected
+    in the current Flask application context. If not, it aborts with a 500 status code.
+    If the extension is connected, it retrieves the CarConnectivity instance and renders
+    the 'garage/garage.html' template with the current application context and the garage
+    data from the CarConnectivity instance.
+
+    Returns:
+        Response: The rendered 'garage/garage.html' template.
+
+    Raises:
+        HTTPException: If the 'car_connectivity' extension is not present or not connected.
+    """
+    if 'car_connectivity' not in flask.current_app.extensions or flask.current_app.extensions['car_connectivity'] is None:
+        flask.abort(500, "car_connectivity instance not connected")
+    car_connectivity: CarConnectivity = flask.current_app.extensions['car_connectivity']
+    return flask.render_template('garage/garage.html', current_app=flask.current_app, garage=car_connectivity.garage)
+
+
+@blueprint.route('/<string:vin>/', methods=['GET'])
+@login_required
+def vehicle(vin: str) -> str:
+    """
+    Render the garage template for a vehicle.
+
+    This function checks if the 'car_connectivity' extension is available in the current Flask application context.
+    If the extension is not available or not connected, it aborts the request with a 500 status code.
+    Otherwise, it retrieves the CarConnectivity instance and renders the 'garage/garage.html' template.
+
+    Args:
+        vin (str): The Vehicle Identification Number of the vehicle.
+
+    Returns:
+        Response: The rendered 'garage/garage.html' template with the current application context and garage data.
+
+    Raises:
+        HTTPException: If the 'car_connectivity' extension is not available or not connected.
+    """
+    if 'car_connectivity' not in flask.current_app.extensions or flask.current_app.extensions['car_connectivity'] is None:
+        flask.abort(500, "car_connectivity instance not connected")
+    car_connectivity: CarConnectivity = flask.current_app.extensions['car_connectivity']
+    vehicle_obj: Optional[GenericVehicle] = car_connectivity.garage.get_vehicle(vin)
+    if vehicle_obj is None:
+        flask.abort(404, f"Vehicle with VIN {vin} not found")
+    return flask.render_template('garage/vehicle.html', current_app=flask.current_app, vehicle=vehicle_obj)
+
+
+@blueprint.route('/<string:vin>-car.png', defaults={'conversion': None}, methods=['GET'])
+@blueprint.route('/<string:vin>-car.png<string:conversion>', methods=['GET'])
+@login_required
+def vehicle_img(vin: str, conversion: Optional[str]) -> Response:
+    """
+    Retrieves the image of a vehicle based on its VIN (Vehicle Identification Number).
+
+    Args:
+        vin (str): The Vehicle Identification Number of the vehicle.
+        conversion (Optional[str]): The desired format for the response. If '.json', the image will be returned as a base64-encoded JSON object.
+        Otherwise, the image will be returned as a PNG file.
+
+    Returns:
+        Response: A Flask response object containing the vehicle image in the requested format.
+
+    Raises:
+        500: If the car_connectivity instance is not connected.
+        404: If the vehicle with the given VIN is not found or has no car picture.
+    """
+    if 'car_connectivity' not in flask.current_app.extensions or flask.current_app.extensions['car_connectivity'] is None:
+        flask.abort(500, "car_connectivity instance not connected")
+    car_connectivity: CarConnectivity = flask.current_app.extensions['car_connectivity']
+    vehicle_obj: Optional[GenericVehicle] = car_connectivity.garage.get_vehicle(vin)
+    if vehicle_obj is None:
+        if 'fallback' in flask.request.args:
+            return flask.redirect(flask.url_for('static', filename=flask.request.args.get('fallback')))
+        flask.abort(404, f"Vehicle with VIN {vin} not found")
+
+    elif not SUPPORT_IMAGES:
+        if 'fallback' in flask.request.args:
+            return flask.redirect(flask.url_for('static', filename=flask.request.args.get('fallback')))
+        flask.abort(500, "PIL module not available, cannot serve vehicle images")
+    else:
+        img_io = io.BytesIO()
+        if 'car_picture' not in vehicle_obj.images.images or not vehicle_obj.images.images['car_picture'].enabled \
+                or vehicle_obj.images.images['car_picture'].value is None:
+            if 'fallback' in flask.request.args:
+                return flask.redirect(flask.url_for('static', filename=flask.request.args.get('fallback')))
+            flask.abort(404, f"Vehicle with VIN {vin} has no car picture")
+        vehicle_obj.images.images['car_picture'].value.save(img_io, 'PNG')
+        img_io.seek(0)
+        if conversion == '.json':
+            json_map: Dict[str, str] = {}
+            json_map['type'] = 'image/png'
+            json_map['encoding'] = 'base64'
+            json_map['data'] = b64encode(img_io.read()).decode()
+            return flask.Response(json.dumps(json_map), mimetype='application/json')
+        return flask.send_file(img_io, mimetype='image/png')
