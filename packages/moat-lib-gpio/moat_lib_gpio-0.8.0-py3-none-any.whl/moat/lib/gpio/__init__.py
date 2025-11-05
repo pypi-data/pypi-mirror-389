b0292b8fc330b207db3eeffbb51b1d53db0aa9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:05:52 +0000

    Add package moat-lib-gpio/0.8.0/moat_lib_gpio-0.8.0-py3-none-any.whl

---
+++ packages/moat-lib-gpio/moat_lib_gpio-0.8.0-py3-none-any.whl/moat/lib/gpio/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for moat.lib.gpio."""
+
+from __future__ import annotations
+
+import sys
+
+from .gpio import Chip, Event, Line  # noqa: F401
+from .libgpiod import *  # noqa:F403 # XXX
+
+
+def open_chip(num=None, label=None, consumer=sys.argv[0]):
+    """Returns an object representing a GPIO chip.
+
+    Arguments:
+        num: Chip number. Defaults to zero.
+
+        consumer: A string for display by kernel utilities.
+            Defaults to the program's name.
+
+    Returns:
+        a :class:`moat.lib.gpio.gpio.Chip` instance.
+    """
+    return Chip(num=num, label=label, consumer=consumer)
