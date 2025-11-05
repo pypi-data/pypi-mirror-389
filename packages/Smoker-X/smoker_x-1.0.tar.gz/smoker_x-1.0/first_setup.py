e8920dacaa16189c05ec55931035936fcdc406bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:46 +0000

    Add package Smoker-X/1.0/smoker_x-1.0.tar.gz

---
+++ packages/Smoker-X/smoker_x-1.0.tar.gz/smoker_x-1.0/first_setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Smoker___X',
+    version='1.0',
+    author='Smoker__X',
+    author_email='gauravchauhan@639710@gmail.com',
+    description='This is speech to text package created by Smoker ',
+    packages = find_packages(),
+    install_requirement=['selenium',
+                         'Webdriver_manager'])
