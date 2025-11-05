90f8a9f4e53f1cd105051d8ecb87647920cf3a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:06:58 +0000

    Add package odinai-sdk/3.0.106/odinai_sdk-3.0.106.tar.gz

---
+++ packages/odinai-sdk/odinai_sdk-3.0.106.tar.gz/odinai_sdk-3.0.106/odin_sdk/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from odin_sdk.api.agents_api import AgentsApi
+from odin_sdk.api.chat_api import ChatApi
+from odin_sdk.api.custom_tools_api import CustomToolsApi
+from odin_sdk.api.data_types_api import DataTypesApi
+from odin_sdk.api.jsons_api import JsonsApi
+from odin_sdk.api.knowledge_base_api import KnowledgeBaseApi
+from odin_sdk.api.projects_api import ProjectsApi
+from odin_sdk.api.roles_api import RolesApi
+from odin_sdk.api.tools_api import ToolsApi
+
