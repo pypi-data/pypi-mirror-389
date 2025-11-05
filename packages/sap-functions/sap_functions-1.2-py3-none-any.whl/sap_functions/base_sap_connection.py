5686f93d75189d2269e24e951f966861f949e1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 05:06:14 +0000

    Add package sap-functions/1.2/sap_functions-1.2-py3-none-any.whl

---
+++ packages/sap-functions/sap_functions-1.2-py3-none-any.whl/sap_functions/base_sap_connection.py
@@ -0,0 +1,15 @@
+from .utils import *
+
+class BaseSapConnection:
+   def __init__(self, window: int = 0):
+
+      connection = get_sap_connection()
+      count_and_create_sap_screens(connection, window)
+      if connection.Children(0).info.user == '':
+         raise Exception(
+               "SAP user is logged out!\nYou need to log in to SAP to run this script! Please log in and try again.")
+
+      if connection.Children(0).info.systemName == 'EQ0':
+         print("You're with SAP Quality Assurance open, (SAP QA)\nMany things may not happen as desired!")
+      self.session = connection.Children(window)
+      self.window = active_window(self)
