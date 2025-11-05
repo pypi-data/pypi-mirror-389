6beb7443268dd76125de6132356b428d1679487f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:06:32 +0000

    Add package foursight-smaht/0.8.15/foursight_smaht-0.8.15-py3-none-any.whl

---
+++ packages/foursight-smaht/foursight_smaht-0.8.15-py3-none-any.whl/chalicelib_smaht/app_utils.py
@@ -0,0 +1,32 @@
+import os
+from foursight_core.app_utils import app  # Chalice object
+from foursight_core.app_utils import AppUtils as AppUtils_from_core
+from foursight_core.identity import apply_identity_globally
+from .vars import FOURSIGHT_PREFIX, HOST
+
+
+class AppUtils(AppUtils_from_core):
+
+    # dmichaels/C4-826: Apply identity globally.
+    apply_identity_globally()
+
+    # Overridden from subclass.
+    APP_PACKAGE_NAME = "foursight-smaht"
+
+    # Note that this is set in the new (as of August 2022) apply_identity code;
+    # see foursight-core/foursight_core/{app_utils.py,identity.py}.
+    es_host = os.environ.get("ES_HOST")
+    if not es_host:
+        raise Exception("Foursight ES_HOST environment variable not set!")
+    HOST = es_host
+
+    # overwriting parent class
+    prefix = FOURSIGHT_PREFIX
+    FAVICON = 'https://data.smaht.org/favicon.ico'
+    host = HOST
+    package_name = 'chalicelib_smaht'
+
+    DEFAULT_ENV = os.environ.get("ENV_NAME", "foursight-smaht-env-uninitialized")
+
+
+app_utils_obj = AppUtils.singleton(AppUtils)
