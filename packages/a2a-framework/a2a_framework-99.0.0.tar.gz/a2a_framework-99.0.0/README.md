1fc8af27df012a8eb0575cf3695c57e91435ab50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:16 +0000

    Add package a2a-framework/99.0.0/a2a_framework-99.0.0.tar.gz

---
+++ packages/a2a-framework/a2a_framework-99.0.0.tar.gz/a2a_framework-99.0.0/README.md
@@ -0,0 +1,19 @@
+# a2a-framework
+
+A useful Python package with utility functions.
+
+## Installation
+
+```bash
+pip install a2a-framework
+```
+
+## Usage
+
+```python
+import a2a-framework
+
+print(a2a-framework.hello_world())
+result = a2a-framework.add_numbers(5, 3)
+print(result)
+```
---
+++ packages/a2a-framework/a2a_framework-99.0.0.tar.gz/a2a_framework-99.0.0/a2a-framework/__init__.py
@@ -0,0 +1,22 @@
+"""
+a2a-framework - A useful Python package
+"""
+
+def hello_world():
+    """A simple greeting function"""
+    return "Hello from a2a-framework!"
+
+def add_numbers(a, b):
+    """Add two numbers together"""
+    return a + b
+
+def get_package_info():
+    """Return basic package information"""
+    return {
+        "name": "a2a-framework",
+        "version": "99.0.0",
+        "description": "A useful Python package"
+    }
+
+# Package version
+__version__ = "99.0.0"
