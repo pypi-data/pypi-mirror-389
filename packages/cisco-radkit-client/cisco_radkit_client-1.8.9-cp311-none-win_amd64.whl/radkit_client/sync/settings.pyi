488dfe65460cc26e62b884afdf7206ada8d18fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:05:13 +0000

    Add package cisco-radkit-client/1.8.9/cisco_radkit_client-1.8.9-cp311-none-win_amd64.whl

---
+++ packages/cisco-radkit-client/cisco_radkit_client-1.8.9-cp311-none-win_amd64.whl/radkit_client/sync/settings.pyi
@@ -0,0 +1,3 @@
+from radkit_client.async_.settings import ClientSettingsLoader as ClientSettingsLoader, UseE2EE as UseE2EE, get_settings as get_settings, load_settings as load_settings
+
+__all__ = ['UseE2EE', 'ClientSettingsLoader', 'get_settings', 'load_settings']
