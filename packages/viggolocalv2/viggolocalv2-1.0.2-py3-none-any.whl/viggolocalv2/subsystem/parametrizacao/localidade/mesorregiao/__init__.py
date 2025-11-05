7332aaf074c9695a0eaac04d49cb79e7257378aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 03:23:31 +0000

    Add package viggolocalv2/1.0.2/viggolocalv2-1.0.2-py3-none-any.whl

---
+++ packages/viggolocalv2/viggolocalv2-1.0.2-py3-none-any.whl/viggolocalv2/subsystem/parametrizacao/localidade/mesorregiao/__init__.py
@@ -0,0 +1,5 @@
+from viggocorev2.common import subsystem
+from viggolocalv2.subsystem.parametrizacao.localidade.mesorregiao import resource
+
+
+subsystem = subsystem.Subsystem(resource=resource.Mesorregiao)
