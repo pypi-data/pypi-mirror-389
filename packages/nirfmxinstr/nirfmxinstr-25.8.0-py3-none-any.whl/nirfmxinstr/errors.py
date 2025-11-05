15e7c3ac2f5c2a48c16b0a05f29e41971ced5893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:41 +0000

    Add package nirfmxinstr/25.8.0/nirfmxinstr-25.8.0-py3-none-any.whl

---
+++ packages/nirfmxinstr/nirfmxinstr-25.8.0-py3-none-any.whl/nirfmxinstr/errors.py
@@ -0,0 +1,89 @@
+"""errors.py - Contains error classes and method(s) to handle error."""
+
+import platform
+from typing import Any
+
+
+def _is_success(code: int) -> bool:
+    return code == 0
+
+
+def _is_error(code: int) -> bool:
+    return code < 0
+
+
+def _is_warning(code: int) -> bool:
+    return code > 0
+
+
+class Error(Exception):
+    """Base error class for NI-RFmxInstr."""
+
+    def __init__(self, message):
+        """Base error class for NI-RFmxInstr."""
+        super(Error, self).__init__(message)
+
+
+class RFmxError(Error):
+    """An error originating from the NI-RFmxInstr driver."""
+
+    def __init__(self, code, description):
+        """An error originating from the NI-RFmxInstr driver."""
+        assert _is_error(code), "Should not raise Error if code is not fatal."
+        self.code = code
+        self.description = description
+        super(RFmxError, self).__init__(self.description)  # type: ignore
+
+
+class DriverNotInstalledError(Error):
+    """An error due to using this module without the driver runtime installed."""
+
+    def __init__(self):
+        """An error due to using this module without the driver runtime installed."""
+        super(DriverNotInstalledError, self).__init__("The NI-RFmxInstr runtime could not be loaded. Make sure it is installed and its bitness matches that of your Python interpreter. Please visit http://www.ni.com/downloads/drivers/ to download and install it.")  # type: ignore
+
+
+class DriverTooOldError(Error):
+    """An error due to using this module with an older version of the NI-RFmxInstr driver runtime."""
+
+    def __init__(self):
+        """An error due to using this module with an older version of the NI-RFmxInstr driver runtime."""
+        super(DriverTooOldError, self).__init__("A function was not found in the NI-RFmxInstr runtime. Please visit http://www.ni.com/downloads/drivers/ to download a newer version and install it.")  # type: ignore
+
+
+class DriverTooNewError(Error):
+    """An error due to the NI-RFmxInstr driver runtime being too new for this module."""
+
+    def __init__(self):
+        """An error due to the NI-RFmxInstr driver runtime being too new for this module."""
+        super(DriverTooNewError, self).__init__("The NI-RFmxInstr runtime returned an unexpected value. This can occur if it is too new for the nirfmxinstr Python module. Upgrade the nirfmxinstr Python module.")  # type: ignore
+
+
+class UnsupportedConfigurationError(Error):
+    """An error due to using this module in an usupported platform."""
+
+    def __init__(self):
+        """An error due to using this module in an usupported platform."""
+        super(UnsupportedConfigurationError, self).__init__("System configuration is unsupported: " + platform.architecture()[0] + " " + platform.system())  # type: ignore
+
+
+def handle_error(
+    library_interpreter: Any, code: int, ignore_warnings: bool, is_error_handling: bool
+) -> None:
+    r"""Helper function for handling errors returned by NI-RFmxInstr Library.
+
+    It calls back into the LibraryInterpreter to get the corresponding error
+    description and raises if necessary.
+    """
+    if _is_success(code) or (_is_warning(code) and ignore_warnings):
+        return
+
+    if is_error_handling:
+        # The caller is in the midst of error handling and an error occurred.
+        # Don't try to get the description or we'll start recursing until the stack overflows.
+        description = ""
+    else:
+        description = library_interpreter.get_error_description(code)
+
+    if _is_error(code):
+        raise RFmxError(code, description)  # type: ignore
