3674c5c8e223aa741e2a8d6c5acf2250f3645c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:11 +0000

    Add package openc/0.0.1/openc-0.0.1.tar.gz

---
+++ packages/openc/openc-0.0.1.tar.gz/openc-0.0.1/README.md
@@ -0,0 +1,14 @@
+# Cryptic
+
+Cryptic is a Python library that provides:
+
+- Functions to build and train machine learning models (image and tabular data).
+- A grid search implementation for neural network hyperparameters.
+- A utility to print the source code of any file.
+
+## Installation
+
+Once published to PyPI, install via pip:
+
+```bash
+pip install opencv
---
+++ packages/openc/openc-0.0.1.tar.gz/openc-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="openc",  # Replace with your package name
+    version="0.0.1",           # Initial version
+    author="CrypticX",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+    ],
+)
