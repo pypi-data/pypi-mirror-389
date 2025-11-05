6443bad4d3f60f878b728cd357cf7568821ec16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 17:08:13 +0000

    Add package azure-simple-email/0.1.9/azure_simple_email-0.1.9.tar.gz

---
+++ packages/azure-simple-email/azure_simple_email-0.1.9.tar.gz/azure_simple_email-0.1.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="azure_simple_email",
+    version="0.1.9",
+    author="Marco Pavanelli",
+    author_email="marco.pavanelli@sasabz.it",
+    description="This is an experimental library to send email using Microsoft Graph API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcotn/azure_simple_email",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'msal',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
