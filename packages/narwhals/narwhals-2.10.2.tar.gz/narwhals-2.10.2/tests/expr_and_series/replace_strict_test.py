3c9405e2b06c78b9e7a822f7ab22e5350317fb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 06:06:34 +0000

    Add package narwhals/2.10.2/narwhals-2.10.2.tar.gz

---
+++ packages/narwhals/narwhals-2.10.2.tar.gz/narwhals-2.10.2/tests/expr_and_series/replace_strict_test.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+import narwhals as nw
+from narwhals.exceptions import NarwhalsError
+from tests.utils import POLARS_VERSION, Constructor, ConstructorEager, assert_equal_data
+
+if TYPE_CHECKING:
+    from narwhals.dtypes import DType
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+@pytest.mark.parametrize("return_dtype", [nw.String(), None])
+def test_replace_strict(
+    constructor: Constructor, request: pytest.FixtureRequest, return_dtype: DType | None
+) -> None:
+    if "dask" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+    if (
+        ("pyspark" in str(constructor))
+        or "duckdb" in str(constructor)
+        or "ibis" in str(constructor)
+    ):
+        request.applymarker(pytest.mark.xfail)
+    df = nw.from_native(constructor({"a": [1, 2, 3]}))
+    result = df.select(
+        nw.col("a").replace_strict(
+            [1, 2, 3], ["one", "two", "three"], return_dtype=return_dtype
+        )
+    )
+    assert_equal_data(result, {"a": ["one", "two", "three"]})
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+@pytest.mark.parametrize("return_dtype", [nw.String(), None])
+def test_replace_strict_series(
+    constructor_eager: ConstructorEager, return_dtype: DType | None
+) -> None:
+    df = nw.from_native(constructor_eager({"a": [1, 2, 3]}))
+    result = df.select(
+        df["a"].replace_strict(
+            [1, 2, 3], ["one", "two", "three"], return_dtype=return_dtype
+        )
+    )
+    assert_equal_data(result, {"a": ["one", "two", "three"]})
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+def test_replace_non_full(
+    constructor: Constructor, request: pytest.FixtureRequest
+) -> None:
+    if "dask" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+    if (
+        ("pyspark" in str(constructor))
+        or "duckdb" in str(constructor)
+        or "ibis" in str(constructor)
+    ):
+        request.applymarker(pytest.mark.xfail)
+    df = nw.from_native(constructor({"a": [1, 2, 3]}))
+    if isinstance(df, nw.LazyFrame):
+        with pytest.raises((ValueError, NarwhalsError)):
+            df.select(
+                nw.col("a").replace_strict([1, 3], [3, 4], return_dtype=nw.Int64)
+            ).collect()
+    else:
+        with pytest.raises((ValueError, NarwhalsError)):
+            df.select(nw.col("a").replace_strict([1, 3], [3, 4], return_dtype=nw.Int64))
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+def test_replace_strict_mapping(
+    constructor: Constructor, request: pytest.FixtureRequest
+) -> None:
+    if "dask" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+    if (
+        ("pyspark" in str(constructor))
+        or "duckdb" in str(constructor)
+        or "ibis" in str(constructor)
+    ):
+        request.applymarker(pytest.mark.xfail)
+
+    df = nw.from_native(constructor({"a": [1, 2, 3]}))
+    result = df.select(
+        nw.col("a").replace_strict(
+            {1: "one", 2: "two", 3: "three"}, return_dtype=nw.String()
+        )
+    )
+    assert_equal_data(result, {"a": ["one", "two", "three"]})
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+def test_replace_strict_series_mapping(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(constructor_eager({"a": [1, 2, 3]}))
+    result = df.select(
+        df["a"].replace_strict({1: "one", 2: "two", 3: "three"}, return_dtype=nw.String())
+    )
+    assert_equal_data(result, {"a": ["one", "two", "three"]})
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+def test_replace_strict_invalid(constructor: Constructor) -> None:
+    df = nw.from_native(constructor({"a": [1, 2, 3]}))
+    with pytest.raises(
+        TypeError,
+        match="`new` argument is required if `old` argument is not a Mapping type",
+    ):
+        df.select(nw.col("a").replace_strict(old=[1, 2, 3]))
+
+
+@pytest.mark.skipif(
+    POLARS_VERSION < (1, 0), reason="replace_strict only available after 1.0"
+)
+def test_replace_strict_series_invalid(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(constructor_eager({"a": [1, 2, 3]}))
+    with pytest.raises(
+        TypeError,
+        match="`new` argument is required if `old` argument is not a Mapping type",
+    ):
+        df["a"].replace_strict([1, 2, 3])
+
+
+def test_replace_strict_pandas_unnamed_series() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    ser = nw.from_native(pd.Series([1, 2, 3]), series_only=True)
+    result = ser.replace_strict([1, 2, 3], ["one", "two", "three"])
+    assert result.name is None
---
+++ packages/narwhals/narwhals-2.10.2.tar.gz/narwhals-2.10.2/tests/frame/with_row_index_test.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+import narwhals as nw
+from tests.utils import (
+    DUCKDB_VERSION,
+    PANDAS_VERSION,
+    Constructor,
+    ConstructorEager,
+    assert_equal_data,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+data = {"abc": ["foo", "bars"], "xyz": [100, 200], "const": [42, 42]}
+
+
+def test_with_row_index_eager(constructor_eager: ConstructorEager) -> None:
+    result = nw.from_native(constructor_eager(data), eager_only=True).with_row_index()
+    expected = {"index": [0, 1], **data}
+    assert_equal_data(result, expected)
+
+
+@pytest.mark.parametrize(
+    ("order_by", "expected_index"),
+    [
+        ("abc", [1, 0]),
+        ("xyz", [0, 1]),
+        (["const", "abc"], [1, 0]),
+        (["const", "xyz"], [0, 1]),
+    ],
+)
+def test_with_row_index_lazy(
+    constructor: Constructor, order_by: str | Sequence[str], expected_index: list[int]
+) -> None:
+    if (
+        "pandas" in str(constructor) and PANDAS_VERSION < (1, 3) and order_by == "abc"
+    ):  # pragma: no cover
+        reason = "ValueError: first not supported for non-numeric data."
+        pytest.skip(reason=reason)
+    if "duckdb" in str(constructor) and DUCKDB_VERSION < (1, 3):
+        pytest.skip()
+
+    result = (
+        nw.from_native(constructor(data))
+        .with_row_index(name="foo bar", order_by=order_by)
+        .sort("xyz")
+    )
+    expected = {"foo bar": expected_index, **data}
+    assert_equal_data(result, expected)
+
+
+def test_with_row_index_lazy_exception(constructor: Constructor) -> None:
+    frame = nw.from_native(constructor(data))
+    msg = r"(LazyFrame\.)?with_row_index\(\) missing 1 required keyword-only argument: 'order_by'$"
+    if isinstance(frame, nw.LazyFrame):
+        with pytest.raises(TypeError, match=msg):
+            frame.with_row_index()  # type: ignore[call-arg]
+    else:
+        result = frame.with_row_index()
+        assert_equal_data(result, {"index": [0, 1], **data})
