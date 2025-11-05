34ae3948a058c54c4216b07d0936f6b71408189b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:06:30 +0000

    Add package google-colab-selenium/1.0.16/google_colab_selenium-1.0.16-py3-none-any.whl

---
+++ packages/google-colab-selenium/google_colab_selenium-1.0.16-py3-none-any.whl/google_colab_selenium/__init__.py
@@ -0,0 +1,30 @@
+from google_colab_selenium.exceptions import (
+    GoogleColabSeleniumError,
+    InstallChromeError,
+    ChromeDriverPathError,
+    StartingChromeDriverError
+)
+
+
+__all__ = (
+    'ChromeDriver', 
+    'UndetectedChromeDriver', 
+    'Chrome', 
+    'UndetectedChrome',
+    'GoogleColabSeleniumError',
+    'InstallChromeError',
+    'ChromeDriverPathError',
+    'StartingChromeDriverError'
+)
+
+
+def __getattr__(name):
+    if name == 'ChromeDriver' or name == 'Chrome':
+        from google_colab_selenium.chromedriver import ChromeDriver
+        return ChromeDriver
+
+    if name == 'UndetectedChromeDriver' or name == 'UndetectedChrome':
+        from google_colab_selenium.undetected_chromedriver import UndetectedChromeDriver
+        return UndetectedChromeDriver
+
+    raise AttributeError(f"module 'google_colab_selenium' has no attribute '{name}'")
