26bd574c4978bffb4f6e43246ed20e484bc1b4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:06:38 +0000

    Add package llm-dataclass/1.1.0/llm_dataclass-1.1.0.tar.gz

---
+++ packages/llm-dataclass/llm_dataclass-1.1.0.tar.gz/llm_dataclass-1.1.0/tests/test_dumps_dataclass_simple.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+from textwrap import dedent
+
+import llm_dataclass
+
+
+def test_dataclass_simple() -> None:
+    @dataclass
+    class Person:
+        name: str
+        age: int
+
+    schema = llm_dataclass.Schema(Person)
+    expected_schema = dedent("""\
+    <Person>
+      <name>...</name>
+      <age>...</age>
+    </Person>""")
+
+    assert schema.dumps() == expected_schema
+
+
+def test_dataclass_simple_with_instance() -> None:
+    @dataclass
+    class Person:
+        name: str
+        age: int
+
+    schema = llm_dataclass.Schema(Person)
+    expected_schema = dedent("""\
+    <Person>
+      <name>John Doe</name>
+      <age>30</age>
+    </Person>""")
+
+    assert schema.dumps(Person(name="John Doe", age=30)) == expected_schema
+
+
+def test_dataclass_array() -> None:
+    @dataclass
+    class Person:
+        name: str
+        pets: list[str] = field(default_factory=list, metadata={"xml": {"name": "pet"}})
+
+    schema = llm_dataclass.Schema(Person)
+    expected_schema = dedent("""\
+    <Person>
+      <name>...</name>
+      <pet>...</pet>
+      <pet>...</pet>
+    </Person>""")
+
+    assert schema.dumps() == expected_schema
