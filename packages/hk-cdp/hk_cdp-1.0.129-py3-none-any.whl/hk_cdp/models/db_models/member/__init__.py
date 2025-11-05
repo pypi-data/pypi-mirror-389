961765fbbd05c660966207fabcfca4f3bd389b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:32 +0000

    Add package hk-cdp/1.0.129/hk_cdp-1.0.129-py3-none-any.whl

---
+++ packages/hk-cdp/hk_cdp-1.0.129-py3-none-any.whl/hk_cdp/models/db_models/member/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJianYi
+@Date: 2024-11-22 17:23:58
+@LastEditTime: 2024-11-22 17:25:30
+@LastEditors: HuangJianYi
+@Description: 
+"""
+__all__ = ["member_asset_log_model","member_asset_model", "member_asset_valid_model", "member_event_model", "member_info_model", "member_level_log_model"]
