88616c16e843b8dc50a8028df1e7fabcda8060ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 15:05:53 +0000

    Add package agentscope-runtime/0.2.0b2/agentscope_runtime-0.2.0b2.tar.gz

---
+++ packages/agentscope-runtime/agentscope_runtime-0.2.0b2.tar.gz/agentscope_runtime-0.2.0b2/src/agentscope_runtime/sandbox/box/base/base_sandbox.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+from ...utils import build_image_uri
+from ...registry import SandboxRegistry
+from ...enums import SandboxType
+from ...box.sandbox import Sandbox
+from ...constant import TIMEOUT
+
+
+@SandboxRegistry.register(
+    build_image_uri("runtime-sandbox-base"),
+    sandbox_type=SandboxType.BASE,
+    security_level="medium",
+    timeout=TIMEOUT,
+    description="Base Sandbox",
+)
+class BaseSandbox(Sandbox):
+    def __init__(
+        self,
+        sandbox_id: Optional[str] = None,
+        timeout: int = 3000,
+        base_url: Optional[str] = None,
+        bearer_token: Optional[str] = None,
+        sandbox_type: SandboxType = SandboxType.BASE,
+    ):
+        super().__init__(
+            sandbox_id,
+            timeout,
+            base_url,
+            bearer_token,
+            sandbox_type,
+        )
+
+    def run_ipython_cell(self, code: str):
+        return self.call_tool("run_ipython_cell", {"code": code})
+
+    def run_shell_command(self, command: str):
+        return self.call_tool("run_shell_command", {"command": command})
