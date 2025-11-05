7ab1adcb2e4abd7354817a29e8351e08baa55080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 05:06:18 +0000

    Add package exasol-toolbox/2.0.0/exasol_toolbox-2.0.0-py3-none-any.whl

---
+++ packages/exasol-toolbox/exasol_toolbox-2.0.0-py3-none-any.whl/exasol/toolbox/config.py
@@ -0,0 +1,79 @@
+from typing import (
+    Annotated,
+)
+
+from pydantic import (
+    AfterValidator,
+    BaseModel,
+    ConfigDict,
+    Field,
+    computed_field,
+)
+
+from exasol.toolbox.util.version import Version
+
+
+def valid_version_string(version_string: str) -> str:
+    Version.from_string(version_string)
+    return version_string
+
+
+ValidVersionStr = Annotated[str, AfterValidator(valid_version_string)]
+
+
+class BaseConfig(BaseModel):
+    """
+    Basic configuration for projects using the PTB
+
+    This configuration class defines the necessary attributes for using the PTB's
+    various nox sessions and GitHub CI workflows. Defaults are provided via the
+    attributes, which makes it easy for a specific project to modify these values
+    as needed. There are properties provided which automate some configuration aspects
+    so that the project-specific modifications are reduced.
+
+    pydantic has been used so that altered attributes are quickly validated.
+    This allows for immediate feedback, instead of waiting for various failed CI
+    runs.
+    """
+
+    python_versions: tuple[ValidVersionStr, ...] = Field(
+        default=("3.10", "3.11", "3.12", "3.13", "3.14"),
+        description="Python versions to use in running CI workflows",
+    )
+
+    exasol_versions: tuple[ValidVersionStr, ...] = Field(
+        default=("7.1.30", "8.29.6", "2025.1.0"),
+        description="Exasol versions to use in running CI workflows for integration tests using the DB",
+    )
+    create_major_version_tags: bool = Field(
+        default=False,
+        description="If true, creates also the major version tags (v*) automatically",
+    )
+    model_config = ConfigDict(frozen=True, arbitrary_types_allowed=True)
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def minimum_python_version(self) -> str:
+        """
+        Minimum Python version declared from the `python_versions` list
+
+        This is used in specific testing scenarios where it would be either
+        costly to run the tests for all `python_versions` or we need a single metric.
+        """
+        versioned = [Version.from_string(v) for v in self.python_versions]
+        min_version = min(versioned)
+        index_min_version = versioned.index(min_version)
+        return self.python_versions[index_min_version]
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def pyupgrade_argument(self) -> tuple[str, ...]:
+        """
+        Default argument to :func:`exasol.toolbox._format._pyupgrade`.
+
+        It uses the minimum Python version to ensure compatibility with all supported
+        versions of a project.
+        """
+        version_parts = self.minimum_python_version.split(".")[:2]
+        version_number = "".join(version_parts)
+        return (f"--py{version_number}-plus",)
---
+++ packages/exasol-toolbox/exasol_toolbox-2.0.0-py3-none-any.whl/exasol/toolbox/nox/_lint.py
@@ -0,0 +1,179 @@
+from __future__ import annotations
+
+import argparse
+import sys
+from collections.abc import Iterable
+from pathlib import Path
+
+import nox
+import rich.console
+import tomlkit
+from nox import Session
+
+from exasol.toolbox.nox._shared import python_files
+from noxconfig import PROJECT_CONFIG
+
+
+def _pylint(session: Session, files: Iterable[str]) -> None:
+    session.run(
+        "pylint",
+        "--output-format",
+        "colorized,json:.lint.json,text:.lint.txt",
+        *files,
+    )
+
+
+def _type_check(session: Session, files: Iterable[str]) -> None:
+    session.run(
+        "mypy",
+        "--explicit-package-bases",
+        "--namespace-packages",
+        "--show-error-codes",
+        "--pretty",
+        "--show-column-numbers",
+        "--show-error-context",
+        "--scripts-are-modules",
+        *files,
+    )
+
+
+def _security_lint(session: Session, files: Iterable[str]) -> None:
+    session.run(
+        "bandit",
+        "--severity-level",
+        "low",
+        "--quiet",
+        "--format",
+        "json",
+        "--output",
+        ".security.json",
+        "--exit-zero",
+        *files,
+    )
+    session.run(
+        "bandit",
+        "--severity-level",
+        "low",
+        "--quiet",
+        "--exit-zero",
+        *files,
+    )
+
+
+def _import_lint(session: Session, path: Path) -> None:
+    session.run("lint-imports", "--config", path)
+
+
+class Dependencies:
+    def __init__(self, illegal: dict[str, list[str]] | None):
+        self._illegal = illegal or {}
+
+    @staticmethod
+    def parse(pyproject_toml: str) -> Dependencies:
+        def _source_filter(version) -> bool:
+            ILLEGAL_SPECIFIERS = ["url", "git", "path"]
+            return any(specifier in version for specifier in ILLEGAL_SPECIFIERS)
+
+        def find_illegal(part) -> list[str]:
+            return [
+                f"{name} = {version}"
+                for name, version in part.items()
+                if _source_filter(version)
+            ]
+
+        illegal: dict[str, list[str]] = {}
+        toml = tomlkit.loads(pyproject_toml)
+        poetry = toml.get("tool", {}).get("poetry", {})
+
+        part = poetry.get("dependencies", {})
+        if illegal_group := find_illegal(part):
+            illegal["tool.poetry.dependencies"] = illegal_group
+
+        part = poetry.get("dev", {}).get("dependencies", {})
+        if illegal_group := find_illegal(part):
+            illegal["tool.poetry.dev.dependencies"] = illegal_group
+
+        part = poetry.get("group", {})
+        for group, content in part.items():
+            illegal_group = find_illegal(content.get("dependencies", {}))
+            if illegal_group:
+                illegal[f"tool.poetry.group.{group}.dependencies"] = illegal_group
+        return Dependencies(illegal)
+
+    @property
+    def illegal(self) -> dict[str, list[str]]:
+        return self._illegal
+
+
+def report_illegal(illegal: dict[str, list[str]], console: rich.console.Console):
+    count = sum(len(deps) for deps in illegal.values())
+    suffix = "y" if count == 1 else "ies"
+    console.print(f"{count} illegal dependenc{suffix}\n", style="red")
+    for section, dependencies in illegal.items():
+        console.print(f"\\[{section}]", style="red")
+        for dependency in dependencies:
+            console.print(dependency, style="red")
+        console.print("")
+
+
+@nox.session(name="lint:code", python=False)
+def lint(session: Session) -> None:
+    """Runs the static code analyzer on the project"""
+    py_files = python_files(PROJECT_CONFIG.root / PROJECT_CONFIG.source)
+    _pylint(session, py_files)
+
+
+@nox.session(name="lint:typing", python=False)
+def type_check(session: Session) -> None:
+    """Runs the type checker on the project"""
+    py_files = [f"{file}" for file in python_files(PROJECT_CONFIG.root)]
+    _type_check(session, py_files)
+
+
+@nox.session(name="lint:security", python=False)
+def security_lint(session: Session) -> None:
+    """Runs the security linter on the project"""
+    py_files = python_files(PROJECT_CONFIG.root / PROJECT_CONFIG.source)
+    _security_lint(session, py_files)
+
+
+@nox.session(name="lint:dependencies", python=False)
+def dependency_check(session: Session) -> None:
+    """Checks if only valid sources of dependencies are used"""
+    content = Path(PROJECT_CONFIG.root, "pyproject.toml").read_text()
+    dependencies = Dependencies.parse(content)
+    console = rich.console.Console()
+    if illegal := dependencies.illegal:
+        report_illegal(illegal, console)
+        sys.exit(1)
+
+
+@nox.session(name="lint:import", python=False)
+def import_lint(session: Session) -> None:
+    """(experimental) Runs import linter on the project"""
+    parser = argparse.ArgumentParser(
+        usage="nox -s import-lint -- [options]",
+        description="Runs the import linter on the project",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        help="path to the configuration file for the importlinter",
+        metavar="TEXT",
+    )
+
+    args: argparse.Namespace = parser.parse_args(args=session.posargs)
+    file: str = args.config
+    path: Path | None = None
+    if file is None:
+        path = getattr(
+            PROJECT_CONFIG, "import_linter_config", Path(".import_linter_config")
+        )
+    else:
+        path = Path(file)
+    if not path.exists():
+        session.error(
+            "Please make sure you have a configuration file for the importlinter"
+        )
+    _import_lint(session=session, path=path)
