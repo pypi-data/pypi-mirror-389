ff69d38c6ad262a49eb33d3d20ba061439b84269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 16:07:37 +0000

    Add package tree-sitter-mips/0.2.0/tree_sitter_mips-0.2.0-cp39-abi3-win_amd64.whl

---
+++ packages/tree-sitter-mips/tree_sitter_mips-0.2.0-cp39-abi3-win_amd64.whl/tree_sitter_mips/queries/indents.scm
@@ -0,0 +1,17 @@
+((global_label) @indent.begin
+  (#set! indent.immediate 1))
+
+((local_label) @indent.begin
+  (#set! indent.immediate 1))
+
+((global_numeric_label) @indent.begin
+  (#set! indent.immediate 1))
+
+((local_numeric_label) @indent.begin
+  (#set! indent.immediate 1))
+
+((instruction) @indent.begin
+  (#set! indent.immediate 1))
+
+((directive) @indent.begin
+  (#set! indent.immediate 1))
