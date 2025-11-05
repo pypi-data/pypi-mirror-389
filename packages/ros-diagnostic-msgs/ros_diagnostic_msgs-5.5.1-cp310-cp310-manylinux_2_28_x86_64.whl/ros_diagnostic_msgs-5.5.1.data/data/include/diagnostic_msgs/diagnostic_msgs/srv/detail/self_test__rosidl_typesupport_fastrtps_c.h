7d6538e76c041ccb3327be809c2196f73e76b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:07:17 +0000

    Add package ros-diagnostic-msgs/5.5.1/ros_diagnostic_msgs-5.5.1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/ros-diagnostic-msgs/ros_diagnostic_msgs-5.5.1-cp310-cp310-manylinux_2_28_x86_64.whl/ros_diagnostic_msgs-5.5.1.data/data/include/diagnostic_msgs/diagnostic_msgs/srv/detail/self_test__rosidl_typesupport_fastrtps_c.h
@@ -0,0 +1,210 @@
+// generated from rosidl_typesupport_fastrtps_c/resource/idl__rosidl_typesupport_fastrtps_c.h.em
+// with input from diagnostic_msgs:srv/SelfTest.idl
+// generated code does not contain a copyright notice
+#ifndef DIAGNOSTIC_MSGS__SRV__DETAIL__SELF_TEST__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+#define DIAGNOSTIC_MSGS__SRV__DETAIL__SELF_TEST__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+
+
+#include <stddef.h>
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "diagnostic_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+#include "diagnostic_msgs/srv/detail/self_test__struct.h"
+#include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_diagnostic_msgs__srv__SelfTest_Request(
+  const diagnostic_msgs__srv__SelfTest_Request * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_deserialize_diagnostic_msgs__srv__SelfTest_Request(
+  eprosima::fastcdr::Cdr &,
+  diagnostic_msgs__srv__SelfTest_Request * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_diagnostic_msgs__srv__SelfTest_Request(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_diagnostic_msgs__srv__SelfTest_Request(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_key_diagnostic_msgs__srv__SelfTest_Request(
+  const diagnostic_msgs__srv__SelfTest_Request * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_key_diagnostic_msgs__srv__SelfTest_Request(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_key_diagnostic_msgs__srv__SelfTest_Request(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, diagnostic_msgs, srv, SelfTest_Request)();
+
+#ifdef __cplusplus
+}
+#endif
+
+// already included above
+// #include <stddef.h>
+// already included above
+// #include "rosidl_runtime_c/message_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "diagnostic_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+// already included above
+// #include "diagnostic_msgs/srv/detail/self_test__struct.h"
+// already included above
+// #include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_diagnostic_msgs__srv__SelfTest_Response(
+  const diagnostic_msgs__srv__SelfTest_Response * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_deserialize_diagnostic_msgs__srv__SelfTest_Response(
+  eprosima::fastcdr::Cdr &,
+  diagnostic_msgs__srv__SelfTest_Response * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_diagnostic_msgs__srv__SelfTest_Response(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_diagnostic_msgs__srv__SelfTest_Response(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_key_diagnostic_msgs__srv__SelfTest_Response(
+  const diagnostic_msgs__srv__SelfTest_Response * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_key_diagnostic_msgs__srv__SelfTest_Response(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_key_diagnostic_msgs__srv__SelfTest_Response(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, diagnostic_msgs, srv, SelfTest_Response)();
+
+#ifdef __cplusplus
+}
+#endif
+
+// already included above
+// #include <stddef.h>
+// already included above
+// #include "rosidl_runtime_c/message_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "diagnostic_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+// already included above
+// #include "diagnostic_msgs/srv/detail/self_test__struct.h"
+// already included above
+// #include "fastcdr/Cdr.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_diagnostic_msgs__srv__SelfTest_Event(
+  const diagnostic_msgs__srv__SelfTest_Event * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_deserialize_diagnostic_msgs__srv__SelfTest_Event(
+  eprosima::fastcdr::Cdr &,
+  diagnostic_msgs__srv__SelfTest_Event * ros_message);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_diagnostic_msgs__srv__SelfTest_Event(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_diagnostic_msgs__srv__SelfTest_Event(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+bool cdr_serialize_key_diagnostic_msgs__srv__SelfTest_Event(
+  const diagnostic_msgs__srv__SelfTest_Event * ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t get_serialized_size_key_diagnostic_msgs__srv__SelfTest_Event(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+size_t max_serialized_size_key_diagnostic_msgs__srv__SelfTest_Event(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, diagnostic_msgs, srv, SelfTest_Event)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "rosidl_runtime_c/service_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "diagnostic_msgs/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_diagnostic_msgs
+const rosidl_service_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, diagnostic_msgs, srv, SelfTest)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // DIAGNOSTIC_MSGS__SRV__DETAIL__SELF_TEST__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
