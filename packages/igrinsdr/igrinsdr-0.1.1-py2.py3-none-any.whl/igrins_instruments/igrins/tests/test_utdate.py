1758fb7cd4e174563daf773940a5aecaf4682365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 05:08:29 +0000

    Add package igrinsdr/0.1.1/igrinsdr-0.1.1-py2.py3-none-any.whl

---
+++ packages/igrinsdr/igrinsdr-0.1.1-py2.py3-none-any.whl/igrins_instruments/igrins/tests/test_utdate.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from astropy.io import fits
+import datetime
+
+import astrodata
+import igrins_instruments
+
+def test_utdatetime():
+    datadir = Path("sample_sky")
+    fn = datadir / "N20240429S0204_H.fits"
+    ad = astrodata.open(fn)
+    dt = datetime.datetime.fromisoformat(ad[0].hdr["UTSTART"])
+    assert ad[0].ut_datetime() == dt
+
+    hdr = ad[0].hdr
+    hdr["UTDATETI"] = hdr["UTSTART"]
+    del hdr["UTSTART"]
+    assert ad[0].ut_datetime() == dt
+
+    assert ad.ut_datetime() == dt
