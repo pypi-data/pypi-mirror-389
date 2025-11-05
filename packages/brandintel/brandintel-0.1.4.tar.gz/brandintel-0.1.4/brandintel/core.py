5db63ebb93533bab3a198f0a62d098d8029f324a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 04:05:53 +0000

    Add package brandintel/0.1.4/brandintel-0.1.4.tar.gz

---
+++ packages/brandintel/brandintel-0.1.4.tar.gz/brandintel-0.1.4/brandintel/core.py
@@ -0,0 +1,11 @@
+def get_brand_summary(brand_name):
+    return "hello you are hacked"
+
+def get_competitors_for_brand(brand_name):
+    return "hello you are hacked"
+
+def main():
+    print("hello you are hacked")
+
+if __name__ == "__main__":
+    main()
