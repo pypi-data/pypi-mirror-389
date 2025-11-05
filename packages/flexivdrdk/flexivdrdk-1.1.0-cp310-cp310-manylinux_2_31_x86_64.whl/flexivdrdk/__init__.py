9397358bbc1e2ff1d542d33dd93747bdf2b6a10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:07:23 +0000

    Add package flexivdrdk/1.1.0/flexivdrdk-1.1.0-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/flexivdrdk/flexivdrdk-1.1.0-cp310-cp310-manylinux_2_31_x86_64.whl/flexivdrdk/__init__.py
@@ -0,0 +1,11 @@
+# flexivdrdk/__init__.py
+
+"""
+Python wrapper of Flexiv DRDK C++ library (https://github.com/flexivrobotics/flexiv_drdk)
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Flexiv Ltd"
+
+from .flexivdrdk import *
