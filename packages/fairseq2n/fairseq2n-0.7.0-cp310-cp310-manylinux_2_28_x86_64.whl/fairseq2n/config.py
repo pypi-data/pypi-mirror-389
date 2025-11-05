f025221bf7a1069f1e6612d64f0552abddccc3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 08:06:46 +0000

    Add package fairseq2n/0.7.0/fairseq2n-0.7.0-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/fairseq2n/fairseq2n-0.7.0-cp310-cp310-manylinux_2_28_x86_64.whl/fairseq2n/config.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from typing import Final
+
+_TORCH_VERSION: Final = "2.9.0+cu128"
+_TORCH_VARIANT: Final = "CUDA 12.8"
+
+_SUPPORTS_IMAGE: Final = True
+
+_SUPPORTS_CUDA: Final = True
+_CUDA_VERSION: Final = (12, 8)
