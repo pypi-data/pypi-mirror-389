f59b222fa86ee55e3b6fde0cc272d17c7a81dfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 14:06:48 +0000

    Add package tyro/0.10.0a4/tyro-0.10.0a4.tar.gz

---
+++ packages/tyro/tyro-0.10.0a4.tar.gz/tyro-0.10.0a4/src/tyro/__init__.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+__version__ = "0.10.0a4"
+
+
+from . import conf as conf
+from . import constructors as constructors
+from . import extras as extras
+from ._cli import cli as cli
+from ._settings import _experimental_options as _experimental_options
+from ._singleton import MISSING as MISSING
+
+# Deprecated interface.
+if not TYPE_CHECKING:
+    from ._deprecated import *  # noqa
+    from .constructors._primitive_spec import (
+        UnsupportedTypeAnnotationError as UnsupportedTypeAnnotationError,
+    )
