2f7e64c4e2e9413e1988700b919e28acbc33ed9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-05 07:07:21 +0000

    Add package llnl-hatchet/2025.1.1/llnl_hatchet-2025.1.1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/llnl-hatchet/llnl_hatchet-2025.1.1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/hatchet/version.py
@@ -0,0 +1,7 @@
+# Copyright 2017-2023 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+__version_info__ = ("2025", "1", "1")
+__version__ = ".".join(__version_info__)
