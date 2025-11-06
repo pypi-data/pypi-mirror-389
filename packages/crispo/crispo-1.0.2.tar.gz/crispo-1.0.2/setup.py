5772350e2a55af5d485cec794bbc08258e09bdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 09:06:30 +0000

    Add package crispo/1.0.2/crispo-1.0.2.tar.gz

---
+++ packages/crispo/crispo-1.0.2.tar.gz/crispo-1.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crispo",
+    version="1.0.2",
+    author="LOOFYYLO",
+    author_email="crispo.contact@gmail.com",
+    description="Automated co-design of ML predictors and learning-augmented algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LOOFYYLO/crispo",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "statsmodels>=0.13.0",
+        "pandas>=1.3.0",
+        "scikit-learn>=1.0.0",
+        "numpy>=1.21.0",
+    ],
+    extras_require={
+        "dev": ["pytest>=7.0", "pytest-cov>=3.0", "black>=22.0", "flake8>=4.0"],
+    },
+    entry_points={
+        "console_scripts": [
+            "crispo=crispo.crispo:main",
+        ],
+    },
+)
