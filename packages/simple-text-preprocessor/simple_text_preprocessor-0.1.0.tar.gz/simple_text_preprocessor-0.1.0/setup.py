ef7b96a428f128903b04972bec1dff98e7af6189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 05:06:34 +0000

    Add package simple-text-preprocessor/0.1.0/simple_text_preprocessor-0.1.0.tar.gz

---
+++ packages/simple-text-preprocessor/simple_text_preprocessor-0.1.0.tar.gz/simple_text_preprocessor-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='simple-text-preprocessor',
+    version='0.1.0',
+    author='Rohan Mudrale',
+    author_email='rohan.mudrale60@nmims.in',
+    description='A single-function utility for flexible NLP text cleaning.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    entry_points={
+        "console_scripts": [
+            "nlp = clean_text.main:clean_text"
+        ]
+    },  # <-- Make sure this comma is present
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'nltk',
+    ],
+)
