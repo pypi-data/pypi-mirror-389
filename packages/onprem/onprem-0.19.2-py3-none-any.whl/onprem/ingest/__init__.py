2c13910f20f7e1f3dc0f278b7a729b01ad742f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 07:08:09 +0000

    Add package onprem/0.19.2/onprem-0.19.2-py3-none-any.whl

---
+++ packages/onprem/onprem-0.19.2-py3-none-any.whl/onprem/ingest/__init__.py
@@ -0,0 +1,4 @@
+from .base import load_single_document, load_documents, load_web_document, load_spreadsheet_documents
+from .base import process_folder, chunk_documents
+from .helpers import extract_files
+from .stores import VectorStoreFactory
