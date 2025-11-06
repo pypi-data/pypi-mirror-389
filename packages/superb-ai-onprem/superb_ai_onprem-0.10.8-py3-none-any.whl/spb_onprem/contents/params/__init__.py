23f58445ee09ff0a206b2273f99f3efe1f43f0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:41 +0000

    Add package superb-ai-onprem/0.10.8/superb_ai_onprem-0.10.8-py3-none-any.whl

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.10.8-py3-none-any.whl/spb_onprem/contents/params/__init__.py
@@ -0,0 +1,12 @@
+
+from .create import create_variables
+from .get_download_url import get_download_url_params
+from .delete_content import delete_content_params
+from .get_upload_url import get_upload_url_params
+
+__all__ = (
+    "create_variables",
+    "get_download_url_params",
+    "delete_content_params",
+    "get_upload_url_params"
+)
