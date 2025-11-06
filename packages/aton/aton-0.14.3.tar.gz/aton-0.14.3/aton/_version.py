02db86074f46330cce670d0bdcb9092d95d556c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:07:50 +0000

    Add package aton/0.14.3/aton-0.14.3.tar.gz

---
+++ packages/aton/aton-0.14.3.tar.gz/aton-0.14.3/aton/_version.py
@@ -0,0 +1,14 @@
+"""
+# Description
+
+Package version is defined here. Follows semantic versioning, as in:
+
+`vMAJOR.MINOR.PATCH.`
+
+More about semantic versioning:
+https://semver.org/
+
+"""
+
+__version__ = 'v0.14.3'
+
