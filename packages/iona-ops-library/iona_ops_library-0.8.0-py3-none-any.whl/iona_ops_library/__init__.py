f666d12a8cf2d8587a5d3d2e237c7e752d5f1814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 17:09:04 +0000

    Add package iona-ops-library/0.8.0/iona_ops_library-0.8.0-py3-none-any.whl

---
+++ packages/iona-ops-library/iona_ops_library-0.8.0-py3-none-any.whl/iona_ops_library/__init__.py
@@ -0,0 +1,5 @@
+from .utils_module import add_numbers, multiply_numbers
+from .data_merger import DeltaMergeLogger
+from ._version import __version__
+
+__all__ = ["add_numbers", "multiply_numbers", "DeltaMergeLogger", "__version__"]
