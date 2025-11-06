0dcc763025d36e12d9198b6556f58465548bbee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 05:07:59 +0000

    Add package ordeq_polars/1.1.0/ordeq_polars-1.1.0.tar.gz

---
+++ packages/ordeq_polars/ordeq_polars-1.1.0.tar.gz/ordeq_polars-1.1.0/src/ordeq_polars/eager/iceberg.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Literal
+
+import polars as pl
+from ordeq import Output
+from pyiceberg.table import Table
+
+
+@dataclass(frozen=True, kw_only=True)
+class PolarsEagerIceberg(Output[pl.DataFrame]):
+    """IO for saving Iceberg tables eagerly using Polars.
+
+    Example:
+
+    ```pycon
+    >>> from ordeq_polars import PolarsEagerIceberg
+    >>> iceberg = PolarsEagerIceberg(
+    ...     path="file:/path/to/iceberg-table/metadata.json",
+    ... )
+
+    ```
+
+    """
+
+    path: str | Table
+
+    def save(
+        self, df: pl.DataFrame, mode: Literal["append", "overwrite"] = "append"
+    ) -> None:
+        """Write a DataFrame to an Iceberg table.
+
+        Args:
+            df: The DataFrame to write
+            mode: The write mode ("append" or "overwrite")
+        """
+
+        df.write_iceberg(target=self.path, mode=mode)
