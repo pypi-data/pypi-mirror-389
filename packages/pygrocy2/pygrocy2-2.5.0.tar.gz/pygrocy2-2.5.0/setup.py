8d42ffef2752a8f6ba2f3844a64734679475d38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 17:08:46 +0000

    Add package pygrocy2/2.5.0/pygrocy2-2.5.0.tar.gz

---
+++ packages/pygrocy2/pygrocy2-2.5.0.tar.gz/pygrocy2-2.5.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygrocy2",
+    version="2.5.0",
+    author="Flipper",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flipper/pygrocy2",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "deprecation~=2.1.0",
+        "pydantic~=2.12.2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
