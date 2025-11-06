5b5109cabb545cd4bf89cdb9373ef853cf62074c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:08:31 +0000

    Add package nnetsauce/0.44.1/nnetsauce-0.44.1.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.44.1.tar.gz/nnetsauce-0.44.1/nnetsauce/mts/__init__.py
@@ -0,0 +1,5 @@
+from .mts import MTS
+from .mlarch import MLARCH
+from .classical import ClassicalMTS
+
+__all__ = ["MTS", "MLARCH", "ClassicalMTS"]
---
+++ packages/nnetsauce/nnetsauce-0.44.1.tar.gz/nnetsauce-0.44.1/nnetsauce/ridge2/__init__.py
@@ -0,0 +1,9 @@
+from .ridge2Classifier import Ridge2Classifier
+from .ridge2Regressor import Ridge2Regressor
+from .ridge2MultitaskClassifier import Ridge2MultitaskClassifier
+
+__all__ = [
+    "Ridge2Classifier",
+    "Ridge2Regressor",
+    "Ridge2MultitaskClassifier",
+]
