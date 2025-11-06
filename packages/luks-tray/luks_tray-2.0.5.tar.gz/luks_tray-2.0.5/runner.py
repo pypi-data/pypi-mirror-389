3a69d5ac0328a1f591cf28bee454bd9e76f2d891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:07:04 +0000

    Add package luks-tray/2.0.5/luks_tray-2.0.5.tar.gz

---
+++ packages/luks-tray/luks_tray-2.0.5.tar.gz/luks_tray-2.0.5/runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# runner.py (at project root) -- used for debugging mostly ... can be used
+# to run the project under development
+
+import sys
+from luks_tray.main import main
+
+if __name__ == "__main__":
+    # Call the main entry point defined inside your package
+    sys.exit(main())
