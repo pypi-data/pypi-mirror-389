dae0dcf814cd1c78c21bf11d760a6854ac8bdc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:43 +0000

    Add package hat-drivers/0.9.32/hat_drivers-0.9.32-cp310.cp311.cp312.cp313-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/hat-drivers/hat_drivers-0.9.32-cp310.cp311.cp312.cp313-abi3-musllinux_1_2_x86_64.whl/hat/drivers/serial/common.py
@@ -0,0 +1,96 @@
+import abc
+import enum
+import logging
+import typing
+
+from hat import aio
+from hat import util
+
+
+class ByteSize(enum.Enum):
+    FIVEBITS = 5
+    SIXBITS = 6
+    SEVENBITS = 7
+    EIGHTBITS = 8
+
+
+class Parity(enum.Enum):
+    NONE = 'N'
+    EVEN = 'E'
+    ODD = 'O'
+    MARK = 'M'
+    SPACE = 'S'
+
+
+class StopBits(enum.Enum):
+    ONE = 1
+    ONE_POINT_FIVE = 1.5
+    TWO = 2
+
+
+class EndpointInfo(typing.NamedTuple):
+    name: str | None
+    port: str
+
+
+class Endpoint(aio.Resource):
+    """Serial endpoint"""
+
+    @property
+    @abc.abstractmethod
+    def info(self) -> EndpointInfo:
+        """Endpoint info"""
+
+    @abc.abstractmethod
+    async def read(self, size: int) -> util.Bytes:
+        """Read
+
+        Args:
+            size: number of bytes to read
+
+        Raises:
+            ConnectionError
+
+        """
+
+    @abc.abstractmethod
+    async def write(self, data: util.Bytes):
+        """Write
+
+        Raises:
+            ConnectionError
+
+        """
+
+    @abc.abstractmethod
+    async def drain(self):
+        """Drain output buffer
+
+        Raises:
+            ConnectionError
+
+        """
+
+    @abc.abstractmethod
+    async def clear_input_buffer(self) -> int:
+        """Reset input buffer
+
+        Returns number of bytes available in buffer immediately before
+        buffer was cleared.
+
+        Raises:
+            ConnectionError
+
+        """
+
+
+def create_logger_adapter(logger: logging.Logger,
+                          communication: bool,
+                          info: EndpointInfo
+                          ) -> logging.LoggerAdapter:
+    extra = {'meta': {'type': 'SerialEndpoint',
+                      'communication': communication,
+                      'name': info.name,
+                      'port': info.port}}
+
+    return logging.LoggerAdapter(logger, extra)
