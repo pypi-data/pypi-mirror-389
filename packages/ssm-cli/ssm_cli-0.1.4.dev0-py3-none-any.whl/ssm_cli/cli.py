a4e04119e511dca1f654c86b6d685bacf90b5563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 05:06:53 +0000

    Add package ssm-cli/0.1.4.dev0/ssm_cli-0.1.4.dev0-py3-none-any.whl

---
+++ packages/ssm-cli/ssm_cli-0.1.4.dev0-py3-none-any.whl/ssm_cli/cli.py
@@ -0,0 +1,105 @@
+import sys
+
+from rich_argparse import ArgumentDefaultsRichHelpFormatter
+
+import confclasses
+from ssm_cli.config import CONFIG
+from ssm_cli.xdg import get_log_file, get_conf_file
+from ssm_cli.commands import COMMANDS
+from ssm_cli.cli_args import CliArgumentParser, ARGS
+from ssm_cli.aws import AWSAuthError, AWSAccessDeniedError
+from ssm_cli.console import console
+from rich.markup import escape
+
+# Setup logging
+import logging
+logging.basicConfig(
+    level=logging.WARNING,
+    filename=get_log_file(),
+    filemode='+wt',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
+def cli(argv: list = None) -> int:
+    if argv is None:
+        argv = sys.argv[1:]
+
+    # Manually set the log level now, so we get accurate logging during argument parsing
+    for i, arg in enumerate(argv):
+        if arg == '--log-level':
+            logging.getLogger().setLevel(argv[i+1].upper())
+        if arg.startswith('--log-level='):
+            logging.getLogger().setLevel(arg.split('=')[1].upper())
+
+    logger.debug(f"CLI called with {argv}")
+
+    # Build the actual parser
+    parser = CliArgumentParser(
+        prog="ssm",
+        description="tool to manage AWS SSM",
+        formatter_class=ArgumentDefaultsRichHelpFormatter,
+    )
+    parser.add_global_argument("--profile", type=str, help="Which AWS profile to use")
+
+    for name, command in COMMANDS.items():
+        command_parser = parser.add_command_parser(name, command.HELP)
+        command.add_arguments(command_parser)
+
+    parser.parse_args(argv)
+
+    logger.debug(f"Arguments: {ARGS}")
+
+    if not ARGS.command:
+        parser.print_help()
+        return 1
+    
+    # Setup is a special case, we cannot load config if we dont have any.
+    if ARGS.command == "setup":
+        return _run()
+    
+    try:
+        with open(get_conf_file(), 'r') as file:
+            confclasses.load(CONFIG, file)
+            ARGS.update_config()
+            logger.debug(f"Config: {CONFIG}")
+    except EnvironmentError as e:
+        console.print(f"Invalid config: {e}", style="red")
+        return 1
+    
+    logging.getLogger().setLevel(CONFIG.log.level.upper())
+
+    
+    for logger_name, level in CONFIG.log.loggers.items():
+        logger.debug(f"setting logger {logger_name} to {level}")
+        logging.getLogger(logger_name).setLevel(level.upper())
+
+
+    if ARGS.command not in COMMANDS:
+        console.print(f"failed to find action {ARGS.action}", style="red")
+        return 1
+    
+    return _run()
+
+def _run():
+    """
+    Run a command, better exceptions and logging
+    """
+    try:
+        COMMANDS[ARGS.command].run()
+        return 0
+    except AWSAuthError as e:
+        console.print(f"AWS Authentication error: {e}", style="red")
+        return 1
+    except AWSAccessDeniedError as e:
+            logger.error(f"access denied: {e}")
+            console.print(f"Access denied, see README for details on required permissions", style="bold red")
+            console.print(escape(str(e.__cause__)), style="grey50")
+    except Exception as e:
+        logger.error(f"Unhandled exception in {ARGS.command}")
+        log_path = str(get_log_file())
+        console.print(f"Unhandled exception, check [link=file://{log_path}]{log_path}[/link] for more information", style="red")
+        console.print(f"Error: {e}", style="red bold")
+        logger.exception(e, stack_info=True, stacklevel=20)
+        return 1
