26357d69f481ca6843b50b8c41f90b69be279a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:06:21 +0000

    Add package SignerPy/0.11.3/signerpy-0.11.3.tar.gz

---
+++ packages/SignerPy/signerpy-0.11.3.tar.gz/signerpy-0.11.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="SignerPy",
+    version="0.11.3",
+    author="L7N",
+    author_email="l7ng4q@gmail.com",
+    url="https://github.com/is-L7N/SignerPy/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "user_agent",
+        "requests",
+        "pycryptodome"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
