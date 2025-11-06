25a93722eebb3a9048532ec3cb83b7047e1e9ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:28 +0000

    Add package chaotic-ngine/0.15.2/chaotic_ngine-0.15.2-py3-none-any.whl

---
+++ packages/chaotic-ngine/chaotic_ngine-0.15.2-py3-none-any.whl/chaotic/__init__.py
@@ -0,0 +1,29 @@
+from chaotic.cloud import Chaotic
+from chaotic.cloud.cloudscale_ch import CloudscaleChChaotic
+from chaotic.cloud.cloudstack import CloudStackChaotic
+from chaotic.cloud.digitalocean import DigitaloceanChaotic
+from chaotic.cloud.hcloud import HcloudChaotic
+from chaotic.cloud.nomad import NomadChaotic
+from chaotic.cloud.proxmox_kvm import ProxmoxKvmChaotic
+from chaotic.cloud.vultr import VultrChaotic
+from chaotic.log import log
+
+
+class ChaoticFactory:
+
+    CLOUD_CLASSES: dict = {
+        "cloudscale_ch": CloudscaleChChaotic,
+        "cloudstack": CloudStackChaotic,
+        "digitalocean": DigitaloceanChaotic,
+        "hcloud": HcloudChaotic,
+        "nomad": NomadChaotic,
+        "proxmox_kvm": ProxmoxKvmChaotic,
+        "vultr": VultrChaotic,
+    }
+
+    def get_instance(self, name: str) -> Chaotic:
+        log.info(f"Instantiate {name}")
+        try:
+            return self.CLOUD_CLASSES[name]()
+        except KeyError as e:
+            raise NotImplementedError(f"{e} not implemented")
