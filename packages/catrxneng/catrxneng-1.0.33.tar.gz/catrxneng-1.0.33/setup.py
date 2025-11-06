01828458ed7afb57ecb0c1b298fb8a5c610e9b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 11:07:26 +0000

    Add package catrxneng/1.0.33/catrxneng-1.0.33.tar.gz

---
+++ packages/catrxneng/catrxneng-1.0.33.tar.gz/catrxneng-1.0.33/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="catrxneng",
+    version='1.0.33',
+    packages=find_packages(),
+    install_requires=[],
+    author="Gordon Brezicki",
+    description="A collection of Python utilities for catalysis and reaction engineering",
+    url="https://github.com/gpbrez/catrxneng",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
