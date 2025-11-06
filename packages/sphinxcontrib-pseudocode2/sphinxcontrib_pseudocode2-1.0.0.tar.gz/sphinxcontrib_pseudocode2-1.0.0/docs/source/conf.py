1d9ed55b5c5140a2d6cb4bf397c5c78471b33acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:40 +0000

    Add package sphinxcontrib-pseudocode2/1.0.0/sphinxcontrib_pseudocode2-1.0.0.tar.gz

---
+++ packages/sphinxcontrib-pseudocode2/sphinxcontrib_pseudocode2-1.0.0.tar.gz/sphinxcontrib_pseudocode2-1.0.0/docs/source/conf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# -- General configuration ------------------------------------------------
+
+extensions = [
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosummary",
+    "sphinxcontrib.pseudocode2",
+]
+
+templates_path = ["_templates"]
+source_suffix = {".rst": "restructuredtext"}
+master_doc = "index"
+
+project = "sphinxcontrib-pseudocode2"
+copyright = "2021, Zeyuan Hu; 2025, WEN Hao"
+author = "Zeyuan Hu; WEN Hao"
+
+version = ""
+release = ""
+
+language = "en"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+pygments_style = "sphinx"
+todo_include_todos = False
+
+# -- HTML -----------------------------------------------------------------
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+html_show_sourcelink = True
+htmlhelp_basename = "sphinxcontrib-pseudocode2-doc"
+
+numfig = True
+
+pseudocode2_math_engine = "katex"
+
+pseudocode2_options = {
+    "lineNumber": True,  # Global default: enable line numbering
+    "lineNumberPunc": " | ",  # Punctuation after line numbers (e.g., "1 | ")
+    "commentDelimiter": "//",  # Global default comment delimiter
+    "noEnd": False,  # Global default: show "END" for control blocks
+    "titlePrefix": "PseudoCode",  # Global default title prefix (replace "Algorithm")
+    "captionCount": True,  # Global default: enable auto-increment numbering
+}
