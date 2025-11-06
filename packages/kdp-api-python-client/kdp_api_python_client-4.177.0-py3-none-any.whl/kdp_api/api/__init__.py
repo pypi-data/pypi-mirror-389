fef6a87b5e9eecbd695ccfd019a15f9016e65c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 09:06:33 +0000

    Add package kdp-api-python-client/4.177.0/kdp_api_python_client-4.177.0-py3-none-any.whl

---
+++ packages/kdp-api-python-client/kdp_api_python_client-4.177.0-py3-none-any.whl/kdp_api/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from kdp_api.api.applications_api import ApplicationsApi
+from kdp_api.api.attribute_based_access_control_api import AttributeBasedAccessControlApi
+from kdp_api.api.authenticate_api import AuthenticateApi
+from kdp_api.api.dataset_pairing_api import DatasetPairingApi
+from kdp_api.api.dataset_permissions_api import DatasetPermissionsApi
+from kdp_api.api.datasets_api import DatasetsApi
+from kdp_api.api.indexing_api import IndexingApi
+from kdp_api.api.manage_records_api import ManageRecordsApi
+from kdp_api.api.read_and_query_api import ReadAndQueryApi
+from kdp_api.api.users_and_groups_api import UsersAndGroupsApi
+from kdp_api.api.workspaces_api import WorkspacesApi
+from kdp_api.api.write_api import WriteApi
+
