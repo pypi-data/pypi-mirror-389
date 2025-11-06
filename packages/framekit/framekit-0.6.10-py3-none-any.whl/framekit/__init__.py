46bc7585c7f57abdb2999e383b45e70b5ef87c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:05:54 +0000

    Add package framekit/0.6.10/framekit-0.6.10-py3-none-any.whl

---
+++ packages/framekit/framekit-0.6.10-py3-none-any.whl/framekit/__init__.py
@@ -0,0 +1,7 @@
+from .master_scene_element import MasterScene
+from .scene_element import Scene
+from .text_element import TextElement
+from .audio_element import AudioElement
+from .image_element import ImageElement
+
+__all__ = ['MasterScene', 'Scene', 'TextElement', 'AudioElement', 'ImageElement']
