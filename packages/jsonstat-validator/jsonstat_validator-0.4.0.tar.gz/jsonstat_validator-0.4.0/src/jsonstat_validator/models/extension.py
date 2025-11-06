2f25f7f0f34630681fb8dc8d9836100e86d7120c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:06:14 +0000

    Add package jsonstat-validator/0.4.0/jsonstat_validator-0.4.0.tar.gz

---
+++ packages/jsonstat-validator/jsonstat_validator-0.4.0.tar.gz/jsonstat_validator-0.4.0/src/jsonstat_validator/models/extension.py
@@ -0,0 +1,12 @@
+from jsonstat_validator.models.base import JSONStatBaseModel
+
+
+class Extension(JSONStatBaseModel):
+    """Extension allows JSON-stat to be extended for particular needs.
+
+    Providers are free to define where they include this property and
+    what children are allowed in each case.
+    """
+
+    # Free-form dictionary of any properties.
+    model_config = {"extra": "allow"}
