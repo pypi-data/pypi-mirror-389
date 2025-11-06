bff6a32945bbe4d5908e0d12e9c455a875d85716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:07:15 +0000

    Add package FABulous-bit-gen/0.0.1b2/fabulous_bit_gen-0.0.1b2-py3-none-any.whl

---
+++ packages/FABulous-bit-gen/fabulous_bit_gen-0.0.1b2-py3-none-any.whl/fabulous_bit_gen/custom_exception.py
@@ -0,0 +1,8 @@
+"""Custom exception classes for the FABulous bit_gen module.
+
+This module defines custom exceptions used during bitstream generation.
+"""
+
+
+class SpecMissMatch(Exception):
+    """Exception raised when a FASM feature is not found in the bitstream specification."""
