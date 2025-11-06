2cfe52c2dbccfdb5ed033c31b77afd985b3c82c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:12:09 +0000

    Add package sourcepp/2025.11.5/sourcepp-2025.11.5-cp314-cp314t-manylinux_2_34_x86_64.whl

---
+++ packages/sourcepp/sourcepp-2025.11.5-cp314-cp314t-manylinux_2_34_x86_64.whl/sourcepp/_sourcepp_impl/__init__.pyi
@@ -0,0 +1,10 @@
+from . import (
+    bsppp as bsppp,
+    gamepp as gamepp,
+    sourcepp as sourcepp,
+    steampp as steampp,
+    toolpp as toolpp,
+    vcryptpp as vcryptpp,
+    vpkpp as vpkpp,
+    vtfpp as vtfpp
+)
