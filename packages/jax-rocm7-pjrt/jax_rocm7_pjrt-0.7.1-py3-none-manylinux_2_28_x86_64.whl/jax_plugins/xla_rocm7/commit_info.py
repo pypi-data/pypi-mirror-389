c2b5b5e69e62fba8801ac95bac7a106efc155bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 06:06:22 +0000

    Add package jax-rocm7-pjrt/0.7.1/jax_rocm7_pjrt-0.7.1-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/jax-rocm7-pjrt/jax_rocm7_pjrt-0.7.1-py3-none-manylinux_2_28_x86_64.whl/jax_plugins/xla_rocm7/commit_info.py
@@ -0,0 +1,7 @@
+
+# auto-generated; do not edit
+
+# Commit information
+xla_commit = "4178517b7d7e5e3318ea4b701b01dc06847b73b0"
+jax_commit = "5712de44e97c455faed1fd45532e821ca66d025a"
+rocm_jax_commit = "7143a751e5df07a8436e728780c3a46d01a56e1f"
