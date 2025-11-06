f2a73b6772cf58d2eba2edf0f71e7ee88cddb2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:09:52 +0000

    Add package autofit/2025.11.5.1/autofit-2025.11.5.1.tar.gz

---
+++ packages/autofit/autofit-2025.11.5.1.tar.gz/autofit-2025.11.5.1/autofit/non_linear/analysis/model_analysis.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from autofit.mapper.prior_model.abstract import AbstractPriorModel
+from .analysis import Analysis
+from ... import SamplesSummary, AbstractPaths, SamplesPDF
+
+
+class ModelAnalysis(Analysis):
+    def __init__(self, analysis: Analysis, model: AbstractPriorModel):
+        """
+        Comprises a model and an analysis that can be applied to instances of that model.
+
+        Parameters
+        ----------
+        analysis
+        model
+        """
+        self.analysis = analysis
+        self.model = model
+
+    def __getattr__(self, item):
+        if item in ("__getstate__", "__setstate__"):
+            raise AttributeError(item)
+        return getattr(self.analysis, item)
+
+    def log_likelihood_function(self, instance):
+        return self.analysis.log_likelihood_function(instance)
+
+    def make_result(
+        self,
+        samples_summary: SamplesSummary,
+        paths: AbstractPaths,
+        samples: Optional[SamplesPDF] = None,
+        search_internal: Optional[object] = None,
+        analysis: Optional[object] = None,
+    ):
+        """
+        Return the correct type of result by calling the underlying analysis.
+        """
+        try:
+            return self.analysis.make_result(
+                samples_summary=samples_summary,
+                paths=paths,
+                samples=samples,
+                search_internal=search_internal,
+            )
+        except TypeError:
+            raise
