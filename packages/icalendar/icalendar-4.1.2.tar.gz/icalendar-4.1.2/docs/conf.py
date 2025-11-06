95dee3c6646e8719d93abd17520d2890a8c8df1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:23 +0000

    Add package icalendar/4.1.2/icalendar-4.1.2.tar.gz

---
+++ packages/icalendar/icalendar-4.1.2.tar.gz/icalendar-4.1.2/docs/conf.py
@@ -0,0 +1,90 @@
+# icalendar documentation build configuration file
+import datetime
+import sys
+from pathlib import Path
+
+HERE = Path(__file__).parent
+SRC = HERE.parent / "src"
+sys.path.insert(0, str(SRC))  # update docs from icalendar source for livehtml
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+source_suffix = {".rst": "restructuredtext"}
+master_doc = "index"
+
+project = "icalendar"
+this_year = datetime.date.today().year  # noqa: DTZ011
+copyright = f"{this_year}, Plone Foundation"  # noqa: A001
+release = version = "4.1.2"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+exclude_patterns = [
+    "reference/api/modules.rst",
+]
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/collective/icalendar",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css"
+            }
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/icalendar",
+            "icon": "fa-custom fa-pypi",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css"
+            }
+        },
+    ],
+    "logo": {
+        "text": "icalendar"
+    },
+    "navbar_start": ["navbar-logo", "version-switcher"],
+    "navigation_with_keys": True,
+    "search_bar_text": "Search",
+    "show_nav_level": 2,
+    "show_toc_level": 2,
+    "show_version_warning_banner": True,
+    "switcher": {
+        "json_url": "https://icalendar.readthedocs.io/en/latest/_static/version-switcher.json",
+        "version_match": version,
+    },
+    "use_edit_page_button": True,
+}
+html_context = {
+#     "github_url": "https://github.com", # or your GitHub Enterprise site
+    "github_user": "collective",
+    "github_repo": "icalendar",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+html_static_path = [
+    "_static",
+]
+html_js_files = [
+    ("js/custom-icons.js", {"defer": "defer"}),
+]
+pygments_style = "sphinx"
+
+man_pages = [("index", "icalendar", "icalendar Documentation", ["Plone Foundation"], 1)]
+
+htmlhelp_basename = "icalendardoc"
