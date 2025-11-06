18464d82524c33544ed2b2e7b5f9a34e7145029c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:08:34 +0000

    Add package elicitlabs/0.4.0/elicitlabs-0.4.0-py3-none-any.whl

---
+++ packages/elicitlabs/elicitlabs-0.4.0-py3-none-any.whl/elicitlabs/types/__init__.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .data_ingest_params import DataIngestParams as DataIngestParams
+from .modal_learn_params import ModalLearnParams as ModalLearnParams
+from .modal_query_params import ModalQueryParams as ModalQueryParams
+from .data_ingest_response import DataIngestResponse as DataIngestResponse
+from .modal_learn_response import ModalLearnResponse as ModalLearnResponse
+from .modal_query_response import ModalQueryResponse as ModalQueryResponse
+from .user_create_or_get_params import UserCreateOrGetParams as UserCreateOrGetParams
+from .user_create_or_get_response import UserCreateOrGetResponse as UserCreateOrGetResponse
