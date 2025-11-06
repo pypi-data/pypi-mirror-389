42053b55065ec4fe0d3c281af3c90b89066400a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 06:07:14 +0000

    Add package xls-aws-sso-cli/0.1.24/xls_aws_sso_cli-0.1.24.tar.gz

---
+++ packages/xls-aws-sso-cli/xls_aws_sso_cli-0.1.24.tar.gz/xls_aws_sso_cli-0.1.24/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="xls-aws-sso-cli",
+    version="0.1.24",
+    author="Fahrizal Shofyan Aziz",
+    author_email="fahrizalshofyanaziz@gmail.com",
+    description="AWS SSO Management CLI Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fahrizalvianaz/python-cli",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "typer>=0.9.0",
+        "rich>=13.7.0",
+        "inquirerpy>=0.3.4",
+    ],
+    entry_points={
+        "console_scripts": [
+            "xls-sso=xls_aws_sso_cli:main",
+        ],
+    },
+)
---
+++ packages/xls-aws-sso-cli/xls_aws_sso_cli-0.1.24.tar.gz/xls_aws_sso_cli-0.1.24/src/xls_aws_sso_cli/__init__.py
@@ -0,0 +1,8 @@
+"""XLS AWS SSO CLI - Manage AWS SSO"""
+
+
+from .cli import app, main
+
+__all__ = ["app", "main", "__version__"]
+__version__ = "0.1.23"
+__version__ = "0.1.24"
