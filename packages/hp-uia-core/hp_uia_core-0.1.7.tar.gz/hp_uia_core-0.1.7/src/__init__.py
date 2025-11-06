3d59904a1cadeab1196c11ed6d89875b661c3133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:54 +0000

    Add package hp-uia-core/0.1.7/hp_uia_core-0.1.7.tar.gz

---
+++ packages/hp-uia-core/hp_uia_core-0.1.7.tar.gz/hp_uia_core-0.1.7/src/__init__.py
@@ -0,0 +1,12 @@
+"""
+HP UIA Core Library
+-------------------
+Core abstraction layer for HP internal UI automation.
+"""
+
+__version__ = "0.1.7"
+__author__ = "HP PC Validation Team"
+
+from .app_manager import AppManager
+
+__all__ = ["AppManager"]
