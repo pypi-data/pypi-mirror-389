170ff07f60cf66f59c66012e6f6ece4794b53787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 06:07:12 +0000

    Add package xls-aws-sso-cli/0.1.23/xls_aws_sso_cli-0.1.23-py3-none-any.whl

---
+++ packages/xls-aws-sso-cli/xls_aws_sso_cli-0.1.23-py3-none-any.whl/xls_aws_sso_cli/__init__.py
@@ -0,0 +1,9 @@
+"""XLS AWS SSO CLI - Manage AWS SSO"""
+
+__version__ = "0.1.22"
+
+from .cli import app, main
+
+__all__ = ["app", "main", "__version__"]__version__ = "0.1.21"
+__version__ = "0.1.22"
+__version__ = "0.1.23"
