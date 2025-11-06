9d6c5787d8a6cdf263cfff2e20445aaf8f1626f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 05:09:28 +0000

    Add package pyect/0.1.0/pyect-0.1.0.tar.gz

---
+++ packages/pyect/pyect-0.1.0.tar.gz/pyect-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyect',
+    version='0.1.0',
+    author='Alex McCleary, Eli Quist, Jack Ruder, Jacob Sriraman',
+    author_email='eli.quist@student.montana.edu',
+    description='Generalized computation of the WECT using PyTorch.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/compTAG/pyECT',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'Pillow',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
