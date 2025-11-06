185c4c4805c4995e602f3a53d702bfcd00a05e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:06:20 +0000

    Add package sclogging/1.1.0/sclogging-1.1.0.tar.gz

---
+++ packages/sclogging/sclogging-1.1.0.tar.gz/sclogging-1.1.0/SECURITY.md
@@ -0,0 +1,14 @@
+# Security Policy
+
+## Supported Versions
+
+| Version  | Supported          |
+|----------| ------------------ |
+| 1.1.0    | :white_check_mark: |
+| <= 1.0.x | :x:                |
+
+
+## Reporting a Vulnerability
+
+Shoot me a message.
+I check my messages several times a day.
---
+++ packages/sclogging/sclogging-1.1.0.tar.gz/sclogging-1.1.0/docs/source/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+import sphinx_rtd_theme
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath("../"))
+
+autodoc_mock_imports = ["coloredlogs", "pyinputplus"]
+
+project = "SCLogging"
+copyright = "2025, sshimek42"
+author = "sshimek42"
+release = "1.0.6"
+version = "1.0.6"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+autodoc_typehints = "both"
+autodoc_typehints_format = "fully-qualified"
+
+napoleon_use_param = True
+napoleon_attr_annotations = True
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+autosummary_generate = True
