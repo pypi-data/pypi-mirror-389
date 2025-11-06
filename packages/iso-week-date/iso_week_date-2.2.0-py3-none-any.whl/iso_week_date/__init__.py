b08664dd5d8e1d9bb07691b0965e81d1abf14c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:08:15 +0000

    Add package iso-week-date/2.2.0/iso_week_date-2.2.0-py3-none-any.whl

---
+++ packages/iso-week-date/iso_week_date-2.2.0-py3-none-any.whl/iso_week_date/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from iso_week_date._patterns import ISOWEEK_PATTERN, ISOWEEKDATE_PATTERN
+from iso_week_date.isoweek import IsoWeek
+from iso_week_date.isoweekdate import IsoWeekDate
+
+__title__ = __name__
+__version__: str
+
+__all__ = (
+    "ISOWEEKDATE_PATTERN",
+    "ISOWEEK_PATTERN",
+    "IsoWeek",
+    "IsoWeekDate",
+)
+
+
+def __getattr__(name: Literal["__version__"]) -> str:  # type: ignore[misc]
+    if name == "__version__":
+        global __version__  # noqa: PLW0603
+
+        from importlib.metadata import version  # noqa: PLC0415
+
+        __version__ = version(__name__)
+        return __version__
+    else:
+        msg = f"module {__name__!r} has no attribute {name!r}"
+        raise AttributeError(msg)
