240f33e49455d1eddcc6c4592280296f91fdf187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:48 +0000

    Add package qBitrr2/5.4.5/qbitrr2-5.4.5.tar.gz

---
+++ packages/qBitrr2/qbitrr2-5.4.5.tar.gz/qbitrr2-5.4.5/qBitrr/bundled_data.py
@@ -0,0 +1,7 @@
+version = "5.4.5"
+git_hash = "155d9043"
+license_text = (
+    "Licence can be found on:\n\nhttps://github.com/Feramance/qBitrr/blob/master/LICENSE"
+)
+patched_version = f"{version}-{git_hash}"
+tagged_version = f"{version}"
