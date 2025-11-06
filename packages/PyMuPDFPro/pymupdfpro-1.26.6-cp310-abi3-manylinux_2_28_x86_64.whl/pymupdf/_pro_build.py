7c65b051ae1680bc689a0546cae414ef2b7db51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:10:06 +0000

    Add package PyMuPDFPro/1.26.6/pymupdfpro-1.26.6-cp310-abi3-manylinux_2_28_x86_64.whl

---
+++ packages/PyMuPDFPro/pymupdfpro-1.26.6-cp310-abi3-manylinux_2_28_x86_64.whl/pymupdf/_pro_build.py
@@ -0,0 +1,8 @@
+
+version = '1.26.6'
+version_tuple = (1, 26, 6)
+pymupdf_version = '1.26.6'
+pymupdf_version_tuple = (1, 26, 6)
+mupdf_version = '1.26.11'
+mupdf_version_tuple = (1, 26, 11)
+platform_python_implementation = 'CPython'
