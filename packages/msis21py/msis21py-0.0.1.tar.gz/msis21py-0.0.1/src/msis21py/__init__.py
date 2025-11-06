1372ecc90368745fbccf23c5e84bffbef572395c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:12:07 +0000

    Add package msis21py/0.0.1/msis21py-0.0.1.tar.gz

---
+++ packages/msis21py/msis21py-0.0.1.tar.gz/msis21py-0.0.1/src/msis21py/__init__.py
@@ -0,0 +1,9 @@
+from .base import NrlMsis21, __version__
+from .settings import Settings
+from .utils import alt_grid
+
+__all__ = [
+    "NrlMsis21", "Settings",
+    "alt_grid",
+    "__version__"
+]
