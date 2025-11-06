e558f1bb4c640da016565187cc9f153da9d410cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:25 +0000

    Add package MeUtils/2025.11.6.15.48.15/meutils-2025.11.6.15.48.15-py3-none-any.whl

---
+++ packages/MeUtils/meutils-2025.11.6.15.48.15-py3-none-any.whl/meutils/apis/aiml/videos.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : videos
+# @Time         : 2025/11/5 18:01
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
