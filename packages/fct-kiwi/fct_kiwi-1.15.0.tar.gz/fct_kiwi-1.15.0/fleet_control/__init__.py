3d2280f78427d170f8a4dfe24521b8e191196649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:05:55 +0000

    Add package fct-kiwi/1.15.0/fct_kiwi-1.15.0.tar.gz

---
+++ packages/fct-kiwi/fct_kiwi-1.15.0.tar.gz/fct_kiwi-1.15.0/fleet_control/__init__.py
@@ -0,0 +1,16 @@
+from .resources.commands import (
+    clone,
+    change,
+    purge,
+    get,
+    delete,
+    move,
+    schedule_change,
+    schedule_update,
+    initialize,
+    rename,
+    pin
+)
+from .utils.python_utils import (
+    converter
+)
