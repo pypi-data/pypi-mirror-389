0e2c0eb850c41ade8896fd45eab4e2c80ee4af76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:09:51 +0000

    Add package homa/0.2.3/homa-0.2.3.tar.gz

---
+++ packages/homa/homa-0.2.3.tar.gz/homa-0.2.3/src/homa/vision/StochasticClassifier.py
@@ -0,0 +1,23 @@
+import torch
+from ..activations import SGELU, LaLU, CaLU, TripleStateSwish
+
+
+class StochasticClassifier:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._activation_pool = [
+            torch.nn.ELU,
+            torch.nn.PReLU,
+            torch.nn.ReLU,
+            torch.nn.ReLU6,
+            torch.nn.RReLU,
+            torch.nn.SELU,
+            torch.nn.CELU,
+            torch.nn.GELU,
+            torch.nn.SiLU,
+            torch.nn.Mish,
+            SGELU,
+            LaLU,
+            CaLU,
+            TripleStateSwish,
+        ]
---
+++ packages/homa/homa-0.2.3.tar.gz/homa-0.2.3/src/homa/vision/__init__.py
@@ -0,0 +1,5 @@
+from .Classifier import Classifier
+from .Model import Model
+from .Resnet import Resnet
+from .StochasticResnet import StochasticResnet
+from .Swin import Swin
