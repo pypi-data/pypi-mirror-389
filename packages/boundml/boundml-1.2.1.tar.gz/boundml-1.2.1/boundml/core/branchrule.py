e9df29f91bf41cfa362308fbd44efc6dbb2c8fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 17:08:53 +0000

    Add package boundml/1.2.1/boundml-1.2.1.tar.gz

---
+++ packages/boundml/boundml-1.2.1.tar.gz/boundml-1.2.1/boundml/core/branchrule.py
@@ -0,0 +1,32 @@
+from typing import Iterable
+
+from pyscipopt import Branchrule, Model, SCIP_RESULT
+
+from boundml.components import BranchingComponent
+
+
+class BoundmlBranchrule(Branchrule):
+    """
+    Branchrule used by ModularSolver.
+    It consists into calling the corresponding BranchingComponent
+    """
+    def __init__(self, model: Model, components: Iterable[BranchingComponent]):
+        self.model = model
+        self.components = components
+
+    def branchexeclp(self, allowaddcons):
+        result = SCIP_RESULT.DIDNOTRUN
+        passive = False
+        for component in self.components:
+            r = component.callback(self.model, passive)
+            if r is not None and r != SCIP_RESULT.DIDNOTRUN:
+                result = r
+                passive = True
+
+        return {"result": result}
+
+    def branchexecps(self, allowaddcons):
+        # Todo Does not run and let SCIP manage this case. Must find a way for branching components to manage this case
+        result = SCIP_RESULT.DIDNOTRUN
+
+        return {"result": result}
