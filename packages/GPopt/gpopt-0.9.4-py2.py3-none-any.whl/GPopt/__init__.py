f6f57905b95aae35fc547e58a81e606a0d4a6a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:08:04 +0000

    Add package GPopt/0.9.4/gpopt-0.9.4-py2.py3-none-any.whl

---
+++ packages/GPopt/gpopt-0.9.4-py2.py3-none-any.whl/GPopt/__init__.py
@@ -0,0 +1,5 @@
+from .GPOpt import GPOpt
+from .BOstopping import BOstopping
+from .genericcv import generic_cv, optimize_ml_model
+
+__all__ = ["GPOpt", "BOstopping", "generic_cv", "optimize_ml_model"]
