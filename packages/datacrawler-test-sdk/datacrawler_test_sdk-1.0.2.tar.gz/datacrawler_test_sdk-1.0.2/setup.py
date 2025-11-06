ee4210b7720ff4476cf12e4f8581820032c8e090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:41 +0000

    Add package datacrawler-test-sdk/1.0.2/datacrawler_test_sdk-1.0.2.tar.gz

---
+++ packages/datacrawler-test-sdk/datacrawler_test_sdk-1.0.2.tar.gz/datacrawler_test_sdk-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="datacrawler-test-sdk",
+    version="1.0.2",
+    description="A Smart, Automatic, Fast and Lightweight Web Scraper for Python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/siddharth-shah-17/Datacrawler-Experimental",
+    author="Siddharth Shah",
+    author_email="team@svector.co.in",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="scraping - scraper - crawler - web scraping",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    python_requires=">=3.6",
+    install_requires=["requests", "bs4", "lxml"],
+)
