7faca934ac8ae9a7b48337b01d06afd1b6fd511e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:10:27 +0000

    Add package tinybird/0.0.1.dev313/tinybird-0.0.1.dev313.tar.gz

---
+++ packages/tinybird/tinybird-0.0.1.dev313.tar.gz/tinybird-0.0.1.dev313/tinybird/tb/__cli__.py
@@ -0,0 +1,8 @@
+
+__name__ = 'tinybird'
+__description__ = 'Tinybird Command Line Tool'
+__url__ = 'https://www.tinybird.co/docs/forward/commands'
+__author__ = 'Tinybird'
+__author_email__ = 'support@tinybird.co'
+__version__ = '0.0.1.dev313'
+__revision__ = '1d6b3bc'
