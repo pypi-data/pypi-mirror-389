87da3b9d174db5d8687e3668c0ff23a1b12f0d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:08:20 +0000

    Add package systemctl/1.2.21/systemctl-1.2.21.tar.gz

---
+++ packages/systemctl/systemctl-1.2.21.tar.gz/systemctl-1.2.21/systemctl/constants/DExitCode.py
@@ -0,0 +1,14 @@
+# constants/DExitCode.py
+#
+#    systemctl - A Python wrapper for the systemctl command line utility.
+#    Author: Nadim-Daniel Ghaznavi
+#    Copyright: (c) 2025 Nadim-Daniel Ghaznavi
+#    GitHub: https://github.com/NadimGhaznavi/systemctl
+#    License: GPL 3.0
+
+
+class DExitCode:
+    """Constants related to the runtime systemctl CLI environment."""
+
+    OK: int = 0
+    ERROR: int = 5
