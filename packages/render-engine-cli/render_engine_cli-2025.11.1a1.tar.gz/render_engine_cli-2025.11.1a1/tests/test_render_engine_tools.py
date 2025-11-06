054c96c501176d51511adfc2228d8a75ee9eaeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:48 +0000

    Add package render-engine-cli/2025.11.1a1/render_engine_cli-2025.11.1a1.tar.gz

---
+++ packages/render-engine-cli/render_engine_cli-2025.11.1a1.tar.gz/render_engine_cli-2025.11.1a1/tests/test_render_engine_tools.py
@@ -0,0 +1,18 @@
+import pathlib
+
+from render_engine.site import Site
+
+from render_engine_cli.utils import remove_output_folder
+
+
+def test_clean_folder(tmp_path):
+    site = Site()
+    site.output_path = tmp_path
+    dirty_output_path = tmp_path / "test"  # Tests that nested folders are also removed
+    dirty_output_path.mkdir()
+    dirty_output_path.joinpath("test.txt").touch()
+    assert dirty_output_path.exists()
+
+    remove_output_folder(pathlib.Path(dirty_output_path))
+    assert not dirty_output_path.exists()
+    assert not list(tmp_path.iterdir())
