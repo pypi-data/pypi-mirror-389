efa5dc607f6855b9b8f92972f42265dbd83d7491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:07:01 +0000

    Add package schwab-api2/0.3.0/schwab_api2-0.3.0.tar.gz

---
+++ packages/schwab-api2/schwab_api2-0.3.0.tar.gz/schwab_api2-0.3.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="schwab_api2",
+    packages=setuptools.find_packages(),
+    version="0.3.0",
+    license="MIT",
+    description="Unofficial Schwab API wrapper in Python 3.",
+    author="Jafer Haider",
+    author_email="itsjafer@gmail.com",
+    url="https://github.com/MaxxRK/schwab-api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/itsjafer/schwab-api/tarball/master",
+    keywords=["schwab", "python3", "api", "unofficial", "schwab-api", "schwab charles api"],
+    install_requires=["playwright", "playwright-sm", "pyotp", "python-vipaccess"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
