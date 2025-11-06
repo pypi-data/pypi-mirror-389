372648f447fd11336c2e641bb7122cb625960259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 05:09:44 +0000

    Add package homa/0.2.6/homa-0.2.6-py3-none-any.whl

---
+++ packages/homa/homa-0.2.6-py3-none-any.whl/homa/vision/StochasticSwin.py
@@ -0,0 +1,10 @@
+import torch
+from .Swin import Swin
+from .StochasticClassifier import StochasticClassifier
+from .utils import replace_activations
+
+
+class StochasticSwin(Swin, StochasticClassifier):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        replace_activations(self.network, torch.nn.GELU, self._activation_pool)
