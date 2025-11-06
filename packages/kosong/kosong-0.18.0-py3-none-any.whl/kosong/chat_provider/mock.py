7e37ff3ab615d554d9b001f05546004497c48840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:59 +0000

    Add package kosong/0.18.0/kosong-0.18.0-py3-none-any.whl

---
+++ packages/kosong/kosong-0.18.0-py3-none-any.whl/kosong/chat_provider/mock.py
@@ -0,0 +1,62 @@
+from collections.abc import AsyncIterator, Sequence
+from typing import TYPE_CHECKING
+
+from kosong.base.chat_provider import ChatProvider, StreamedMessagePart, TokenUsage
+from kosong.base.message import Message
+from kosong.base.tool import Tool
+
+if TYPE_CHECKING:
+
+    def type_check(mock: "MockChatProvider"):
+        _: ChatProvider = mock
+
+
+class MockChatProvider:
+    """
+    A mock chat provider.
+    """
+
+    name = "mock"
+
+    def __init__(
+        self,
+        message_parts: list[StreamedMessagePart],
+    ):
+        self._message_parts = message_parts
+
+    @property
+    def model_name(self) -> str:
+        return "mock"
+
+    async def generate(
+        self,
+        system_prompt: str,
+        tools: Sequence[Tool],
+        history: Sequence[Message],
+    ) -> "MockStreamedMessage":
+        return MockStreamedMessage(self._message_parts)
+
+
+class MockStreamedMessage:
+    def __init__(self, message_parts: list[StreamedMessagePart]):
+        self._iter = self._to_stream(message_parts)
+
+    def __aiter__(self) -> AsyncIterator[StreamedMessagePart]:
+        return self
+
+    async def __anext__(self) -> StreamedMessagePart:
+        return await self._iter.__anext__()
+
+    async def _to_stream(
+        self, message_parts: list[StreamedMessagePart]
+    ) -> AsyncIterator[StreamedMessagePart]:
+        for part in message_parts:
+            yield part
+
+    @property
+    def id(self) -> str:
+        return "mock"
+
+    @property
+    def usage(self) -> TokenUsage | None:
+        return None
