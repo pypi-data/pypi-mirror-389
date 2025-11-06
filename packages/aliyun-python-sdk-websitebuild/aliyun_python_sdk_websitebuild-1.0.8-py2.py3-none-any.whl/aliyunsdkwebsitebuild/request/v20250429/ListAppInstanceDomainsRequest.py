d59f2128234c0e4d2f9d8058764fc8ef09287a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:41 +0000

    Add package aliyun-python-sdk-websitebuild/1.0.8/aliyun_python_sdk_websitebuild-1.0.8-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-websitebuild/aliyun_python_sdk_websitebuild-1.0.8-py2.py3-none-any.whl/aliyunsdkwebsitebuild/request/v20250429/ListAppInstanceDomainsRequest.py
@@ -0,0 +1,63 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class ListAppInstanceDomainsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'WebsiteBuild', '2025-04-29', 'ListAppInstanceDomains')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+	def get_PageNum(self): # Integer
+		return self.get_query_params().get('PageNum')
+
+	def set_PageNum(self, PageNum):  # Integer
+		self.add_query_param('PageNum', PageNum)
+	def get_OrderColumn(self): # String
+		return self.get_query_params().get('OrderColumn')
+
+	def set_OrderColumn(self, OrderColumn):  # String
+		self.add_query_param('OrderColumn', OrderColumn)
+	def get_NextToken(self): # String
+		return self.get_query_params().get('NextToken')
+
+	def set_NextToken(self, NextToken):  # String
+		self.add_query_param('NextToken', NextToken)
+	def get_BizId(self): # String
+		return self.get_query_params().get('BizId')
+
+	def set_BizId(self, BizId):  # String
+		self.add_query_param('BizId', BizId)
+	def get_PageSize(self): # Integer
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self, PageSize):  # Integer
+		self.add_query_param('PageSize', PageSize)
+	def get_MaxResults(self): # Integer
+		return self.get_query_params().get('MaxResults')
+
+	def set_MaxResults(self, MaxResults):  # Integer
+		self.add_query_param('MaxResults', MaxResults)
+	def get_OrderType(self): # String
+		return self.get_query_params().get('OrderType')
+
+	def set_OrderType(self, OrderType):  # String
+		self.add_query_param('OrderType', OrderType)
---
+++ packages/aliyun-python-sdk-websitebuild/aliyun_python_sdk_websitebuild-1.0.8-py2.py3-none-any.whl/aliyunsdkwebsitebuild/request/v20250429/SetAppDomainCertificateRequest.py
@@ -0,0 +1,58 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class SetAppDomainCertificateRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'WebsiteBuild', '2025-04-29', 'SetAppDomainCertificate')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+	def get_DomainName(self): # String
+		return self.get_query_params().get('DomainName')
+
+	def set_DomainName(self, DomainName):  # String
+		self.add_query_param('DomainName', DomainName)
+	def get_PublicKey(self): # String
+		return self.get_query_params().get('PublicKey')
+
+	def set_PublicKey(self, PublicKey):  # String
+		self.add_query_param('PublicKey', PublicKey)
+	def get_CertificateType(self): # String
+		return self.get_query_params().get('CertificateType')
+
+	def set_CertificateType(self, CertificateType):  # String
+		self.add_query_param('CertificateType', CertificateType)
+	def get_PrivateKey(self): # String
+		return self.get_query_params().get('PrivateKey')
+
+	def set_PrivateKey(self, PrivateKey):  # String
+		self.add_query_param('PrivateKey', PrivateKey)
+	def get_BizId(self): # String
+		return self.get_query_params().get('BizId')
+
+	def set_BizId(self, BizId):  # String
+		self.add_query_param('BizId', BizId)
+	def get_CertificateName(self): # String
+		return self.get_query_params().get('CertificateName')
+
+	def set_CertificateName(self, CertificateName):  # String
+		self.add_query_param('CertificateName', CertificateName)
