67f562c599c21a516610d2f37c43ae6b8cf8230b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:11:50 +0000

    Add package bluemath-tk/1.1.13/bluemath_tk-1.1.13-py3-none-any.whl

---
+++ packages/bluemath-tk/bluemath_tk-1.1.13-py3-none-any.whl/bluemath_tk/teslakit/__init__.py
@@ -0,0 +1,29 @@
+"""
+Project: BlueMath_tk
+Sub-Module: teslakit
+Author: GeoOcean Research Group, Universidad de Cantabria
+Repository: https://github.com/GeoOcean/BlueMath_tk.git
+Status: OLD VERSION WORKING (MUST be updated soon)
+"""
+
+from . import (
+    alr,
+    climate_emulator,
+    database,
+    estela,
+    extremes,
+    intradaily,
+    io,
+    kma,
+    mda,
+    mjo,
+    numerical_models,
+    pca,
+    plotting,
+    rbf,
+    statistical,
+    storms,
+    tides,
+    util,
+    waves,
+)
