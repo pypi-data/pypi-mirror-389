ca64442eac8a06262bdaa8e9ff52ebf03516cf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:55 +0000

    Add package win64-process-toolkit/1.0.1/win64_process_toolkit-1.0.1-py3-none-win_amd64.whl

---
+++ packages/win64-process-toolkit/win64_process_toolkit-1.0.1-py3-none-win_amd64.whl/win64_process_toolkit/__init__.py
@@ -0,0 +1,162 @@
+from typing import List, Union, overload
+from win64_process_toolkit.__internal.native_lib import __get_native_lib
+
+
+class ProcessInfo:
+    """Windows Process Information"""
+
+    @property
+    def name(self):
+        """Process Name (read only)"""
+        return getattr(self, "__name")
+
+    @property
+    def id(self):
+        """Process ID (read only)"""
+        return getattr(self, "__id")
+
+
+def get_process_id(process_name: str) -> int:
+    """
+    Get the ID of a process by name
+
+    Args:
+        process_name (str): _description_
+
+    Returns:
+        int: Process ID (0 if process not exist)
+    """
+    return __get_native_lib().get_process_id(process_name)
+
+
+@overload
+def inject_dll(process: int, dll_path: str) -> bool:
+    """Inject a DLL into an existed process
+
+    Args:
+        process (int): The process ID
+        dll_path (str): The file path of the DLL
+
+    Returns:
+        bool: True if succeed
+    """
+
+
+@overload
+def inject_dll(process: str, dll_path: str) -> bool:
+    """Inject a DLL into an existed process
+
+    Args:
+        process (str): The process name
+        dll_path (str): The file path of the DLL
+
+    Returns:
+        bool: True if succeed
+    """
+
+
+def inject_dll(process: Union[int, str], dll_path: str) -> bool:
+    if isinstance(process, str):
+        process = get_process_id(process)
+    if process == 0:
+        return False
+    return __get_native_lib().inject_dll(process, dll_path)
+
+
+def get_runtime_processes() -> List[ProcessInfo]:
+    """Get all running process information
+
+    Returns:
+        List[ProcessInfo]: Runtime processes
+    """
+    result = []
+    lib = __get_native_lib()
+    count = lib.create_process_snapshot()
+    for index in range(count):
+        info = ProcessInfo()
+        setattr(info, "__name", lib.get_process_name_from_snapshot(index))
+        setattr(info, "__id", lib.get_process_id_from_snapshot(index))
+        result.append(info)
+    lib.free_process_snapshot()
+    return result
+
+
+def get_runtime_process_names() -> List[str]:
+    """Get all running process names
+
+    Returns:
+        List[str]: Runtime process names
+    """
+    result = []
+    lib = __get_native_lib()
+    count = lib.create_process_snapshot()
+    for index in range(count):
+        result.append(lib.get_process_name_from_snapshot(index))
+    lib.free_process_snapshot()
+    return result
+
+
+@overload
+def kill_process(process: int, exit_code: int = 1) -> bool:
+    """Kill an existing process
+
+    Args:
+        process (int): The process ID
+        exit_code (int, optional): Process exit code. Defaults to 1.
+
+    Returns:
+        bool: True if succeed
+    """
+
+
+@overload
+def kill_process(process: str, exit_code: int = 1) -> bool:
+    """Kill an existing process
+
+    Args:
+        process (str): The process name
+        exit_code (int, optional): Process exit code. Defaults to 1.
+
+    Returns:
+        bool: True if succeed
+    """
+
+
+@overload
+def check_process(process: int) -> bool:
+    """Check process existing
+
+    Args:
+        process (int): The process ID
+
+    Returns:
+        bool: True if exists
+    """
+
+
+@overload
+def check_process(process: str) -> bool:
+    """Check process existing
+
+    Args:
+        process (str): The process name
+
+    Returns:
+        bool: True if exists
+    """
+
+
+def kill_process(process: Union[int, str], exit_code: int = 1) -> bool:
+    if isinstance(process, str):
+        process = get_process_id(process)
+    if process == 0:
+        return False
+    return __get_native_lib().kill_process(process, exit_code)
+
+
+def check_process(process: Union[int, str]) -> bool:
+    if isinstance(process, str):
+        process = get_process_id(process)
+    if process == 0:
+        return False
+    return __get_native_lib().check_process(process)
