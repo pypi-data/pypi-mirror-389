8e546f0b53f08261638e208215f73c45536de7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:36 +0000

    Add package edi-core/0.8.0rc2/edi_core-0.8.0rc2.tar.gz

---
+++ packages/edi-core/edi_core-0.8.0rc2.tar.gz/edi_core-0.8.0rc2/src/edi_core/util/audio.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+from edi_core.util.file import dir_exists, get_file_name_without_extension
+
+
+def extract_audio(file_path: str, output_path: str):
+    result = subprocess.run(
+        ['ffmpeg', '-i', file_path, '-q:a', '0', '-map', 'a', output_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to extract audio: {result.stderr.decode()}")
+
+
+def split_audio(source_file: str, dest_dir: str, seconds: int):
+    if not os.path.isfile(source_file):
+        raise ValueError(f"Source file does not exist: {source_file}")
+    if not dir_exists(dest_dir):
+        os.makedirs(dest_dir, exist_ok=True)
+
+    base_name = get_file_name_without_extension(source_file)
+    segment_pattern = os.path.join(dest_dir, f"{base_name}_%03d.mp3")
+
+    result = subprocess.run(
+        ['ffmpeg', '-i', source_file, '-f', 'segment', '-segment_time', str(seconds), '-c', 'copy', segment_pattern],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to split audio: {result.stderr.decode()}")
+
+    return sorted([os.path.join(dest_dir, f) for f in os.listdir(dest_dir) if f.startswith(base_name)],
+                  key=lambda x: x.lower())
---
+++ packages/edi-core/edi_core-0.8.0rc2.tar.gz/edi_core-0.8.0rc2/src/edi_core/util/env.py
@@ -0,0 +1,37 @@
+import os
+import platform
+
+from dotenv import load_dotenv
+
+dotenv_loaded = False
+
+
+def load_env():
+    global dotenv_loaded
+    if not dotenv_loaded:
+        load_dotenv()
+        dotenv_loaded = True
+
+
+def get_env(key: str, default: str = None):
+    load_env()
+    value = os.environ.get(key)
+    if value:
+        return value
+
+    if default is None:
+        raise ValueError('env key not found')
+    else:
+        return default
+
+
+def get_platform():
+    return platform.platform()
+
+
+def get_system():
+    return platform.system()
+
+
+def is_windows():
+    return get_system() == 'Windows'
