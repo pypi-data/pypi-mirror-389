d6286afa4bbc47ffad3aa18ca3691a1f2cb6ae22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:31 +0000

    Add package imio.smartweb.common/1.2.40/imio_smartweb_common-1.2.40.tar.gz

---
+++ packages/imio.smartweb.common/imio_smartweb_common-1.2.40.tar.gz/imio_smartweb_common-1.2.40/src/imio/smartweb/common/config.py
@@ -0,0 +1,14 @@
+# Used to limit description field on all content types
+DESCRIPTION_MAX_LENGTH = 700
+
+# Used to translated terms from brains metadatas
+# Must be extended by other products
+VOCABULARIES_MAPPING = {
+    "iam": "imio.smartweb.vocabulary.IAm",
+    "topics": "imio.smartweb.vocabulary.Topics",
+}
+
+# Used for vocabularies that allow explicitly choosen lang translations
+TRANSLATED_VOCABULARIES = [
+    "imio.smartweb.vocabulary.Countries",
+]
