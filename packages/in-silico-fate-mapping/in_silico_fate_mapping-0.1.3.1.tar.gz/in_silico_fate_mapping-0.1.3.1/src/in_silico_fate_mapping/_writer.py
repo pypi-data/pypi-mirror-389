2c3bbe666d984a8e5c2d9b6c4d91544e97de23b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 14:06:12 +0000

    Add package in-silico-fate-mapping/0.1.3.1/in_silico_fate_mapping-0.1.3.1.tar.gz

---
+++ packages/in-silico-fate-mapping/in_silico_fate_mapping-0.1.3.1.tar.gz/in_silico_fate_mapping-0.1.3.1/src/in_silico_fate_mapping/_writer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+from in_silico_fate_mapping._reader import TRACKS_HEADER
+
+
+def napari_write_tracks(path: str, data: np.ndarray, meta: dict) -> List[str]:
+
+    # first position are the default
+    header = list(c[0] for c in TRACKS_HEADER)
+    if data.shape[1] == 4:
+        header.remove("z")
+
+    df = pd.DataFrame(data, columns=header)
+    df.to_csv(path, index=False)
+
+    return [path]
