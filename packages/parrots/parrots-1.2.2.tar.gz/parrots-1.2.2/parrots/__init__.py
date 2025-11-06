e4c0078ab6a7712eea7b95b7bbd72677cad54cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:09:59 +0000

    Add package parrots/1.2.2/parrots-1.2.2.tar.gz

---
+++ packages/parrots/parrots-1.2.2.tar.gz/parrots-1.2.2/parrots/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+__version__ = '1.2.1'
+
+from parrots.asr import SpeechRecognition
+from parrots.pinyin2hanzi import Pinyin2Hanzi
+from parrots.tts import TextToSpeech
+from parrots import indextts
+from parrots.log import logger, set_log_level, add_file_logger, LOG_LEVEL
