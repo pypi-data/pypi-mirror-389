20dd9e6ce0e05d5e35cb53fe44792ccacf787152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:09:48 +0000

    Add package autoconf/2025.11.5.1/autoconf-2025.11.5.1.tar.gz

---
+++ packages/autoconf/autoconf-2025.11.5.1.tar.gz/autoconf-2025.11.5.1/autoconf/__init__.py
@@ -0,0 +1,13 @@
+from . import exc
+from .tools.decorators import cached_property
+from .conf import Config
+from .conf import instance
+from .json_prior.config import default_prior
+from .json_prior.config import make_config_for_class
+from .json_prior.config import path_for_class
+from .json_prior.config import JSONPriorConfig
+
+from .setup_colab import for_autolens
+
+
+__version__ = "2025.11.5.1"
