1afc89fe67151e36ac7f2c71664f93038d4ec793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 03:26:15 +0000

    Add package gbrl/1.1.2/gbrl-1.1.2-cp312-cp312-macosx_15_0_arm64.whl

---
+++ packages/gbrl/gbrl-1.1.2-cp312-cp312-macosx_15_0_arm64.whl/gbrl/src/cpp/data_structs.cpp
@@ -0,0 +1,70 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024-2025, NVIDIA Corporation. All rights reserved.
+//
+// This work is made available under the Nvidia Source Code License-NC.
+// To view a copy of this license, visit
+// https://nvlabs.github.io/gbrl/license.html
+//
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file data_structs.cpp
+ * @brief Implementation of fundamental data structures
+ */
+
+#include <iostream>
+
+#include "data_structs.h"
+#include "types.h"
+
+template <typename T>
+stack<T>::stack(int max_elements) 
+    : max_size(max_elements), top_index(-1) {
+    data = new T[max_size];
+}
+
+template <typename T>
+stack<T>::~stack() {
+    delete[] data;
+}
+
+template <typename T>
+bool stack<T>::is_empty() const {
+    return top_index == -1;
+}
+
+template <typename T>
+bool stack<T>::is_full() const {
+    return top_index == max_size - 1;
+}
+
+template <typename T>
+void stack<T>::push(const T &element) {
+    if (is_full()) {
+        std::cerr << "Stack overflow: cannot push element\n";
+        return;
+    }
+    data[++top_index] = element;
+}
+
+template <typename T>
+void stack<T>::pop() {
+    if (is_empty()) {
+        std::cerr << "Stack underflow: cannot pop from empty stack\n";
+        return;
+    }
+    --top_index;
+}
+
+template <typename T>
+T& stack<T>::top() const {
+    if (is_empty()) {
+        throw std::out_of_range("Stack is empty: cannot access top element");
+    }
+    return data[top_index];
+}
+
+// Explicit template instantiations
+template struct stack<int>;
+template struct stack<nodeInfo>;
