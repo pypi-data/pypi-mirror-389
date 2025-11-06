b9cc8ee6b309be8300794947545a89806aa0e6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:07:47 +0000

    Add package amharic-tokenizer/0.2.1/amharic_tokenizer-0.2.1.tar.gz

---
+++ packages/amharic-tokenizer/amharic_tokenizer-0.2.1.tar.gz/amharic_tokenizer-0.2.1/tests/test_basic.py
@@ -0,0 +1,21 @@
+"""Test script for basic roundtrip functionality of the AmharicTokenizer."""
+
+from amharic_tokenizer import AmharicTokenizer
+
+
+def test_roundtrip_basic():
+    """Load a trained tokenizer, tokenize text, convert to IDs, and detokenize."""
+    tok = AmharicTokenizer.load("amh_bpe_v0.2.1")
+    text = (
+        "የኮሪደር ልማት ገፀ በረከት የሆናቸው የከተማችን ሰፈሮች በነዋሪዎች አንደበት በሰዓት 209 ኪሎ ሜትር የሚጓዘው አውሎ ንፋስ ከጃማይካ ቀጥሎ ኩባ ደርሷል ጠቅላይ" )
+
+    # Tokenize text
+    ids = tok.encode(text)
+    print("Tokens", tok.decode(ids))
+    print(ids)
+    print(tok.tokenize(text))
+    print(tok.detokenize(tok.tokenize(text)))
+    
+
+if __name__ == "__main__":
+    test_roundtrip_basic()
