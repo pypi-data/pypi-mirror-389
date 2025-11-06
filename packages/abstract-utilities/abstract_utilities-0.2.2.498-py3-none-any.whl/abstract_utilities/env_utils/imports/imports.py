7a08ffa6260d8426d3bff68d5edd66dddbda5b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:11:12 +0000

    Add package abstract-utilities/0.2.2.498/abstract_utilities-0.2.2.498-py3-none-any.whl

---
+++ packages/abstract-utilities/abstract_utilities-0.2.2.498-py3-none-any.whl/abstract_utilities/env_utils/imports/imports.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from ...string_clean import eatAll,eatInner,eatOuter,safe_split
+from ...compare_utils import line_contains
+from ...type_utils import is_list,is_bool
+from ...path_utils import get_slash,path_join,if_not_last_child_join,get_home_folder,simple_path_join,is_file
+DEFAULT_FILE_NAME = '.env'
+DEFAULT_KEY = 'MY_PASSWORD'
