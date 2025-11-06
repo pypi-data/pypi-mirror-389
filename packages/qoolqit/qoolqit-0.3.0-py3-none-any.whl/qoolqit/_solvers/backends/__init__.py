425bca0aeecf43c7de6c10dfb2119b1e5e3dad32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 06:07:10 +0000

    Add package qoolqit/0.3.0/qoolqit-0.3.0-py3-none-any.whl

---
+++ packages/qoolqit/qoolqit-0.3.0-py3-none-any.whl/qoolqit/_solvers/backends/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from .base_backend import BaseBackend
+from .get_backend import get_backend
+from .local_backends import BaseLocalBackend, EmuMPSBackend, EmuSVBackend, QutipBackend
+from .remote_backends import (
+    BaseRemoteBackend,
+    RemoteEmuFREEBackend,
+    RemoteEmuMPSBackend,
+    RemoteEmuTNBackend,
+    RemoteJob,
+    RemoteQPUBackend,
+)
+
+__all__ = [
+    "BaseBackend",
+    "BaseLocalBackend",
+    "BaseRemoteBackend",
+    "QutipBackend",
+    "RemoteQPUBackend",
+    "RemoteEmuMPSBackend",
+    "RemoteEmuTNBackend",
+    "RemoteEmuFREEBackend",
+    "get_backend",
+    "RemoteJob",
+    "EmuMPSBackend",
+    "EmuSVBackend",
+]
