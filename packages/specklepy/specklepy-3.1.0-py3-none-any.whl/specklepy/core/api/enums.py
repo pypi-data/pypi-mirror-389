95d7d002b395df5b111cbac6c12b0a373f60f3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 09:07:04 +0000

    Add package specklepy/3.1.0/specklepy-3.1.0-py3-none-any.whl

---
+++ packages/specklepy/specklepy-3.1.0-py3-none-any.whl/specklepy/core/api/enums.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class ProjectVisibility(str, Enum):
+    """Supported project visibility types"""
+
+    PRIVATE = "PRIVATE"
+    PUBLIC = "PUBLIC"
+    UNLISTED = "UNLISTED"
+    WORKSPACE = "WORKSPACE"
+
+
+class UserProjectsUpdatedMessageType(str, Enum):
+    ADDED = "ADDED"
+    REMOVED = "REMOVED"
+
+
+class ProjectModelsUpdatedMessageType(str, Enum):
+    CREATED = "CREATED"
+    DELETED = "DELETED"
+    UPDATED = "UPDATED"
+
+
+class ProjectUpdatedMessageType(str, Enum):
+    DELETED = "DELETED"
+    UPDATED = "UPDATED"
+
+
+class ProjectVersionsUpdatedMessageType(str, Enum):
+    CREATED = "CREATED"
+    DELETED = "DELETED"
+    UPDATED = "UPDATED"
