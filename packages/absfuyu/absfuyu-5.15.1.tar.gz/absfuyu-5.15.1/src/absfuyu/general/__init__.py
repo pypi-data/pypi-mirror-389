332ee42cc842fdd43320f11ba6e7fa16b20a47af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 04:10:05 +0000

    Add package absfuyu/5.15.1/absfuyu-5.15.1.tar.gz

---
+++ packages/absfuyu/absfuyu-5.15.1.tar.gz/absfuyu-5.15.1/src/absfuyu/general/__init__.py
@@ -0,0 +1,23 @@
+"""
+Absfuyu: General
+----------------
+Collection of useful classes
+
+Version: 5.15.0
+Date updated: 03/11/2025 (dd/mm/yyyy)
+
+Features:
+---------
+- content
+- generator
+- human
+"""
+
+# Module level
+# ---------------------------------------------------------------------------
+__all__ = ["content", "shape", "human"]
+
+
+# Libary
+# ---------------------------------------------------------------------------
+from absfuyu.general import content, human, shape
