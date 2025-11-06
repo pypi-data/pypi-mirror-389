2c8448ffff19240ee06e888234eafb64fd63c40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:06:51 +0000

    Add package egos-helpers/1.0.1/egos_helpers-1.0.1-py3-none-any.whl

---
+++ packages/egos-helpers/egos_helpers-1.0.1-py3-none-any.whl/egos_helpers/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Constants declarations """
+
+# These get overwritten at build time. See build.sh
+VERSION = '1.0.1'
+BUILD = '2142921238'
+NAME = 'egos-helpers'
