9b5111a1d9dd9282293c057dbd18e13162f2f95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 16:12:05 +0000

    Add package pyrobosim/4.3.2/pyrobosim-4.3.2.tar.gz

---
+++ packages/pyrobosim/pyrobosim-4.3.2.tar.gz/pyrobosim-4.3.2/test/test_pyrobosim.py
@@ -0,0 +1,15 @@
+"""
+Basic sanity checks for PyRoboSim module.
+"""
+
+import importlib
+from importlib.metadata import version
+
+
+def test_import() -> None:
+    assert importlib.util.find_spec("pyrobosim")
+
+
+def test_version() -> None:
+    ver = version("pyrobosim")
+    assert ver == "4.3.2", "Incorrect pyrobosim version"
