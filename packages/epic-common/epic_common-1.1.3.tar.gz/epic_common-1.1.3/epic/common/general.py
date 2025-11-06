91bebd8daf098433558b1412fab5e34296077412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 15:07:00 +0000

    Add package epic-common/1.1.3/epic_common-1.1.3.tar.gz

---
+++ packages/epic-common/epic_common-1.1.3.tar.gz/epic_common-1.1.3/epic/common/general.py
@@ -0,0 +1,294 @@
+import math
+import logging
+import numbers
+import functools
+
+from io import IOBase
+from types import MappingProxyType
+from collections import defaultdict
+from collections.abc import Iterable, Callable, Hashable, Mapping
+from typing import TypeVar, TypeGuard, Generic, ParamSpec, Concatenate, overload, cast
+
+B = TypeVar('B', bound=bytes | bytearray)
+C_contra = TypeVar('C_contra', bound=type, contravariant=True)
+P = ParamSpec('P')
+R = TypeVar('R', bound=numbers.Real)
+S = TypeVar('S')
+T = TypeVar('T')
+T_co = TypeVar('T_co', covariant=True)
+I = TypeVar("I", bound=Iterable)
+IterableException = TypeVar("IterableException", bound=str | bytes | bytearray | Mapping)
+
+__all__ = [
+    'is_iterable', 'to_iterable', 'to_list', 'to_bytes', 'to_number', 'classproperty',
+    'indexer_dict', 'hash_content', 'human_readable', 'get_single',
+    'pass_none', 'coalesce',
+]
+
+
+def is_iterable(obj) -> TypeGuard[Iterable]:
+    """
+    Test whether an object is iterable, but NOT a string or a bytes instance.
+    """
+    return isinstance(obj, Iterable) and not isinstance(obj, str | bytes | bytearray)
+
+
+@overload
+def to_iterable(obj: IterableException) -> tuple[IterableException]: ...
+@overload
+def to_iterable(obj: I) -> I: ...
+@overload
+def to_iterable(obj: T) -> tuple[T]: ...
+
+def to_iterable(obj):
+    """
+    Convert an object to an iterable.
+
+    Strings, bytes, bytearrays and Mappings are treated as single objects.
+    """
+    # Mappings need special handling since iterating over them means only iterating over the keys
+    if is_iterable(obj) and not isinstance(obj, Mapping):
+        return obj
+    return obj,
+
+
+@overload
+def to_list(obj: IterableException) -> list[IterableException]: ...
+@overload
+def to_list(obj: Iterable[T]) -> list[T]: ...
+@overload
+def to_list(obj: T) -> list[T]: ...
+
+def to_list(obj):
+    """
+    Convert an object to a list.
+
+    - If already a list, the object is returned as is; no new list is created.
+    - If a single item (including a string, a bytes or a Mapping), a list of
+      length 1 containing the object is returned.
+    - If an iterable, the object is converted to a list.
+    """
+    # Avoid making a new list if already given a list
+    return obj if isinstance(obj, list) else list(to_iterable(obj))
+
+
+@overload
+def to_bytes(obj: B, encoding: str = ..., errors: str = ...) -> B: ...
+@overload
+def to_bytes(obj, encoding: str = ..., errors: str = ...) -> bytes: ...
+
+def to_bytes(obj, encoding='utf-8', errors='replace'):
+    """
+    Convert an object to a byte sequence - either a bytes or a bytearray.
+    """
+    if isinstance(obj, bytes | bytearray):
+        return obj
+    if isinstance(obj, memoryview):
+        return obj.tobytes()
+    if not isinstance(obj, str):
+        obj = str(obj)
+    return obj.encode(encoding, errors=errors)
+
+
+@overload
+def to_number(obj: R) -> R: ...
+@overload
+def to_number(obj) -> numbers.Real: ...
+
+def to_number(obj):
+    """
+    Convert an object to a number.
+    """
+    if isinstance(obj, numbers.Real):
+        return obj
+    try:
+        return int(obj)
+    except ValueError:
+        return float(obj)
+
+
+# noinspection PyPep8Naming
+class classproperty(Generic[C_contra, T_co]):
+    """
+    Class property decorator.
+    The property is read-only.
+
+    Examples
+    --------
+    >>> class MyClass:
+    ...   X = 1
+    ...
+    ...   @classproperty
+    ...   def prop(cls):
+    ...     return cls.X + 1
+    ...
+    >>> MyClass.prop
+    2
+    """
+    def __init__(self, func: Callable[[C_contra], T_co]):
+        self.func = func
+
+    def __get__(self, owner, owner_cls: C_contra) -> T_co:
+        return self.func(owner_cls)
+
+
+def indexer_dict() -> defaultdict[Hashable, int]:
+    """
+    Create and return a new `collections.defaultdict` which assigns to new keys the current number of elements.
+
+    In essence, it gives a collection of items the unique indices `{0, ..., n-1}`, where `n` is the
+    number of unique items.
+
+    Returns
+    -------
+    collections.defaultdict
+    """
+    dd = defaultdict[Hashable, int]()
+    dd.default_factory = dd.__len__
+    return dd
+
+
+def hash_content(obj) -> int:
+    """
+    Hash the content of an object, even if the object itself is not hashable.
+
+    For hashable objects, returns their built-in hash.
+    For other objects, attempts to obtain a "representation" of the object's content,
+    either its state (using `__getstate__`), its `__dict__` or its items, and hash them recursively,
+    together with information about the object's type.
+    That means that for mutable objects, multiple calls to `hash_content` may not return the same value.
+
+    Parameters
+    ----------
+    obj : object
+        The object to hash.
+
+    Returns
+    -------
+    int
+        A value consistent with the current content of `obj`.
+    """
+    if isinstance(obj, Hashable):
+        try:
+            return hash(obj)
+        except TypeError:
+            pass
+    typename = type(obj).__name__
+    if hasattr(obj, '__getstate__') and (state := obj.__getstate__()) is not None:
+        obj = state
+    elif isinstance(obj, logging.getLoggerClass()):
+        obj = obj.name
+    elif hasattr(obj, '__dict__'):
+        if isinstance(obj.__dict__, MappingProxyType):
+            objdict = {k: v for k, v in obj.__dict__.items() if not k.startswith('__')}
+        else:
+            objdict = obj.__dict__.copy()
+        objdict['___name'] = getattr(obj, '__name__', None)
+        objdict['___classname'] = typename
+        if isinstance(obj, dict):
+            obj = obj.copy()
+            obj['___objdict'] = objdict
+        else:
+            obj = objdict
+    if isinstance(obj, Mapping):
+        obj = frozenset((k, hash_content(v)) for k, v in obj.items())
+    elif is_iterable(obj) and not isinstance(obj, IOBase):
+        obj = tuple(map(hash_content, obj))
+    return hash((obj, typename))
+
+
+def human_readable(number: float, binary: bool = False, n_digits: int = 2) -> str:
+    """
+    Convert a number to a string representation that is easier for humans to read.
+    The number is displayed using the standard SI prefixes for very large and small scales.
+
+    Parameters
+    ----------
+    number : int or float
+        The number to convert.
+
+    binary : bool, default False
+        Whether to use the standard binary prefixes (Kibi, etc.), and a multiplier of 1024 instead of 1000.
+
+    n_digits : int, default 2
+        The maximum number of digits to display after the decimal point.
+
+    Returns
+    -------
+    str
+        Representation of the input number.
+    """
+    if number > 0:
+        sign = ''
+    elif number < 0:
+        sign = '-'
+        number = -number
+    else:
+        return '0'
+    # The generic math.log with base 2/10 gives less accurate results, which can cause rounding errors.
+    log, exponent, base, kilo, units_suffix = (
+        math.log2, 10, 1024, 'K', 'i',
+    ) if binary else (
+        math.log10, 3, 1000, 'k', '',
+    )
+    scale = int(log(number) // exponent)
+    scaled_number = number / base ** scale
+    precision = int(math.log10(scaled_number)) + n_digits + 1  # log10 because, still, digits...
+    if scale > 0:
+        units = f" {(kilo + 'MGTPEZYRQ')[scale - 1]}{units_suffix}"
+    elif scale < 0:
+        units = f" {'mµnpfazyrq'[-scale - 1]}{units_suffix}"
+    else:
+        units = ''
+    return f"{sign}{scaled_number:.{precision}g}{units}"
+
+
+def get_single(items: Iterable[T]) -> T:
+    """
+    Get THE single item from the given iterable.
+    If the iterable contain more or fewer than a single item, an exception is raised.
+
+    Parameters
+    ----------
+    items : iterable
+        An iterable containing only a single item.
+
+    Returns
+    -------
+    object
+        The single item in `items`.
+
+    Raises
+    ------
+    ValueError
+        If `items` contain more or fewer than a single item.
+    """
+    it = iter(items)
+    try:
+        item = next(it)
+    except StopIteration:
+        raise ValueError("Cannot get single value; no values found")
+    try:
+        next(it)
+    except StopIteration:
+        return item
+    raise ValueError(f"Cannot get single value; more than one value found: {items!r}")
+
+
+def pass_none(func: Callable[Concatenate[T, P], S]) -> Callable[Concatenate[T | None, P], S | None]:
+    """
+    Decorate a function so that if called with None as its first argument, it returns None.
+    """
+    @functools.wraps(func)
+    def none_shall_pass(value: T | None, *args: P.args, **kwargs: P.kwargs) -> S | None:
+        return None if value is None else func(value, *args, **kwargs)
+    return none_shall_pass
+
+
+def coalesce(*args: T) -> T | None:
+    """
+    Return the first argument that is not None, or None if no such argument was provided.
+    """
+    for arg in args:
+        if arg is not None:
+            return arg
---
+++ packages/epic-common/epic_common-1.1.3.tar.gz/epic_common-1.1.3/epic/common/time.py
@@ -0,0 +1,269 @@
+import time
+import signal
+import threading
+import datetime as dt
+
+from decorator import decorate
+from contextlib import contextmanager
+from typing import TypeVar, Any, TypeAlias
+from collections.abc import Callable, Iterable
+
+from .general import to_list
+
+
+class Timer:
+    """
+    A context manager for timing the execution of a code block.
+    """
+    def __init__(self):
+        self._store = threading.local()
+        self.delta = 0
+
+    @property
+    def delta(self) -> dt.timedelta:
+        return self._store.delta
+
+    @delta.setter
+    def delta(self, value: float):
+        self._store.delta = dt.timedelta(seconds=value)
+
+    def __enter__(self) -> "Timer":
+        self._store.reference_time = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.delta = time.perf_counter() - self._store.reference_time
+
+    def __str__(self):
+        return str(self.delta)
+
+    @property
+    def total_seconds(self) -> float:
+        return self.delta.total_seconds()
+
+
+C = TypeVar("C", bound=Callable)
+
+
+def timed(function: C) -> C:
+    """
+    A decorator for timing functions.
+
+    Attaches a Timer object to the function (as a "timer" attribute), which times each execution of the function.
+    """
+    timer = Timer()
+
+    def timeit(func, *args, **kwargs):
+        with timer:
+            result = func(*args, **kwargs)
+        return result
+
+    wrapped = decorate(function, timeit)
+    wrapped.timer = timer
+    return wrapped
+
+
+class TimeTracker(dict[Any, list]):
+    """
+    A dictionary subclass which tracks the total time spent between calls.
+    It maps the given code section names to lists of floats - the number of seconds since tracking has started.
+
+    Examples
+    --------
+    >>> class MyClass:
+    ...   def __init__(self):
+    ...     self.time_tracker = TimeTracker()
+    ...
+    ...   def calculate(self):
+    ...     # Start the clock for this method
+    ...     tracker = self.time_tracker.tracker("calculate")
+    ...     ...  # do stuff
+    ...     # Save elapsed time and start a new clock
+    ...     tracker.track("do stuff")
+    ...     ...  # do more stuff
+    ...     # Again, save elapsed time and start a new clock
+    ...     tracker.track("do more stuff")
+    ...     ...  # do stuff here that is not tracked
+    ...
+    >>> mc = MyClass()
+    >>> for i in range(2):
+    ...   mc.calculate()
+    ...
+    >>> mc.time_tracker
+    {"calculate: do_stuff": [0.3, 0.6], "calculate: do more stuff": [0.5, 0.1]}
+    """
+    class Tracker:
+        def __init__(self, parent: "TimeTracker", prefix=None):
+            self.parent = parent
+            self.prefix = prefix
+            self.reference_time = time.perf_counter()
+
+        def track(self, name) -> None:
+            """
+            Add a new timing point to the list, under the given name.
+
+            Parameters
+            ----------
+            name : object
+                Name for the current timing entry.
+
+            Returns
+            -------
+            None
+            """
+            if self.prefix is not None:
+                name = f"{self.prefix}: {name}"
+            current_time = time.perf_counter()
+            self.parent.setdefault(name, []).append(current_time - self.reference_time)
+            self.reference_time = current_time
+
+    def tracker(self, prefix=None) -> Tracker:
+        """
+        Create a new Tracker and start its clock.
+
+        Parameters
+        ----------
+        prefix : object, optional
+            A common prefix for all entries of this tracker.
+
+        Returns
+        -------
+        Tracker
+        """
+        return self.Tracker(self, prefix)
+
+
+Timestamp: TypeAlias = dt.datetime | dt.date | str | float | int
+TimeFormat: TypeAlias = str | Iterable[str] | None
+
+
+def to_datetime(timestamp: Timestamp, time_format: TimeFormat = None) -> dt.datetime:
+    """
+    Convert a wide range of inputs into a datetime.datetime.
+
+    Parameters
+    ----------
+    timestamp : datetime, date, str, float or int
+        The input to convert.
+        If a number, it is the standard POSIX timestamp.
+        If a string, can either be a formatted time string (see `time_format` below), or
+        one of "today", "yesterday" or "now" (case-insensitive).
+
+    time_format : str or iterable of str, optional
+        Possible formats to consider when trying to convert a formatted time string.
+        If not given, some reasonable defaults are tried.
+
+    Returns
+    -------
+    datetime.datetime
+        Converted input.
+    """
+    def d2dt(date: dt.date) -> dt.datetime:
+        return dt.datetime.combine(date, dt.time.min)
+
+    match timestamp:
+        case dt.datetime():
+            return timestamp
+        case dt.date():
+            return d2dt(timestamp)
+        case str():
+            match timestamp.lower():
+                case 'today':
+                    return d2dt(dt.date.today())
+                case 'yesterday':
+                    return d2dt(dt.date.today()) - dt.timedelta(days=1)
+                case 'now':
+                    return dt.datetime.now()
+            if time_format is None:
+                try:
+                    return dt.datetime.fromisoformat(timestamp)
+                except ValueError:
+                    pass
+                bases = ("%Y{0}%m{0}%d", "%d{0}%m{0}%y")
+                separators = [''] + list('-./')
+                times = [''] + ['{tsep}%H:%M{sec}'.format(tsep=ts, sec=s) for ts in ' T' for s in (':%S', '')]
+                formats = [base.format(sep) + t for base in bases for t in times for sep in separators]
+            else:
+                formats = to_list(time_format)
+            for f in formats:
+                try:
+                    return dt.datetime.strptime(timestamp, f)
+                except ValueError:
+                    if f == formats[-1]:
+                        raise ValueError(
+                            f"Input {timestamp!r} does not match any of "
+                            f"{'expected' if time_format is None else 'given'} formats"
+                        )
+        case float() | int():
+            return dt.datetime.fromtimestamp(timestamp)
+        case _:
+            raise TypeError("inappropriate timestamp type: " + type(timestamp).__name__)
+
+
+def to_epoch_time(timestamp: Timestamp, time_format: TimeFormat = None) -> float:
+    """
+    Convert a wide range of inputs to seconds since the Epoch.
+
+    Parameters
+    ----------
+    timestamp : datetime, date, str, float or int
+        The input to convert.
+        See `to_datetime` for more information.
+
+    time_format : str or iterable of str, optional
+        See `to_datetime` for more information.
+
+    Returns
+    -------
+    float
+        Seconds since the Epoch for the given input.
+
+    See Also
+    --------
+    to_datetime : Convert to datetime.datetime.
+    """
+    datetime = to_datetime(timestamp, time_format)
+    return time.mktime(datetime.timetuple()) + datetime.microsecond / 1e6
+
+
+# Inspired by http://stackoverflow.com/a/13821695/221917
+@contextmanager
+def timeout_if_possible(timeout_duration: int | None):
+    """
+    Execute the contained code block with a timeout. If exceeded, a TimeoutError is raised.
+    Note that timeouts are not supported on Windows.
+    Also, they are supported only in the main thread and only in whole seconds resolution.
+
+    Parameters
+    ----------
+    timeout_duration : int or None
+        Number of seconds until a timeout.
+        If None, then no timeout is set.
+    """
+    if timeout_duration is None:
+        # No timeout
+        yield
+        return
+    elif timeout_duration != int(timeout_duration):
+        raise TypeError(f"Only integer timeouts are allowed; got {timeout_duration}")
+    timeout_duration = int(timeout_duration)
+
+    if not (hasattr(signal, "alarm") and hasattr(signal, "SIGALRM")):
+        # Likely not on Linux / Mac
+        yield
+    elif threading.current_thread() is not threading.main_thread():
+        # Only main thread can be signal-interrupted
+        yield
+    else:
+        def handler(_signum, _frame):
+            raise TimeoutError()
+
+        orig_signal_handler = signal.signal(signal.SIGALRM, handler)
+        try:
+            # Set the timeout handler
+            # TODO: If _prev_alarm is nonzero, we just stole another handler's alarm! We should restore it at the end.
+            _prev_alarm = signal.alarm(timeout_duration)
+            yield
+        finally:
+            signal.alarm(0)
+            signal.signal(signal.SIGALRM, orig_signal_handler)
