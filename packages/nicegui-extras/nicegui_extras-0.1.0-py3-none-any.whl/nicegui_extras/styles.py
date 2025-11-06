d673ee1823a8d3634975e464ea2334c51f57bb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:08:16 +0000

    Add package nicegui-extras/0.1.0/nicegui_extras-0.1.0-py3-none-any.whl

---
+++ packages/nicegui-extras/nicegui_extras-0.1.0-py3-none-any.whl/nicegui_extras/styles.py
@@ -0,0 +1,14 @@
+menu_row = '''
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #222;
+    color: white;
+    padding: 10px;
+    z-index: 1000;
+'''
+
+link_btn = 'q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle'
+
+animated_dialog = 'backdrop-filter="blur(8px) brightness(20%)"'
---
+++ packages/nicegui-extras/nicegui_extras-0.1.0-py3-none-any.whl/nicegui_extras/utils.py
@@ -0,0 +1,18 @@
+farsi_rtl = '''
+        <link href="https://cdn.jsdelivr.net/npm/vazir-font@30.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
+
+        <style>
+        body, .q-layout, .q-page-container, .q-page {
+            direction: rtl !important;
+            text-align: right !important;
+            font-family: Vazir, sans-serif !important; 
+        }
+
+
+        .q-btn, .q-input, .q-field__native, .q-table, .q-card, .q-toolbar {
+            font-family: Vazir, sans-serif !important;
+        }
+        </style>
+        '''
+
+no_scroll = '<style>body { overflow: hidden; }</style>'
