ae951580375f13920c53991bcc5d131a22a87d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 17:08:39 +0000

    Add package python-requestr/0.16/python_requestr-0.16.tar.gz

---
+++ packages/python-requestr/python_requestr-0.16.tar.gz/python_requestr-0.16/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'python_requestr',
+    packages = ['requestr',],
+    install_requires=['requests', 'python-objectifier', 'beautifulsoup4', 'requests-futures'],
+    version = '0.16',
+    description = 'requestr',
+    url = 'https://github.com/xjxckk/python-requestr/',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+    )
