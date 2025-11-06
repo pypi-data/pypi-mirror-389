c5e2fa2d64230e77e9e5f7108dbd65ae2c5ac0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-06 08:07:33 +0000

    Add package chiapos/2.0.12/chiapos-2.0.12.tar.gz

---
+++ packages/chiapos/chiapos-2.0.12.tar.gz/chiapos-2.0.12/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+import os
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=["./"])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build"
+                + " the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir),
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
+
+        if os.getenv("CP_USE_GREEN_REAPER") == "1":
+            cmake_args.append("-DCP_LINK_BLADEBIT_HARVESTER=ON")
+
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
+        else:
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j", "6"]
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
+
+setup(
+    name="chiapos",
+    author="Mariano Sorgente",
+    author_email="mariano@chia.net",
+    description="Chia proof of space plotting, proving, and verifying (wraps C++)",
+    license="Apache-2.0",
+    python_requires=">=3.7",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chia-Network/chiapos",
+    tests_require=["pytest"],
+    ext_modules=[CMakeExtension("chiapos", ".")],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
