65de8e7099c285580c621bc5eb6ae07a544ca36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:07:26 +0000

    Add package mtkdiskrit-himpunan-group1-ucmakassar/1.0.2/mtkdiskrit_himpunan_group1_ucmakassar-1.0.2.tar.gz

---
+++ packages/mtkdiskrit-himpunan-group1-ucmakassar/mtkdiskrit_himpunan_group1_ucmakassar-1.0.2.tar.gz/mtkdiskrit_himpunan_group1_ucmakassar-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='himpunan-group1-ucmakassar',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Kelompok 2',
+    author_email='mchlstvny@gmail.com',
+    description='Implementasi konsep himpunan dengan Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mchlstvny/himpunan-project-discrete-mathematics.git',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
