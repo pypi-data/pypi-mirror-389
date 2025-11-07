e054c3cc8030aafcdfb3f27a0b6fd38e95addf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:04 +0000

    Add package bezalel/0.0.21/bezalel-0.0.21.tar.gz

---
+++ packages/bezalel/bezalel-0.0.21.tar.gz/bezalel-0.0.21/README.md
@@ -0,0 +1,206 @@
+# bezalel
+
+A library for ingesting data provided by paginated HTTP APIs
+
+
+# Usage
+
+## Basic use case
+
+If you have to pull data from HTTP API that has an endpoint accepting parameters:
+
+```
+pageNumber=1,2,...
+```
+
+And returning JSON:
+
+```json
+{
+    "pageCount": 5,
+    "entities": [
+      {"key":  "val1", ...},
+      {"key":  "val2", ...},
+      ...
+    ]
+}
+```
+
+Then you can iterate over all pages with following code:
+```python
+import requests
+from bezalel import PaginatedApiIterator
+
+
+for page in PaginatedApiIterator(requests.Session(), url=f"http://localhost:5000/page-api",
+                                     request_page_number_param_name="pageNumber",
+                                     response_page_count_field_name="pageCount",
+                                     response_records_field_name="entities"):
+    print(f"Page: {page}")
+```
+
+It will print:
+
+```
+Page: [{"key":  "val1", ...}, {"key":  "val2", ...}, ...]
+Page: [{"key":  "val100", ...}, {"key":  "val101", ...}, ...]
+Page: [{"key":  "val200", ...}, {"key":  "val201", ...}, ...]
+...
+```
+
+
+## Grouping with `BufferingIterator`
+
+If HTTP API doesn't allow you setting high number of records per page, use `BufferingIterator`.
+
+```python
+import requests
+from bezalel import PaginatedApiIterator, BufferingIterator
+
+
+for page in BufferingIterator(PaginatedApiIterator(requests.Session(), url=f"http://localhost:5000/page-api",
+                                                       request_page_number_param_name="pageNumber",
+                                                       response_page_count_field_name="pageCount",
+                                                       response_records_field_name="entities"), buffer_size=2):
+    print(f"Page: {page}")
+```
+
+It will combine multiple pages into one array, so that 
+```
+Page: [{"key":  "val1", ...}, {"key":  "val2", ...}, ..., {"key":  "val100", ...}, {"key":  "val101", ...}, ...]
+Page: [{"key":  "val200", ...}, {"key":  "val201", ...}, ..., {"key":  "val300", ...}, {"key":  "val301", ...}, ...]
+...
+```
+
+This is useful for fetching many records and storing them in fewer files (every file would be bigger). 
+
+
+## Iterating over all records
+
+TODO: this API will be improved in future release.
+
+```python
+import itertools
+import requests
+from bezalel import PaginatedApiIterator
+
+
+all_elems = list(itertools.chain.from_iterable(PaginatedApiIterator(requests.Session(), url=f"https://your/api",
+                                                   request_page_number_param_name="pageNumber",
+                                                   response_page_count_field_name="pageCount",
+                                                   response_records_field_name="entities"))):
+print(f"len={len(all_elems)}: {all_elems}")
+```
+
+will print
+
+```
+len=12300: [{"key":  "val1", ...}, {"key":  "val2", ...}, ...]
+```
+
+## Helper function: `normalize_with_prototype()`
+
+Normalize python dict, so that it has all the fields and only the fields specified in a prototype dict.
+
+```python
+from bezalel import normalize_with_prototype
+
+object_from_api = {
+    "id": 123,
+    "name:": "John",
+    "country": "Poland",
+    "customDict": {
+        "some": 123,
+        "complex": 345,
+        "structure": 546
+    },
+    # city is not provided here (but present in prototype)
+    "pets": [
+        {"id": 101, "type": "dog", "name": "Barky"},
+        {"id": 102, "type": "snail"},   # name is not provided here (but present in prototype)
+    ],
+    "unspecifiedField": 123     # this field is not present in prototype below
+}
+
+prototype_from_swagger = {
+    "id": 0,
+    "name:": "",
+    "country": "",
+    "customDict": {},
+    "city": "",
+    "pets": [
+        {"id": 0, "type": "", "name": ""},
+    ]
+}
+
+result = normalize_with_prototype(prototype_from_swagger, object_from_api, pass_through_paths=[".customDict"])
+# pass_through_paths is optional and it marks an object as something that should not be normalized
+```
+
+would return
+
+```python
+result = {
+    "id": 123,
+    "name:": "John",
+    "country": "Poland",
+    "customDict": {
+        "some": 123,
+        "complex": 345,
+        "structure": 546
+    },
+    "city": None,   # city was added
+    "pets": [
+        {"id": 101, "type": "dog", "name": "Barky"},
+        {"id": 102, "type": "snail", "name": None}, # name was added
+    ]
+}
+```
+
+
+## Helper function: `normalize_dicts()`
+
+Normalize list of nested python dicts to a list of one-level dicts.
+
+Example:
+```python
+from bezalel import normalize_dicts
+
+data = [
+    {
+        "id": 1, "name": "John Smith",
+        "pets": [
+            {"id": 101, "type": "cat", "name": "Kitty", "toys": [{"name": "toy1"}, {"name": "toy2"}]},
+            {"id": 102, "type": "dog", "name": "Barky", "toys": [{"name": "toy3"}]}
+        ]
+    },
+    {
+        "id": 2, "name": "Sue Smith",
+        "pets": [
+            {"id": 201, "type": "cat", "name": "Kitten", "toys": [{"name": "toy4"}, {"name": "toy5"}, {"name": "toy6"}]},
+            {"id": 202, "type": "dog", "name": "Fury", "toys": []}
+        ]
+    },
+]
+
+normalize_dicts(data, ["pets", "toys"])
+```
+
+would return:
+
+```python
+[{'id': 1, 'name': 'John Smith', 'pets.id': 101, 'pets.type': 'cat', 'pets.name': 'Kitty', 'pets.toys.name': 'toy1'},
+ {'id': 1, 'name': 'John Smith', 'pets.id': 101, 'pets.type': 'cat', 'pets.name': 'Kitty', 'pets.toys.name': 'toy2'},
+ {'id': 1, 'name': 'John Smith', 'pets.id': 102, 'pets.type': 'dog', 'pets.name': 'Barky', 'pets.toys.name': 'toy3'},
+ {'id': 2, 'name': 'Sue Smith', 'pets.id': 201, 'pets.type': 'cat', 'pets.name': 'Kitten', 'pets.toys.name': 'toy4'},
+ {'id': 2, 'name': 'Sue Smith', 'pets.id': 201, 'pets.type': 'cat', 'pets.name': 'Kitten', 'pets.toys.name': 'toy5'},
+ {'id': 2, 'name': 'Sue Smith', 'pets.id': 201, 'pets.type': 'cat', 'pets.name': 'Kitten', 'pets.toys.name': 'toy6'},
+ {'id': 2, 'name': 'Sue Smith', 'pets.id': 202, 'pets.type': 'dog', 'pets.name': 'Fury'}]
+```
+
+Presence of the last record can be controlled by flag `return_incomplete_records`. If `return_incomplete_records=False`
+then last record in the example would not have been returned.
+
+Additional options:
+- jsonify_lists - when set to True, then if a list is encountered (not in main path), it is dumped as a JSON string.
+- jsonify_dicts - list of paths for where to expect a dict. That dict will be then dumped as a JSON string.
---
+++ packages/bezalel/bezalel-0.0.21.tar.gz/bezalel-0.0.21/src/bezalel/impl/normalize_with_prototype.py
@@ -0,0 +1,164 @@
+import datetime
+
+
+class NormalizeException(Exception):
+    def __init__(self, path, message):
+        super().__init__(f"{path}: {message}")
+        self._path = path
+        self._message = message
+
+
+def normalize_with_prototype(prototype, object_to_norm, freestyle_attrs_name="freestyle_attrs", pass_through_paths=[],
+                             strict_types=True, type_converter=None):
+    """
+    Normalize python dict, so that it has all the fields and only the fields specified in a prototype dict.
+
+    :param prototype: A prototype dict, for example from Swagger doc.
+    :param object_to_norm: A dict that comes from API.
+    :param freestyle_attrs_name: it is a name for attribute, when prototype contains a dict like this: "field": {"": ""}
+    :param pass_through_paths: list of paths which will be passed through without normalization (for example dicts with
+        unpredicted structure, that will be handled after normalization)
+    :param strict_types: if True, then fail on types mismatch between prototype and object_to_norm.
+        If False, function will attempt to parse string, i.e. "123.45" to 123.45 float.
+        It will fail on parsing errors anyway.
+    :param type_converter: a function with signature:
+        type_converter(prototype_object: any, object_to_norm: any, path_info: str) -> any
+        called only when strict_types=False and type(prototype_object) != type(object_to_norm)
+    :return:
+    """
+    if not type_converter:
+        type_converter = lambda prototype_object, object_to_norm, path_info: object_to_norm
+
+    def normalize_with_prototype_rec(prototype, object_to_norm, path_info, prototype_path):
+        try:
+            if prototype_path in pass_through_paths:
+                return object_to_norm
+            if isinstance(prototype, dict):
+                if object_to_norm is None:
+                    object_to_norm = {}
+                elif not isinstance(object_to_norm, dict) and not strict_types:
+                    object_to_norm = type_converter(prototype, object_to_norm, path_info)
+                if not isinstance(object_to_norm, dict):
+                    raise NormalizeException(path_info, f"object_to_norm is not dict (of value {object_to_norm})")
+                normalized_object = {}
+                for k, v in prototype.items():
+                    if k != "":
+                        normalized_object[k] = normalize_with_prototype_rec(prototype[k], object_to_norm.get(k), path_info=f"{path_info}.{k}", prototype_path=f"{prototype_path}.{k}" if prototype_path != "" else k)
+                    else:
+                        normalized_object[freestyle_attrs_name] = [
+                            {"key": key, "value": value} for key, value in object_to_norm.items() if key not in prototype.keys() or key == ""
+                        ]
+                return normalized_object
+            elif isinstance(prototype, list):
+                if object_to_norm is None:
+                    object_to_norm = []
+                elif not isinstance(object_to_norm, list) and not strict_types:
+                    object_to_norm = type_converter(prototype, object_to_norm, path_info)
+                if not isinstance(object_to_norm, list):
+                    raise NormalizeException(path_info, f"object_to_norm is not list (of value {object_to_norm})")
+                return [normalize_with_prototype_rec(prototype[0], e, path_info=f"{path_info}[{idx}]", prototype_path=prototype_path) for idx, e in enumerate(object_to_norm)]
+            elif isinstance(prototype, str):
+                if object_to_norm is None:
+                    return None
+                if type(prototype) != type(object_to_norm):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        return str(type_converter(prototype, object_to_norm, path_info))
+                return object_to_norm
+            elif isinstance(prototype, bool):
+                if object_to_norm is None:
+                    return None
+                if type(prototype) != type(object_to_norm):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        object_to_norm = type_converter(prototype, object_to_norm, path_info)
+                        if not isinstance(object_to_norm, bool):
+                            if isinstance(object_to_norm, str):
+                                if object_to_norm.lower().strip() in {'1', 'true', 'yes'}:
+                                    object_to_norm = True
+                                elif object_to_norm.lower().strip() in {'0', 'false', 'no'}:
+                                    object_to_norm = False
+                                else:
+                                    object_to_norm = None
+                            else:
+                                object_to_norm = bool(object_to_norm)
+                return object_to_norm
+            elif isinstance(prototype, int):
+                if object_to_norm is None:
+                    return None
+                if not isinstance(object_to_norm, int):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if object_to_norm == [] or object_to_norm == {} or isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        try:
+                            object_to_norm = int(type_converter(prototype, object_to_norm, path_info))
+                        except Exception as e:
+                            raise NormalizeException(path_info, f"can't parse int '{object_to_norm}': {e}")
+                return object_to_norm
+            elif isinstance(prototype, float):
+                if object_to_norm is None:
+                    return None
+                if not isinstance(object_to_norm, (int, float)):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if object_to_norm == [] or object_to_norm == {} or isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        try:
+                            object_to_norm = float(type_converter(prototype, object_to_norm, path_info))
+                        except Exception as e:
+                            raise NormalizeException(path_info, f"can't parse float '{object_to_norm}': {e}")
+                if not isinstance(object_to_norm, float):
+                    try:
+                        object_to_norm = float(object_to_norm)
+                    except Exception as e:
+                        raise NormalizeException(path_info, f"can't parse float '{object_to_norm}': {e}")
+                return object_to_norm
+            elif isinstance(prototype, datetime.datetime):
+                # datetime.datetime() is an instance of both datetime.datetime and datetime.date.
+                # datetime.date() is NOT an instance of datetime.datetime
+                # that's why `isinstance(prototype, datetime.datetime)` check comes before `isinstance(prototype, datetime.date)`.
+                if object_to_norm is None:
+                    return None
+                if not isinstance(object_to_norm, datetime.datetime):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if object_to_norm == [] or object_to_norm == {} or isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        try:
+                            object_to_norm = type_converter(prototype, object_to_norm, path_info)
+                            if not isinstance(object_to_norm, datetime.datetime):
+                                object_to_norm = datetime.datetime.fromisoformat(object_to_norm)
+                        except Exception as e:
+                            raise NormalizeException(path_info, f"can't parse datetime fromisoformat '{object_to_norm}': {e}")
+                return object_to_norm
+            elif isinstance(prototype, datetime.date):
+                if object_to_norm is None:
+                    return None
+                if not isinstance(object_to_norm, datetime.date):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if object_to_norm == [] or object_to_norm == {} or isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        try:
+                            object_to_norm = type_converter(prototype, object_to_norm, path_info)
+                            if not isinstance(object_to_norm, datetime.date):
+                                object_to_norm = datetime.date.fromisoformat(object_to_norm)
+                        except Exception as e:
+                            raise NormalizeException(path_info, f"can't parse date fromisoformat '{object_to_norm}': {e}")
+                if isinstance(object_to_norm, datetime.datetime):
+                    object_to_norm = object_to_norm.date()
+                return object_to_norm
+            else:
+                raise NormalizeException(path_info, f"prototype data type not supported: {type(prototype)}")
+        except NormalizeException as e:
+            raise
+        except Exception as e:
+            raise NormalizeException(path_info, str(e))
+    return normalize_with_prototype_rec(prototype, object_to_norm, "", "")
