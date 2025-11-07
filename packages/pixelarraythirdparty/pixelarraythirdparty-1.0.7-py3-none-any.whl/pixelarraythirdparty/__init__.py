49a3964a45b706b4585d02e43781ac2ccfd63491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:07:59 +0000

    Add package pixelarraythirdparty/1.0.7/pixelarraythirdparty-1.0.7-py3-none-any.whl

---
+++ packages/pixelarraythirdparty/pixelarraythirdparty-1.0.7-py3-none-any.whl/pixelarraythirdparty/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+PixelArray 第三方微服务客户端
+这个库包含了常用的开发工具和服务集成：
+- order: 订单管理模块和支付模块
+- product: 产品管理模块
+- cron: 定时任务管理模块
+- user: 用户管理模块
+"""
+
+__version__ = "1.0.7"
+__author__ = "Lu qi"
+__email__ = "qi.lu@pixelarrayai.com"
+
+# 导出主要模块
+__all__ = [
+    "product",
+    "cron",
+    "user",
+    "order",
+]
