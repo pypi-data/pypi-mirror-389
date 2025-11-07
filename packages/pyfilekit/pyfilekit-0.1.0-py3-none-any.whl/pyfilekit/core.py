a929615d5897cf0e591e6727a41d7af27096c85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:26:49 +0000

    Add package pyfilekit/0.1.0/pyfilekit-0.1.0-py3-none-any.whl

---
+++ packages/pyfilekit/pyfilekit-0.1.0-py3-none-any.whl/pyfilekit/core.py
@@ -0,0 +1,14 @@
+"""Core functions for pyfilekit."""
+
+def safe_read(*args, **kwargs):
+    """safe_read function."""
+    return None
+
+def safe_write(*args, **kwargs):
+    """safe_write function."""
+    return None
+
+def temp_file(*args, **kwargs):
+    """temp_file function."""
+    return None
+
