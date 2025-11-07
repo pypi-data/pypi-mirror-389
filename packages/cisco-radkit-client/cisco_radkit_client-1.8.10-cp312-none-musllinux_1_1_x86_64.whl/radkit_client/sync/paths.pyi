df632bab2670a6171cca49457ef1cec2a36481b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:25:56 +0000

    Add package cisco-radkit-client/1.8.10/cisco_radkit_client-1.8.10-cp312-none-musllinux_1_1_x86_64.whl

---
+++ packages/cisco-radkit-client/cisco_radkit_client-1.8.10-cp312-none-musllinux_1_1_x86_64.whl/radkit_client/sync/paths.pyi
@@ -0,0 +1,3 @@
+from radkit_client.async_.paths import get_client_dir_path as get_client_dir_path, get_client_logs_dir_path as get_client_logs_dir_path, get_console_dir_path as get_console_dir_path, get_default_settings_path as get_default_settings_path
+
+__all__ = ['get_client_dir_path', 'get_client_logs_dir_path', 'get_console_dir_path', 'get_default_settings_path']
