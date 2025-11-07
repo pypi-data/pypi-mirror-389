a3d3fcd4c3d423c3fa112328d245ef91f3dedf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:07:25 +0000

    Add package ordeq_files/1.1.0/ordeq_files-1.1.0.tar.gz

---
+++ packages/ordeq_files/ordeq_files-1.1.0.tar.gz/ordeq_files-1.1.0/src/ordeq_files/__init__.py
@@ -0,0 +1,9 @@
+from ordeq_files.bytes import Bytes
+from ordeq_files.csv import CSV
+from ordeq_files.glob import Glob
+from ordeq_files.json import JSON
+from ordeq_files.pickle import Pickle
+from ordeq_files.text import Text
+from ordeq_files.text_lines_stream import TextLinesStream
+
+__all__ = ("CSV", "JSON", "Bytes", "Glob", "Pickle", "Text", "TextLinesStream")
