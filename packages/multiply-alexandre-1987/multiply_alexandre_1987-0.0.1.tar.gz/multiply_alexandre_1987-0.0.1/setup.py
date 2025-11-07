c9d0ebc759cb4e4510ffa94baec43d09902d0f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:07:59 +0000

    Add package multiply-alexandre-1987/0.0.1/multiply_alexandre_1987-0.0.1.tar.gz

---
+++ packages/multiply-alexandre-1987/multiply_alexandre_1987-0.0.1.tar.gz/multiply_alexandre_1987-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Primeiro Pacote Python'
+LONG_DESCRIPTION = 'Primeiro Pacote Python, mas com uma descrição mais longa'
+
+setup(
+    name='multiply-alexandre-1987',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author='Alexandre Vieira',
+    author_email='sites.alexandre@gmail.com',
+    keywords=['multiply-alexandre-1987', 'primeiro', 'pacote', 'python', 'python3'],
+
+)
