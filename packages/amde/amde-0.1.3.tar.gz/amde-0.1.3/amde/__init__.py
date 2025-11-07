d4b082bf2dbf43c75b2db5ce7138b91ca4dec92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:25:27 +0000

    Add package amde/0.1.3/amde-0.1.3.tar.gz

---
+++ packages/amde/amde-0.1.3.tar.gz/amde-0.1.3/amde/__init__.py
@@ -0,0 +1,42 @@
+import requests
+
+ 
+
+class EmbeddingResponse:
+
+    def __init__(self, embedding):
+
+        self.embedding = embedding
+
+ 
+class Amde:
+
+    def __init__(self, api_key=None, base_url="https://api.amde.dev/v1"):
+
+        self.api_key = api_key
+
+        self.base_url = base_url
+
+ 
+
+    def embed(self, model: str, input_data: str):
+
+        data = {
+
+            "model": model,
+
+            "api_key": self.api_key,
+
+            "input_text": input_data  
+
+        }
+
+ 
+
+        resp = requests.post(f"{self.base_url}/embedding", json=data)  
+
+        resp.raise_for_status()
+
+        return EmbeddingResponse(resp.json()["data"])
+
+ 
