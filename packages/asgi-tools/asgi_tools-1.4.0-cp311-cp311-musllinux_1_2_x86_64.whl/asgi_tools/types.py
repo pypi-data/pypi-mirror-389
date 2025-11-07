b5aa77e99d606822947cb22332b898440d287c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:25:45 +0000

    Add package asgi-tools/1.4.0/asgi_tools-1.4.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/asgi-tools/asgi_tools-1.4.0-cp311-cp311-musllinux_1_2_x86_64.whl/asgi_tools/types.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Awaitable,
+    Callable,
+    Coroutine,
+    Mapping,
+    MutableMapping,
+    TypeVar,
+)
+
+if TYPE_CHECKING:
+    from .request import Request
+
+TASGIMessage = Mapping[str, Any]
+TASGISend = Callable[[TASGIMessage], Awaitable[None]]
+TASGIReceive = Callable[[], Awaitable[TASGIMessage]]
+TASGIScope = MutableMapping[str, Any]
+TASGIHeaders = list[tuple[bytes, bytes]]
+TASGIApp = Callable[[TASGIScope, TASGIReceive, TASGISend], Awaitable[Any]]
+
+TJSON = None | bool | int | float | str | list["TJSON"] | Mapping[str, "TJSON"]
+TExceptionHandler = Callable[["Request", BaseException], Coroutine[None, None, Any]]
+
+TV = TypeVar("TV")
+TVCallable = TypeVar("TVCallable", bound=Callable)
+TVAsyncCallable = TypeVar("TVAsyncCallable", bound=Callable[..., Coroutine])
+TVExceptionHandler = TypeVar("TVExceptionHandler", bound=TExceptionHandler)
