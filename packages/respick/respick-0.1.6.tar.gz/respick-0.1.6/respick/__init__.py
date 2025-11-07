b90aa157a1ea3d7979bc6834ba09b4ea21883cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:45 +0000

    Add package respick/0.1.6/respick-0.1.6.tar.gz

---
+++ packages/respick/respick-0.1.6.tar.gz/respick-0.1.6/respick/__init__.py
@@ -0,0 +1,3 @@
+from .core import find_best_divider  # 导出核心函数
+
+__all__ = ["find_best_divider"]      # 控制 from yourcli import * 的行为
---
+++ packages/respick/respick-0.1.6.tar.gz/respick-0.1.6/tests/test_core.py
@@ -0,0 +1,4 @@
+from respick.core import find_best_divider
+
+def test_dummy():
+    assert find_best_divider(3.3, 0.6) == []
