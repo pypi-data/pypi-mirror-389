c69c16181a5fff711335fa46dc7b731d84a14b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:03 +0000

    Add package agentbill-py-langchain/5.0.1/agentbill_py_langchain-5.0.1-py3-none-any.whl

---
+++ packages/agentbill-py-langchain/agentbill_py_langchain-5.0.1-py3-none-any.whl/agentbill_langchain/__init__.py
@@ -0,0 +1,9 @@
+"""AgentBill LangChain Integration
+
+Zero-config callback handler for tracking LangChain usage in AgentBill.
+"""
+
+from .callback import AgentBillCallback
+
+__version__ = "5.0.1"
+__all__ = ["AgentBillCallback"]
