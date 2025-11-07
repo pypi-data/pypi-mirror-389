5062b9dc23d2e9c036dda4249081e3c1b18fb6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:25:48 +0000

    Add package farn/0.4.2/farn-0.4.2.tar.gz

---
+++ packages/farn/farn-0.4.2.tar.gz/farn-0.4.2/src/farn/core/parameter.py
@@ -0,0 +1,42 @@
+import logging
+
+import numpy as np
+
+__ALL__ = ["Parameter"]
+
+logger = logging.getLogger(__name__)
+
+
+class Parameter:
+    """Dataclass holding the parameter attributes 'name' and 'value'."""
+
+    def __init__(
+        self,
+        name: str = "",
+        value: float | int | bool | str | None = None,  # noqa: FBT001
+    ) -> None:
+        self.name: str = name
+        self.value: float | int | bool | str | None = value
+
+    @property
+    def type(self) -> type[float] | type[int] | type[bool] | type[str] | None:
+        """Returns the Python type of the parameter.
+
+        Returns
+        -------
+        Union[Type[float], Type[int], Type[bool], Type[str], None]
+            the Python type
+        """
+        return None if self.value is None else type(self.value)
+
+    @property
+    def dtype(self) -> np.dtype[np.float64] | np.dtype[np.int32] | np.dtype[np.bool_] | np.dtype[np.str_] | None:
+        """Returns the numpy dtype of the parameter.
+
+        Returns
+        -------
+        np.dtype[np.float64] | np.dtype[np.int32] | np.dtype[np.bool] | np.dtype[np.str] | None
+            the numpy dtype, or None
+        """
+        _type = self.type
+        return None if _type is None else np.dtype(_type)  # type: ignore[return-value, reportReturnType]
---
+++ packages/farn/farn-0.4.2.tar.gz/farn-0.4.2/tests/conftest.py
@@ -0,0 +1,84 @@
+import logging
+import os
+from pathlib import Path
+from shutil import rmtree
+
+import pytest
+
+
+@pytest.fixture(scope="package", autouse=True)
+def chdir() -> None:
+    """
+    Fixture that changes the current working directory to the 'test_working_directory' folder.
+    This fixture is automatically used for the entire package.
+    """
+    os.chdir(Path(__file__).parent.absolute() / "test_working_directory")
+
+
+@pytest.fixture(scope="package", autouse=True)
+def test_dir() -> Path:
+    """
+    Fixture that returns the absolute path of the directory containing the current file.
+    This fixture is automatically used for the entire package.
+    """
+    return Path(__file__).parent.absolute()
+
+
+output_dirs = [
+    "cases",
+    "cases_one_layer",
+    "cases_two_layers",
+    "cases_no_filtering",
+    "cases_initial_filtering",
+    "cases_exclude_filtering",
+    "dump",
+    "logs",
+    "results",
+    "templates",
+]
+output_files = [
+    "sampled*",
+    "queueList*",
+    "*.copy",
+    "splash.png",
+    "caseList*",
+]
+
+
+@pytest.fixture(autouse=True)
+def default_setup_and_teardown():
+    """
+    Fixture that performs setup and teardown actions before and after each test function.
+    It removes the output directories and files specified in 'output_dirs' and 'output_files' lists.
+    """
+    _remove_output_dirs_and_files()
+    yield
+    _remove_output_dirs_and_files()
+
+
+def _remove_output_dirs_and_files() -> None:
+    """
+    Helper function that removes the output directories and files specified in 'output_dirs' and 'output_files' lists.
+    """
+    for folder in output_dirs:
+        rmtree(folder, ignore_errors=True)
+    for pattern in output_files:
+        for file in Path.cwd().glob(pattern):
+            _file = Path(file)
+            _file.unlink(missing_ok=True)
+
+
+@pytest.fixture(autouse=True)
+def setup_logging(caplog: pytest.LogCaptureFixture) -> None:
+    """
+    Fixture that sets up logging for each test function.
+    It sets the log level to 'INFO' and clears the log capture.
+    """
+    caplog.set_level("INFO")
+    caplog.clear()
+
+
+@pytest.fixture(autouse=True)
+def logger() -> logging.Logger:
+    """Fixture that returns the logger object."""
+    return logging.getLogger()
