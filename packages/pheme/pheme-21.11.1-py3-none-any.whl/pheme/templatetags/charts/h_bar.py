20cca52c822e9f0b0c44feb3b319b5643aee2fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:26:37 +0000

    Add package pheme/21.11.1/pheme-21.11.1-py3-none-any.whl

---
+++ packages/pheme/pheme-21.11.1-py3-none-any.whl/pheme/templatetags/charts/h_bar.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+# pheme/templatetags/bar_chart.py
+# Copyright (C) 2020-2021 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import itertools
+from typing import Dict
+
+from django.utils.safestring import SafeText
+from PIL import ImageFont
+
+from pheme.templatetags.charts import (
+    _severity_class_colors,
+    build_legend,
+    calculate_legend_start_height,
+    register,
+)
+
+__ORIENTATION_LINE_TEMPLATE = """
+<rect x="{x}" y="0" height="{height}" width="1" style="fill: #000000;"></rect>
+"""
+
+__ORIENTATION_LINE_TEXT_TEMPLATE = """
+<text class="orientation label" y="22" x="{x}" fill="#4C4C4D" dominant-baseline="central"
+style="font-size:{font_size}px;font-family:{font_family};text-anchor: middle;" width="{width}">{label}
+</text>
+"""
+
+__X_AXIS_LABEL = """
+<text class="x axis label" y="{y}" x="{x}" fill="#000000"
+style="font-size:{font_size}px;font-family:{font_family};text-anchor: middle;">{axis_label}
+</text>
+"""
+
+__BAR_ELEMENT_TEMPLATE = """
+<rect x="{x}" y="17" height="10" width="{width}" style="fill: {color};"></rect>
+"""
+
+__BAR_TEMPLATE = """
+<g class="entry" transform="translate(0, {y})">
+<text class="label category" y="22" x="87.5" fill="#4C4C4D" dominant-baseline="central"
+style="font-size:{font_size}px;font-family:{font_family};text-anchor: right;" width="{max_hostname_len}">{key}
+</text>
+<g transform="translate({max_hostname_len}, 0)">
+{orientation_lines}
+{bar_elements}
+</g>
+<g transform="translate({begin_total}, 0)">
+<text class="sum" y="22" x="10" fill="#4C4C4D" dominant-baseline="central"
+style="font-size:{font_size}px;font-family:{font_family};text-anchor: left;" width="100">{total}</text>
+</g>
+</g>
+"""
+__BAR_CHART_TEMPLATE = """
+<svg width="{width}" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
+{bars}
+<g transform="translate(0, {bar_legend_y})">
+<text 
+    class="label category" 
+    y="22" x="87.5" 
+    fill="#4C4C4D" dominant-baseline="central"
+    style="font-size:{font_size}px;font-family:{font_family};text-anchor: right;" 
+    width="{max_hostname_len}">
+    {x_title}
+    </text>
+</g>
+<g transform="translate({max_hostname_len}, {bar_legend_y})">
+{bar_legend}
+</g>
+{legend}
+{x_axis_label}
+</svg>
+"""
+
+
+@register.filter
+@register.simple_tag
+def h_bar_chart(
+    chart_data: Dict[str, Dict[str, int]],
+    x_title: str = "",
+    title_color=None,
+    svg_width=800,
+    bar_jump=44,
+    orientation_marker=6,
+    limit=10,
+    font_family="Dejavu Sans",
+    font_file_name="DejaVuSans.ttf",
+    font_size=10,
+) -> SafeText:
+    """
+    Returns a stacked horizontal bar chart in svg.
+
+    In order to function it needs a dict with label as key containing a dict
+    with color key and amount.
+
+    An example for valid input is:
+
+    {
+      "192.168.123.52": {
+        "High": 3,
+        "Medium": 3,
+        "Low": 1
+      }
+    }
+
+    parameter:
+        chart_data - The dict containing the values for the chart
+        title_color - A colortable, defaulting to __severity_class_colors
+        svg_width - The overall width of the output chart, default 800
+        bar_jump - The amount of y pixels to jump from bar element to the next.
+            Default is 44.
+        orientation_marker - if higher than 0 than there will be a vertical line
+            each nth step.
+        limit - limits the data by N first elements
+        font_family - the font family used within text elements
+        font_size - the font size used within text elements
+        x_title - the title of the x axis
+    """
+    data = dict(itertools.islice(chart_data.items(), limit))
+    if not title_color:
+        title_color = _severity_class_colors
+    if not data.values():
+        return SafeText("")
+    try:
+        font = ImageFont.truetype(font_file_name, font_size)
+        # add 87.5 + 10 for legend
+        max_hostname_len = (
+            max(  # noqa: PLW3301
+                max(font.getlength(k) for k in data.keys()),
+                font.getlength(x_title),
+            )
+            + 87.5
+            + 10
+        )
+    except OSError:
+        # multiply by 1.25 for kerning and add 87.5 for legend
+        max_hostname_len = (
+            max(max(len(k) for k in data.keys()), len(x_title))  # noqa: PLW3301
+            * font_size
+            * 1.25
+            + 87.5
+        )
+    max_width = svg_width - max_hostname_len - 100  # key and total placeholder
+    # highest sum of counts
+    max_sum = max([sum(list(counts.values())) for counts in data.values()])
+    if max_sum == 0:
+        max_sum = 1
+
+    orientation_lines = ""
+    orientation_labels = ""
+    orientation_basis = (
+        int(max_sum / orientation_marker) if orientation_marker > 0 else 0
+    )
+    if max_sum > orientation_basis and orientation_basis > 0:
+        overhead = max_sum % orientation_basis
+        # if it's already adjusted we don't need to add anything
+        if overhead == 0:
+            overhead = orientation_basis
+        max_sum = max_sum + orientation_basis - overhead
+
+        for i in range(int(max_sum / orientation_basis + 1)):
+            x_pos = i * orientation_basis / max_sum * max_width
+            label = str(i * orientation_basis)
+            orientation_lines += __ORIENTATION_LINE_TEMPLATE.format(
+                x=x_pos,
+                height=bar_jump + 10,
+                font_family=font_family,
+                font_size=font_size,
+            )
+            orientation_labels += __ORIENTATION_LINE_TEXT_TEMPLATE.format(
+                x=x_pos,
+                width=len(label),
+                label=label,
+                font_family=font_family,
+                font_size=font_size,
+            )
+
+    bars = ""
+    for i, (key, counts) in enumerate(data.items()):
+        element_x = 0
+        elements = ""
+        for category, count in counts.items():
+            color = title_color.get(category)
+            width = count / max_sum * max_width
+            elements += __BAR_ELEMENT_TEMPLATE.format(
+                x=element_x,
+                width=width,
+                color=color,
+                font_family=font_family,
+                font_size=font_size,
+                max_hostname_len=max_hostname_len,
+            )
+            element_x += width
+
+        bars += __BAR_TEMPLATE.format(
+            y=i * bar_jump,
+            key=key,
+            bar_elements=elements,
+            orientation_lines=orientation_lines,
+            total=sum(counts.values()),
+            font_family=font_family,
+            font_size=font_size,
+            max_hostname_len=max_hostname_len,
+            begin_total=svg_width - 100,
+        )
+    svg_element_lengths = len(data.keys()) * bar_jump + 60 + font_size
+    legend_start = calculate_legend_start_height(
+        svg_element_lengths, title_color, font_size
+    )
+    svg_chart = __BAR_CHART_TEMPLATE.format(
+        width=svg_width,
+        height=svg_element_lengths,
+        bars=bars,
+        bar_legend=orientation_labels,
+        bar_legend_y=len(data.keys()) * bar_jump + 20,
+        legend=build_legend(legend_start, title_color),
+        font_family=font_family,
+        font_size=font_size,
+        max_hostname_len=max_hostname_len,
+        x_axis_label=__X_AXIS_LABEL.format(
+            axis_label="Number of Vulnerabilities",
+            font_size=font_size,
+            font_family=font_family,
+            x=svg_width / 2,
+            y=svg_element_lengths,
+        ),
+        x_title=x_title,
+    )
+    return SafeText(svg_chart)
---
+++ packages/pheme/pheme-21.11.1-py3-none-any.whl/pheme/transformation/scanreport/renderer.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+# pheme/transformation/scanreport/renderer.py
+# Copyright (C) 2020-2021 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+from base64 import b64encode
+from typing import Dict
+
+from django.core.cache import cache
+from django.template import Context, Template
+from rest_framework import renderers
+from rest_framework.request import Request
+from weasyprint import CSS, HTML
+
+from pheme.authentication import get_username_role
+from pheme.errors import TemplateNotFoundError
+from pheme.parameter import load_params
+from pheme.settings import DEBUG
+
+logger = logging.getLogger(__name__)
+
+
+def _load_template(name: str, params: Dict = None) -> Template:
+    if not params:
+        params = load_params()
+    templ = params.get(name)
+    if not templ:
+        raise TemplateNotFoundError(name)
+    return Template(templ)
+
+
+def _enrich(name: str, data: Dict, parameter: Dict) -> Dict:
+    data["internal_name"] = name
+    return {**parameter, **data}
+
+
+def _get_request(renderer_context: Dict) -> Request:
+    renderer_context = renderer_context or {}  # to throw key error
+    return renderer_context["request"]
+
+
+def _default_not_found_response(
+    renderer_context: Dict, request: Request
+) -> str:
+    resp = renderer_context["response"]
+    resp.status_code = 404
+    # pylint: disable=W0212
+    path = request._request.path
+    report_id = path[path.rfind("/") + 1 :]
+    return f'"not data found for {report_id}"'
+
+
+class Report(renderers.BaseRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        request = _get_request(renderer_context)
+        if not data:
+            return _default_not_found_response(renderer_context, request)
+
+        name = data.get("internal_name")
+        cache_key = f"{self.media_type}/{name}" if name else None
+        logger.debug("generating report %s", cache_key)
+
+        if cache_key and not DEBUG:
+            cached = cache.get(cache_key)
+            if cached:
+                return cached
+        params = load_params()
+        # separate user specific parameter
+        user_parameter = params.pop("user_specific", {})
+        username, _ = get_username_role(request)
+        if username:
+            params = {**params, **user_parameter.get(username, {})}
+        data["version"] = params.get("version")
+
+        result = self.apply(name, data, params)
+        if cache_key:
+            cache.set(cache_key, result)
+        return result
+
+    def apply(self, name: str, data: Dict, parameter: Dict):
+        raise NotImplementedError(
+            "Report class requires .apply() to be implemented"
+        )
+
+
+class VulnerabilityHTMLReport(Report):
+    __template = "vulnerability_report_html_template"
+    __css_template = "vulnerability_report_html_css"
+    media_type = "text/html"
+    format = "html"
+
+    def apply(self, name: str, data: Dict, parameter: Dict):
+        css = _load_template(self.__css_template, parameter).render(
+            Context(parameter)
+        )
+        data["css"] = css
+
+        return _load_template(self.__template).render(
+            Context(_enrich(name, data, parameter))
+        )
+
+
+def _replace_inline_svg_with_img_tags(
+    html: str, from_index: int = 0, open_tag="<svg ", close_tag="</svg>"
+) -> str:
+    """
+    Is a workaround because WeasyPrint is not capable of dealing with inline svg
+
+    It searches for svg tags and replaces the inline svg within a given
+    html document with img containing a base64 encoded data link.
+
+    It is known that we lose the css styling with that hack, but in the moment
+    it is considered better than losing the image altogether or trying to
+    produce the images before rendering the template.
+
+    Please replace this method as soon as possible with a proper solution.
+    """
+    # abort condition is when either open_tag or close_tag was not found
+    while True:
+        from_index = html.find(open_tag, from_index)
+        if from_index == -1:
+            return html
+        to_index = html.find(close_tag, from_index + len(open_tag))
+        if to_index == -1:
+            return html
+        to_index += len(close_tag)
+        to_encode = html[from_index:to_index]
+        encoded = b64encode(to_encode.encode()).decode()
+        img = (
+            f'<img src="data:image/svg+xml;charset=utf-8;base64, {encoded}" />'
+        )
+        html = html[:from_index] + img + html[to_index:]
+        from_index = to_index
+
+
+def enforce_limit(
+    data: Dict, parameter: Dict, format_type: str = "pdf"
+) -> Dict:
+    if "results" not in data:
+        return data
+
+    limits = parameter.get("limits", {}).get(format_type, {})
+    max_hosts = limits.get("hosts")
+    max_results_in_host = limits.get("results")
+    logger.debug(
+        "limit max_results %s and max_hosts %s", max_results_in_host, max_hosts
+    )
+    if not max_hosts and not max_results_in_host:
+        return data
+    host_cut = False
+    if max_hosts and len(data["results"]) > max_hosts:
+        data["results"] = data["results"][:max_hosts]
+        host_cut = True
+    result_cut = False
+    if max_results_in_host:
+        for result in data["results"]:
+            if len(result["results"]) > max_results_in_host:
+                result["results"] = result["results"][:max_results_in_host]
+                result_cut = True
+    if host_cut and not result_cut:
+        data["comment"] = limits.get(
+            "host_limit_msg", f"Host limit: {max_hosts}."
+        )
+    elif result_cut and not host_cut:
+        data["comment"] = limits.get(
+            "result_limit_msg", f"Result limit: {max_results_in_host}."
+        )
+    elif host_cut and result_cut:
+        data["comment"] = limits.get(
+            "host_result_limit_msg",
+            f"Host limit {max_hosts}; Result limit: {max_results_in_host}.",
+        )
+
+    return data
+
+
+class VulnerabilityPDFReport(Report):
+    """
+    Is used to generate vulnerability reports in PDF.
+    It renders html and css templates given data struct and then translate
+    the html document to PDF
+    """
+
+    __template = "vulnerability_report_pdf_template"
+    __css_template = "vulnerability_report_pdf_css"
+    media_type = "application/pdf"
+    format = "binary"
+
+    def apply(self, name: str, data: Dict, parameter: Dict):
+        data = enforce_limit(data, parameter)
+        css = _load_template(self.__css_template, parameter).render(
+            Context(parameter)
+        )
+        html_template = _load_template(self.__template, parameter)
+        html = html_template.render(Context(_enrich(name, data, parameter)))
+        html = _replace_inline_svg_with_img_tags(html)
+        logger.debug("created html")
+        pdf = HTML(string=html).write_pdf(stylesheets=[CSS(string=css)])
+        logger.debug("created pdf")
+        return pdf
+
+
+class ReportFormatHTMLReport(renderers.BaseRenderer):
+    media_type = "text/html+report_format_editor"
+    format = "html"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        request = _get_request(renderer_context)
+        if not data:
+            return _default_not_found_response(renderer_context, request)
+        template = Template(data["template"])
+        data["vulnerability_report"]["css"] = data["html_css"]
+        data["vulnerability_report"]["images"] = data.get("images")
+        context = Context(data["vulnerability_report"])
+        html = template.render(context)
+        logger.debug("created html")
+        return html
+
+
+class ReportFormatPDFReport(renderers.BaseRenderer):
+    media_type = "application/pdf+report_format_editor"
+    format = "binary"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        request = _get_request(renderer_context)
+        if not data:
+            return _default_not_found_response(renderer_context, request)
+        data["vulnerability_report"]["images"] = data.get("images")
+        template = Template(data["template"])
+        context = Context(data["vulnerability_report"])
+        html = template.render(context)
+        logger.debug("created html")
+        css = Template(data["pdf_css"]).render(context)
+        pdf = HTML(string=html).write_pdf(stylesheets=[CSS(string=css)])
+        logger.debug("created pdf")
+        return pdf
