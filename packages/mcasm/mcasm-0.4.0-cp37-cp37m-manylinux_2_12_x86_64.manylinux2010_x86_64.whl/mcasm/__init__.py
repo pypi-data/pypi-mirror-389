663d4166f752d289c17d329a3c20139201a70c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:40 +0000

    Add package mcasm/0.4.0/mcasm-0.4.0-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/mcasm/mcasm-0.4.0-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/mcasm/__init__.py
@@ -0,0 +1,4 @@
+from mcasm._core import Assembler, ParserState, Streamer, X86Syntax, mc
+from mcasm.version import __version__  # noqa: F401
+
+__all__ = ["Assembler", "ParserState", "Streamer", "X86Syntax", "mc"]
