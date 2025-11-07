30af226718435fb127b5d8336a937ab83ae7a1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:44 +0000

    Add package tc-admin/5.1.1/tc_admin-5.1.1.tar.gz

---
+++ packages/tc-admin/tc_admin-5.1.1.tar.gz/tc_admin-5.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="tc-admin",
+    version="5.1.1",
+    description="Administration of Taskcluster runtime configuration",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Dustin Mitchell",
+    author_email="dustin@mozilla.com",
+    url="https://github.com/taskcluster/tc-admin",
+    packages=find_packages("."),
+    install_requires=[
+        "taskcluster",
+        "click>=8.0.0,<8.4",
+        "blessings~=1.7",
+        "attrs>=21.4.0,<25.5",
+        "sortedcontainers~=2.4.0",
+        "aiohttp>=3.8.0,<3.14",
+        "pytest>=7.0.0,<8.5",
+        "pyyaml~=6.0",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+    entry_points={
+        "console_scripts": [
+            "tc-admin = tcadmin.boot:boot",
+        ]},
+)
