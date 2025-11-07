52952141a128ef7288d85ca8ddee5f3b04848cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:08:40 +0000

    Add package ai21/4.3.0/ai21-4.3.0.tar.gz

---
+++ packages/ai21/ai21-4.3.0.tar.gz/ai21-4.3.0/ai21/clients/studio/resources/beta/async_beta.py
@@ -0,0 +1,18 @@
+from ai21.clients.studio.resources.agents.async_agents import AsyncAgents
+from ai21.clients.studio.resources.batch.async_batches import AsyncBatches
+from ai21.clients.studio.resources.maestro.maestro import AsyncMaestro
+from ai21.clients.studio.resources.studio_conversational_rag import (
+    AsyncStudioConversationalRag,
+)
+from ai21.clients.studio.resources.studio_resource import AsyncStudioResource
+from ai21.http_client.async_http_client import AsyncAI21HTTPClient
+
+
+class AsyncBeta(AsyncStudioResource):
+    def __init__(self, client: AsyncAI21HTTPClient):
+        super().__init__(client)
+
+        self.maestro = AsyncMaestro(client)
+        self.agents = AsyncAgents(client, self.maestro)
+        self.conversational_rag = AsyncStudioConversationalRag(client)
+        self.batches = AsyncBatches(client)
