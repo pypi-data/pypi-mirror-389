d6fd832d9102c1938be4f4026851ecc248951982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:26:15 +0000

    Add package defoldsdk/0.1.0/defoldsdk-0.1.0.tar.gz

---
+++ packages/defoldsdk/defoldsdk-0.1.0.tar.gz/defoldsdk-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="defoldsdk",
+    version="0.1.0",
+    author="issam Mhadhbi",
+    author_email="mhadhbixissam@gmail.com",
+    description="Defold protobuff compiled to python package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MhadhbiXissam/defoldsdk.git",
+    packages=find_packages(),
+    install_requires=[
+        "protobuff",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+)
